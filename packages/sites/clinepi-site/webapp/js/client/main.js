c019e65325bfdf82b145cb8101b46e0623eb88d0
Dave Falke <dmfalke@gmail.com>
2017-05-04 18:08:48 +0000

    use project id ClinEpiDB, and update to latest ebrc api
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk@81570 735e2a04-f8fc-0310-8a1b-f2942603c481

--- packages/sites/clinepi-site/webapp/js/client/main.js
+++ packages/sites/clinepi-site/webapp/js/client/main.js
@@ -1,7 +1,5 @@
-import 'eupathdb/wdkCustomization/css/client.css';
 import 'site/css/ClinEpiSite.css';
 
-import { takeWhile, dropWhile } from 'lodash';
 import { initialize } from 'eupathdb/wdkCustomization/js/client/bootstrap';
 import componentWrappers from './component-wrappers';
 
@@ -12,10 +10,19 @@ initialize({
   componentWrappers
 });
 
-function mainMenuItems(props, defaultEntries) {
-  return takeWhile(defaultEntries, entry => entry.id !== 'profile-or-login');
+function mainMenuItems(props, defaultItems) {
+  return [
+    defaultItems.search,
+    defaultItems.strategies,
+    defaultItems.basket,
+    defaultItems.favorites
+  ]
 }
 
-function smallMenuItems(props, defaultEntries) {
-  return dropWhile(defaultEntries, entry => entry.id !== 'profile-or-login');
+function smallMenuItems(props, defaultItems) {
+  return [
+    defaultItems.profileOrLogin,
+    defaultItems.registerOrLogout,
+    defaultItems.contactUs
+  ]
 }
