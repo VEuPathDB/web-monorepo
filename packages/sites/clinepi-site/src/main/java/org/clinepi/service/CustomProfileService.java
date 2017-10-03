c9e695ba5be7d128ce6c88add34b06329e5b5ad8
Ryan Doherty <tech@conical.org>
2017-10-03 18:16:11 +0000

    Fix custom profile service- forgot userid param annotation
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk@84828 735e2a04-f8fc-0310-8a1b-f2942603c481

--- packages/sites/clinepi-site/src/main/java/org/clinepi/service/CustomProfileService.java
+++ packages/sites/clinepi-site/src/main/java/org/clinepi/service/CustomProfileService.java
@@ -5,6 +5,8 @@ import static org.gusdb.fgputil.FormatUtil.join;
 import java.util.Collections;
 import java.util.List;
 
+import javax.ws.rs.PathParam;
+
 import org.gusdb.fgputil.accountdb.UserPropertyName;
 import org.gusdb.wdk.model.WdkModel;
 import org.gusdb.wdk.model.user.User;
@@ -17,7 +19,7 @@ public class CustomProfileService extends ProfileService {
 
   private static final String APPROVED_STUDIES_KEY = "approvedStudies";
 
-  public CustomProfileService(String uid) {
+  public CustomProfileService(@PathParam(USER_ID_PATH_PARAM) String uid) {
     super(uid);
   }
 
