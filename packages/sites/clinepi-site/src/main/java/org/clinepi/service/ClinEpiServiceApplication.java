4fe1460dcf50b3cbf33438ef51e28825e8e7bd49
Ryan Doherty <tech@conical.org>
2017-10-12 19:51:06 +0000

    Use new SetBuilder method to simplify service class replacement
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk@85024 735e2a04-f8fc-0310-8a1b-f2942603c481

--- packages/sites/clinepi-site/src/main/java/org/clinepi/service/ClinEpiServiceApplication.java
+++ packages/sites/clinepi-site/src/main/java/org/clinepi/service/ClinEpiServiceApplication.java
@@ -2,8 +2,6 @@ package org.clinepi.service;
 
 import java.util.Set;
 
-import static org.gusdb.fgputil.functional.Functions.filter;
-
 import org.gusdb.fgputil.SetBuilder;
 import org.gusdb.wdk.service.WdkServiceApplication;
 import org.gusdb.wdk.service.service.user.ProfileService;
@@ -14,11 +12,11 @@ public class ClinEpiServiceApplication extends WdkServiceApplication {
   public Set<Class<?>> getClasses() {
     return new SetBuilder<Class<?>>()
 
-      // add all WDK classes (not using Ebrc!) except ProfileService which we will override
-      .addAll(filter(super.getClasses(), clazz -> !clazz.getName().equals(ProfileService.class.getName())))
+      // add all WDK classes (not using Ebrc!)
+      .addAll(super.getClasses())
 
-      // add clin-epi custom profile service
-      .add(CustomProfileService.class)
+      // replace ProfileService with custom version for ClinEpi
+      .replace(ProfileService.class, CustomProfileService.class)
 
       .toSet();
   }
