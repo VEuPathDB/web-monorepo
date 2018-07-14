ea7c4d25c5bfe09543ea9f76895919b412e6293e
Dave Falke <dmfalke@gmail.com>
2018-07-14 05:12:10 +0000

    call super.handleAction first to prevent potentially losing studies property
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk@90084 735e2a04-f8fc-0310-8a1b-f2942603c481

--- packages/sites/clinepi-site/webapp/js/client/store-wrappers/GlobalDataStore.js
+++ packages/sites/clinepi-site/webapp/js/client/store-wrappers/GlobalDataStore.js
@@ -1,12 +1,13 @@
 import studiesReducer from 'Client/App/Studies/StudyReducer';
 export default GlobalDataStore => class ClinEpiGlobalDataStore extends GlobalDataStore {
   handleAction(state = {}, action) {
-    const studies = studiesReducer(state.studies, action);
+    state = super.handleAction(state, action);
+
     // Update state only if `studies` has changed to prevent false-positives
     // when attempting to detect if globalData has changed.
-    if (state.studies !== studies) {
-      state = { ...state, studies }
-    }
-    return super.handleAction(state, action);
+    const studies = studiesReducer(state.studies, action);
+    return state.studies !== studies
+      ? { ...state, studies }
+      : state;
   }
 }
