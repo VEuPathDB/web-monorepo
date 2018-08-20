903734c11b2f37c404a7140d691ca3300c2b0d30
Dave Falke <dmfalke@gmail.com>
2018-08-20 21:06:50 +0000

    refactor and add basket and record actions
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk@90951 735e2a04-f8fc-0310-8a1b-f2942603c481

--- packages/sites/clinepi-site/webapp/js/legacy/dataRestriction.js
+++ packages/sites/clinepi-site/webapp/js/legacy/dataRestriction.js
@@ -1,70 +1,62 @@
-import { getIdFromRecordClassName } from 'Client/App/DataRestriction/DataRestrictionUtils';
+/* global ebrc, wdk */
+
+import { flow } from 'lodash';
+
+import { getIdFromRecordClassName, Action } from 'Client/App/DataRestriction/DataRestrictionUtils';
 import { attemptAction } from 'Client/App/DataRestriction/DataRestrictionActionCreators';
 
 wdk.namespace('wdk.dataRestriction', (ns, $) => {
 
-  ns.restrictionController = (element) => {
-    const { recordClass, restrictionType } = element.data();
+  ns.restrictionController = ($element) => {
+    const { recordClass, restrictionType } = $element.data();
     const studyId = getIdFromRecordClassName(recordClass);
-    const elements = { rawEl: element, jqEl: $(element) };
-    console.info('RestrictionController initialized:', { recordClass, restrictionType, elements });
+    const attempt = flow(attemptAction, ebrc.context.dispatchAction);
+    console.info('RestrictionController initialized:', { recordClass, restrictionType, $element });
 
-    const isSearchPage = restrictionType && restrictionType === 'search';
-    if (isSearchPage) {
-      attemptAction('search', { studyId });
+    const isSearchPage = restrictionType && restrictionType === Action.search;
+    if (isSearchPage) attempt(Action.search, { studyId });
+
+    const isResultsPage = $element.children('.Results_Div').length !== 0;
+    if (isResultsPage) attempt(Action.results, { studyId });
+
+    addHandler('.analysis-selector', 'click', Action.analysis);
+    addHandler('.paging-table table:first a, .paging-table table:first .paging-button', 'click', Action.paginate);
+    addHandler('a[id^=basket]', 'click', Action.basket);
+    addHandler('a.step-download-link', 'click', Action.download);
+    // approximate selector for record links
+    addHandler(`a[href*="\\/record\\/${studyId}_"]`, 'click', Action.record);
+
+    function addHandler(selector, eventType, action) {
+      $element.find(selector).toArray().forEach(function(el) {
+        // Add a jquery event handler. The param `isHandled` is an extra param
+        // that jquery allows. This allows this function to determine if the click
+        // event was already handled.
+        $(el).on(eventType, function (event, isHandled) {
+          if (!isHandled) {
+            event.stopPropagation();
+            event.stopImmediatePropagation();
+            event.preventDefault();
+            attempt(action, { studyId, onSuccess });
+          }
+        });
+
+        function onSuccess() {
+          $(el).trigger(eventType, [true]);
+          // FIXME Honor modifiers (ctrl+click => new tab; shift+click => new window)
+          if (el.href) window.location.assign(el.href);
+        }
+
+        // if there is an on-handler (e.g., onclick attribute), save a reference to
+        // it, remove it, and add as a jquery event handler
+        const onHandler = el['on' + eventType];
+        if (onHandler) {
+          $(el).attr('on' + eventType, null).on(eventType, () => {
+            onHandler.call(el);
+          });
+        }
+      });
     }
 
-    const isResultsPage = element.children('.Results_Div').length !== 0;
-    if (isResultsPage) {
-      attemptAction('results', { studyId });
-    }
-
-    const analysisTiles = element.find('.analysis-selector');
-    if (analysisTiles) analysisTiles.each((index, tile) => {
-      ns.analysisTileController($(tile), studyId)
-    });
-
-    const pagingTables = element.children('.paging-table');
-    if (pagingTables) pagingTables.each((index, table) => {
-      ns.pagingController($(table), studyId)
-    });
-
-    const downloadResultLink = element.find('a.step-download-link');
-    if (downloadResultLink) ns.downloadLinkController(downloadResultLink, studyId);
-
   };
-
-  ns.analysisTileController = (element, studyId) => {
-    element.on('click', makeHandler('analysis', studyId));
-  };
-
-  ns.pagingController = (element, studyId) => {
-    element.on('click', makeHandler('paginate', studyId));
-    // Remove inline onclick handler and replace with jquery handler.
-    // Rely on ordering and event.stopImmediatePropagation.
-    element.find('input.paging-button')
-      .attr('onclick', null)
-      .on('click', makeHandler('paginate', studyId))
-      .on('click', event => wdk.resultsPage.openAdvancedPaging(event.currentTarget))
-  };
-
-  ns.downloadLinkController = (element, studyId) => {
-    element.on('click', makeHandler('download', studyId));
-  };
-
-  function makeHandler(action, studyId) {
-    // Create a jquery event handler. The param `isHandled` is an extra param
-    // that jquery allows. This allows this function to determine if the click
-    // event was already handled.
-    return function handle(event, isHandled) {
-      if (isHandled) return;
-
-      const onSuccess = () => $(event.currentTarget).trigger(event.type, [ true ]);
-      event.stopPropagation();
-      event.stopImmediatePropagation();
-      event.preventDefault();
-      ebrc.context.dispatchAction(attemptAction(action, { studyId, onSuccess }));
-    }
-  }
 
 });
