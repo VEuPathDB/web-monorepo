13acbaaf63aa660cf155af85270f8edbaac4a1b3
Dave Falke <dmfalke@gmail.com>
2018-09-10 17:55:03 +0000

    create controllers to be used for redux migration
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/branches/redux-migration@22913 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ packages/libs/wdk-client/src/Core/Controllers/PageController.tsx
@@ -0,0 +1,64 @@
+import { mapValues, isEqual } from 'lodash';
+import { parse } from 'querystring';
+import React from 'react';
+
+import Page from '../../Components/Layout/Page';
+import ViewController, { ViewControllerProps } from './ViewController';
+import { RouteComponentProps } from 'react-router';
+
+/**
+ * A ViewController that is intended to render a UI on an entire screen.
+ */
+export default class PageController<Props = {}, State = {}> extends ViewController<Props & RouteComponentProps<any>, State> {
+
+  /*--------------- Methods to override to display content ---------------*/
+
+  /**
+   * Returns the title of this page
+   */
+  getTitle(): string {
+    return "WDK";
+  }
+
+  getQueryParams() {
+    return mapValues(parse(this.props.location.search.slice(1)), String);
+  }
+
+  setDocumentTitle(): void {
+    if (this.isRenderDataLoadError()) {
+      document.title = "Error";
+    }
+    else if (this.isRenderDataNotFound()) {
+      document.title = "Page not found";
+    }
+    else if (this.isRenderDataPermissionDenied()) {
+      document.title = "Permission denied";
+    }
+    else if (!this.isRenderDataLoaded()) {
+      document.title = "Loading...";
+    }
+    else {
+      document.title = this.getTitle();
+    }
+  }
+
+  componentDidMount(): void {
+    super.componentDidMount()
+    this.setDocumentTitle();
+  }
+
+  componentDidUpdate(prevProps: Props & ViewControllerProps & RouteComponentProps<any>): void {
+    // only call loadData if router props have changed
+    if (!isEqual(prevProps.location, this.props.location)) {
+      this.loadData(prevProps);
+    }
+    this.setDocumentTitle();
+  }
+
+  render() {
+    return (
+      <Page>{super.render()}</Page>
+    );
+  }
+
+}
