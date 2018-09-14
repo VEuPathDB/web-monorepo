8b2919cfed13bf3e0d5cae73a78acc6d8e77db31
Steve Fischer <sfischer@upenn.edu>
2018-09-14 10:44:28 +0000

    rename Keys.java to JsonKeys.java
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk@91523 735e2a04-f8fc-0310-8a1b-f2942603c481

--- packages/sites/clinepi-site/src/main/java/org/clinepi/service/CustomProfileService.java
+++ packages/sites/clinepi-site/src/main/java/org/clinepi/service/CustomProfileService.java
@@ -23,7 +23,7 @@ import org.gusdb.wdk.model.question.Question;
 import org.gusdb.wdk.model.record.RecordInstance;
 import org.gusdb.wdk.model.record.attribute.AttributeField;
 import org.gusdb.wdk.model.user.User;
-import org.gusdb.wdk.service.formatter.Keys;
+import org.gusdb.wdk.service.formatter.JsonKeys;
 import org.gusdb.wdk.service.formatter.UserFormatter;
 import org.gusdb.wdk.service.service.user.ProfileService;
 import org.json.JSONArray;
@@ -50,7 +50,7 @@ public class CustomProfileService extends ProfileService {
     if (isSessionUser) {
       // append property telling which studies this user has special access to
       basicUser
-        .getJSONObject(Keys.PROPERTIES)
+        .getJSONObject(JsonKeys.PROPERTIES)
         .put(APPROVED_STUDIES_KEY, new JSONArray(getApprovedStudies(user)));
     }
     return basicUser;
