9aa2253fac606bf396c867ae2b341c239e2e4771
Jamie Long <jtlong3rd@gmail.com>
2018-10-04 17:52:27 +0000

    Migrate Access Request store+controller to redux.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/branches/redux-migration@92146 735e2a04-f8fc-0310-8a1b-f2942603c481

--- packages/sites/clinepi-site/webapp/js/client/wrapStoreModules.js
+++ packages/sites/clinepi-site/webapp/js/client/wrapStoreModules.js
@@ -3,7 +3,10 @@ import studies from 'Client/App/Studies/StudyReducer';
 import dataRestriction from 'Client/App/DataRestriction/DataRestrictionReducer';
 import { newsReducer } from 'Client/App/NewsSidebar/NewsModule';
 
+import * as accessReqeust from './store-modules/AccessRequestStoreModule';
+
 export default compose(
+  set('accessRequest', accessReqeust),
   set('studies', { reduce: studies }),
   set('dataRestriction', { reduce: dataRestriction }),
   set('newsSidebar', { reduce: newsReducer }),
