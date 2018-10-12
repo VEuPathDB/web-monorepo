eff09a2143b49819c7250decab501cee8a2488d5
Jamie Long <jtlong3rd@gmail.com>
2018-10-12 18:22:57 +0000

    Display login button on Data Restriction Modal whenever user is a guest
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk@92533 735e2a04-f8fc-0310-8a1b-f2942603c481

--- packages/sites/clinepi-site/webapp/js/client/wrapStoreModules.js
+++ packages/sites/clinepi-site/webapp/js/client/wrapStoreModules.js
@@ -1,13 +1,13 @@
-import { compose, set} from 'lodash/fp';
+import { compose, set } from 'lodash/fp';
 import studies from 'Client/App/Studies/StudyReducer';
 import dataRestriction from 'Client/App/DataRestriction/DataRestrictionReducer';
 import { newsReducer } from 'Client/App/NewsSidebar/NewsModule';
 
-import * as accessReqeust from './store-modules/AccessRequestStoreModule';
+import * as accessRequest from './store-modules/AccessRequestStoreModule';
 
 export default compose(
-  set('accessRequest', accessReqeust),
+  set('accessRequest', accessRequest),
   set('studies', { reduce: studies }),
   set('dataRestriction', { reduce: dataRestriction }),
   set('newsSidebar', { reduce: newsReducer }),
-)
+);
