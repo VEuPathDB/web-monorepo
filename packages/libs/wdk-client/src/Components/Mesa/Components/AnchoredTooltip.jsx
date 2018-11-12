79cdc644add804149a2175e9eb54ba90e6c02712
Dave Falke <dmfalke@gmail.com>
2018-11-12 20:04:43 +0000

    fix bug in AnchoredTooltip #32331
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/trunk@23428 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/Components/Mesa/Components/AnchoredTooltip.jsx
+++ packages/libs/wdk-client/src/Components/Mesa/Components/AnchoredTooltip.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { debounce } from 'lodash';
 
 import Tooltip from 'wdk-client/Components/Mesa/Components/Tooltip';
 import Events from 'wdk-client/Components/Mesa/Utils/Events';
@@ -6,8 +7,8 @@ import Events from 'wdk-client/Components/Mesa/Utils/Events';
 class AnchoredTooltip extends React.Component {
   constructor (props) {
     super(props);
-    this.forceUpdate = this.forceUpdate.bind(this);
     this.getPosition = this.getPosition.bind(this);
+    this.updatePosition = debounce(this.updatePosition.bind(this), 100);
     this.componentDidMount = this.componentDidMount.bind(this);
     this.componentWillUnmount = this.componentWillUnmount.bind(this);
     this.childWrapperRef = React.createRef();
@@ -15,15 +16,19 @@ class AnchoredTooltip extends React.Component {
 
   componentDidMount () {
     this.listeners = {
-      scroll: Events.add('scroll', this.forceUpdate),
-      resize: Events.add('resize', this.forceUpdate),
-      MesaScroll: Events.add('MesaScroll', this.forceUpdate),
-      MesaReflow: Events.add('MesaReflow', this.forceUpdate)
+      scroll: Events.add('scroll', this.updatePosition),
+      resize: Events.add('resize', this.updatePosition),
+      MesaScroll: Events.add('MesaScroll', this.updatePosition),
+      MesaReflow: Events.add('MesaReflow', this.updatePosition)
     };
   }
 
   componentWillUnmount () {
     Object.values(this.listeners).forEach(listenerId => Events.remove(listenerId));
+  }
+
+  updatePosition() {
+    this.forceUpdate();
   }
 
   getPosition () {
