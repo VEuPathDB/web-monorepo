0d5e5da305ea12bb850fe119f3a11cc8f19fd93e
Cristina Aurrecoechea <aurreco@uga.edu>
2019-01-11 18:39:22 +0000

    closes #33023
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk@94396 735e2a04-f8fc-0310-8a1b-f2942603c481

--- packages/sites/clinepi-site/webapp/js/client/components/About.jsx
+++ packages/sites/clinepi-site/webapp/js/client/components/About.jsx
@@ -15,25 +15,25 @@ export default function About() {
       <h2 id="how-was-it-made">How was it made?</h2>
       <div>
         <div>
-          IMAGE
+          <img src="/a/images/ClinEpiDB/ClinEpi_About_page_data_processing.png" />
         </div>
-        <p>ClinEpiDB was developed using the existing infrastructure of <a href="https://eupathdb.org">EuPathDB</a>, a collection of databases covering 170+ eukaryotic pathogens, along with relevant free-living and non-pathogenic species and select pathogen hosts, which provides a sophisticated search strategy system enabling complex interrogations of underlying data. Currently, data integration for ClinEpiDB has occurred or is in process for NIH-supported International Centers for Excellence in Malaria Research (ICEMR), the Gates Foundation-supported Malnutrition and Enteric Diseases Network (MAL-ED), and the Global Enteric Multicenter Study (GEMS) projects. Greyed-out cards on the homepage indicate studies that have been loaded into a QA site, but are not yet publicly available. In the process of data integration, a unified semantic web framework has been used to describe data generated from these studies. Over 1500 different data variables about participants, their disease episodes, households, and other potential exposure factors were collected in these clinical epidemiology studies and mapped for web display.
+        <p>ClinEpiDB was developed using the existing infrastructure of <a target="_blank" href="https://eupathdb.org">EuPathDB</a>, a collection of databases covering 170+ eukaryotic pathogens, along with relevant free-living and non-pathogenic species and select pathogen hosts, which provides a sophisticated search strategy system enabling complex interrogations of underlying data. Currently, data integration for ClinEpiDB has occurred or is in process for NIH-supported International Centers for Excellence in Malaria Research (ICEMR), the Gates Foundation-supported Malnutrition and Enteric Diseases Network (MAL-ED), and the Global Enteric Multicenter Study (GEMS) projects. Greyed-out cards on the homepage indicate studies that have been loaded into a QA site, but are not yet publicly available. In the process of data integration, a unified semantic web framework has been used to describe data generated from these studies. Over 1500 different data variables about participants, their disease episodes, households, and other potential exposure factors were collected in these clinical epidemiology studies and mapped for web display.
         </p>
-        <p>The data is loaded into a GUS4 schema. When combined with the unified semantic web framework and the extensive web toolkit and infrastructure developed by EuPathDB, the user is presented with a point-and-click web interface that provides insight into data distributions and exploratory associations with any observational covariates. Query results can be statistically analyzed and graphically visualized via interactive web applications built in <a href="https://shiny.rstudio.com">Shiny</a> that is launched directly in the ClinEpiDB browser.
+        <p>The data is loaded into a GUS4 schema. When combined with the unified semantic web framework and the extensive web toolkit and infrastructure developed by EuPathDB, the user is presented with a point-and-click web interface that provides insight into data distributions and exploratory associations with any observational covariates. Query results can be statistically analyzed and graphically visualized via interactive web applications built in <a target="_blank" href="https://shiny.rstudio.com">Shiny</a> that is launched directly in the ClinEpiDB browser.
         </p>
-        <p>As we continue to load datasets, we are also working on expanding our functionality. By developing the ability to query across EuPathDB databases, we plan to allow users to identify samples of interest in ClinEpiDB and match them to molecular data loaded in other EuPathDB database resources such as <a href="http://microbiomedb.org">MicrobiomeDB</a> and <a href="http://plasmodb.org">PlasmoDB</a>, and vice versa.
+        <p>As we continue to load datasets, we are also working on expanding our functionality. By developing the ability to query across EuPathDB databases, we plan to allow users to identify samples of interest in ClinEpiDB and match them to molecular data loaded in other EuPathDB database resources such as <a target="_blank" href="http://microbiomedb.org">MicrobiomeDB</a> and <a target="_blank" href="http://plasmodb.org">PlasmoDB</a>, and vice versa.
         </p>
       </div>
 
       <h2 id="How-do-i-cite-site">How do I cite ClinEpiDB?</h2>
       <div>
-        <p>You can reference ClinEpiDB as part of the EuPathDB project <a href="https://www.ncbi.nlm.nih.gov/pubmed/27903906">(Aurrecoechea et al. 2017)</a>.
+        <p>You can reference ClinEpiDB as part of the EuPathDB project <a target="_blank" href="https://www.ncbi.nlm.nih.gov/pubmed/27903906">(Aurrecoechea et al. 2017)</a>.
         </p>
       </div>
 
       <h2 id="how-do-i-use-site">How do I use ClinEpiDB?</h2>
       <div>
-        <p>See our written <a href="https://qa.icemr.clinepidb.org/ce.icemr.qa/showXmlDataContent.do?name=XmlQuestions.Tutorials">Tutorials and Resources</a> and <a href="https://www.youtube.com/playlist?list=PLWzQB3i5sYAIp4urzLGB8jxvVZr6jvkZh">Video Tutorials</a>.
+        <p>See our written <a href="/a/showXmlDataContent.do?name=XmlQuestions.Tutorials">Tutorials and Resources</a> and <a target="_blank" href="https://www.youtube.com/playlist?list=PLWzQB3i5sYAIp4urzLGB8jxvVZr6jvkZh">Video Tutorials</a>.
         </p>
       </div>
 
@@ -45,7 +45,7 @@ export default function About() {
 
       <h2 id="Can-i-access-code-used-for-site">Can I access code used for ClinEpiDB?</h2>
       <div>
-        <p>Yes! Our code is maintained using Subversion, and we welcome input from the community. Check out our <a href="https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk/View/lib/R/shiny/apps/">scripts for each of the Shiny apps</a>.
+        <p>Yes! Our code is maintained using Subversion, and we welcome input from the community. Check out our <a target="_blank" href="https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk/View/lib/R/shiny/apps/">scripts for each of the Shiny apps</a>.
         </p>
       </div>
 
@@ -59,20 +59,19 @@ export default function About() {
         <div style={{ padding: '1em 0' }}>
           <div style={{ textDecoration: 'underline' }}>The EuPathDB Team</div>
           <div>
-            Cristina Aurrecoechea <sup>2</sup>,            
-            John Brestelli <sup>3</sup>,
-            Brian Brunk <sup>1</sup>,            
-            Danielle Callan <sup>1</sup>,
-            Brianna Lindsey <sup>3</sup>,
-            Dave Falke <sup>2</sup>,
-            Steve Fischer <sup>4</sup>,
-            Omar Harb <sup>1</sup>,
-            Danica Helb <sup>1</sup>,
-            Jay Humphrey <sup>2</sup>,
-            John Judkins <sup>3</sup>,
-            Brianna Lindsay <sup>1</sup>,
-            Sheena Shah Tomko <sup>1</sup>,
-            Jie Zheng <sup>3,5</sup>
+            Cristina Aurrecoechea<sup>2</sup>,            
+            John Brestelli<sup>3</sup>,
+            Brian Brunk<sup>1</sup>,            
+            Danielle Callan<sup>1</sup>,
+            Dave Falke<sup>2</sup>,
+            Steve Fischer<sup>4</sup>,
+            Omar Harb<sup>1</sup>,
+            Danica Helb<sup>1</sup>,
+            Jay Humphrey<sup>2</sup>,
+            John Judkins<sup>3</sup>,
+            Brianna Lindsay<sup>1</sup>,
+            Sheena Shah Tomko<sup>1</sup>,
+            Jie Zheng<sup>3,5</sup>
           </div>
         </div>
 
