f7f1dd93ce1bfbeea10e9594034e908b8293e9b3
Dave Falke <dmfalke@gmail.com>
2019-01-28 16:23:36 +0000

    formatting
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/branches/reactResultPanel@23813 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/Actions/BasketActions.ts
+++ packages/libs/wdk-client/src/Actions/BasketActions.ts
@@ -1,18 +1,29 @@
-import { makeActionCreator, InferAction } from 'wdk-client/Utils/ActionCreatorUtils';
-import { PrimaryKey } from "wdk-client/Utils/WdkModel";
-import { BasketOperation } from "wdk-client/Utils/WdkService";
-export type BasketScope = "global" | "project";
+import {
+  makeActionCreator,
+  InferAction
+} from 'wdk-client/Utils/ActionCreatorUtils';
+import { PrimaryKey } from 'wdk-client/Utils/WdkModel';
+import { BasketOperation } from 'wdk-client/Utils/WdkService';
+export type BasketScope = 'global' | 'project';
 
 export const requestUpdateBasket = makeActionCreator(
-    'requestUpdateBasket',
-    (operation: BasketOperation, recordClassName: string, primaryKeys: Set<PrimaryKey>) => ({ operation: operation, recordClassName,  primaryKeys})
-    );
-    
+  'requestUpdateBasket',
+  (
+    operation: BasketOperation,
+    recordClassName: string,
+    primaryKeys: Set<PrimaryKey>
+  ) => ({ operation: operation, recordClassName, primaryKeys })
+);
+
 export const fulfillUpdateBasket = makeActionCreator(
-        'fulfillUpdateBasket',
-        (operation: BasketOperation, recordClassName: string, primaryKeys: Set<PrimaryKey>) => ({ operation, recordClassName,  primaryKeys})
-        );
-        
+  'fulfillUpdateBasket',
+  (
+    operation: BasketOperation,
+    recordClassName: string,
+    primaryKeys: Set<PrimaryKey>
+  ) => ({ operation, recordClassName, primaryKeys })
+);
+
 export type Action =
-    InferAction<typeof requestUpdateBasket>
-    | InferAction<typeof fulfillUpdateBasket>
+  | InferAction<typeof requestUpdateBasket>
+  | InferAction<typeof fulfillUpdateBasket>;
