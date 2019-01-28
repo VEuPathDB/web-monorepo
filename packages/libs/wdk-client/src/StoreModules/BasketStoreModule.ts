9954fb8b05d0342e1ffc3e94acfea15a471d51bb
Dave Falke <dmfalke@gmail.com>
2019-01-28 16:31:01 +0000

    formatting
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/branches/reactResultPanel@23814 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/StoreModules/BasketStoreModule.ts
+++ packages/libs/wdk-client/src/StoreModules/BasketStoreModule.ts
@@ -1,32 +1,44 @@
-import { requestUpdateBasket, fulfillUpdateBasket} from 'wdk-client/Actions/BasketActions';
+import {
+  requestUpdateBasket,
+  fulfillUpdateBasket
+} from 'wdk-client/Actions/BasketActions';
 import { InferAction } from 'wdk-client/Utils/ActionCreatorUtils';
 
 import { Action } from 'wdk-client/Actions';
 import { EpicDependencies } from 'wdk-client/Core/Store';
 
 import { Observable } from 'rxjs';
-import {mergeMapRequestActionsToEpic} from 'wdk-client/Utils/ActionCreatorUtils';
-import { combineEpics} from 'redux-observable';
+import { mergeMapRequestActionsToEpic } from 'wdk-client/Utils/ActionCreatorUtils';
+import { combineEpics } from 'redux-observable';
 
 export const key = 'basket';
 
-export type State = {
-};
+export type State = {};
 
-const initialState: State = {
-};
+const initialState: State = {};
 
 export function reduce(state: State = initialState, action: Action): State {
-    return state;
+  return state;
 }
 
-async function getFulfillUpdateBasket([requestAction]:  [InferAction<typeof requestUpdateBasket>], state$: Observable<State>, { wdkService }: EpicDependencies) : Promise<InferAction<typeof fulfillUpdateBasket>> {
-    let payload = requestAction.payload;
-    await wdkService.updateBasketStatus(payload.operation, payload.recordClassName, payload.primaryKeys);
-    return fulfillUpdateBasket(payload.operation, payload.recordClassName, payload.primaryKeys);
+async function getFulfillUpdateBasket(
+  [requestAction]: [InferAction<typeof requestUpdateBasket>],
+  state$: Observable<State>,
+  { wdkService }: EpicDependencies
+): Promise<InferAction<typeof fulfillUpdateBasket>> {
+  let payload = requestAction.payload;
+  await wdkService.updateBasketStatus(
+    payload.operation,
+    payload.recordClassName,
+    payload.primaryKeys
+  );
+  return fulfillUpdateBasket(
+    payload.operation,
+    payload.recordClassName,
+    payload.primaryKeys
+  );
 }
 
-export const observe =
-     combineEpics(
-         mergeMapRequestActionsToEpic([requestUpdateBasket], getFulfillUpdateBasket)
-     );
+export const observe = combineEpics(
+  mergeMapRequestActionsToEpic([requestUpdateBasket], getFulfillUpdateBasket)
+);
