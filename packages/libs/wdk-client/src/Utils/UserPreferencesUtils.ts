39b6e457ae31beb6678af8e0958432e72059269a
Ryan Doherty <tech@conical.org>
2019-03-28 05:14:22 +0000

    one more fix
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/branches/strategy-loading@24466 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/Utils/UserPreferencesUtils.ts
+++ packages/libs/wdk-client/src/Utils/UserPreferencesUtils.ts
@@ -2,9 +2,9 @@ import { parseInt, isInteger } from 'lodash/fp';
 import WdkService from 'wdk-client/Utils/WdkService';
 import { decode, arrayOf, combine, field, string, Decoder, optional, ok } from 'wdk-client/Utils/Json';
 import {UserPreferences} from 'wdk-client/Utils/WdkUser';
-import { AttributeSortingSpec, AnswerSpec } from "wdk-client/Utils/WdkModel"
+import { AttributeSortingSpec, SearchConfig } from "wdk-client/Utils/WdkModel"
 
-type ViewFilters = AnswerSpec['viewFilters'];
+type ViewFilters = SearchConfig['viewFilters'];
 
 function isValidDirection(direction: string): direction is 'ASC' | 'DESC' {
     return direction === 'ASC' || direction === 'DESC' 
