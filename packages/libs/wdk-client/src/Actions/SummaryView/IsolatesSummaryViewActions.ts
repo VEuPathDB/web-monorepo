c8a090b69452884e7bf04b79a447ac0832db187e
Dave Falke <dmfalke@gmail.com>
2019-04-04 17:07:57 +0000

    formatting
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/trunk@24548 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/Actions/SummaryView/IsolatesSummaryViewActions.ts
+++ packages/libs/wdk-client/src/Actions/SummaryView/IsolatesSummaryViewActions.ts
@@ -1,16 +1,21 @@
-import { makeActionCreator, InferAction } from 'wdk-client/Utils/ActionCreatorUtils';
-import { IsolatesSummaryViewReport } from "wdk-client/Utils/WdkModel";
+import {
+  InferAction,
+  makeActionCreator
+} from 'wdk-client/Utils/ActionCreatorUtils';
+import { IsolatesSummaryViewReport } from 'wdk-client/Utils/WdkModel';
 
 export const requestIsolatesSummaryReport = makeActionCreator(
-    'isolatesSummaryView/requestIsolatesSummaryReport',
-    (stepId: number) => ({ stepId })
-    );
+  'isolatesSummaryView/requestIsolatesSummaryReport',
+  (stepId: number) => ({ stepId })
+);
 
 export const fulfillIsolatesSummaryReport = makeActionCreator(
-        'isolatesSummaryView/fulfillIsolatesSummaryReport',
-        (isolatesSummaryViewReport: IsolatesSummaryViewReport) => ({ isolatesSummaryViewReport })
-        );
-    
+  'isolatesSummaryView/fulfillIsolatesSummaryReport',
+  (isolatesSummaryViewReport: IsolatesSummaryViewReport) => ({
+    isolatesSummaryViewReport
+  })
+);
+
 export type Action =
-    | InferAction<typeof requestIsolatesSummaryReport>
-    | InferAction<typeof fulfillIsolatesSummaryReport>
+  | InferAction<typeof requestIsolatesSummaryReport>
+  | InferAction<typeof fulfillIsolatesSummaryReport>;
