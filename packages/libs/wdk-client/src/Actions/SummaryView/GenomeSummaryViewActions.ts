c15cfb246d7b79bb0559dbb52ad304bcd657d038
Dave Falke <dmfalke@gmail.com>
2019-04-04 15:41:29 +0000

    formatting
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/trunk@24546 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/Actions/SummaryView/GenomeSummaryViewActions.ts
+++ packages/libs/wdk-client/src/Actions/SummaryView/GenomeSummaryViewActions.ts
@@ -1,38 +1,47 @@
-import { makeActionCreator, InferAction } from 'wdk-client/Utils/ActionCreatorUtils';
-import { GenomeSummaryViewReport, RecordClass } from "wdk-client/Utils/WdkModel";
+import {
+  makeActionCreator,
+  InferAction
+} from 'wdk-client/Utils/ActionCreatorUtils';
+import {
+  GenomeSummaryViewReport,
+  RecordClass
+} from 'wdk-client/Utils/WdkModel';
 
 export const requestGenomeSummaryReport = makeActionCreator(
-    'genomeSummaryView/requestGenomeSummaryReport',
-    (stepId: number) => ({ stepId })
-    );
+  'genomeSummaryView/requestGenomeSummaryReport',
+  (stepId: number) => ({ stepId })
+);
 
 export const fulfillGenomeSummaryReport = makeActionCreator(
-        'genomeSummaryView/fulfillGenomeSummaryReport',
-        (genomeSummaryViewReport: GenomeSummaryViewReport, recordClass: RecordClass) => ({ genomeSummaryViewReport, recordClass })
-        );
-    
+  'genomeSummaryView/fulfillGenomeSummaryReport',
+  (
+    genomeSummaryViewReport: GenomeSummaryViewReport,
+    recordClass: RecordClass
+  ) => ({ genomeSummaryViewReport, recordClass })
+);
+
 export const showRegionDialog = makeActionCreator(
-    'genomeSummaryView/showRegionDialog',
-    (regionId: string) => ({ regionId })
+  'genomeSummaryView/showRegionDialog',
+  (regionId: string) => ({ regionId })
 );
 
 export const hideRegionDialog = makeActionCreator(
-    'genomeSummaryView/hideRegionDialog',
-    (regionId: string) => ({ regionId })
+  'genomeSummaryView/hideRegionDialog',
+  (regionId: string) => ({ regionId })
 );
 
 export const applyEmptyChromosomesFilter = makeActionCreator(
-    'genomeSummaryView/applyEmptyChromosomesFilter'
+  'genomeSummaryView/applyEmptyChromosomesFilter'
 );
 
 export const unapplyEmptyChromosomesFilter = makeActionCreator(
-    'genomeSummaryView/unapplyEmptyChromosomesFilter'
+  'genomeSummaryView/unapplyEmptyChromosomesFilter'
 );
 
 export type Action =
-    | InferAction<typeof requestGenomeSummaryReport>
-    | InferAction<typeof fulfillGenomeSummaryReport>
-    | InferAction<typeof showRegionDialog>
-    | InferAction<typeof hideRegionDialog>
-    | InferAction<typeof applyEmptyChromosomesFilter>
-    | InferAction<typeof unapplyEmptyChromosomesFilter>;
+  | InferAction<typeof requestGenomeSummaryReport>
+  | InferAction<typeof fulfillGenomeSummaryReport>
+  | InferAction<typeof showRegionDialog>
+  | InferAction<typeof hideRegionDialog>
+  | InferAction<typeof applyEmptyChromosomesFilter>
+  | InferAction<typeof unapplyEmptyChromosomesFilter>;
