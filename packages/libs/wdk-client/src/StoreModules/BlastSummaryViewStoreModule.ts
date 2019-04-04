c019bbcee1c599daea3b4aa9dfb6771b1aca57fe
Dave Falke <dmfalke@gmail.com>
2019-04-04 15:16:36 +0000

    formatting
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/trunk@24543 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/StoreModules/BlastSummaryViewStoreModule.ts
+++ packages/libs/wdk-client/src/StoreModules/BlastSummaryViewStoreModule.ts
@@ -1,42 +1,42 @@
-import { requestBlastSummaryReport, fulfillBlastSummaryReport} from 'wdk-client/Actions/SummaryView/BlastSummaryViewActions';
-import { InferAction } from 'wdk-client/Utils/ActionCreatorUtils';
-
-import { Action } from 'wdk-client/Actions';
-import { BlastSummaryViewReport } from 'wdk-client/Utils/WdkModel';
-import { EpicDependencies } from 'wdk-client/Core/Store';
-
+import { combineEpics } from 'redux-observable';
 import { Observable } from 'rxjs';
-import {mergeMapRequestActionsToEpic} from 'wdk-client/Utils/ActionCreatorUtils';
-import { combineEpics} from 'redux-observable';
+import { Action } from 'wdk-client/Actions';
+import { fulfillBlastSummaryReport, requestBlastSummaryReport } from 'wdk-client/Actions/SummaryView/BlastSummaryViewActions';
 import { RootState } from 'wdk-client/Core/State/Types';
+import { EpicDependencies } from 'wdk-client/Core/Store';
+import { InferAction, mergeMapRequestActionsToEpic } from 'wdk-client/Utils/ActionCreatorUtils';
+import { BlastSummaryViewReport } from 'wdk-client/Utils/WdkModel';
+
+
 
 export const key = 'blastSummaryView';
 
 export type State = {
-    blastSummaryData?: BlastSummaryViewReport,
+  blastSummaryData?: BlastSummaryViewReport,
 };
 
 const initialState: State = {
-    blastSummaryData: undefined,
+  blastSummaryData: undefined,
 };
 
 export function reduce(state: State = initialState, action: Action): State {
-    switch (action.type) {
-        case fulfillBlastSummaryReport.type: {
-            return { ...state, blastSummaryData: action.payload.blastInfo }
-        } default: {
-            return state;
-        }
+  switch (action.type) {
+    case fulfillBlastSummaryReport.type: {
+      return { ...state, blastSummaryData: action.payload.blastInfo }
     }
+    default: {
+      return state;
+    }
+  }
 }
 
 async function getBlastSummaryViewReport([requestAction]:  [InferAction<typeof requestBlastSummaryReport>], state$: Observable<RootState>, { wdkService }: EpicDependencies) : Promise<InferAction<typeof fulfillBlastSummaryReport>> {
-    let formatting = { format: 'blastSummaryView', formatConfig: { attributes: ['summary', 'alignment']} };
-    let report = await wdkService.getStepAnswer(requestAction.payload.stepId, formatting)
-    return fulfillBlastSummaryReport(report);
+  let formatting = { format: 'blastSummaryView', formatConfig: { attributes: ['summary', 'alignment']} };
+  let report = await wdkService.getStepAnswer(requestAction.payload.stepId, formatting)
+  return fulfillBlastSummaryReport(report);
 }
 
 export const observe =
-     combineEpics(
-         mergeMapRequestActionsToEpic([requestBlastSummaryReport], getBlastSummaryViewReport)
-     );
+  combineEpics(
+    mergeMapRequestActionsToEpic([requestBlastSummaryReport], getBlastSummaryViewReport)
+  );
