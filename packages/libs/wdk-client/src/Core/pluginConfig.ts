03d67727213cecc3fc1822b3f598e0aa5ce3a423
Jamie Long <jtlong3rd@gmail.com>
2019-04-11 13:34:07 +0000

    Add ByGenomeNumber custom search page
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/branches/strategy-loading@24609 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/Core/pluginConfig.ts
+++ packages/libs/wdk-client/src/Core/pluginConfig.ts
@@ -1,13 +1,26 @@
 import { ClientPluginRegistryEntry } from 'wdk-client/Utils/ClientPlugin';
 import DefaultQuestionForm from 'wdk-client/Views/Question/DefaultQuestionForm';
 import ParameterComponent from 'wdk-client/Views/Question/ParameterComponent';
+import { ByGenotypeNumberCheckbox } from 'wdk-client/Views/Question/Params/ByGenotypeNumberCheckbox/ByGenotypeNumberCheckbox'
+import { ByGenotypeNumber } from 'wdk-client/Views/Question/Forms/ByGenotypeNumber/ByGenotypeNumber';
 
 // Default set of plugins provided by wdk
 // FIXME Make this typesafe by enumerating
 const pluginConfig: ClientPluginRegistryEntry<any>[] = [
   {
     type: 'questionForm',
+    searchName: 'ByGenotypeNumber',
+    component: ByGenotypeNumber
+  },
+  {
+    type: 'questionForm',
     component: DefaultQuestionForm
+  },
+  {
+    type: 'questionFormParameter',
+    name: 'genotype',
+    searchName: 'ByGenotypeNumber',
+    component: ByGenotypeNumberCheckbox
   },
   {
     type: 'questionFormParameter',
---
+++ packages/libs/wdk-client/src/Views/Question/Forms/ByGenotypeNumber/ByGenotypeNumber.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+
+import { Link } from 'wdk-client/Components';
+import { DispatchAction } from 'wdk-client/Core/CommonTypes';
+import { makeClassNameHelper } from 'wdk-client/Utils/ComponentUtils';
+import { QuestionState } from 'wdk-client/StoreModules/QuestionStoreModule';
+import {
+  changeGroupVisibility,
+  updateParamValue,
+  submitQuestion
+} from 'wdk-client/Actions/QuestionActions';
+import 'wdk-client/Views/Question/DefaultQuestionForm.scss';
+import { memoize } from 'lodash';
+
+type EventHandlers = {
+  setGroupVisibility: typeof changeGroupVisibility,
+  updateParamValue: typeof updateParamValue
+}
+
+type Props = {
+  state: QuestionState;
+  dispatchAction: DispatchAction;
+  eventHandlers: EventHandlers;
+  parameterElements: Record<string, React.ReactNode>;
+}
+
+const cx = makeClassNameHelper('wdk-QuestionForm');
+
+const onSubmit = memoize((dispatchAction: DispatchAction, urlSegment: string) => (e: React.FormEvent) => {
+  e.preventDefault();
+  dispatchAction(submitQuestion({ searchName: urlSegment }));
+});
+
+export const ByGenotypeNumber: React.SFC<Props> = ({
+  dispatchAction,
+  state: {
+    question: {
+      parametersByName,
+      urlSegment
+    }
+  }
+}) =>
+  <div className={`${cx()} ${cx('ByGenotypeNumber')}`}>
+    <h1>Identify RFLP Genotype Isolates based on RFLP Genotype Number</h1>
+    <form onSubmit={onSubmit(dispatchAction, urlSegment)}>
+      {parametersByName.genotype}
+      <div className={cx('SubmitSection')}>
+        <button type="submit" className="btn">
+          Get Answer
+        </button>
+      </div>
+      <hr />
+      <b>Data Sets used by this search</b>
+      <Link to="/record/dataset/DS_5dfd0d0bb2">
+        <i>T. gondii</i> RFLP genotypes (Chunlei Su lab)
+      </Link>
+    </form>
+  </div>;
