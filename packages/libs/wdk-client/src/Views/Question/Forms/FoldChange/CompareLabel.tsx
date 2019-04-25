aaf01b37242eee7077bae07a5adaae3ac844b8d0
Jamie Long <jtlong3rd@gmail.com>
2019-04-25 20:16:57 +0000

    Render RHS of Fold Change Param Group for Custom Question Pages
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/branches/strategy-loading@24702 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/Views/Question/Forms/FoldChange/CompareLabel.tsx
+++
@@ -1,5 +0,0 @@
-import { sampleLabel } from 'wdk-client/Views/Question/Forms/FoldChange/sampleLabel';
-
-import 'wdk-client/Views/Question/Forms/FoldChange/CompareLabel.scss';
-
-export const CompareLabel = sampleLabel('wdk-CompareLabel');
---
+++ packages/libs/wdk-client/src/Views/Question/Forms/FoldChange/ComparisonLabel.tsx
@@ -0,0 +1,5 @@
+import { sampleLabel } from 'wdk-client/Views/Question/Forms/FoldChange/sampleLabel';
+
+import 'wdk-client/Views/Question/Forms/FoldChange/ComparisonLabel.scss';
+
+export const ComparisonLabel = sampleLabel('wdk-ComparisonLabel');
