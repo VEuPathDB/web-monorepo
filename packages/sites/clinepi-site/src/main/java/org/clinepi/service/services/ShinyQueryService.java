12eb029fdf8fb37482e40fc4859cbe78cb373d7d
Ryan Doherty <tech@conical.org>
2019-05-03 03:25:29 +0000

    Factor to provide command line tool that can access same data as all endpoints
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiWebsite/trunk@97921 735e2a04-f8fc-0310-8a1b-f2942603c481

--- packages/sites/clinepi-site/src/main/java/org/clinepi/service/services/ShinyQueryService.java
+++ packages/sites/clinepi-site/src/main/java/org/clinepi/service/services/ShinyQueryService.java
@@ -1,10 +1,13 @@
 package org.clinepi.service.services;
 
+import static org.gusdb.fgputil.FormatUtil.NL;
 import static org.gusdb.fgputil.db.stream.ResultSetInputStream.getResultSetStream;
 
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -33,6 +36,22 @@ public class ShinyQueryService extends AbstractWdkService {
   @SuppressWarnings("unused")
   private static final Logger LOG = Logger.getLogger(ShinyQueryService.class);
 
+  private enum DataType {
+    Participant,
+    Household,
+    Observation,
+    ObservationNames,
+    Sample;
+
+    public static boolean isValueOf(String str) {
+      try { valueOf(str); return true; } catch(Exception e) { return false; }
+    }
+
+    public static String valuesAsString() {
+      return "[ " + Arrays.stream(values()).map(DataType::name).collect(Collectors.joining(", ")) + " ]";
+    }
+  }
+
   private Response getStreamingResponse(String sql, String queryName, String errorMsgOnFail) throws WdkModelException {
     return Response.ok(
       getStreamingOutput(
@@ -44,55 +63,19 @@ public class ShinyQueryService extends AbstractWdkService {
     ).build();
   }
 
-  @GET
-  @Path("Participant/{tblPrefix}/{sourceId}")
-  @Produces(MediaType.APPLICATION_JSON)
-  public Response getShinyParticipantData(
-      @PathParam("sourceId") String sourceId,
-      @PathParam("tblPrefix") String tblPrefix,
-      @DefaultValue("none") @QueryParam("timeSourceId") String timeSourceId)
-          throws WdkModelException {
-
-    String sql = timeSourceId.equals("none")
-        ? " select pa.name as Participant_Id" + 
-                ", pa." + sourceId +
-          " from apidbtuning." + tblPrefix + "Participants pa" +
-          " where pa." + sourceId + " is not null"
-        : " select pa.name as Participant_Id" +
-                ", pa." + sourceId + 
-                ", oa." + timeSourceId +
-          " from apidbtuning." + tblPrefix + "Participants pa" +
-              ", apidbtuning." + tblPrefix + "Observations oa" +
-              ", apidbtuning." + tblPrefix + "PartObsIO io" +
-          " where pa.pan_id = io.participant_id" +
-          " and io.observation_id = oa.pan_id" +
-          " and pa." + sourceId + " is not null";
-
-    return getStreamingResponse(sql, "getShinyParticipantData",
-        "Failed running SQL to fetch participant data.");
-  }
-
-  @GET
-  @Path("Household/{tblPrefix}/{sourceId}")
-  @Produces(MediaType.APPLICATION_JSON)
-  public Response getShinyHouseholdData(
-      @PathParam("sourceId") String sourceId,
-      @PathParam("tblPrefix") String tblPrefix,
-      @DefaultValue("none") @QueryParam("timeSourceId") String timeSourceId) 
-          throws WdkModelException {
-    return getStreamingResponse(getHouseholdSql(sourceId, tblPrefix, timeSourceId),
-        "getShinyHouseholdData", "Failed running SQL to fetch household data.");
-  }
-
   public static void main(String[] args) throws WdkModelException, IOException {
-    if (args.length != 2) {
-      System.err.println("USAGE: fgpJava " + ShinyQueryService.class.getName() + " <projectId> <outputFile>");
+    if (args.length != 6 || !DataType.isValueOf(args[1])) {
+      System.err.println(NL +
+          "USAGE: fgpJava " + ShinyQueryService.class.getName() +
+          " <projectId> <dataType> <tblPrefix> <sourceId> <timeSourceId> <outputFile>" + NL + NL +
+          "Note: dataType must be one of " + DataType.valuesAsString() + NL + NL);
       System.exit(1);
     }
     try (WdkModel wdkModel = WdkModel.construct(args[0], GusHome.getGusHome());
-         BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(args[1]), 10240)) {
+         BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(args[5]), 10240)) {
       Timer t = new Timer();
-      String sql = getHouseholdSql("OBI_0001627", "D2a6ace17a1", "EUPATH_0015467");
+      //String sql = getSql(DataType.Household, "OBI_0001627", "D2a6ace17a1", "EUPATH_0015467");
+      String sql = getSql(DataType.valueOf(args[1]), args[2], args[3], args[4]);
       ResultSetRowConverter rowConverter = new ResultSetToNdJsonConverter();
       new SQLRunner(wdkModel.getAppDb().getDataSource(), sql)
         .executeQuery(rs -> {
@@ -116,10 +99,86 @@ public class ShinyQueryService extends AbstractWdkService {
     }
   }
 
-  private static String getHouseholdSql(String sourceId, String tblPrefix, String timeSourceId) {
-    // have to remember when calling this that timeSourceId representing time is different for households
-    // so ask for a different sourceId, the houseObs equivalent of timeSourceId (ex: gems BFO_0000015->EUPATH_0015467)
-    return timeSourceId.equals("none")
+  @GET
+  @Path("Participant/{tblPrefix}/{sourceId}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getShinyParticipantData(
+      @PathParam("tblPrefix") String tblPrefix,
+      @PathParam("sourceId") String sourceId,
+      @DefaultValue("none") @QueryParam("timeSourceId") String timeSourceId)
+          throws WdkModelException {
+    return getStreamingResponse(getSql(DataType.Participant, tblPrefix, sourceId, timeSourceId),
+        "getShinyParticipantData", "Failed running SQL to fetch participant data.");
+  }
+
+  @GET
+  @Path("Household/{tblPrefix}/{sourceId}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getShinyHouseholdData(
+      @PathParam("tblPrefix") String tblPrefix,
+      @PathParam("sourceId") String sourceId,
+      @DefaultValue("none") @QueryParam("timeSourceId") String timeSourceId) 
+          throws WdkModelException {
+    return getStreamingResponse(getSql(DataType.Household, tblPrefix, sourceId, timeSourceId),
+        "getShinyHouseholdData", "Failed running SQL to fetch household data.");
+  }
+
+  @GET
+  @Path("Observation/{tblPrefix}/{sourceId}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getShinyObservationData(
+      @PathParam("tblPrefix") String tblPrefix,
+      @PathParam("sourceId") String sourceId,
+      @DefaultValue("none") @QueryParam("timeSourceId") String timeSourceId) 
+          throws WdkModelException {
+    return getStreamingResponse(getSql(DataType.Observation, tblPrefix, sourceId, timeSourceId),
+        "getShinyObservationData", "Failed running SQL to fetch observation data.");
+  }
+
+  @GET
+  @Path("ObservationNames/{tblPrefix}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getShinyObservationNamesData(
+      @PathParam("tblPrefix") String tblPrefix,
+      @DefaultValue("none") @QueryParam("timeSourceId") String timeSourceId) 
+          throws WdkModelException {
+    return getStreamingResponse(getSql(DataType.ObservationNames, tblPrefix, null, timeSourceId),
+        "getShinyObservationNamesData", "Failed running SQL to fetch observation name data.");
+  }
+
+  @GET
+  @Path("Sample/{tblPrefix}/{sourceId}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getShinySampleData(
+      @PathParam("tblPrefix") String tblPrefix,
+      @PathParam("sourceId") String sourceId,
+      @DefaultValue("none") @QueryParam("timeSourceId") String timeSourceId)
+          throws WdkModelException {
+    return getStreamingResponse(getSql(DataType.Sample, tblPrefix, sourceId, timeSourceId),
+        "getShinySampleData", "Failed running SQL to fetch sample data.");
+  }
+
+  private static String getSql(DataType dataType, String sourceId, String tblPrefix, String timeSourceId) {
+    switch(dataType) {
+      case Participant:
+        return timeSourceId.equals("none")
+            ? " select pa.name as Participant_Id" + 
+                    ", pa." + sourceId +
+              " from apidbtuning." + tblPrefix + "Participants pa" +
+              " where pa." + sourceId + " is not null"
+            : " select pa.name as Participant_Id" +
+                    ", pa." + sourceId + 
+                    ", oa." + timeSourceId +
+              " from apidbtuning." + tblPrefix + "Participants pa" +
+                  ", apidbtuning." + tblPrefix + "Observations oa" +
+                  ", apidbtuning." + tblPrefix + "PartObsIO io" +
+              " where pa.pan_id = io.participant_id" +
+              " and io.observation_id = oa.pan_id" +
+              " and pa." + sourceId + " is not null";
+      case Household:
+        // have to remember when calling this that timeSourceId representing time is different for households
+        // so ask for a different sourceId, the houseObs equivalent of timeSourceId (ex: gems BFO_0000015->EUPATH_0015467)
+        return timeSourceId.equals("none")
             ? " select pa.name as Participant_Id" +
                     ", h.string_value as " + sourceId +
               " from apidbtuning." + tblPrefix + "HouseholdMD h" +
@@ -164,18 +223,8 @@ public class ShinyQueryService extends AbstractWdkService {
                     ", time." + timeSourceId +
               " from time, term" +
               " where term.key = time.key";
-  }
-
-  @GET
-  @Path("Observation/{tblPrefix}/{sourceId}")
-  @Produces(MediaType.APPLICATION_JSON)
-  public Response getShinyObservationData(
-      @PathParam("sourceId") String sourceId,
-      @PathParam("tblPrefix") String tblPrefix,
-      @DefaultValue("none") @QueryParam("timeSourceId") String timeSourceId) 
-          throws WdkModelException {
-
-    String sql = timeSourceId.equals("none")
+      case Observation:
+        return timeSourceId.equals("none")
             ? " select pa.name as participant_id" +
                     ", string_value as " + sourceId +
               " from apidbtuning." + tblPrefix + "ObservationMD o" +
@@ -203,20 +252,8 @@ public class ShinyQueryService extends AbstractWdkService {
                     ", time." + timeSourceId + 
               " from time, term" + 
               " where term.sub_observation_id = time.sub_observation_id";
-
-    return getStreamingResponse(sql, "getShinyObservationData",
-        "Failed running SQL to fetch observation data.");
-  }
-
-  @GET
-  @Path("ObservationNames/{tblPrefix}")
-  @Produces(MediaType.APPLICATION_JSON)
-  public Response getShinyObservationNamesData(
-      @PathParam("tblPrefix") String tblPrefix,
-      @DefaultValue("none") @QueryParam("timeSourceId") String timeSourceId) 
-          throws WdkModelException {
-
-    String sql = timeSourceId.equals("none")
+      case ObservationNames:
+        return timeSourceId.equals("none")
             ? " select pa.name as participant_id" +
                     ", oa.name as observation_id" +
               " from apidbtuning." + tblPrefix + "Observations oa" +
@@ -232,21 +269,8 @@ public class ShinyQueryService extends AbstractWdkService {
                   ", apidbtuning." + tblPrefix + "PartObsIO po" +
               " where oa.pan_id = po.observation_id" +
               " and po.participant_id = pa.pan_id";
-
-    return getStreamingResponse(sql, "getShinyObservationNamesData",
-        "Failed running SQL to fetch observation data.");
-  }
-
-  @GET
-  @Path("Sample/{tblPrefix}/{sourceId}")
-  @Produces(MediaType.APPLICATION_JSON)
-  public Response getShinySampleData(
-      @PathParam("sourceId") String sourceId,
-      @PathParam("tblPrefix") String tblPrefix,
-      @DefaultValue("none") @QueryParam("timeSourceId") String timeSourceId)
-          throws WdkModelException {
-
-    String sql = timeSourceId.equals("none")
+      case Sample:
+        return timeSourceId.equals("none")
             ? " select pa.name as Participant_Id, sa." + sourceId +
               " from apidbtuning." + tblPrefix + "Participants pa" +
                   ", apidbtuning." + tblPrefix + "Samples sa "+
@@ -269,8 +293,8 @@ public class ShinyQueryService extends AbstractWdkService {
               " and io.observation_id = io2.observation_id" +
               " and io2.sample_id = sa.pan_id" +
               " and sa." + sourceId + " is not null";
-
-    return getStreamingResponse(sql, "getShinySampleData",
-        "Failed running SQL to fetch sample data.");
+        default:
+          throw new IllegalArgumentException("Unsupported data type: " + dataType);
+    }
   }
 }
