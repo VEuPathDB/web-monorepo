3a1c5f0ecfdb58da4c2963c7373b800e5f1f3e80
Cristina Aurrecoechea <aurreco@uga.edu>
2019-06-11 22:31:04 +0000

    refs #34447
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/trunk@24949 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/Components/AttributeFilter/UnknownCount.jsx
+++ packages/libs/wdk-client/src/Components/AttributeFilter/UnknownCount.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 export default function UnknownCount(props) {
   const { activeFieldState, dataCount, displayName } = props;
   const unknownCount = dataCount - activeFieldState.summary.internalsCount;
+  const percent = Math.round(activeFieldState.summary.internalsCount*100/dataCount);
   return unknownCount > 0
     ? (
       <div className="unknown-count">
-        <b>{unknownCount.toLocaleString()} of {dataCount.toLocaleString()}</b> {displayName} have no data provided for this filter
+         {/*  <b>{unknownCount.toLocaleString()} of {dataCount.toLocaleString()}</b> {displayName} have no data provided for this filter */}
+        <b>{activeFieldState.summary.internalsCount.toLocaleString()} of {dataCount.toLocaleString()} ({percent}%)</b> {displayName} have data for this filter
       </div>
     )
     : null
