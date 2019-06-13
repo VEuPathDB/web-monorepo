f606c2d8363339e9254a341d80352d47cdc4bf92
Cristina Aurrecoechea <aurreco@uga.edu>
2019-06-13 23:06:17 +0000

    position percentage in right place
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/trunk@24964 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/Components/AttributeFilter/UnknownCount.jsx
+++ packages/libs/wdk-client/src/Components/AttributeFilter/UnknownCount.jsx
@@ -8,7 +8,7 @@ export default function UnknownCount(props) {
     ? (
       <div className="unknown-count">
          {/*  <b>{unknownCount.toLocaleString()} of {dataCount.toLocaleString()}</b> {displayName} have no data provided for this filter */}
-        <b>{activeFieldState.summary.internalsCount.toLocaleString()} of {dataCount.toLocaleString()} ({percent}%)</b> {displayName} have data for this filter
+        <b>{activeFieldState.summary.internalsCount.toLocaleString()} ({percent}%) of {dataCount.toLocaleString()}</b> {displayName} have data for this filter
       </div>
     )
     : null
