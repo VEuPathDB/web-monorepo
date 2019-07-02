ab3172930f27533c0f4813f4838696e8d371df5e
Dave Falke <dmfalke@gmail.com>
2019-07-02 18:01:48 +0000

    convert to typescript
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/branches/strategy-loading@25098 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ packages/libs/wdk-client/src/Components/Display/CollapsibleSection.tsx
@@ -0,0 +1,71 @@
+/**
+ * Simple collapsible section that renders a header and content, along with an
+ * icon indicating collapsed status.
+ *
+ * This is a fully controlled widget.
+ */
+
+import { zipWith } from 'lodash';
+import React, { useState } from 'react';
+import { wrappable } from 'wdk-client/Utils/ComponentUtils';
+import './CollapsibleSection.css';
+
+interface Props {
+  id?: string;
+  isCollapsed?: boolean;
+  onCollapsedChange: (isCollapsed: boolean) => void;
+  headerContent: React.ReactNode;
+  headerComponent?: React.ReactType;
+  className?: string;
+  children: React.ReactNode;
+}
+
+const defaultClassName = 'wdk-CollapsibleSection';
+
+function CollapsibleSection(props: Props) {
+  const { className, id, isCollapsed = true, headerContent, children, onCollapsedChange } = props;
+  const [ shouldRenderChildren, setShouldRenderChildren ] = useState(!isCollapsed);
+  const [ containerClassName, headerClassName, contentClassName ] =
+    makeClassNames(isCollapsed, defaultClassName, className);
+  const Header = props.headerComponent || 'div';
+
+  function handleCollapsedChange() {
+    setShouldRenderChildren(true);
+    onCollapsedChange(!isCollapsed);
+  }
+  return (
+    <div id={id} className={containerClassName}>
+      <Header className={headerClassName} onClick={handleCollapsedChange}>
+        {headerContent}
+      </Header>
+      <div className={contentClassName}>
+        {shouldRenderChildren ? children : null}
+      </div>
+    </div>
+  )
+}
+
+
+// Helper to generate class names based on an array of baseNames,
+// and then to zip them into a space-separated array of class names.
+//
+// This will add suffixes to baseNames for the header and content container
+// elements.
+function makeClassNames(isCollapsed: boolean, ...baseNames: (string|undefined)[]) {
+  let classNames = baseNames
+  .filter(baseName => baseName != null)
+  .map(function(baseName) {
+    return ['', 'Header', 'Content']
+    .map(function(suffix) {
+      return baseName + suffix;
+    })
+    .map(function(className) {
+      return className + (isCollapsed ? ' ' + className + '__collapsed' : '');
+    });
+  });
+  return zipWith(...classNames, function(...args) {
+    return args.slice(0, classNames.length).join(' ');
+  });
+}
+
+export default wrappable(CollapsibleSection);
