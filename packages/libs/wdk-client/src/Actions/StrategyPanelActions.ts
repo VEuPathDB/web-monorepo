a47643f57649f8120ddf72452b7d81575e2cad67
Dave Falke <dmfalke@gmail.com>
2019-07-09 20:03:12 +0000

    formatting
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/branches/strategy-loading@25121 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/Actions/StrategyPanelActions.ts
+++ packages/libs/wdk-client/src/Actions/StrategyPanelActions.ts
@@ -1,55 +1,55 @@
 import { makeActionCreator, InferAction } from 'wdk-client/Utils/ActionCreatorUtils';
 
 export const openStrategyPanel = makeActionCreator(
-    'strategyPanel/open',
-    (viewId: string) => ({ viewId })
-  );
-  
+  'strategyPanel/open',
+  (viewId: string) => ({ viewId })
+);
+
 export const closeStrategyPanel = makeActionCreator(
-    'strategyPanel/close',
-    (viewId: string) => ({ viewId })
-  );
-  
+  'strategyPanel/close',
+  (viewId: string) => ({ viewId })
+);
+
 export const requestStrategyPanelVisibilityPreference = makeActionCreator(
-    'strategyPanel/requestStrategyPanelVisibilityPreference',
-    (viewId: string, isVisible: boolean) => ({ isVisible, viewId })
-  );
-  
+  'strategyPanel/requestStrategyPanelVisibilityPreference',
+  (viewId: string, isVisible: boolean) => ({ isVisible, viewId })
+);
+
 export const requestStrategyPanelVisibilityChange = makeActionCreator(
-    'strategyPanel/requestStrategyPanelVisibilityChange',
-    (viewId: string, isVisible: boolean) => ({ isVisible, viewId })
- );
-  
+  'strategyPanel/requestStrategyPanelVisibilityChange',
+  (viewId: string, isVisible: boolean) => ({ isVisible, viewId })
+);
+
 export const fulfillStrategyPanelVisibility = makeActionCreator(
-    'strategyPanel/fulfillStrategyPanelVisibility',
-    (viewId: string, isVisible: boolean) => ({ isVisible, viewId })
-    );
-  
+  'strategyPanel/fulfillStrategyPanelVisibility',
+  (viewId: string, isVisible: boolean) => ({ isVisible, viewId })
+);
+
 export const setStepDetailsVisibility = makeActionCreator(
-    'strategyPanel/setStepDetailsVisibility',
-    (viewId: string, stepId: number | undefined) => ({ stepId, viewId })
-    );
+  'strategyPanel/setStepDetailsVisibility',
+  (viewId: string, stepId: number | undefined) => ({ stepId, viewId })
+);
 
 export const setInsertStepWizardVisibility = makeActionCreator(
-    'strategyPanel/setInsertStepWizardVisibility',
-    (viewId: string, stepId: number | undefined) => ({ stepId, viewId })
-    );
+  'strategyPanel/setInsertStepWizardVisibility',
+  (viewId: string, stepId: number | undefined) => ({ stepId, viewId })
+);
 
 export const setDeleteStepDialogVisibilty = makeActionCreator(
-    'strategyPanel/setDeleteStepDialogVisibilty',
-    (viewId: string, stepId: number | undefined) => ({ stepId, viewId })
-    );
+  'strategyPanel/setDeleteStepDialogVisibilty',
+  (viewId: string, stepId: number | undefined) => ({ stepId, viewId })
+);
 
 export const setDeleteStrategyDialogVisibilty = makeActionCreator(
-    'strategyPanel/setDeleteStrategyDialogVisibilty',
-    (viewId: string, strategyId: number | undefined) => ({ strategyId, viewId })
-    );
+  'strategyPanel/setDeleteStrategyDialogVisibilty',
+  (viewId: string, strategyId: number | undefined) => ({ strategyId, viewId })
+);
 
 export const setStrategyPanelHeightOverride = makeActionCreator(
-    'strategyPanel/setStrategyPanelHeightOverride',
-    (viewId: string, heightOverride: number) => ({ heightOverride, viewId })
-    );
-    
+  'strategyPanel/setStrategyPanelHeightOverride',
+  (viewId: string, heightOverride: number) => ({ heightOverride, viewId })
+);
+
 /*
 setDidYouKnowVisibility
 nextDidYouKnow
@@ -61,9 +61,9 @@ export type Action =
   | InferAction<typeof openStrategyPanel>
   | InferAction<typeof closeStrategyPanel>
   | InferAction<typeof requestStrategyPanelVisibilityChange>
-  | InferAction<typeof fulfillStrategyPanelVisibility> 
+  | InferAction<typeof fulfillStrategyPanelVisibility>
   | InferAction<typeof setStepDetailsVisibility>
   | InferAction<typeof setInsertStepWizardVisibility>
-  | InferAction<typeof setDeleteStepDialogVisibilty> 
+  | InferAction<typeof setDeleteStepDialogVisibilty>
   | InferAction<typeof setDeleteStrategyDialogVisibilty>
   | InferAction<typeof setStrategyPanelHeightOverride>
