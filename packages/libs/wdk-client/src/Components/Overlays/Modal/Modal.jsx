a403843f8c0abcf1a4ab262f4f2e657b6488dafd
Dave Falke <dmfalke@gmail.com>
2019-07-17 18:08:03 +0000

    fix bad import
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/branches/strategy-loading@25204 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/Components/Overlays/Modal/Modal.jsx
+++ packages/libs/wdk-client/src/Components/Overlays/Modal/Modal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import 'wdk-client/Components/Overlays/Modal/Modal.scss';
 import { BodyLayer } from 'wdk-client/Components/Mesa';
-import { useBodyScrollManager } from 'wdk-client/Components/Overlays/BodyScrollHook';
+import { useBodyScrollManager } from 'wdk-client/Components/Overlays/BodyScrollManager';
 
 function Modal(props) {
   const { when, wrapperClass, ...otherProps } = props;
