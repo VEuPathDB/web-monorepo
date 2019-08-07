999892ef2bff4622a27bd97d379431fad5e3dbe0
Dave Falke <dmfalke@gmail.com>
2019-08-07 14:36:09 +0000

    always show "unknown counts"
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/trunk@25317 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- packages/libs/wdk-client/src/Components/AttributeFilter/UnknownCount.jsx
+++ packages/libs/wdk-client/src/Components/AttributeFilter/UnknownCount.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 
 export default function UnknownCount(props) {
   const { activeFieldState, dataCount, displayName } = props;
-  const unknownCount = dataCount - activeFieldState.summary.internalsCount;
   const percent = Math.round(activeFieldState.summary.internalsCount*100/dataCount);
-  return unknownCount > 0
-    ? (
-      <div className="unknown-count">
-         {/*  <b>{unknownCount.toLocaleString()} of {dataCount.toLocaleString()}</b> {displayName} have no data provided for this filter */}
-        <b>{activeFieldState.summary.internalsCount.toLocaleString()} ({percent}%) of {dataCount.toLocaleString()}</b> {displayName} have data for this filter
+  return (
+    <div className="unknown-count">
+      <b>{activeFieldState.summary.internalsCount.toLocaleString()} ({percent}%) of {dataCount.toLocaleString()}</b> {displayName} have data for this filter
       </div>
-    )
-    : null
+  );
 }
