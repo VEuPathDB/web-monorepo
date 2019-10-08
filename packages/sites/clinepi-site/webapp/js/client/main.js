b82309a08deeb7220726a32aac83d554d590f184
Dave Falke <dmfalke@gmail.com>
2019-10-08 16:02:11 -0400

    revert change

--- packages/sites/clinepi-site/webapp/js/client/main.js
+++ packages/sites/clinepi-site/webapp/js/client/main.js
@@ -1,10 +1,10 @@
+import 'site/css/ClinEpiSite.scss';
 import { initialize } from 'ebrc-client/bootstrap';
 import componentWrappers from './component-wrappers';
 import wrapStoreModules from './wrapStoreModules';
 import { wrapRoutes } from './routes';
 import { reduxMiddleware } from 'ebrc-client/App/DataRestriction/DataRestrictionUtils'
 
-import 'site/css/ClinEpiSite.scss';
 
 initialize({
   componentWrappers,
