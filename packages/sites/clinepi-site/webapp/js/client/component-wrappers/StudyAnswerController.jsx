0250392227093eeb04a7e4e6b498474d4c7dfcb7
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2019-10-15 21:20:58 -0400

    render DownloadLink, no need to pass attemptAction as prop

--- packages/sites/clinepi-site/webapp/js/client/component-wrappers/StudyAnswerController.jsx
+++ packages/sites/clinepi-site/webapp/js/client/component-wrappers/StudyAnswerController.jsx
@@ -1,11 +1,5 @@
-import { get } from 'lodash';
 import React from 'react';
-import { connect } from 'react-redux';
-import { Seq } from 'wdk-client/IterableUtils';
-import { makeClassNameHelper } from 'wdk-client/ComponentUtils';
-import StudySearches from 'ebrc-client/App/Studies/StudySearches';
 import DownloadLink from 'ebrc-client/App/Studies/DownloadLink';
-import { attemptAction } from 'ebrc-client/App/DataRestriction/DataRestrictionActionCreators';
 
 function StudyAnswerController(props) {
   return (
@@ -19,7 +13,6 @@ function StudyAnswerController(props) {
     </React.Fragment>
   );
 }
-
 
 /* defined in WDKClient/../AnswerController.jsx
 interface CellContentProps {
@@ -43,18 +36,11 @@ const deriveRowClassName = props => {
   return 'greyed-out';
 };
 
-
 const renderCellContent = props => {
   if (props.attribute.name === 'bulk_download_url') {
-    return <DownloadLink studyId={props.record.id.value} studyUrl= {props.record.attributes.bulk_download_url.url} attemptAction={attemptAction}/>;
+    return <DownloadLink studyId={props.record.id[0].value} studyUrl= {props.record.attributes.bulk_download_url.url}/>;
   }
   return <props.CellContent {...props}/>
 };
 
-
-
-const mapDispatchToProps = {
-  attemptAction
-};
-
-export default connect(null, mapDispatchToProps)(StudyAnswerController);
+export default StudyAnswerController;
