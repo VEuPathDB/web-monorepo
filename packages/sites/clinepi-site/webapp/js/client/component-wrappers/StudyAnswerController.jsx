f5c773aceaf1c1a652948718637213a09103d9d3
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2019-10-17 20:43:33 -0400

    Clinepi All STudies page, columns: categories and searches

--- packages/sites/clinepi-site/webapp/js/client/component-wrappers/StudyAnswerController.jsx
+++ packages/sites/clinepi-site/webapp/js/client/component-wrappers/StudyAnswerController.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import DownloadLink from 'ebrc-client/App/Studies/DownloadLink';
+import CategoryIcon from 'ebrc-client/App/Categories/CategoryIcon';
+import { makeClassNameHelper } from 'wdk-client/ComponentUtils';
 
 function StudyAnswerController(props) {
   return (
@@ -36,7 +38,34 @@ const deriveRowClassName = props => {
   return 'greyed-out';
 };
 
+const cx = makeClassNameHelper('ce-StudySearchIconLinks');
+
 const renderCellContent = props => {
+  if (props.attribute.name === 'study_categories') {
+    let studyCategories = JSON.parse(props.record.attributes.study_categories);
+    return studyCategories.map(cat => (
+              <CategoryIcon category={cat} key={cat} />
+            ));
+  }
+  if (props.attribute.name === 'card_questions') { 
+    let cardQuestions = JSON.parse(props.record.attributes.card_questions);
+    let iconNames = {"participants":"fa fa-male",
+                     "observations":"fa fa-stethoscope",
+                     "lighttraps":"fa fa-bug",
+                     "households":"fa fa-home"
+                   };
+    return (
+    <div className={cx()}>
+      {Object.entries(cardQuestions).map(([key,value]) => {
+        <div key={key} className={cx('Item')}>
+            <a href={`ce/showQuestion.do?questionFullName=${value}`}>
+              <i className={iconNames[key]}/>
+            </a>
+        </div>
+        })
+      }</div>
+    );
+  }
   if (props.attribute.name === 'bulk_download_url') {
     return <DownloadLink studyId={props.record.id[0].value} studyUrl= {props.record.attributes.bulk_download_url.url}/>;
   }
