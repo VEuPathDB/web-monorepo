3edd47dc404e2ecd5636fd14ba544f38e6fb0e85
Dave Falke <dmfalke@gmail.com>
2019-10-25 13:09:05 -0400

    Move StudyRecordHeading to EbrcWebsiteCommon and add new props

--- packages/sites/clinepi-site/webapp/js/client/component-wrappers/RecordHeading.jsx
+++ packages/sites/clinepi-site/webapp/js/client/component-wrappers/RecordHeading.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import StudyRecordHeading from './StudyRecordHeading';
+import StudyRecordHeading from 'ebrc-client/component-wrappers/StudyRecordHeading';
 
 export default RecordHeading => props => (
   props.recordClass.urlSegment === 'dataset'
-    ? <StudyRecordHeading {...props} DefaultComponent={RecordHeading} />
+    ? <StudyRecordHeading {...props} DefaultComponent={RecordHeading} showSearches showDownload />
     : <RecordHeading {...props} />
 )
