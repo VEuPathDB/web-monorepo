21e50153b5e3a11e9feb296a23f00345ccdc0431
Jamie Long <jtlong3rd@gmail.com>
2019-12-24 12:39:54 -0500

    Add tooltip configuration props to HelpIcon (in anticipation of future accessibility work); make HelpIcon tabbable

--- packages/libs/wdk-client/src/Components/Icon/HelpIcon.tsx
+++ packages/libs/wdk-client/src/Components/Icon/HelpIcon.tsx
@@ -3,17 +3,26 @@ import React from 'react';
 import Icon from 'wdk-client/Components/Icon/IconAlt';
 import Tooltip, { TooltipPosition } from 'wdk-client/Components/Overlays/Tooltip';
 
+import 'wdk-client/Components/Icon/HelpIcon.scss';
+
 type Props = {
   children: string | React.ReactElement<any>;
   tooltipPosition?: TooltipPosition;
-}
+  tooltipShowEvent?: string;
+  tooltipHideEvent?: string;
+};
 
 export default function HelpIcon (props: Props) {
   return (
-    <Tooltip content={props.children} position={props.tooltipPosition}>
-      <div className="HelpTrigger">
+    <Tooltip 
+      content={props.children} 
+      position={props.tooltipPosition} 
+      showEvent={props.tooltipShowEvent}
+      hideEvent={props.tooltipHideEvent}
+    >
+      <button type="button" className="link HelpTrigger">
         <Icon fa="question-circle"/>
-      </div>
+      </button>
     </Tooltip>
   )
 };
