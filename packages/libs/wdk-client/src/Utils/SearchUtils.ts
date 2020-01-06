3c79dce0246048cb9fb961a1c94321ed4ed009ee
Dave Falke <dmfalke@gmail.com>
2020-01-06 15:14:54 -0500

    formatting

--- packages/libs/wdk-client/src/Utils/SearchUtils.ts
+++ packages/libs/wdk-client/src/Utils/SearchUtils.ts
@@ -14,11 +14,11 @@
 * @return {Array<Object>} an Array of items that pass the filter
 */
 export function filterItems<T>(items: Array<T>, itemToSearchableString: (item: T) => string, searchQueryString: string) {
-    if (!searchQueryString || !items) return items;
+  if (!searchQueryString || !items) return items;
 
-    let terms = parseSearchQueryString(searchQueryString);
-    let predicate = function (item: T) { return areTermsInString(terms, itemToSearchableString(item))};
-    return items.filter(predicate);
+  let terms = parseSearchQueryString(searchQueryString);
+  let predicate = function (item: T) { return areTermsInString(terms, itemToSearchableString(item)) };
+  return items.filter(predicate);
 }
 
 /**
@@ -27,12 +27,12 @@ export function filterItems<T>(items: Array<T>, itemToSearchableString: (item: T
  * @returns {Array<String>} A set of query terms parsed from searchQueryString
  */
 export function parseSearchQueryString(searchQueryString: string) {
-    let match = searchQueryString.match(/\w+|"[^"]*"/g);
-    if (match == null) return [];
-    return match.map(function(queryTerm) {
-        // remove wrapping quotes from phrases
-        return queryTerm.replace(/(^")|("$)/g, '');
-    });
+  let match = searchQueryString.match(/\w+|"[^"]*"/g);
+  if (match == null) return [];
+  return match.map(function (queryTerm) {
+    // remove wrapping quotes from phrases
+    return queryTerm.replace(/(^")|("$)/g, '');
+  });
 }
 
 /**
@@ -52,7 +52,7 @@ export function areTermsInString(queryTerms: Array<string>, searchableString: st
  * @returns {boolean} true if a match
  */
 export function isTermInString(queryTerm: string, searchableString: string = '') {
-    return !queryTerm || (searchableString.toLowerCase().includes(queryTerm.toLowerCase()));
+  return !queryTerm || (searchableString.toLowerCase().includes(queryTerm.toLowerCase()));
 }
 
 
