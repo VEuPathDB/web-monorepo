f73212b86ab37c68244bfef4768a60156778ed60
Dave Falke <dmfalke@gmail.com>
2020-01-15 11:02:51 -0500

    Add binning feature to filterParam

--- packages/libs/wdk-client/src/Components/AttributeFilter/Histogram.jsx
+++ packages/libs/wdk-client/src/Components/AttributeFilter/Histogram.jsx
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import { debounce, isEqual, memoize, noop, orderBy, throttle } from 'lodash';
+import { debounce, isEqual, memoize, noop, orderBy, throttle, get } from 'lodash';
 import PropTypes from 'prop-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -8,7 +8,10 @@ import { Seq } from 'wdk-client/Utils/IterableUtils';
 import DateRangeSelector from 'wdk-client/Components/InputControls/DateRangeSelector';
 import NumberRangeSelector from 'wdk-client/Components/InputControls/NumberRangeSelector';
 import { formatDate, formatNumber, parseDate } from 'wdk-client/Components/AttributeFilter/AttributeFilterUtils';
+import Icon from 'wdk-client/Components/Icon/IconAlt';
+import { CollapsibleSection } from 'wdk-client/Components';
 
+const DAY = 1000 * 60 * 60 * 24;
 
 var distributionEntryPropType = PropTypes.shape({
   value: PropTypes.number.isRequired,
@@ -39,6 +42,8 @@ const transforms = {
   }
 }
 
+const PLOT_SETTINGS_OPEN_KEY = 'wdk/filterParam/plotSettingsOpen';
+
 var Histogram = (function() {
 
   /** Common histogram component */
@@ -50,6 +55,7 @@ var Histogram = (function() {
       this.emitStateChange = debounce(this.emitStateChange, 100);
       this.state = {
         uiState: this.getStateFromProps(props),
+        showSettings: sessionStorage.getItem(PLOT_SETTINGS_OPEN_KEY) !== 'false'
       };
       this.getRange = memoize(this.getRange);
       this.getNumFixedDigits = memoize(this.getNumFixedDigits);
@@ -106,10 +112,10 @@ var Histogram = (function() {
 
     getStateFromProps(props) {
       // Set default yAxis max based on distribution
-      var yaxisMax = this.getYAxisMax(props);
+      var yaxisMax = props.uiState.yaxisMax != null ? props.uiState.yaxisMax : this.getYAxisMax(props);
       var { xaxisMin, xaxisMax } = this.getXAxisMinMax(props);
-      var { scaleYAxis = false } = props.uiState;
-      return { yaxisMax, xaxisMin, xaxisMax, scaleYAxis };
+      var { scaleYAxis = false, binSize = 0, binStart = Math.min(xaxisMin, 0), yaxisMin = 0 } = props.uiState;
+      return { yaxisMax, xaxisMin, xaxisMax, scaleYAxis, binSize, binStart, yaxisMin };
     }
 
     getRange(distribution) {
@@ -128,8 +134,6 @@ var Histogram = (function() {
     }
 
     getYAxisMax(props) {
-      if (props.uiState.yaxisMax != null) return props.uiState.yaxisMax;
-
       var counts = props.distribution.map(entry => entry.count);
       // Reverse sort, then pull out first and second highest values
       var [ max, nextMax ] = counts.sort((a, b) => a < b ? 1 : -1);
@@ -163,14 +167,20 @@ var Histogram = (function() {
       }), { prev: null, minDistance: Infinity });
       return Math.max(minDistance * padding, minWidth);
     }
+    
+    getBinnedDistribution(binSize, binStart, distribution) {
+      return createBinnedDistribution(
+        this.props.chartType === 'date' ? binSize * DAY : binSize,
+        this.props.chartType === 'date' ? binStart * DAY : binStart,
+        distribution
+      );
+    }
 
     getSeriesData(distribution) {
       return [{
         data: distribution.map(entry => [ entry.value, entry.count ]),
-        color: '#AAAAAA'
       },{
         data: distribution.map(entry => [ entry.value, entry.filteredCount ]),
-        color: '#DA7272',
         hoverable: false,
         // points: { show: true }
       }];
@@ -209,9 +219,6 @@ var Histogram = (function() {
       const currentSelection = unwrapXaxisRange(this.plot.getSelection());
       const { selectedMin, selectedMax } = this.props;
       const { min, max } = this.getRange(this.props.distribution);
-      const plotOptions = this.plot.getOptions();
-
-      const { series: { bars: { barWidth } } } = plotOptions;
 
       // Selection already matches current state
       if (selectedMin === currentSelection.min && selectedMax === currentSelection.max) {
@@ -223,19 +230,27 @@ var Histogram = (function() {
       } else {
         this.plot.setSelection({
           xaxis: {
-            from: (selectedMin === null ? min : selectedMin) - (barWidth / 2),
-            to: (selectedMax === null ? max : selectedMax) + (barWidth / 2)
+            from: (selectedMin === null ? min : selectedMin)/*  - (barWidth / 2) */,
+            to: (selectedMax === null ? max : selectedMax)/*  + (barWidth / 2) */
           }
         }, true);
       }
     }
 
     createPlot() {
-      var { distribution, chartType, timeformat } = this.props;
+      var { chartType, timeformat } = this.props;
       var { uiState } = this.state;
-      const clampedDistribution = this.getClampedDistribution(distribution, uiState);
+      var { binSize, binStart } = uiState;
+      const distribution = binSize ? this.getBinnedDistribution(binSize, binStart, this.props.distribution) : this.props.distribution;
+      const clampedDistribution = binSize ? distribution : this.getClampedDistribution(distribution, uiState);
 
-      var barWidth = this.getBarWidth(clampedDistribution);
+      var barWidth = binSize
+        ? (
+          chartType === 'date'
+            ? binSize * DAY
+            : binSize
+        )
+        : this.getBarWidth(clampedDistribution);
 
       var xaxisBaseOptions = chartType === 'date'
         ? { mode: 'time', timeformat: timeformat }
@@ -248,23 +263,28 @@ var Histogram = (function() {
 
       var yTransform = this.state.uiState.scaleYAxis ? transforms['10'] : transforms.none;
 
+      const opacity = binSize ? 0.75 : 1;
+      const lineWidth = binSize ? 1 : 0;
+
       var plotOptions = {
         series: {
           bars: {
             show: true,
-            fillColor: { colors: [{ opacity: 1 }, { opacity: 1 }] },
             barWidth: barWidth,
-            lineWidth: 0,
-            align: 'center'
+            fill: true,
+            fillColor: { colors: [{ opacity }, { opacity }] },
+            lineWidth,
+            align: binSize ? 'left' : 'center'
           }
         },
+        colors: [ '#AAAAAA', '#DA7272' ],
         xaxis: Object.assign({
           tickLength: 0,
-          min: uiState.xaxisMin - barWidth,
-          max: uiState.xaxisMax + barWidth
+          min: binSize ? uiState.xaxisMin : uiState.xaxisMin - barWidth,
+          max: binSize ? uiState.xaxisMax  : uiState.xaxisMax + barWidth
         }, xaxisBaseOptions),
         yaxis: {
-          min: 0,
+          min: uiState.yaxisMin,
           max: uiState.yaxisMax,
           transform: yTransform.xform,
           inverseTransform: yTransform.inverse,
@@ -293,15 +313,15 @@ var Histogram = (function() {
           prerender: true,
           content: ' ',
           position: {
-            target: 'mouse',
-            viewport: this.$el,
-            my: 'bottom center'
+            target: this.$chart,
+            viewport: 'window',
+            my: 'bottom center',
+            at: 'top left',
           },
           show: false,
           hide: {
-            event: false,
+            delay: 2000,
             fixed: true,
-            inactive: 2000
           },
           style: {
             classes: 'qtip-wdk'
@@ -311,14 +331,14 @@ var Histogram = (function() {
     }
 
     handlePlotHover(event, pos, item) {
-      var qtipApi = this.tooltip.qtip('api'),
-        previousPoint;
+      var qtipApi = this.tooltip.qtip('api');
 
-      if (!item) return;
-
-      previousPoint = qtipApi.cache.point;
-
-      if (previousPoint !== item.dataIndex) {
+      if (!item) {
+        qtipApi.cache.point = false;
+        return;
+      }
+      
+      if (qtipApi.cache.point !== item.dataIndex) {
         qtipApi.cache.point = item.dataIndex;
         const [
           { data: unfilteredData },
@@ -326,46 +346,25 @@ var Histogram = (function() {
         ] = this.plot.getData();
         const [ value, unfilteredCount ] = unfilteredData[item.dataIndex];
         const [ , filteredCount ] = filteredData[item.dataIndex];
+        const pointToPixelFactor = item.series.xaxis.box.width / (item.series.xaxis.max - item.series.xaxis.min);
+        const barWidthPx = item.series.bars.barWidth / 2 * pointToPixelFactor;
         var formattedValue = this.props.chartType === 'date'
           ? formatDate(this.props.timeformat, value)
-          : formatNumber(value, { useScientificNotation: true });
-
+          : formatNumber(value, { useScientificNotation: false });
+        var formattedBinEnd = this.props.chartType === 'date'
+          ? formatDate(this.props.timeformat, value + (this.state.uiState.binSize || 0) * DAY)
+          : formatNumber(value + (this.state.uiState.binSize || 0), { useScientificNotation: false });
+        var binRange = `[${formattedValue}, ${formattedBinEnd})`;
+        var valueDisplay = this.state.uiState.binSize ? binRange : formattedValue;
         qtipApi.set('content.text',
-          this.props.xaxisLabel + ': ' + formattedValue +
+          this.props.xaxisLabel + ': ' + valueDisplay +
           '<br/>All ' + this.props.yaxisLabel + ': ' + unfilteredCount +
           '<br/>Remaining ' + this.props.yaxisLabel + ': ' + filteredCount);
+        const offset = this.$chart.offset();
         qtipApi.elements.tooltip.stop(1, 1);
+        qtipApi.set('position.adjust.x', item.pageX - offset.left + (this.state.uiState.binSize ? barWidthPx : 0));
+        qtipApi.set('position.adjust.y', item.pageY - offset.top);
         qtipApi.show(item);
-      }
-    }
-
-    updatePlotScale(partialUiState) {
-      const { yaxisMax, xaxisMin, xaxisMax } = partialUiState;
-      const clampedDistribution = this.getClampedDistribution(this.props.distribution, partialUiState);
-      const seriesData = this.getSeriesData(clampedDistribution);
-
-      if (yaxisMax == null && xaxisMin == null && xaxisMax == null) return;
-
-      const plotOptions = this.plot.getOptions();
-
-      const {
-        series: { bars: { barWidth } },
-        yaxes: { 0: { max: currYaxisMax } },
-        xaxes: { 0: { min: currXaxisMin, max: currXaxisMax } }
-      } = plotOptions;
-
-      if (
-        (yaxisMax != null && yaxisMax !== currYaxisMax) ||
-        (xaxisMin != null && xaxisMin !== currXaxisMin) ||
-        (xaxisMax != null && xaxisMax !== currXaxisMax)
-      ) {
-        if (yaxisMax != null) plotOptions.yaxes[0].max = yaxisMax;
-        if (xaxisMin != null) plotOptions.xaxes[0].min = xaxisMin - barWidth;
-        if (xaxisMax != null) plotOptions.xaxes[0].max = xaxisMax + barWidth;
-        this.plot.setData(seriesData);
-        this.plot.setupGrid();
-        this.drawPlotSelection();
-        this.plot.draw();
       }
     }
 
@@ -378,11 +377,11 @@ var Histogram = (function() {
       this.props.onUiStateChange(uiState);
     }
 
-    setYAxisMax(yaxisMax) {
-      this.updateUIState(Object.assign({}, this.state.uiState, { yaxisMax }));
+    setYAxisRange(yaxisMin, yaxisMax) {
+      this.updateUIState(Object.assign({}, this.state.uiState, { yaxisMin, yaxisMax }));
     }
 
-    setXAxisScale(xaxisMin, xaxisMax) {
+    setXAxisRange(xaxisMin, xaxisMax) {
       this.updateUIState(Object.assign({}, this.state.uiState, { xaxisMin, xaxisMax }));
     }
 
@@ -390,27 +389,59 @@ var Histogram = (function() {
       this.updateUIState({ ...this.state.uiState, scaleYAxis })
     }
 
+    setBinSize(binSize) {
+      const { binStart } = this.state.uiState;
+      const distribution = binSize ? this.getBinnedDistribution(binSize, binStart, this.props.distribution) : this.props.distribution;
+      const { min: xaxisMin, max: xaxisMax } = this.getRange(distribution);
+      const yaxisMax = binSize
+        ? Math.max(...distribution.map(entry => entry.count))
+        : this.getYAxisMax(this.props)
+      this.updateUIState({ ...this.state.uiState, binSize, yaxisMax, xaxisMin, xaxisMax: xaxisMax + (binSize || 0) });
+    }
+
+    setBinStart(binStart) {
+      const { binSize } = this.state.uiState;
+      const distribution = binSize ? this.getBinnedDistribution(binSize, binStart, this.props.distribution) : this.props.distribution;
+      const { min: xaxisMin, max: xaxisMax } = this.getRange(distribution);
+      const yaxisMax = binSize
+        ? Math.max(...distribution.map(entry => entry.count))
+        : this.getYAxisMax(this.props)
+      this.updateUIState({ ...this.state.uiState, binStart, yaxisMax, xaxisMin, xaxisMax: xaxisMax + (binSize || 0) });
+    }
+
+    setSettingsOpen(showSettings) {
+      sessionStorage.setItem(PLOT_SETTINGS_OPEN_KEY, JSON.stringify(Boolean(showSettings)));
+      this.setState({ showSettings })
+    }
+
+    resetUiState() {
+      const { min: xaxisMin, max: xaxisMax } = this.getRange(this.props.distribution);
+      const yaxisMax = this.getYAxisMax(this.props);
+      const binSize = 0;
+      const binStart = Math.min(0, xaxisMin);
+      this.updateUIState({ ...this.state.uiState, binSize, binStart, yaxisMax, xaxisMin, xaxisMax });
+    }
+
     render() {
       var { xaxisLabel, yaxisLabel, chartType, timeformat, distribution } = this.props;
-      var { yaxisMax, xaxisMin, xaxisMax } = this.state.uiState;
+      var { showSettings } = this.state;
+      var { yaxisMin, yaxisMax, xaxisMin, xaxisMax } = this.state.uiState;
 
       var numFixedDigits = this.getNumFixedDigits(distribution);
       var step = 1 * Math.pow(10, numFixedDigits * -1);
-
-      var counts = distribution.map(entry => entry.count);
-      var countsMin = Math.min(...counts);
-      var countsMax = Math.max(...counts);
 
       var values = distribution.map(entry => entry.value);
       var valuesMin = Math.min(...values);
       var valuesMax = Math.max(...values);
 
-      var scaleSelector = chartType === 'date' ? (
+      var xaxisScaleSelector = chartType === 'date' ? (
         <DateRangeSelector
+          inline
+          hideReset
           value={{ min: formatDate(timeformat, xaxisMin), max: formatDate(timeformat, xaxisMax) }}
           start={formatDate(timeformat, valuesMin)}
           end={formatDate(timeformat, valuesMax)}
-          onChange={value => this.setXAxisScale(parseDate(value.min).getTime(), parseDate(value.max).getTime())}
+          onChange={value => this.setXAxisRange(parseDate(value.min).getTime(), parseDate(value.max).getTime())}
         />
       ) : (
         <React.Fragment>
@@ -419,14 +450,19 @@ var Histogram = (function() {
             start={-Infinity}
             end={Infinity}
             step={step}
-            onChange={value => this.setXAxisScale(Number(value.min), Number(value.max))}
-          /> &nbsp;
-          <button
-            type="button"
-            onClick={() => this.setXAxisScale(valuesMin, valuesMax)}
-          >reset</button>
+            onChange={value => this.setXAxisRange(Number(value.min), Number(value.max))}
+          />
         </React.Fragment>
-      )
+      );
+
+      var yAxisScaleSelector = (
+        <NumberRangeSelector
+          value={{ min: yaxisMin, max: yaxisMax }}
+          start={0}
+          end={Infinity}
+          onChange={value => this.setYAxisRange(Number(value.min), Number(value.max))}
+        />
+      );
 
       return (
         <div className="chart-container">
@@ -435,29 +471,39 @@ var Histogram = (function() {
               <div>
                 {yaxisLabel}
               </div>
-              <div>
-                <input
-                  style={{width: '90%'}}
-                  type="range"
-                  min={Math.max(countsMin, 1.1)}
-                  max={countsMax + countsMax * 0.1}
-                  title={yaxisMax}
-                  value={yaxisMax}
-                  onChange={e => this.setYAxisMax(Number(e.target.value))}/>
-              </div>
             </div>
           <div className="chart-title x-axis">{xaxisLabel}</div>
-          <div className="chart-controls">
-            <div>
-              Zoom: {scaleSelector}
-            </div>
-            <div>
-              <label>
-                <input type="checkbox" checked={this.state.uiState.scaleYAxis} onChange={event => this.setScaleYAxis(event.target.checked)}/>
-                &nbsp;Scale counts (log<sub>10</sub>)
-              </label>
-            </div>
-          </div>
+          <CollapsibleSection
+            isCollapsed={!showSettings}
+            onCollapsedChange={isCollasped => this.setSettingsOpen(!isCollasped)}
+            headerContent={<div><Icon fa="gear"/>&nbsp;&nbsp;Plot Settings</div>}
+            className="chart-controls"
+          >
+            <fieldset>
+              <legend>x-axis{/*xaxisLabel*/}</legend>
+              <div>
+                <div>Range: {xaxisScaleSelector}</div>
+              </div>
+              {chartType !== 'date' && <div>Bin start: <input type="number" max={valuesMin} value={this.state.uiState.binStart} onChange={e => this.setBinStart(eventToNumber(e))}/></div>}
+              <div>Bin size: <input type="number" min={0} value={this.state.uiState.binSize} onChange={e => this.setBinSize(eventToNumber(e))}/></div>
+            </fieldset>
+            <fieldset>
+              <legend>y-axis{/*yaxisLabel*/}</legend>
+              <div>Range: {yAxisScaleSelector}&nbsp;&nbsp;</div>
+              <div>Scale counts:&nbsp;&nbsp;
+                <label>
+                  <input type="radio" checked={!this.state.uiState.scaleYAxis} onChange={() => this.setScaleYAxis(false)}/>
+                  &nbsp;linear
+                </label>
+                &nbsp;&nbsp;&nbsp;
+                <label>
+                  <input type="radio" checked={this.state.uiState.scaleYAxis} onChange={() => this.setScaleYAxis(true)}/>
+                  &nbsp;log<sub>10</sub>
+                </label>
+              </div>
+            </fieldset>
+            <div><button type="button" onClick={() => this.resetUiState()}>Reset to defaults</button></div>
+          </CollapsibleSection>
         </div>
       );
     }
@@ -476,7 +522,11 @@ var Histogram = (function() {
     uiState: PropTypes.shape({
       xaxisMin: PropTypes.number,
       xaxisMax: PropTypes.number,
-      yaxisMax: PropTypes.number
+      yaxisMin: PropTypes.number,
+      yaxisMax: PropTypes.number,
+      binSize: PropTypes.number,
+      binStart: PropTypes.number,
+      scaleYAxis: PropTypes.bool,
     }),
 
     onUiStateChange: PropTypes.func,
@@ -526,4 +576,41 @@ const FIXED_DIGITS_RE = /^(\d*)\.?(\d*)$/;
 function getNumFixedDigits(num) {
   const matches = FIXED_DIGITS_RE.exec(String(num));
   return matches == null ? 0 : matches[2].length;
+}
+
+// # Notes on binning
+//
+// - Bins will be left-inclusive, right-exclusive. E.g., [1-3)
+// - There are two controls: binSize and binStart.
+// - Binning will be done by transforming datapoints into
+//   the start value of the bin in which it will be assigned.
+// - Bar width will be determined by binSize.
+//
+
+function assignBin(binSize, binStart, value) {
+  if (value < binStart) return;
+  const shift = binStart % binSize;
+  const shiftedValue = value - shift;
+  const multiplier = Math.floor(shiftedValue / binSize);
+  const bin = binSize * multiplier;
+  return bin + shift
+}
+
+function createBinnedDistribution(binSize, binStart, distribution) {
+  const binnedObject = distribution.reduce((binnedDist, entry) => {
+    const bin = assignBin(binSize, binStart, entry.value);
+    if (bin == null) return binnedDist;
+    const count = get(binnedDist, [bin, 'count'], 0) + entry.count;
+    const filteredCount = get(binnedDist, [bin, 'filteredCount'], 0) + entry.filteredCount;
+    return Object.assign(binnedDist, { [bin]: { count, filteredCount }});
+  }, {});
+  return Object.entries(binnedObject).map(entry => ({
+    value: Number(entry[0]),
+    ...entry[1]
+  }));
+}
+
+function eventToNumber(event) {
+  const value = event.target.value.trim();
+  return value === '' ? undefined : Number(value);
 }
