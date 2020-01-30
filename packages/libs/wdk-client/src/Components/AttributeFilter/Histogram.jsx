4c4f3d95042ddcb2a501f64b408d7077b51b18f0
Dave Falke <dmfalke@gmail.com>
2020-01-30 10:20:18 -0500

    retain xaxis range when bin size changes

--- packages/libs/wdk-client/src/Components/AttributeFilter/Histogram.jsx
+++ packages/libs/wdk-client/src/Components/AttributeFilter/Histogram.jsx
@@ -394,12 +394,11 @@ var Histogram = (function() {
     }
 
     setXAxisBinState(binStart, binSize) {
-      const { min: xaxisMin, max: xaxisMax } = this.getRange(this.props.distribution);
       const distribution = binSize ? this.getBinnedDistribution(binSize, binStart, this.props.distribution) : this.props.distribution;
       const yaxisMax = binSize
         ? Math.max(...distribution.map(entry => entry.count))
         : this.getYAxisMax(this.props)
-      this.updateUIState(uiState => ({ ...uiState, binStart, binSize, yaxisMax, xaxisMin, xaxisMax: xaxisMax + (binSize || 0) }));
+      this.updateUIState(uiState => ({ ...uiState, binStart, binSize, yaxisMax }));
     }
 
     resetXAxisState() {
