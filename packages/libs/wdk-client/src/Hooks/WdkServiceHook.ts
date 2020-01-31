52d95921da00ce7a78ddeb52d114746688ea8162
Dave Falke <dmfalke@gmail.com>
2020-01-31 13:57:14 -0500

    new hook to extract data from wdk service

---
+++ packages/libs/wdk-client/src/Hooks/WdkServiceHook.ts
@@ -0,0 +1,29 @@
+import WdkService, { useWdkEffect } from "wdk-client/Service/WdkService";
+import { useState } from "react";
+
+interface WdkServiceCallback<T> {
+  (wdkService: WdkService): Promise<T>;
+}
+
+/**
+ * Use WdkService to extract data from the WDK Service REST API.
+ * @param callback Returns a Promise whose resolved value is used
+ *                 as the return value of this hook.
+ */
+export function useWdkService<T>(callback: WdkServiceCallback<T>): T | undefined {
+  const [ value, setValue ] = useState<T>();
+  const [ active, setActive ] = useState(true);
+  useWdkEffect(wdkService => {
+    callback(wdkService).then(
+      value => {
+        if (active) setValue(value);
+      },
+      error => {
+        if (active) {
+          alert('Oops... something went wrong\n' + error);
+          wdkService.submitErrorIfNot500(error);        }
+      });
+    return () => { setActive(false) }
+  }, [callback])
+  return value;
+}
