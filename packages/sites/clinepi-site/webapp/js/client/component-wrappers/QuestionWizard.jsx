583af6f7a33a51b3099149b2a64793b0f516d2f5
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2020-03-03 08:22:31 -0500

    #39893 in progress

--- packages/sites/clinepi-site/webapp/js/client/component-wrappers/QuestionWizard.jsx
+++ packages/sites/clinepi-site/webapp/js/client/component-wrappers/QuestionWizard.jsx
@@ -9,7 +9,7 @@ const injectSearchStudy = connect((state, props) => ({
 
 
 export default QuestionWizard => injectSearchStudy(props => {
-  let { activeStudy } = props;
+  let { activeStudy, wizardState } = props;
   return (
     <QuestionWizard
       {...props}
@@ -23,6 +23,14 @@ export default QuestionWizard => injectSearchStudy(props => {
           </div>
         ) : (
           <div>Could not find study based on record class.</div>
+        )}
+      questionSummary={wizardState.question
+        ? (
+          <div className="clinepi-Summary">
+            <span dangerouslySetInnerHTML={{__html: wizardState.question.summary}}/>
+          </div>
+        ) : (
+          <div>Could not find question.</div>
         )}
     />
   );
