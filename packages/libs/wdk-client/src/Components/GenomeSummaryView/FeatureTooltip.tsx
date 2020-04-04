e08d331ce5b82214d1c7ab41ffc0fdb92fbed3c7
Jamie Long <jtlong3rd@gmail.com>
2020-04-03 23:36:07 -0400

    Genome Summary View: only display JBrowse and Gene ID links on component sites

--- packages/libs/wdk-client/src/Components/GenomeSummaryView/FeatureTooltip.tsx
+++ packages/libs/wdk-client/src/Components/GenomeSummaryView/FeatureTooltip.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useWdkService } from 'wdk-client/Hooks/WdkServiceHook';
 import { GenomeViewFeatureModel, GenomeViewSequenceModel } from 'wdk-client/Utils/GenomeSummaryViewUtils';
+
+const PORTAL_SITE_PROJECT_ID = 'EuPathDB';
 
 interface FeatureTooltipProps {
   feature: GenomeViewFeatureModel;
@@ -12,30 +15,38 @@ export const FeatureTooltip: React.SFC<FeatureTooltipProps> = ({
   feature,
   sequence,
   recordType,
-}) => (
-  <div id={feature.sourceId}>
-    <h4>{feature.sourceId}</h4>
-    <p>
-      start: {`${feature.startFormatted}, end: ${feature.endFormatted}, on ${feature.strand} strand of ${sequence.sourceId}`}
-    </p>
-    <p>
-      {feature.description}
-    </p>
-    <ul>
-      <li>
-        <Link to={`/record/${recordType}/${feature.sourceId}`} target="_blank">
-          <u>
-            Record page
-          </u>
-        </Link>
-      </li>
-      <li>
-        <Link to={`/jbrowse?loc=${feature.context}&tracks=gene&data=/a/service/jbrowse/tracks/${sequence.organismAbbrev}`} target="_blank">
-          <u>
-            Genome browser
-          </u>
-        </Link>
-      </li>
-    </ul>
-  </div>
-);
+}) => {
+  const config = useWdkService(wdkService => wdkService.getConfig(), []);
+  const isPortalSite = config?.projectId === PORTAL_SITE_PROJECT_ID;
+
+  return (
+    <div id={feature.sourceId}>
+      <h4>{feature.sourceId}</h4>
+      <p>
+        start: {`${feature.startFormatted}, end: ${feature.endFormatted}, on ${feature.strand} strand of ${sequence.sourceId}`}
+      </p>
+      <p>
+        {feature.description}
+      </p>
+      {
+        !isPortalSite &&
+        <ul>
+          <li>
+            <Link to={`/record/${recordType}/${feature.sourceId}`} target="_blank">
+              <u>
+                Record page
+              </u>
+            </Link>
+          </li>
+          <li>
+            <Link to={`/jbrowse?loc=${feature.context}&tracks=gene&data=/a/service/jbrowse/tracks/${sequence.organismAbbrev}`} target="_blank">
+              <u>
+                Genome browser
+              </u>
+            </Link>
+          </li>
+        </ul>
+      }
+    </div>
+  );
+};
