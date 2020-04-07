d862fb288862974d85b96e665b78217585345e7e
Jamie Long <jtlong3rd@gmail.com>
2020-04-07 15:41:39 -0400

    Move logic for scrolling to top of a page upon certain route changes into a hook

--- packages/libs/wdk-client/src/Components/Layout/Page.tsx
+++ packages/libs/wdk-client/src/Components/Layout/Page.tsx
@@ -7,6 +7,7 @@ import {wrappable, makeClassNameHelper} from 'wdk-client/Utils/ComponentUtils';
 import Header from 'wdk-client/Components/Layout/Header';
 import Footer from 'wdk-client/Components/Layout/Footer';
 import ErrorBoundary from 'wdk-client/Core/Controllers/ErrorBoundary';
+import { useScrollUpOnRouteChange } from 'wdk-client/Hooks/Page';
 
 type Props = RouteComponentProps<any> & {
   classNameModifier?: string;
@@ -15,27 +16,16 @@ type Props = RouteComponentProps<any> & {
 
 const cx = makeClassNameHelper('wdk-RootContainer');
 
-class Page extends React.Component<Props> {
-  componentDidUpdate(prevProps: Props) {
-    if (
-      this.props.history.action !== 'REPLACE' &&
-      (
-        (prevProps.location.pathname !== this.props.location.pathname) ||
-        (prevProps.location.search !== this.props.location.search)
-      )
-    ) {
-      window.scrollTo(0, 0);
-    }
-  }
-  render () {
-    return (
-      <div className={cx('', this.props.classNameModifier)}>
-        <ErrorBoundary><Header/></ErrorBoundary>
-        <div className="wdk-PageContent">{this.props.children}</div>
-        <ErrorBoundary><Footer/></ErrorBoundary>
-      </div>
-    );
-  }
+function Page(props: Props) {
+  useScrollUpOnRouteChange();
+
+  return (
+    <div className={cx('', props.classNameModifier)}>
+      <ErrorBoundary><Header/></ErrorBoundary>
+      <div className="wdk-PageContent">{props.children}</div>
+      <ErrorBoundary><Footer/></ErrorBoundary>
+    </div>
+  );
 }
 
 export default wrappable(withRouter(Page));
