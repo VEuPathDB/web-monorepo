fca3fb87737fa8b451ede32e41e33f14fa82d441
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2020-05-06 16:21:26 -0400

    minor margin
    (cherry picked from commit d79c7fcc29b26db511c2c7aaa7ef67f88b9c734c)

--- packages/libs/wdk-client/src/Views/ReporterForm/ReporterSortMessage.tsx
+++ packages/libs/wdk-client/src/Views/ReporterForm/ReporterSortMessage.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 let ReporterSortMessage: StatelessComponent<Props> = props =>
     (props.scope === "record" ? <noscript/> :
-      <div style={{margin: '1em 0 0 0'}}>
+      <div style={{margin: '0.3em 0 0 0'}}>
         <i>Note: IDs will automatically be included in the report and the report will be sorted by ID.</i>
       </div>
     );
