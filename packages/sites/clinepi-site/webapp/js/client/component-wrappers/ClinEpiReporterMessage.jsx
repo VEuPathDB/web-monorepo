c90052a5c10f418ce000bdb6b8858b56d2f1d9d4
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2020-05-06 15:32:53 -0400

    download page: remove 'Note' on first sentence
    (cherry picked from commit e6f3b965451cadbf2db0321f7a6b3b3f8ae5dd12)

--- packages/sites/clinepi-site/webapp/js/client/component-wrappers/ClinEpiReporterMessage.jsx
+++ packages/sites/clinepi-site/webapp/js/client/component-wrappers/ClinEpiReporterMessage.jsx
@@ -5,7 +5,7 @@ function ClinEpiReporterMessage(props) {
   return (
     <React.Fragment>
       <div style={{margin: '1em 0 0 0'}}>
-        <b>Note: All dates were obfuscated per participant through the application of a random number algorithm 
+        <b>All dates were obfuscated per participant through the application of a random number algorithm 
            that shifted dates no more than seven days to comply with the ethical conduct of human subjects research.</b> 
       </div>
       <props.DefaultComponent
