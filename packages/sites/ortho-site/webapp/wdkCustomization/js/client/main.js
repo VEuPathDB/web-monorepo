204c37b8d630b814815155e96d6ead665894efd1
Jamie Long <jtlong3rd@gmail.com>
2020-05-11 13:11:25 -0400

    Import Ebrc-specific styles

--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/main.js
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/main.js
@@ -1,6 +1,8 @@
 import { initialize } from 'ebrc-client/bootstrap';
 import componentWrappers from './component-wrappers';
 
+import 'eupathdb/wdkCustomization/css/client.scss';
+
 initialize({
   componentWrappers
 });
