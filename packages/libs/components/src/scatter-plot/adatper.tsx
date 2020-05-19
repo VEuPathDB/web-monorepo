2c41a519bc4294e29b035662f5678d87035c06e0
Dave Falke <dmfalke@gmail.com>
2020-05-19 11:49:26 -0400

    initial commit

---
+++ packages/libs/components/src/scatter-plot/adatper.tsx
@@ -0,0 +1,37 @@
+import ScatterPlot from '.';
+import { useState } from 'react';
+
+interface Props {
+  studyId: string;
+  xVariableId: string;
+  yVariableId: string;
+}
+
+/**
+ * Adapter to use scatter plot in ClinEpiDB workspace.
+ * This would reside in ClinEpiWebsite.
+ * @param props 
+ */
+export default function ScatterPlotAdapter(props: Props) {
+  const { studyId, xVariableId, yVariableId } = props;
+  const data = useClinEpiPlotReporter(studyId, xVariableId, yVariableId);
+  const [ plotState, setPlotState ] = useState();
+  return (
+    <ScatterPlot
+      xData={data.xData}
+      yData={data.yData}
+      xLabel={xVariableId}
+      yLabel={yVariableId}
+      onUpdate={setPlotState}
+    />
+  )
+}
+
+// stubbed out functions
+function useClinEpiPlotReporter(studyId: string, xVariableId: string, yVariableId: string) {
+  // request data from wdk rest service and return response
+  return {
+    xData: [1,2,3],
+    yData: [4,5,6]
+  }
+}
---
+++ packages/libs/components/src/scatter-plot/index.tsx
@@ -0,0 +1,48 @@
+import { useState, useCallback } from "react";
+import Plot, { Figure } from "react-plotly.js";
+
+/**
+ * Data passed to this component, typically gathered by an adapter.
+ */
+interface Props {
+  xData: number[],
+  yData: number[],
+  xLabel: string,
+  yLabel: string,
+  onUpdate?: (state: any) => void;
+}
+
+/**
+ * Renders a scatter plot.
+ * 
+ * This is some really excellent documentation about how to use ScatterPlot.
+ * 
+ * @param props 
+ */
+export default function ScatterPlot(props: Props) {
+  const { xData, yData, onUpdate } = props;
+  
+  const [ state, updateState ] = useState<Figure>({
+    data: [{
+      type: 'scatter',
+      x: xData,
+      y: yData
+    }],
+    layout: {},
+    frames: []
+  });
+
+  const handleUpdate = useCallback((figure: Figure, graphDiv: HTMLElement) => {
+    updateState(figure);
+    if (onUpdate) onUpdate(figure);
+  }, [ updateState, onUpdate ]);
+
+  return (
+    <Plot
+      data={state.data}
+      layout={state.layout}
+      frames={state.frames || undefined}
+      onUpdate={handleUpdate}
+    />
+  )
+}
