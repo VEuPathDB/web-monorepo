80136efdbefa7273e2273adff2ac4b6e1d7328c0
Dave Falke <dmfalke@gmail.com>
2020-05-26 16:18:20 -0400

    Show error message in modal.

--- packages/libs/wdk-client/src/Controllers/UnhandledErrorsController.tsx
+++ packages/libs/wdk-client/src/Controllers/UnhandledErrorsController.tsx
@@ -1,15 +1,18 @@
-import React, { ReactChild } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, ReactElement } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'wdk-client/Core/State/Types';
 import UnhandledErrors from 'wdk-client/Views/UnhandledErrors/UnhandledErrors';
+import { clearUnhandledErrors } from 'wdk-client/Actions/UnhandledErrorActions';
 
 interface Props {
-  children?: ReactChild;
+  children: ReactElement;
 }
 export default function UnhandledErrorsController(props: Props) {
   const errors = useSelector<RootState, any[]>(state => state.unhandledErrors.errors);
+  const dispatch = useDispatch();
+  const clearErrors = useCallback(() => dispatch(clearUnhandledErrors()), [ dispatch ])
   return (
-    <UnhandledErrors errors={errors} showDetails>
+    <UnhandledErrors errors={errors} showDetails={process.env.NODE_ENV === 'production'} clearErrors={clearErrors}>
       {props.children}
     </UnhandledErrors>
   );
--- packages/libs/wdk-client/src/Views/UnhandledErrors/UnhandledErrors.tsx
+++ packages/libs/wdk-client/src/Views/UnhandledErrors/UnhandledErrors.tsx
@@ -1,31 +1,54 @@
-import React, { ReactNode } from "react";
-import { wrappable } from "wdk-client/Utils/ComponentUtils";
+import React, { ReactElement } from "react";
+import { wrappable, makeClassNameHelper } from "wdk-client/Utils/ComponentUtils";
 import ErrorStatus from "wdk-client/Components/PageStatus/Error";
+import Modal from "wdk-client/Components/Overlays/Modal";
+import Icon from "wdk-client/Components/Icon/Icon";
 
 import './UnhandledErrors.scss';
+
+const cx = makeClassNameHelper('UnhandledErrors');
 
 interface Props {
   errors?: any[];
   showDetails: boolean;
-  children?: ReactNode;
+  clearErrors: () => void;
+  children: ReactElement;
 }
 
 function UnhandledError(props: Props) {
-  const { children = null, errors, showDetails } = props;
+  const { children, clearErrors, errors, showDetails } = props;
 
-  if (errors == null || errors.length === 0) return <>{children}</>;
+  const modal = errors && errors.length > 0 && (
+    <Modal>
+      <div className={cx()}>
+        <button type="button" onClick={clearErrors}>
+          <Icon type="close"/>
+        </button>
+        <ErrorStatus/>
+        {showDetails && (
+          <div className={cx('--Details')}>
+            {errors.map(error => <ErrorDetail error={error}/>)}
+          </div>
+        )}
+      </div>
+    </Modal>
+  );
 
   return (
-    <div className="UnhandledErrors">
-      <ErrorStatus/>
-      {showDetails && errors.map(error => <ErrorDetail error={error}/>)}
-    </div>
-  )
+    <>
+      {children}
+      {modal}
+    </>
+  );
 }
 
 function ErrorDetail(props: { error: any }) {
   const { error } = props;
-  return <p>{String(error)}</p>;
+  return (
+    <pre className={cx('--DetailItem')}>
+      {'stack' in error ? error.stack : String(error)}
+    </pre>
+  );
 }
 
 export default wrappable(UnhandledError);
