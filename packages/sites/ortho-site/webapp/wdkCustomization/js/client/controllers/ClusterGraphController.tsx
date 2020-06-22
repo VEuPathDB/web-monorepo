2e3d29a3978ee98a8b9901b900c21bbd159f55f8
Jamie Long <jtlong3rd@gmail.com>
2020-06-22 10:20:57 -0400

    Initial commit for cluster graph and associated test component

---
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/controllers/ClusterGraphController.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function ClusterGraphController() {
+  return <div>Future home of Cluster Graph</div>;
+}
--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/routes.tsx
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/routes.tsx
@@ -1,6 +1,7 @@
 import { RouteEntry } from 'wdk-client/Core/RouteEntry';
 
 import { OrthoMCLHomePageController } from './controllers/OrthoMCLHomePageController';
+import { ClusterGraphController } from './controllers/ClusterGraphController';
 
 export function wrapRoutes(ebrcRoutes: RouteEntry[]): RouteEntry[] {
   return [
@@ -8,6 +9,12 @@ export function wrapRoutes(ebrcRoutes: RouteEntry[]): RouteEntry[] {
       path: '/',
       component: OrthoMCLHomePageController,
       rootClassNameModifier: 'home-page'
+    },
+    // TODO: Delete this route once the initial implementation
+    // TODO: of the cluster graph is complete
+    {
+      path: '/cluster-graph/:groupName',
+      component: ClusterGraphController
     },
     ...ebrcRoutes
   ];
