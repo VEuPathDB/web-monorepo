2fd8154a320cd50e0870448d29838b4b6d7a1d18
Jamie Long <jtlong3rd@gmail.com>
2020-06-22 14:11:47 -0400

    Initial commit of client-side OrthoService (NB: types/decoders for service responses to be implemented in tandem with relevant client features)

--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/main.js
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/main.js
@@ -1,11 +1,13 @@
 import { initialize } from 'ebrc-client/bootstrap';
 import componentWrappers from './component-wrappers';
 import { wrapRoutes } from './routes';
+import { wrapWdkService } from './services';
 
 import 'eupathdb/wdkCustomization/css/client.scss';
 import '../../css/client.scss';
 
 initialize({
   componentWrappers,
-  wrapRoutes
+  wrapRoutes,
+  wrapWdkService
 });
---
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/services.tsx
@@ -0,0 +1,64 @@
+import { WdkService } from 'wdk-client/Core';
+import { ok } from 'wdk-client/Utils/Json';
+
+export function wrapWdkService(wdkService: WdkService): OrthoService {
+  return ({
+    ...wdkService,
+    getGenomeSources: orthoServiceWrappers.getGenomeSources(wdkService),
+    getGenomeStatistics: orthoServiceWrappers.getGenomeStatistics(wdkService),
+    getGroupLayout: orthoServiceWrappers.getGroupLayout(wdkService),
+    getTaxons: orthoServiceWrappers.getTaxons(wdkService)
+  });
+};
+
+const orthoServiceWrappers = {
+  getGenomeSources: (wdkService: WdkService) => () =>
+    wdkService.sendRequest<unknown>(
+      ok,
+      {
+        useCache: true,
+        method: 'get',
+        path: `/data-summary/genome-sources`
+      }
+    ),
+  getGenomeStatistics: (wdkService: WdkService) => () =>
+    wdkService.sendRequest<unknown>(
+      ok,
+      {
+        useCache: true,
+        method: 'get',
+        path: `/data-summary/genome-statistics`
+      }
+    ),
+  getGroupLayout: (wdkService: WdkService) => (groupName: string) =>
+    wdkService.sendRequest<unknown>(
+      ok,
+      {
+        useCache: true,
+        method: 'get',
+        path: `/group/${groupName}/layout`
+      }
+    ),
+  getTaxons: (wdkService: WdkService) => () =>
+    wdkService.sendRequest<unknown>(
+      ok,
+      {
+        useCache: true,
+        method: 'get',
+        path: `/data-summary/taxons`
+      }
+    )
+};
+
+export interface OrthoService extends WdkService {
+  getGenomeSources: () => Promise<unknown>;
+  getGenomeStatistics: () => Promise<unknown>;
+  getGroupLayout: (groupName: string) => Promise<unknown>;
+  getTaxons: () => Promise<unknown>;
+}
+
+export function isOrthoService(wdkService: WdkService): wdkService is OrthoService {
+  return Object.keys(orthoServiceWrappers).every(
+    orthoServiceKey => orthoServiceKey in wdkService
+  );
+}
