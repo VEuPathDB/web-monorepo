fec31e6e025eff4b8a90ac40e76cc936e0cca365
Dave Falke <dmfalke@gmail.com>
2020-06-29 14:04:19 -0400

    Move contact us link rendering to EbrcWebsiteCommon

--- packages/libs/wdk-client/src/Views/UnhandledErrors/UnhandledErrors.tsx
+++ packages/libs/wdk-client/src/Views/UnhandledErrors/UnhandledErrors.tsx
@@ -3,10 +3,10 @@ import React, { ReactElement } from "react";
 import { wrappable, makeClassNameHelper } from "wdk-client/Utils/ComponentUtils";
 import Modal from "wdk-client/Components/Overlays/Modal";
 import Icon from "wdk-client/Components/Icon/Icon";
+import { UnhandledError } from 'wdk-client/Actions/UnhandledErrorActions';
+import ErrorStatus from 'wdk-client/Components/PageStatus/Error';
 
 import './UnhandledErrors.scss';
-import { UnhandledError } from 'wdk-client/Actions/UnhandledErrorActions';
-import Link from 'wdk-client/Components/Link';
 
 const cx = makeClassNameHelper('UnhandledErrors');
 
@@ -21,32 +21,26 @@ function UnhandledErrors(props: Props) {
   const { children, clearErrors, errors, showStackTraces } = props;
   const groupedErrors = groupBy(errors, 'type');
   const errorTypes = [ 'input', 'runtime', 'server', 'client' ] as const;
-  const contactUsMessage = errorTypes.map(errorType => {
-    const typedErrors: UnhandledError[] | undefined = groupedErrors[errorType];
-    if (typedErrors == null) return '';
-    return '# ' + capitalize(errorType) +' errors: ' + typedErrors.map(te => te.id).join(', ');
-  }).join('\n')
-
   const modal = errors && errors.length > 0 && (
     <Modal>
       <div className={cx()}>
         <button type="button" onClick={clearErrors}>
           <Icon type="close"/>
         </button>
-        <h1>We're sorry, something went wrong.</h1>
-        <p>
-          Please try again later, and <Link to={`/contact-us?ctx=${encodeURIComponent(contactUsMessage)}`} target="_blank">contact us</Link> if the problem persists.
-        </p>
-        <div className={cx('--Details')}>
-          {errorTypes.map(type => {
-            const typedErrors: UnhandledError[] | undefined = groupedErrors[type];
-            return typedErrors && <React.Fragment key={type}>
-              <h2>{capitalize(type)} errors</h2>
-              {typedErrors.map(({ error, id, message }) =>
-                <ErrorDetail key={id} id={id} error={error} showStackTraces={showStackTraces} message={message}/>)}
-            </React.Fragment>;
-          })}
-        </div>
+        <ErrorStatus
+          message={
+            <div className={cx('--Details')}>
+              {errorTypes.map(type => {
+                const typedErrors: UnhandledError[] | undefined = groupedErrors[type];
+                return typedErrors && <React.Fragment key={type}>
+                  <h2>{capitalize(type)} errors</h2>
+                  {typedErrors.map(({ error, id, message }) =>
+                    <ErrorDetail key={id} id={id} error={error} showStackTraces={showStackTraces} message={message}/>)}
+                </React.Fragment>;
+              })}
+            </div>
+          }
+        />
       </div>
     </Modal>
   );
