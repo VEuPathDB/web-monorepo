74821816caf623f7ff56d86f04fb68e7a5d3bfc9
Dave Falke <dmfalke@gmail.com>
2020-06-29 14:03:54 -0400

    Generalize prop

--- packages/libs/wdk-client/src/Components/PageStatus/Error.tsx
+++ packages/libs/wdk-client/src/Components/PageStatus/Error.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { wrappable } from 'wdk-client/Utils/ComponentUtils';
 
 export interface Props {
-  message?: string;
+  message?: React.ReactNode;
   children?: React.ReactNode;
 }
 
