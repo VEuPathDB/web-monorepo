8059530ed52cbd05d7d3d269b47d66cf6b0ec5e0
Jamie Long <jtlong3rd@gmail.com>
2020-07-01 11:13:57 -0400

    Initial commit of Sequence Details table

--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/components/cluster-graph/NodeDetails.tsx
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/components/cluster-graph/NodeDetails.tsx
@@ -2,6 +2,7 @@ import React, { useMemo } from 'react';
 
 import {
   GraphInformationTabProps,
+  SequenceInformation,
   layoutAndAccessionToBlastScoreRows,
   layoutAndAccessionToEcNumberRows,
   layoutAndAccessionToSequenceInformation,
@@ -15,28 +16,35 @@ export function NodeDetails({ layout, selectedNode }: GraphInformationTabProps) 
   const nodeDetails = useNodeDetails(layout, selectedNode);
 
   return (
-    <React.Fragment>
-      <GraphAccordion title="Sequence Information">
-        <pre>
-          {JSON.stringify(nodeDetails?.sequenceInformation, null, 2)}
-        </pre>
-      </GraphAccordion>
-      <GraphAccordion title="BLAST Scores">
-        <pre>
-          {JSON.stringify(nodeDetails?.blastScoreRows, null, 2)}
-        </pre>
-      </GraphAccordion>
-      <GraphAccordion title="PFam Domains">
-      <pre>
-        {JSON.stringify(nodeDetails?.pfamDomainRows, null, 2)}
-      </pre>
-      </GraphAccordion>
-      <GraphAccordion title="EC Numbers">
-      <pre>
-        {JSON.stringify(nodeDetails?.ecNumberRows, null, 2)}
-      </pre>
-      </GraphAccordion>
-    </React.Fragment>
+    <div className="NodeDetails">
+      {
+        nodeDetails == null
+          ? <h2 className="NoNodeSelected">Click a node to see details</h2>
+          : <React.Fragment>
+              <div className="NodeDetailsHeader">
+                {selectedNode}
+              </div>
+              <GraphAccordion title="Sequence Information">
+                <SequenceInformationTable {...nodeDetails.sequenceInformation} />
+              </GraphAccordion>
+              <GraphAccordion title="BLAST Scores">
+                <pre>
+                  {JSON.stringify(nodeDetails?.blastScoreRows, null, 2)}
+                </pre>
+              </GraphAccordion>
+              <GraphAccordion title="PFam Domains">
+              <pre>
+                {JSON.stringify(nodeDetails?.pfamDomainRows, null, 2)}
+              </pre>
+              </GraphAccordion>
+              <GraphAccordion title="EC Numbers">
+              <pre>
+                {JSON.stringify(nodeDetails?.ecNumberRows, null, 2)}
+              </pre>
+              </GraphAccordion>
+            </React.Fragment>
+      }
+    </div>
   );
 }
 
@@ -51,5 +59,30 @@ function useNodeDetails(layout: GroupLayout, accession: string | undefined) {
           ecNumberRows: layoutAndAccessionToEcNumberRows(layout, accession),
         }),
     [ layout, accession ]
+  );
+}
+
+function SequenceInformationTable(props: SequenceInformation) {
+  return (
+    <table className="SequenceInformation">
+      <tbody>
+        <tr>
+          <th>Source ID:</th>
+          <td>{props.sourceId}</td>
+          <th>Length:</th>
+          <td>{props.length}</td>
+        </tr>
+        <tr>
+          <th>Organism:</th>
+          <td>{props.organism}</td>
+          <th>Taxon:</th>
+          <td>{props.taxon}</td>
+        </tr>
+        <tr>
+          <th>Description:</th>
+          <td colSpan={3}>{props.description}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
