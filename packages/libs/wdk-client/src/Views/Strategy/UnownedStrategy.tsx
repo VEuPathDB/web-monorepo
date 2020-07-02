d295449f82b3835a71701842829b3d9b913fcf5d
Dave Falke <dmfalke@gmail.com>
2020-07-02 14:10:16 -0400

    Update verbiage based on feedback

--- packages/libs/wdk-client/src/Views/Strategy/UnownedStrategy.tsx
+++ packages/libs/wdk-client/src/Views/Strategy/UnownedStrategy.tsx
@@ -9,7 +9,7 @@ export default function UnownedStrategy() {
     <Banner banner={{
       type: 'danger',
       message: <div style={{ fontSize: '1.25em' }}>
-        The requested strategy does not exist, or you do not own it. If the requested strategy belongs to someone else, ask them to use the share button (<IconAlt fa={StrategyActions.share.iconName}/>) to share the strategy with you. <Link to="/workspace/strategies">Dismiss</Link>
+        The requested strategy does not exist, or it belongs to another user. In the latter case, ask them to use the share button (<IconAlt fa={StrategyActions.share.iconName}/>) to generate a valid URL that you may use to make a copy of their strategy. <Link to="/workspace/strategies">Dismiss</Link>
       </div>
     }}/>
   );
