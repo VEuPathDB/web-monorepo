1cf51ab7b6abafa320f1fe8ec34a2fb6cc3b742f
Jamie Long <jtlong3rd@gmail.com>
2020-07-17 17:46:32 -0400

    Use Decode.record in place of Decode.combine + Decode.field

--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/utils/groupLayout.ts
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/utils/groupLayout.ts
@@ -1,5 +1,13 @@
-import * as Decode from 'wdk-client/Utils/Json';
-
+import {
+  Decoder,
+  arrayOf,
+  constant,
+  number,
+  objectOf,
+  oneOf,
+  record,
+  string
+} from 'wdk-client/Utils/Json';
 import { EdgeType } from './clusterGraph';
 import { TaxonEntries, TaxonEntry, taxonEntryDecoder, taxonEntriesDecoder} from './taxons';
 
@@ -65,73 +73,73 @@ export interface PfamDomainEntry {
   symbol: string;
 }
 
-export const edgeTypeDecoder: Decode.Decoder<EdgeType> = Decode.oneOf(
-  Decode.constant('O'),
-  Decode.constant('C'),
-  Decode.constant('P'),
-  Decode.constant('L'),
-  Decode.constant('M'),
-  Decode.constant('N')
+export const edgeTypeDecoder: Decoder<EdgeType> = oneOf(
+  constant('O'),
+  constant('C'),
+  constant('P'),
+  constant('L'),
+  constant('M'),
+  constant('N')
 );
 
-export const edgeEntryDecoder: Decode.Decoder<EdgeEntry> = Decode.combine(
-  Decode.field('E', Decode.string),
-  Decode.field('Q', Decode.number),
-  Decode.field('S', Decode.number),
-  Decode.field('T', edgeTypeDecoder),
-  Decode.field('queryId', Decode.string),
-  Decode.field('score', Decode.number),
-  Decode.field('subjectId', Decode.string)
-);
+export const edgeEntryDecoder: Decoder<EdgeEntry> = record({
+  E: string,
+  Q: number,
+  S: number,
+  T: edgeTypeDecoder,
+  queryId: string,
+  score: number,
+  subjectId: string
+});
 
-export const edgeEntriesDecoder: Decode.Decoder<EdgeEntries> = Decode.objectOf(edgeEntryDecoder);
+export const edgeEntriesDecoder: Decoder<EdgeEntries> = objectOf(edgeEntryDecoder);
 
-export const nodeEntryDecoder: Decode.Decoder<NodeEntry> = Decode.combine(
-  Decode.field('x', Decode.string),
-  Decode.field('y', Decode.string),
-  Decode.field('i', Decode.number),
-  Decode.field('id', Decode.string)
-);
+export const nodeEntryDecoder: Decoder<NodeEntry> = record({
+  x: string,
+  y: string,
+  i: number,
+  id: string
+});
 
-export const nodeEntriesDecoder: Decode.Decoder<NodeEntries> = Decode.objectOf(nodeEntryDecoder);
+export const nodeEntriesDecoder: Decoder<NodeEntries> = objectOf(nodeEntryDecoder);
 
-export const ecNumberEntryDecoder: Decode.Decoder<EcNumberEntry> = Decode.combine(
-  Decode.field('code', Decode.string),
-  Decode.field('color', Decode.string),
-  Decode.field('count', Decode.number),
-  Decode.field('index', Decode.number)
-);
+export const ecNumberEntryDecoder: Decoder<EcNumberEntry> = record({
+  code: string,
+  color: string,
+  count: number,
+  index: number
+});
 
-export const geneEntryDecoder: Decode.Decoder<GeneEntry> = Decode.combine(
-  Decode.field('description', Decode.string),
-  Decode.field('ecNumbers', Decode.arrayOf(Decode.string)),
-  Decode.field('length', Decode.number),
-  Decode.field('pfamDomains', Decode.objectOf(Decode.arrayOf(Decode.number))),
-  Decode.field('taxon', taxonEntryDecoder)
-);
+export const geneEntryDecoder: Decoder<GeneEntry> = record({
+  description: string,
+  ecNumbers: arrayOf(string),
+  length: number,
+  pfamDomains: objectOf(arrayOf(number)),
+  taxon: taxonEntryDecoder
+});
 
-export const pfamDomainEntryDecoder: Decode.Decoder<PfamDomainEntry> = Decode.combine(
-  Decode.field('accession', Decode.string),
-  Decode.field('color', Decode.string),
-  Decode.field('count', Decode.number),
-  Decode.field('description', Decode.string),
-  Decode.field('index', Decode.number),
-  Decode.field('symbol', Decode.string)
-);
+export const pfamDomainEntryDecoder: Decoder<PfamDomainEntry> = record({
+  accession: string,
+  color: string,
+  count: number,
+  description: string,
+  index: number,
+  symbol: string
+});
 
-export const groupFieldDecoder: Decode.Decoder<GroupField> = Decode.combine(
-  Decode.field('ecNumbers', Decode.objectOf(ecNumberEntryDecoder)),
-  Decode.field('genes', Decode.objectOf(geneEntryDecoder)),
-  Decode.field('pfamDomains', Decode.objectOf(pfamDomainEntryDecoder))
-);
+export const groupFieldDecoder: Decoder<GroupField> = record({
+  ecNumbers: objectOf(ecNumberEntryDecoder),
+  genes: objectOf(geneEntryDecoder),
+  pfamDomains: objectOf(pfamDomainEntryDecoder)
+});
 
-export const groupLayoutDecoder: Decode.Decoder<GroupLayout> = Decode.combine(
-  Decode.field('edges', edgeEntriesDecoder),
-  Decode.field('nodes', nodeEntriesDecoder),
-  Decode.field('group', groupFieldDecoder),
-  Decode.field('minEvalueExp', Decode.number),
-  Decode.field('maxEvalueExp', Decode.number),
-  Decode.field('size', Decode.number),
-  Decode.field('taxonCounts', Decode.objectOf(Decode.number)),
-  Decode.field('taxons', taxonEntriesDecoder)
-);
+export const groupLayoutDecoder: Decoder<GroupLayout> = record({
+  edges: edgeEntriesDecoder,
+  nodes: nodeEntriesDecoder,
+  group: groupFieldDecoder,
+  minEvalueExp: number,
+  maxEvalueExp: number,
+  size: number,
+  taxonCounts: objectOf(number),
+  taxons: taxonEntriesDecoder
+});
--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/utils/taxons.ts
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/utils/taxons.ts
@@ -1,4 +1,12 @@
-import * as Decode from 'wdk-client/Utils/Json';
+import {
+  Decoder,
+  boolean,
+  lazy,
+  number,
+  objectOf,
+  record,
+  string
+} from 'wdk-client/Utils/Json';
 
 export interface TaxonEntry {
   abbrev: string;
@@ -12,14 +20,14 @@ export interface TaxonEntry {
 
 export type TaxonEntries = Record<string, TaxonEntry>;
 
-export const taxonEntryDecoder: Decode.Decoder<TaxonEntry> = Decode.combine(
-  Decode.field('abbrev', Decode.string),
-  Decode.field('children', Decode.lazy(() => Decode.objectOf(taxonEntryDecoder))),
-  Decode.field('commonName', Decode.string),
-  Decode.field('id', Decode.number),
-  Decode.field('name', Decode.string),
-  Decode.field('sortIndex', Decode.number),
-  Decode.field('species', Decode.boolean)
-);
+export const taxonEntryDecoder: Decoder<TaxonEntry> = record({
+  abbrev: string,
+  children: lazy(() => objectOf(taxonEntryDecoder)),
+  commonName: string,
+  id: number,
+  name: string,
+  sortIndex: number,
+  species: boolean
+});
 
-export const taxonEntriesDecoder: Decode.Decoder<TaxonEntries> = Decode.objectOf(taxonEntryDecoder);
+export const taxonEntriesDecoder: Decoder<TaxonEntries> = objectOf(taxonEntryDecoder);
