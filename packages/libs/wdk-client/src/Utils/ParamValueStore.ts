e323f7f3fe6836830fb8f4cd8d59ae360a14ada5
Jamie Long <jtlong3rd@gmail.com>
2020-08-04 13:07:04 -0400

    Back ParamValueStore using localforage

--- packages/libs/wdk-client/src/Utils/ParamValueStore.ts
+++ packages/libs/wdk-client/src/Utils/ParamValueStore.ts
@@ -1,4 +1,5 @@
-import { memoize, noop } from 'lodash';
+import { memoize } from 'lodash';
+import localforage from 'localforage';
 
 import { WdkService } from 'wdk-client/Core';
 import { SearchConfig } from 'wdk-client/Utils/WdkModel';
@@ -6,26 +7,53 @@ import { SearchConfig } from 'wdk-client/Utils/WdkModel';
 type ParamValues = SearchConfig['parameters'];
 
 interface ParamValueStore {
-  clearParamValues: () =>  void;
+  clearParamValues: () => Promise<void>;
 
   fetchParamValues: (
+    prefix: string | undefined,
     recordClassUrlSegment: string,
-    searchUrlSegment: string
+    searchUrlSegment: string,
+    parameterName: string,
   ) => Promise<ParamValues>;
 
   updateParamValues: (
+    newParamValues: ParamValues,
+    prefix: string | undefined,
     recordClassUrlSegment: string,
     searchUrlSegment: string,
-    newParamValues: ParamValues
-  ) => void;
+    parameterName: string
+  ) => Promise<ParamValues>;
 }
 
 export const getInstance = memoize(makeInstance);
 
-function makeInstance(wdkService: WdkService): ParamValueStore {
+function makeInstance(serviceUrl: string, wdkService: WdkService): ParamValueStore {
+  const _store = localforage.createInstance({
+    name: `ParamValueStore/${serviceUrl}`
+  });
+
   return {
-    clearParamValues: noop,
-    fetchParamValues: () => Promise.resolve({}),
-    updateParamValues: noop
+    clearParamValues: () => {
+      return _store.clear();
+    },
+    fetchParamValues: (...args) => {
+      const storeKey = makeStoreKey(...args);
+
+      return _store.getItem(storeKey);
+    },
+    updateParamValues: (newParamValues, ...keyArgs) => {
+      const storeKey = makeStoreKey(...keyArgs);
+
+      return _store.setItem(storeKey, newParamValues);
+    }
   };
+}
+
+function makeStoreKey(
+  prefix: string | undefined,
+  recordClassUrlSegment: string,
+  searchUrlSegment: string,
+  parameterName: string
+) {
+  return `${prefix ?? 'parameter'}/${recordClassUrlSegment}/${searchUrlSegment}/${parameterName}`;
 }
