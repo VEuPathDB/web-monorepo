ba3213c25354e438a790aa891c2be52ae1a05404
Jamie Long <jtlong3rd@gmail.com>
2020-08-06 10:18:14 -0400

    Invalidate ParamValueStore whenever the model number has changed

--- packages/libs/wdk-client/src/Utils/ParamValueStore.ts
+++ packages/libs/wdk-client/src/Utils/ParamValueStore.ts
@@ -4,6 +4,8 @@ import localforage from 'localforage';
 import { WdkService } from 'wdk-client/Core';
 import { ParameterValues } from 'wdk-client/Utils/WdkModel';
 
+const MODEL_VERSION_STORE_KEY = '__version';
+
 export interface ParamValueStore {
   clearParamValues: () => Promise<void>;
 
@@ -22,16 +24,37 @@ function makeInstance(serviceUrl: string, wdkService: WdkService): ParamValueSto
     name: `ParamValueStore/${serviceUrl}`
   });
 
+  function _fetchVersionNumber(): Promise<number | null> {
+    return _store.getItem(MODEL_VERSION_STORE_KEY);
+  }
+
+  async function _checkModelVersion(): Promise<number> {
+    const [ storeVersion, serviceVersion ] = await Promise.all([
+      _fetchVersionNumber(),
+      wdkService.getVersion()
+    ] as const);
+
+    if (storeVersion != null && storeVersion !== serviceVersion) {
+      await _store.clear();
+    }
+
+    return _store.setItem(MODEL_VERSION_STORE_KEY, serviceVersion);
+  }
+
   return {
     clearParamValues: () => {
       return _store.clear();
     },
-    fetchParamValues: paramContext => {
+    fetchParamValues: async paramContext => {
+      await _checkModelVersion();
+
       const storeKey = makeParamStoreKey(paramContext);
 
       return _store.getItem(storeKey);
     },
-    updateParamValues: (paramContext, newParamValues) => {
+    updateParamValues: async (paramContext, newParamValues) => {
+      await _checkModelVersion();
+
       const storeKey = makeParamStoreKey(paramContext);
 
       return _store.setItem(storeKey, newParamValues);
