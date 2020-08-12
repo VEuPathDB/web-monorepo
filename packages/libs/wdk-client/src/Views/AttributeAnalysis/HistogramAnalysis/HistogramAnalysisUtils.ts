d0fa19768cd868c7045787eb75f045e3e2761dbd
Dave Falke <dmfalke@gmail.com>
2020-08-12 15:59:31 -0400

    Pick better default bin size

--- packages/libs/wdk-client/src/Views/AttributeAnalysis/HistogramAnalysis/HistogramAnalysisUtils.ts
+++ packages/libs/wdk-client/src/Views/AttributeAnalysis/HistogramAnalysis/HistogramAnalysisUtils.ts
@@ -69,7 +69,7 @@ export function getDefaultBinSize(report: HistogramReport, applyLog: boolean) {
     : isTypeFloat(report.type) || applyLog ? MAX_DEFUALT_NUM_BINS
     : Math.min(MAX_DEFUALT_NUM_BINS, max - min);
 
-  return Math.abs(isTypeFloat(report.type) || applyLog
-    ? Math.ceil(max / numBins * 100) / 100
-    : Math.max(1, Math.ceil((max - min + 1) / numBins)));
+  return isTypeFloat(report.type) || applyLog
+    ? Math.ceil(Math.max(Math.abs(min), Math.abs(max)) / numBins * 100) / 100
+    : Math.max(1, Math.ceil((max - min + 1) / numBins));
 }
