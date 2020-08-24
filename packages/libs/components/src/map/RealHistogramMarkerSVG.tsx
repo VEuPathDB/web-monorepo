75cc2b17af471fe6b930decc1633fad25bcadb61
dkwon <dkwon@localhost.localdomain>
2020-08-24 15:01:48 -0400

    [DK] SVG histogram with real test data

---
+++ packages/libs/components/src/map/RealHistogramMarkerSVG.tsx
@@ -0,0 +1,135 @@
+import React from "react";
+import { Marker, Tooltip } from "react-leaflet";
+import { MarkerProps } from './Types';
+
+//DKDK leaflet
+import L from "leaflet";
+
+//DKDK ts definition for HistogramMarkerSVGProps: need some adjustment but for now, just use Donut marker one
+interface HistogramMarkerSVGProps extends MarkerProps {
+  labels: Array<string>, // the labels (not likely to be shown at normal marker size)
+  values: Array<number>, // the counts or totals to be shown in the donut
+  colors?: Array<string> | null, // bar colors: set to be optional with array or null type
+  // isAtomic?: boolean,      // add a special thumbtack icon if this is true (it's a marker that won't disaggregate if zoomed in further)
+  yAxisRange?: Array<number> | null, // y-axis range for setting global max
+  onClick?: (event: L.LeafletMouseEvent) => void | undefined,
+  onMouseOver?: (event: L.LeafletMouseEvent) => void | undefined,
+  onMouseOut?: (event: L.LeafletMouseEvent) => void | undefined,
+}
+
+/**
+ * DKDK this is a SVG histogram marker icon
+ */
+export default function RealHistogramMarkerSVG(props: HistogramMarkerSVGProps) {
+//   console.log('at DonutMarkerSVG.tsx')
+  /**
+   * DKDK icon with demo data: mroe realistic example can be found at dk-donut1 branch
+   */
+  let fullStat = []
+  //DKDK set defaultColor to be skyblue (#7cb5ec) if props.colors does not exist
+  let defaultColor: string = ''
+  let defaultLineColor: string = ''
+  //DKDK need to make a temporary stats array of objects to show marker colors - only works for demo data, not real solr data
+  for (let i = 0; i < props.values.length; i++) {
+    if (props.colors) {
+      defaultColor = props.colors[i]
+      defaultLineColor = 'grey'
+    } else {
+      defaultColor = '#7cb5ec'
+      defaultLineColor = '#7cb5ec'
+    }
+    fullStat.push({
+      // color: props.colors[i],
+      color: defaultColor,
+      label: props.labels[i],
+      value: props.values[i],
+    })
+  }
+
+  //DKDK construct histogram marker icon
+  const size = 50  //DKDK histogram marker icon size: note that popbio/mapveu donut marker icons = 40
+  let svgHTML: string = ''  //DKDK divIcon HTML contents
+  svgHTML += '<svg width="' + size + '" height="' + size + '">'   //DKDK initiate svg marker icon
+  let count = fullStat.length
+  let sumValues: number = fullStat.map(o => o.value).reduce((a, c) => { return a + c })     //DKDK summation of fullStat.value per marker icon
+  var maxValues: number = Math.max(...fullStat.map(o=>o.value));                            //DKDK max of fullStat.value per marker icon
+  const roundX = 10     //DKDK round corner in pixel
+  const roundY = 10     //DKDK round corner in pixel
+  const marginX = 5     //DKDK margin to start drawing bars in left and right ends of svg marker: plot area = (size - 2*marginX)
+  const marginY = 5     //DKDK margin to start drawing bars in Y
+  //DKDK draw outer box with round corners: changed border color (stroke)
+  svgHTML += '<rect x="0" y="0" rx=' + roundX + ' ry=' + roundY + ' width=' + size + ' height=' + size + ' fill="white" stroke="' + defaultLineColor +'" stroke-width="1" opacity="1.0" />'
+
+  // //DKDK add text?
+  // svgHTML += '<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" opacity="0.5" style="position:relative;z-index:10000">' + sumValues + '</text>'
+  //DKDK set globalMaxValue non-zero if props.yAxisRange exists
+  let globalMaxValue: number = 0
+  if (props.yAxisRange) {
+    globalMaxValue = props.yAxisRange[1]-props.yAxisRange[0];
+  }
+
+  // console.log('props.yAxisRange = ',props.yAxisRange)
+  // console.log('globalMaxValue = ',globalMaxValue)
+
+  //DKDK initialize variables for using at if-else
+  let barWidth: number, startingX: number, barHeight: number, startingY: number
+  let iter: number = 0    //DKDK number of iteration to set up X position that varies
+
+  if (globalMaxValue) {
+    fullStat.forEach(function (el: {color: string, label: string, value: number}) {
+      // console.log('global approach')
+      //DKDK for the case of y-axis range input: a global approach that take global max = icon height
+      barWidth = (size-2*marginX)/count               //DKDK bar width
+      startingX = marginX + barWidth*iter             //DKDK x in <react> tag: note that (0,0) is top left of the marker icon
+      barHeight = el.value/globalMaxValue*(size-2*marginY) //DKDK bar height: used 2*marginY to have margins at both top and bottom
+      startingY = (size-marginY)-barHeight            //DKDK y in <react> tag: note that (0,0) is top left of the marker icon
+
+      svgHTML += '<rect x=' + startingX + ' y=' + startingY + ' width=' + barWidth + ' height=' + barHeight + ' fill=' + el.color + ' />'
+      iter += 1
+    })
+  } else {
+    fullStat.forEach(function (el: {color: string, label: string, value: number}) {
+      // //DKDK for relative plot: sum of bars = icon height - just leave this as a reference
+      // let barWidth = (size-2*marginX)/count               //DKDK bar width
+      // let startingX = marginX + barWidth*iter             //DKDK x in <react> tag: note that (0,0) is top left of the marker icon
+      // let barHeight = el.value/sumValues*(size-2*marginY)   //DKDK bar height
+      // let startingY = (size-marginY)-barHeight            //DKDK y in <react> tag: note that (0,0) is top left of the marker icon
+
+      //DKDK for the case of auto-scale y-axis: a local approach that take local max = icon height
+      barWidth = (size-2*marginX)/count               //DKDK bar width
+      startingX = marginX + barWidth*iter             //DKDK x in <react> tag: note that (0,0) is top left of the marker icon
+      barHeight = el.value/maxValues*(size-2*marginY) //DKDK bar height: used 2*marginY to have margins at both top and bottom
+      startingY = (size-marginY)-barHeight            //DKDK y in <react> tag: note that (0,0) is top left of the marker icon
+
+      svgHTML += '<rect x=' + startingX + ' y=' + startingY + ' width=' + barWidth + ' height=' + barHeight + ' fill=' + el.color + ' />'
+      iter += 1
+    })
+  }
+  //DKDK a test to add text (here, total sum) at the center of the marker icon: please uncomment for testing
+  // svgHTML += '<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" opacity="0.7">' + sumValues + '</text>'
+  //DKDK closing svg tag
+  svgHTML += '</svg>'
+
+  //DKDK set icon
+  let HistogramIcon: any = L.divIcon({
+    className: 'leaflet-canvas-icon',        //DKDK need to change this className but just leave it as it for now
+    iconSize: new L.Point(0, 0),             //DKDKset iconSize = 0
+    iconAnchor: new L.Point(size/2,size/2),  //DKDK location of topleft corner: this is used for centering of the icon like transform/translate in CSS
+    html: svgHTML                            //DKDK divIcon HTML svg code generated above
+  });
+
+  return (
+    <Marker {...props} icon={HistogramIcon}>
+      {/* DKDK Below Tooltip also works but we may simply use title attribute as well */}
+      {/* However, Connor found coordinates issue and I realized that somehow "title" did not update coordinates correctly */}
+      {/* But both Popup and Tooltip do not have such an issue */}
+      {/* <Popup>{props.position.toString()}</Popup> */}
+      {/* <Tooltip>{props.position.toString()}</Tooltip> */}
+      <Tooltip>
+        labels: {props.labels.join(" ")} <br/>
+	      values: {props.values.join(" ")} <br />
+        latlong: {props.position.toString()}
+      </Tooltip>
+    </Marker>
+  );
+}
---
+++ packages/libs/components/src/map/RealHistogramsSVG.stories.tsx
@@ -0,0 +1,313 @@
+import React, { ReactElement, useState, useCallback } from 'react';
+// import { action } from '@storybook/addon-actions';
+import MapVEuMap from './MapVEuMap';
+import { BoundsViewport, MarkerProps } from './Types';
+import { Marker, Tooltip } from 'react-leaflet';
+import './TempIconHack';
+
+import sampleSizeData from './test-data/geoclust-numeric-binning-testing.json';
+import collectionDateData from './test-data/geoclust-date-binning-testing.json';
+import irData from './test-data/geoclust-irrescaled-binning-testing.json';
+
+import { latLng, LeafletMouseEvent } from "leaflet";
+import RealHistogramMarkerSVG from './RealHistogramMarkerSVG'; // TO BE CREATED
+
+export default {
+  title: 'Numeric and Date SVG',
+  component: MapVEuMap,
+};
+
+// some colors randomly pasted from the old mapveu code
+// these are NOT the final decided colors for MapVEu 2.0
+const all_colors_hex = [
+  "#FFB300", // Vivid Yellow
+  "#803E75", // Strong Purple
+  "#FF6800", // Vivid Orange
+  "#A6BDD7", // Very Light Blue
+  "#C10020", // Vivid Red
+  "#CEA262", // Grayish Yellow
+  // "#817066", // Medium Gray
+
+  // The following don't work well for people with defective color vision
+  "#007D34", // Vivid Green
+  "#F6768E", // Strong Purplish Pink
+  "#00538A", // Strong Blue
+  "#FF7A5C", // Strong Yellowish Pink
+  "#53377A", // Strong Violet
+  "#FF8E00", // Vivid Orange Yellow
+  "#B32851", // Strong Purplish Red
+  "#F4C800", // Vivid Greenish Yellow
+  "#7F180D", // Strong Reddish Brown
+  "#93AA00", // Vivid Yellowish Green
+  "#593315", // Deep Yellowish Brown
+  "#F13A13", // Vivid Reddish Orange
+  "#232C16" // Dark Olive Green
+];
+
+/**
+ * DKDK gathering functions here temporarily
+ * Need to add export to be used in the other component
+ */
+
+/**
+   This is a trivial marker data generator.  It returns 10 random points within the given bounds.
+   The real thing should something with zoomLevel.
+*/
+const getSampleSizeMarkerElements = (yAxisRange: Array<number> | null) => {
+  let iii=1   //DKDK
+  return sampleSizeData.facets.geo.buckets.map((bucket, index) => {
+    const lat = bucket.ltAvg;
+    const long = bucket.lnAvg;
+    let labels = [];
+    let values = [];
+    let colors: string[] = [];
+    let noDataValue:number = 0;
+    bucket.term.buckets.forEach((bucket, index) => {
+      const start = bucket.val;
+      labels.push(start+"-"+(start+50));
+      values.push(bucket.count);
+      colors.push(all_colors_hex[index]);     //DKDK set color palette
+    });
+    labels.push("200+");
+    values.push(bucket.term.after.count);
+    colors.push(all_colors_hex[labels.length-1]);     //DKDK fill the last color
+
+    //DKDK calculate the number of no data and make 6th bar
+    noDataValue = bucket.count - bucket.term.before.count - bucket.term.after.count - bucket.term.between.count
+    labels.push("noData");
+    values.push(noDataValue);
+    colors.push("grey");     //DKDK fill the last color
+
+    // console.log('noDataValue', noDataValue)
+    // //DKDK temp
+    // if (iii == 1) {
+    //   console.log('labels', labels)
+    //   console.log('values', values)
+    //   console.log('colors', colors)
+    // }
+    iii +=1
+
+    return (
+      <RealHistogramMarkerSVG
+        key={`marker_${index}`}
+        //DKDK change position format
+        position={[lat, long]}
+        labels={labels}
+        values={values}
+        //DKDK colors is set to be optional props, if null (e.g., comment out) then bars will have skyblue-like defaultColor
+        colors={colors}
+        //DKDK disable isAtomic for histogram
+        // isAtomic={atomicValue}
+        //DKDK yAxisRange can be commented out - defined as optional at HistogramMarkerSVG.tsx (HistogramMarkerSVGProps)
+        yAxisRange ={yAxisRange}
+        // onClick={handleClick}
+        // onMouseOut={handleMouseOut}
+        // onMouseOver={handleMouseOver}
+      />
+      )
+  });
+}
+
+
+export const SampleSizeLocal = () => {
+  //DKDK set global or local
+  // const yAxisRange: Array<number> | null = [0, 1104]
+  const yAxisRange = null
+  const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
+  const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
+    setMarkerElements(getSampleSizeMarkerElements(yAxisRange));
+  }, [setMarkerElements])
+
+  return (
+    <MapVEuMap
+    viewport={{center: [ 13.449566, -9.304301 ], zoom: 6}}
+    height="100vh" width="100vw"
+    onViewportChanged={handleViewportChanged}
+    markers={markerElements}
+    />
+  );
+}
+
+export const SampleSizeGlobal = () => {
+  //DKDK set global or local
+  const yAxisRange: Array<number> | null = [0, 1104]
+  // const yAxisRange = null
+  const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
+  const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
+    setMarkerElements(getSampleSizeMarkerElements(yAxisRange));
+  }, [setMarkerElements])
+
+  return (
+    <MapVEuMap
+    viewport={{center: [ 13.449566, -9.304301 ], zoom: 6}}
+    height="100vh" width="100vw"
+    onViewportChanged={handleViewportChanged}
+    markers={markerElements}
+    />
+  );
+}
+
+
+const getCollectionDateMarkerElements = (yAxisRange: Array<number> | null) => {
+  return collectionDateData.facets.geo.buckets.map((bucket, index) => {
+    const lat = bucket.ltAvg;
+    const long = bucket.lnAvg;
+    let labels = [];
+    let values = [];
+    let colors: string[] = [];
+    let noDataValue:number = 0;
+    bucket.term.buckets.forEach((bucket, index) => {
+      const start = bucket.val.substring(0,4);
+      labels.push(start);
+      values.push(bucket.count);
+      colors.push(all_colors_hex[index]);     //DKDK set color palette
+    });
+
+    //DKDK calculate the number of no data and make 6th bar
+    noDataValue = bucket.count - bucket.term.before.count - bucket.term.after.count - bucket.term.between.count
+    labels.push("noData");
+    values.push(noDataValue);
+    colors.push("grey");     //DKDK fill the last color
+
+  return (
+    <RealHistogramMarkerSVG
+      key={`marker_${index}`}
+      //DKDK change position format
+      position={[lat, long]}
+      labels={labels}
+      values={values}
+      //DKDK colors is set to be optional props, if null (e.g., comment out) then bars will have skyblue-like defaultColor
+      colors={colors}
+      //DKDK disable isAtomic for histogram
+      // isAtomic={atomicValue}
+      //DKDK yAxisRange can be commented out - defined as optional at HistogramMarkerSVG.tsx (HistogramMarkerSVGProps)
+      yAxisRange ={yAxisRange}
+      // onClick={handleClick}
+      // onMouseOut={handleMouseOut}
+      // onMouseOver={handleMouseOver}
+    />
+    )
+  });
+}
+
+
+export const CollectionDateLocal = () => {
+  //DKDK set global or local
+  // const yAxisRange: Array<number> | null = [0, 1104]
+  const yAxisRange = null
+  const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
+  const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
+    setMarkerElements(getCollectionDateMarkerElements(yAxisRange));
+  }, [setMarkerElements])
+
+  return (
+    <MapVEuMap
+    viewport={{center: [ 13.449566, -9.304301 ], zoom: 6}}
+    height="100vh" width="100vw"
+    onViewportChanged={handleViewportChanged}
+    markers={markerElements}
+    />
+  );
+}
+
+export const CollectionDateGlobal = () => {
+  //DKDK set global or local
+  const yAxisRange: Array<number> | null = [0, 5972]
+  // const yAxisRange = null
+  const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
+  const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
+    setMarkerElements(getCollectionDateMarkerElements(yAxisRange));
+  }, [setMarkerElements])
+
+  return (
+    <MapVEuMap
+    viewport={{center: [ 13.449566, -9.304301 ], zoom: 6}}
+    height="100vh" width="100vw"
+    onViewportChanged={handleViewportChanged}
+    markers={markerElements}
+    />
+  );
+}
+
+const getIRDataMarkerElements = (yAxisRange: Array<number> | null) => {
+  return irData.facets.geo.buckets.map((bucket, index) => {
+    const lat = bucket.ltAvg;
+    const long = bucket.lnAvg;
+    let labels = [];
+    let values = [];
+    let colors: string[] = [];
+    let noDataValue:number = 0;
+    bucket.term.buckets.forEach((bucket, index) => {
+      const start = bucket.val;
+      labels.push(start+"-"+(start+0.2));
+      values.push(bucket.count);
+      colors.push(all_colors_hex[index]);     //DKDK set color palette
+    });
+
+    //DKDK calculate the number of no data and make 6th bar
+    noDataValue = bucket.count - bucket.term.before.count - bucket.term.after.count - bucket.term.between.count
+    labels.push("noData");
+    values.push(noDataValue);
+    colors.push("grey");     //DKDK fill the last color
+
+    return (
+      <RealHistogramMarkerSVG
+        key={`marker_${index}`}
+        //DKDK change position format
+        position={[lat, long]}
+        labels={labels}
+        values={values}
+        //DKDK colors is set to be optional props, if null (e.g., comment out) then bars will have skyblue-like defaultColor
+        colors={colors}
+        //DKDK disable isAtomic for histogram
+        // isAtomic={atomicValue}
+        //DKDK yAxisRange can be commented out - defined as optional at HistogramMarkerSVG.tsx (HistogramMarkerSVGProps)
+        yAxisRange ={yAxisRange}
+        // onClick={handleClick}
+        // onMouseOut={handleMouseOut}
+        // onMouseOver={handleMouseOver}
+      />
+      )
+  });
+}
+
+
+export const IRDataLocal = () => {
+  //DKDK set global or local
+  // const yAxisRange: Array<number> | null = [0, 1104]
+  const yAxisRange = null
+  const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
+  const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
+    setMarkerElements(getIRDataMarkerElements(yAxisRange));
+  }, [setMarkerElements])
+
+  return (
+    <MapVEuMap
+    viewport={{center: [ 13.449566, -9.304301 ], zoom: 6}}
+    height="100vh" width="100vw"
+    onViewportChanged={handleViewportChanged}
+    markers={markerElements}
+    />
+  );
+}
+
+export const IRDataGlobal = () => {
+  //DKDK set global or local
+  const yAxisRange: Array<number> | null = [0, 1749]
+  // const yAxisRange = null
+  const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
+  const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
+    setMarkerElements(getIRDataMarkerElements(yAxisRange));
+  }, [setMarkerElements])
+
+  return (
+    <MapVEuMap
+    viewport={{center: [ 13.449566, -9.304301 ], zoom: 6}}
+    height="100vh" width="100vw"
+    onViewportChanged={handleViewportChanged}
+    markers={markerElements}
+    />
+  );
+}
+
+
