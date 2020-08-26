76c7eef4fdb39b4e91a68099b94ee0a16ac417c4
Jamie Long <jtlong3rd@gmail.com>
2020-08-26 14:20:57 -0400

    Make "Category" rows + column for Genome Statistics

--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/controllers/GenomeStatisticsController.tsx
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/controllers/GenomeStatisticsController.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+
+import { Loading } from 'wdk-client/Components';
+import { useSetDocumentTitle } from 'wdk-client/Utils/ComponentUtils';
+
+import { OrthoDataTable } from 'ortho-client/components/OrthoDataTable';
 import { useTaxonUiMetadata } from 'ortho-client/hooks/taxons';
 import { useGenomeStatisticsRows } from 'ortho-client/hooks/dataSummary';
-import { useSetDocumentTitle } from 'wdk-client/Utils/ComponentUtils';
-import { OrthoDataTable } from 'ortho-client/components/OrthoDataTable';
-import { Loading } from 'wdk-client/Components';
+import { GenomeStatisticsRows } from 'ortho-client/utils/dataSummary';
+import { DataTableColumns, DataTableColumnKey } from 'ortho-client/utils/dataTables';
+import { TaxonUiMetadata } from 'ortho-client/utils/taxons';
 
 export function GenomeStatisticsController() {
   useSetDocumentTitle('Release Summary - Genome Statistics');
@@ -15,17 +20,68 @@ export function GenomeStatisticsController() {
     : <OrthoDataTable {...props} />;
 }
 
+interface GenomeStatisticsTableRow {
+  root_taxon: string;
+}
+
 function useDataTableProps() {
   const taxonUiMetadata = useTaxonUiMetadata();
   const genomeStatisticsRows = useGenomeStatisticsRows();
 
-  if (taxonUiMetadata == null || genomeStatisticsRows == null) {
-    return undefined;
-  }
+  const rows = useMemo(
+    () => (
+      taxonUiMetadata &&
+      genomeStatisticsRows &&
+      makeDataTableRows(
+        taxonUiMetadata,
+        genomeStatisticsRows
+      )
+    ),
+    [ genomeStatisticsRows ]
+  );
 
+  const columns = useMemo(
+    () => makeDataTableColumns(),
+    []
+  );
+
+  const columnOrder = useMemo(
+    () => makeDataTableColumnOrder(),
+    []
+  );
+
+  return rows == undefined
+    ? undefined
+    : {
+        rows,
+        columns,
+        columnOrder
+      };
+}
+
+function makeDataTableRows(
+  { species }: TaxonUiMetadata,
+  genomeStatisticsRows: GenomeStatisticsRows
+): GenomeStatisticsTableRow[] {
+  return genomeStatisticsRows.map(
+    ({ three_letter_abbrev }) => ({
+      root_taxon: species[three_letter_abbrev].rootTaxon
+    })
+  );
+}
+
+function makeDataTableColumns(): DataTableColumns<GenomeStatisticsTableRow, 'root_taxon'> {
   return {
-    rows: [],
-    columns: [],
-    columnOrder: []
+    root_taxon: {
+      key: 'root_taxon',
+      name: 'Category',
+      sortable: true
+    }
   };
+}
+
+function makeDataTableColumnOrder(): DataTableColumnKey<GenomeStatisticsTableRow>[] {
+  return [
+    'root_taxon'
+  ];
 }
