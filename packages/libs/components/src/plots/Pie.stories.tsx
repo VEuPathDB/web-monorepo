eb866867a0b537cb0a9d9b326e382baf982aa934
Connor Howington <chowingt@nd.edu>
2020-09-15 14:30:14 -0400

    Implement all pie functionality

--- packages/libs/components/src/plots/Pie.stories.tsx
+++ packages/libs/components/src/plots/Pie.stories.tsx
@@ -7,20 +7,84 @@ export default {
   component: Pie,
 };
 
-export const Basic = () => <Pie
-  //onPlotUpdate={action('state updated')}
-  data={
-    [{
-      value: 10,
-      label: 'Foo',
-    },
-    {
-      value: 2,
-      label: 'Bar',
-    },
-    {
-      value: 30,
-      label: 'Baz',
-    }]
+let data = [
+  {
+    value: 10,
+    label: 'Foo',
+  },
+  {
+    value: 2,
+    label: 'Bar',
+  },
+  {
+    value: 30,
+    label: 'Baz',
   }
+];
+
+let coloredData = [
+  {
+    value: 10,
+    label: 'Green',
+    color: 'green',
+  },
+  {
+    value: 2,
+    label: 'Red',
+    color: 'red',
+  },
+  {
+    value: 30,
+    label: 'Default',
+  }
+];
+
+export const BasicPie = () => <Pie
+  //onPlotUpdate={action('state updated')}
+  data={data}
+/>
+
+export const PieCustomColors = () => <Pie
+  //onPlotUpdate={action('state updated')}
+  data={coloredData}
+/>
+
+export const BasicDonut = () => <Pie
+  //onPlotUpdate={action('state updated')}
+  data={data}
+  interior={{
+    heightPercentage: 0.4,
+  }}
+/>
+
+export const DonutText = () => <Pie
+  //onPlotUpdate={action('state updated')}
+  data={data}
+  interior={{
+    heightPercentage: 0.4,
+    text: 'Pie Hole',
+    textColor: 'purple',
+    fontSize: 20,
+  }}
+/>
+
+export const DonutCreamyFilling = () => <Pie
+  //onPlotUpdate={action('state updated')}
+  data={data}
+  interior={{
+    heightPercentage: 0.4,
+    backgroundColor: 'yellow',
+  }}
+/>
+
+export const EverythingBagel = () => <Pie
+  //onPlotUpdate={action('state updated')}
+  data={coloredData}
+  interior={{
+    heightPercentage: 0.4,
+    text: 'Pie Hole',
+    textColor: 'purple',
+    fontSize: 20,
+    backgroundColor: 'yellow',
+  }}
 />
--- packages/libs/components/src/plots/Pie.tsx
+++ packages/libs/components/src/plots/Pie.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PlotlyPlot from "./PlotlyPlot";
+import { defaults } from "plotly.js/src/components/color/attributes";
 
 type Value = number | Date;
 
@@ -25,13 +26,108 @@ interface PieProps {
 export default function Pie(props: PieProps) {
   const { data, interior = null } = props;
 
+  // A generator function for the default plotly.js colors
+  function* defaultColors() {
+    let myDefaults: string[] = Array.from(defaults);
+
+    while (true) {
+      let nextColor = myDefaults.shift() as string;
+      myDefaults.push(nextColor);
+      yield nextColor;
+    }
+  }
+
+  let colorGen = defaultColors();
+
   // Preprocess data for PlotlyPlot
-  const reducer = (accumulatorObj: {values: Value[], labels: string[]}, currObj: PiePlotDatum) => {
+  const reducer = (accumulatorObj: {values: Value[], labels: string[], marker: {colors: string[]}}, currObj: PiePlotDatum) => {
     accumulatorObj.values.push(currObj.value);
     accumulatorObj.labels.push(currObj.label);
+
+    // Use the provided color or the next default plotly color if none is provided
+    let color = currObj.color || colorGen.next().value as string;
+    accumulatorObj.marker.colors.push(color);
+
     return accumulatorObj;
   };
-  const newData = [data.reduce(reducer, {values: [], labels: []})];
 
-  return <PlotlyPlot<'values'> data={newData} type="pie"/>
+  let interiorProps;
+  let layout = {};
+
+  if (interior) {
+    interiorProps = {
+      hole: interior.heightPercentage,
+    };
+
+    if (interior.text) {
+      layout = {
+        annotations: [{
+          font: {
+            size: interior.fontSize || 12,
+            color: interior.textColor || 'black',
+          },
+          showarrow: false,
+          text: interior.text,
+          x: 0.5,
+          y: 0.5,
+        }]
+      }
+    }
+
+    // Tried implementing backgroundColor using the shapes interface,
+    // but it didn't work very well. Committing for posterity's sake.
+    // if (interior.backgroundColor) {
+    //   let dountWidth = 0.5 * (1 - interior.heightPercentage);
+
+    //   layout.shapes = [
+    //     {
+    //       opacity: 1,
+    //       xref: 'x',
+    //       yref: 'y',
+    //       fillcolor: interior.backgroundColor,
+    //       x0: dountWidth,
+    //       y0: 1 - dountWidth,
+    //       x1: dountWidth,
+    //       y1: 1 - dountWidth,
+    //       type: 'circle',
+    //     }
+    //   ];
+    // }
+  }
+
+  let newData = [{
+    ...interiorProps,
+    ...data.reduce(reducer, {values: [], labels: [], marker: {colors: []}}),
+    direction: 'clockwise',
+    sort: false,
+  }];
+
+  if (interior && interior.backgroundColor) {
+    // The width of the donut. This is measured along invisible axes that seem
+    // to exist in plotly even for a pie chart. It seems like the left side of
+    // the pie chart starts at 0 on the x axis and the right side of the chart
+    // ends at 1. The middle of the pie is at 0.5 x,y.
+    const dountWidth = 0.5 * (1 - interior.heightPercentage);
+
+    // To implement the donut hole background color, we add a feaux data trace
+    // inside the hole with no markings
+    newData.push({
+      values: [1],
+      labels: [''],
+      marker: {
+        colors: [interior.backgroundColor],
+      },
+      // Where this data trace should be "plotted" on the "axes".
+      // This places it in the center of the donut.
+      domain: {
+        x: [dountWidth, 1 - dountWidth],
+        y: [dountWidth, 1 - dountWidth],
+      },
+      hoverinfo: 'none',
+      textinfo: 'none',
+      showlegend: false,
+    })
+  }
+
+  return <PlotlyPlot<'values'> data={newData} layout={layout} type="pie"/>
 }
