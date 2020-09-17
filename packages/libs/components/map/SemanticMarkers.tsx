248fae25a125d4551ad7aa1489af105b0940872d
Brett Fox <brettmfox33@gmail.com>
2020-09-17 12:47:06 -0400

    Added handling for no matching markers

--- packages/libs/components/map/SemanticMarkers.tsx
+++ packages/libs/components/map/SemanticMarkers.tsx
@@ -162,10 +162,14 @@ function updateMarkers(toChangeMarkers: Array<ReactElement<MarkerProps>>,
       return obj.key === sourceHash
     });
 
-    // Clone marker element with new position
-    const markerCloneProps = {
-      position: matchingMarkers[0].props.position
-    };
+    let markerCloneProps = {};
+    if (matchingMarkers.length == 1) {
+      // Clone marker element with new position
+      markerCloneProps = {
+        position: matchingMarkers[0].props.position
+      };
+    }
+
     return cloneElement(markerObj, markerCloneProps);
   });
 
