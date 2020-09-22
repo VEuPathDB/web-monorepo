a0900e86a19b22f0d899e96b6cec41666ed9c475
Bob <uncoolbob@gmail.com>
2020-09-22 16:29:51 +0100

    added real categorical fixture data and changed SVG donut stories to show this

--- packages/libs/components/src/map/SVGDonutMarker.stories.tsx
+++ packages/libs/components/src/map/SVGDonutMarker.stories.tsx
@@ -5,9 +5,7 @@ import { BoundsViewport, MarkerProps } from './Types';
 import { Marker, Tooltip } from 'react-leaflet';
 import './TempIconHack';
 
-import sampleSizeData from './test-data/geoclust-numeric-binning-testing.json';
-import collectionDateData from './test-data/geoclust-date-binning-testing.json';
-import irData from './test-data/geoclust-irrescaled-binning-testing.json';
+import speciesData from './test-data/geoclust-species-testing.json';
 
 import { latLng, LeafletMouseEvent } from "leaflet";
 import SVGDonutMarker from './SVGDonutMarker'; // TO BE CREATED
@@ -20,11 +18,11 @@ export default {
 // some colors randomly pasted from the old mapveu code
 // these are NOT the final decided colors for MapVEu 2.0
 const all_colors_hex = [
-  "#0200C5", // Bob1
-  "#6300C5", // Bob2
-  "#C400C5", // Bob3
-  "#C50045", // Bob4
-  "#C50000", // Bob5
+  "#FFB300", // Vivid Yellow
+  "#803E75", // Strong Purple
+  "#FF6800", // Vivid Orange
+  "#A6BDD7", // Very Light Blue
+  "#C10020", // Vivid Red
   "#CEA262", // Grayish Yellow
   // "#817066", // Medium Gray
 
@@ -86,8 +84,8 @@ const handleClick = (e: LeafletMouseEvent) => {
    This is a trivial marker data generator.  It returns 10 random points within the given bounds.
    The real thing should something with zoomLevel.
 */
-const getSampleSizeMarkerElements = () => {
-  return sampleSizeData.facets.geo.buckets.map((bucket, index) => {
+const getSpeciesMarkerElements = () => {
+  return speciesData.facets.geo.buckets.map((bucket, index) => {
     const lat = bucket.ltAvg;
     const long = bucket.lnAvg;
     let labels = [];
@@ -95,21 +93,10 @@ const getSampleSizeMarkerElements = () => {
     let colors: string[] = [];
     let noDataValue:number = 0;
     bucket.term.buckets.forEach((bucket, index) => {
-      const start = bucket.val;
-      labels.push(start+"-"+(start+50));
+      labels.push(bucket.val);
       values.push(bucket.count);
       colors.push(all_colors_hex[index]);     //DKDK set color palette
     });
-    labels.push("200+");
-    values.push(bucket.term.after.count);
-    colors.push(all_colors_hex[labels.length]);     //DKDK fill the last color
-    // colors.push(all_colors_hex[labels.length-1]);     //DKDK without fill the last color
-
-    //DKDK calculate the number of no data and add it for coloring
-    noDataValue = bucket.count - bucket.term.before.count - bucket.term.after.count - bucket.term.between.count
-    labels.push("noData");
-    values.push(noDataValue);
-    colors.push("silver");     //DKDK fill the last color
 
     //DKDK check isAtomic
     let atomicValue = (bucket.atomicCount && bucket.atomicCount === 1) ? true : false
@@ -133,13 +120,13 @@ const getSampleSizeMarkerElements = () => {
 }
 
 
-export const SampleSize = () => {
+export const Species = () => {
   //DKDK set global or local
   // const yAxisRange: Array<number> | null = [0, 1104]
   // const yAxisRange: Array<number> | null = []
   const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
   const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
-    setMarkerElements(getSampleSizeMarkerElements());
+    setMarkerElements(getSpeciesMarkerElements());
   }, [setMarkerElements])
 
   return (
@@ -152,94 +139,13 @@ export const SampleSize = () => {
   );
 }
 
-export const SampleSizeNudged = () => {
+export const SpeciesNudged = () => {
   //DKDK set global or local
   // const yAxisRange: Array<number> | null = [0, 1104]
   // const yAxisRange: Array<number> | null = []
   const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
   const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
-    setMarkerElements(getSampleSizeMarkerElements());
-  }, [setMarkerElements])
-
-  return (
-    <MapVEuMap
-    viewport={{center: [ 13.449566, -2.304301 ], zoom: 7}}
-    height="100vh" width="100vw"
-    onViewportChanged={handleViewportChanged}
-    markers={markerElements}
-    nudge="geohash"
-    />
-  );
-}
-
-const getCollectionDateMarkerElements = () => {
-  return collectionDateData.facets.geo.buckets.map((bucket, index) => {
-    const lat = bucket.ltAvg;
-    const long = bucket.lnAvg;
-    let labels = [];
-    let values = [];
-    let colors: string[] = [];
-    let noDataValue:number = 0;
-    bucket.term.buckets.forEach((bucket, index) => {
-      const start = bucket.val.substring(0,4);
-      labels.push(start);
-      values.push(bucket.count);
-      colors.push(all_colors_hex[index]);     //DKDK set color palette
-    });
-
-    //DKDK calculate the number of no data and make 6th bar
-    noDataValue = bucket.count - bucket.term.before.count - bucket.term.after.count - bucket.term.between.count
-    labels.push("noData");
-    values.push(noDataValue);
-    colors.push("silver");     //DKDK fill the last color
-
-    //DKDK check isAtomic
-    let atomicValue = (bucket.atomicCount && bucket.atomicCount === 1) ? true : false
-
-  return (
-    <SVGDonutMarker
-      key={bucket.val}
-      //DKDK change position format
-      position={[lat, long]}
-      labels={labels}
-      values={values}
-      //DKDK colors is set to be optional props, if null (e.g., comment out) then bars will have skyblue-like defaultColor
-      colors={colors}
-      isAtomic={atomicValue}
-      onClick={handleClick}
-      onMouseOut={handleMouseOut}
-      onMouseOver={handleMouseOver}
-    />
-    )
-  });
-}
-
-export const CollectionDate = () => {
-  //DKDK set global or local
-  // const yAxisRange: Array<number> | null = [0, 1104]
-  // const yAxisRange: Array<number> | null = []
-  const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
-  const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
-    setMarkerElements(getCollectionDateMarkerElements());
-  }, [setMarkerElements])
-
-  return (
-    <MapVEuMap
-    viewport={{center: [ 13.449566, -2.304301 ], zoom: 7}}
-    height="100vh" width="100vw"
-    onViewportChanged={handleViewportChanged}
-    markers={markerElements}
-    />
-  );
-}
-
-export const CollectionDateNudged = () => {
-  //DKDK set global or local
-  // const yAxisRange: Array<number> | null = [0, 1104]
-  // const yAxisRange: Array<number> | null = []
-  const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
-  const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
-    setMarkerElements(getCollectionDateMarkerElements());
+    setMarkerElements(getSpeciesMarkerElements());
   }, [setMarkerElements])
 
   return (
@@ -253,84 +159,3 @@ export const CollectionDateNudged = () => {
   );
 }
 
-
-const getIRDataMarkerElements = () => {
-  return irData.facets.geo.buckets.map((bucket, index) => {
-    const lat = bucket.ltAvg;
-    const long = bucket.lnAvg;
-    let labels = [];
-    let values = [];
-    let colors: string[] = [];
-    let noDataValue:number = 0;
-    bucket.term.buckets.forEach((bucket, index) => {
-      const start = bucket.val;
-      labels.push(start+"-"+(start+0.2));
-      values.push(bucket.count);
-      colors.push(all_colors_hex[index]);     //DKDK set color palette
-    });
-
-    //DKDK calculate the number of no data and make 6th bar
-    noDataValue = bucket.count - bucket.term.before.count - bucket.term.after.count - bucket.term.between.count
-    labels.push("noData");
-    values.push(noDataValue);
-    colors.push("white");     //DKDK fill the last color
-
-    //DKDK check isAtomic
-    let atomicValue = (bucket.atomicCount && bucket.atomicCount === 1) ? true : false
-
-    return (
-      <SVGDonutMarker
-        key={bucket.val}
-        //DKDK change position format
-        position={[lat, long]}
-        labels={labels}
-        values={values}
-        //DKDK colors is set to be optional props, if null (e.g., comment out) then bars will have skyblue-like defaultColor
-        colors={colors}
-        isAtomic={atomicValue}
-        onClick={handleClick}
-        onMouseOut={handleMouseOut}
-        onMouseOver={handleMouseOver}
-      />
-      )
-  });
-}
-
-export const IRData = () => {
-  //DKDK set global or local
-  // const yAxisRange: Array<number> | null = [0, 1104]
-  // const yAxisRange: Array<number> | null = []
-  const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
-  const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
-    setMarkerElements(getIRDataMarkerElements());
-  }, [setMarkerElements])
-
-  return (
-    <MapVEuMap
-    viewport={{center: [ 13.449566, -2.304301 ], zoom: 7}}
-    height="100vh" width="100vw"
-    onViewportChanged={handleViewportChanged}
-    markers={markerElements}
-    />
-  );
-}
-
-export const IRDataNudged = () => {
-  //DKDK set global or local
-  // const yAxisRange: Array<number> | null = [0, 1104]
-  // const yAxisRange: Array<number> | null = []
-  const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
-  const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
-    setMarkerElements(getIRDataMarkerElements());
-  }, [setMarkerElements])
-
-  return (
-    <MapVEuMap
-    viewport={{center: [ 13.449566, -2.304301 ], zoom: 7}}
-    height="100vh" width="100vw"
-    onViewportChanged={handleViewportChanged}
-    markers={markerElements}
-    nudge="geohash"
-    />
-  );
-}
