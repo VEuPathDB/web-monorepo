2217adaa866f2697796d7b232edf5a71b7aa078e
Brett Fox <brettmfox33@gmail.com>
2020-10-09 13:23:26 -0400

    Grid mostly in working state

--- packages/libs/components/src/map/utilities/ZoomLevelToGeoHashLevel.tsx
+++ packages/libs/components/src/map/utilities/ZoomLevelToGeoHashLevel.tsx
@@ -3,28 +3,6 @@ interface ZoomLevelToGeoHashLevel {
 }
 
 export default function ZoomLevelToGeoHashLevel({zoomLevel}: ZoomLevelToGeoHashLevel) {
-    // const zoomLevelToGeohashLevelDefs: {0: number} = {
-    //     0: 1,
-    //     1: 1,
-    //     2: 1,
-    //     3: 1,
-    //     4: 2,
-    //     5: 2,
-    //     6: 2,
-    //     7: 3,
-    //     8: 3,
-    //     9: 3,
-    //     10: 4,
-    //     11: 4,
-    //     12: 4,
-    //     13: 5,
-    //     14: 5,
-    //     15: 5,
-    //     16: 6,
-    //     17: 6,
-    //     18: 7
-    // };
-    console.log(zoomLevel);
     const zoomLevelToGeohashLevelDefs = [
         1, // 0
         1, // 1
@@ -46,6 +24,5 @@ export default function ZoomLevelToGeoHashLevel({zoomLevel}: ZoomLevelToGeoHashL
         6, // 17
         7  // 18
     ];
-
     return(zoomLevelToGeohashLevelDefs[zoomLevel])
 }
