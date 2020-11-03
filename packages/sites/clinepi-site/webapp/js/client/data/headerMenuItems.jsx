bbd8b35aeb8fef3d31f1f28e22a7cbd68084d017
Jamie Long <jtlong3rd@gmail.com>
2020-11-03 14:41:21 -0500

    Pass "permissions" to StudyMenuItems which appear in ClinEpi sites

--- packages/sites/clinepi-site/webapp/js/client/data/headerMenuItems.jsx
+++ packages/sites/clinepi-site/webapp/js/client/data/headerMenuItems.jsx
@@ -3,6 +3,9 @@ import { StudyMenuItem } from 'ebrc-client/App/Studies';
 import { menuItemsFromSocials, iconMenuItemsFromSocials } from 'ebrc-client/App/Utils/Utils';
 import { getStaticSiteData } from '../selectors/siteData';
 import { STATIC_ROUTE_PATH } from 'ebrc-client/routes';
+import { withPermissions } from 'ebrc-client/components/Permissions';
+
+const ClinEpiStudyMenuItem = withPermissions(StudyMenuItem);
 
 export default function headerMenuItems (state) {
   const { siteConfig } = state.globalData;
@@ -22,7 +25,7 @@ export default function headerMenuItems (state) {
             route: '/search/dataset/Studies/result'
           }].concat(  
           studies.entities != null
-          ? studies.entities.map(study => ({ text: <StudyMenuItem study={study} config={siteConfig} /> }))
+          ? studies.entities.map(study => ({ text: <ClinEpiStudyMenuItem study={study} config={siteConfig} /> }))
           : [{ text: <i style={{ fontSize: '13em' }} className="fa fa-align-justify"/> }])
       },
       {
