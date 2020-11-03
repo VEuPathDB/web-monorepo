448371c5ca7c4972f3f0bdf98ae5639cf7e4d466
Jamie Long <jtlong3rd@gmail.com>
2020-11-03 15:41:35 -0500

    Pass "permissions" to ClinEpi StudyRecordHeadings

--- packages/sites/clinepi-site/webapp/js/client/component-wrappers/RecordHeading.jsx
+++ packages/sites/clinepi-site/webapp/js/client/component-wrappers/RecordHeading.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+
+import { withPermissions } from 'ebrc-client/components/Permissions';
 import StudyRecordHeading from 'ebrc-client/component-wrappers/StudyRecordHeading';
+
+const ClinEpiStudyRecordHeading = withPermissions(StudyRecordHeading);
 
 export default RecordHeading => props => (
   props.recordClass.urlSegment === 'dataset'
-    ? <StudyRecordHeading {...props} DefaultComponent={RecordHeading} showSearches showDownload />
+    ? <ClinEpiStudyRecordHeading {...props} DefaultComponent={RecordHeading} showSearches showDownload />
     : <RecordHeading {...props} />
 )
