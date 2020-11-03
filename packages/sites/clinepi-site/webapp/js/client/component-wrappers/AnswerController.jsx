08d1fb6aae503c195be66498a20df35019a8d023
Jamie Long <jtlong3rd@gmail.com>
2020-11-03 15:25:15 -0500

    Pass "permissions" to ClinEpi's StudyAnswerController

--- packages/sites/clinepi-site/webapp/js/client/component-wrappers/AnswerController.jsx
+++ packages/sites/clinepi-site/webapp/js/client/component-wrappers/AnswerController.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+
+import { withPermissions } from 'ebrc-client/components/Permissions';
 import StudyAnswerController from 'ebrc-client/component-wrappers/StudyAnswerController';
+
+const ClinEpiStudyAnswerController = withPermissions(StudyAnswerController);
 
 export default AnswerController => props => {
   if (props.ownProps.recordClass  === 'dataset') {
-    return <StudyAnswerController {...props} DefaultComponent={AnswerController} />;
+    return <ClinEpiStudyAnswerController {...props} DefaultComponent={AnswerController} />;
   }
   return <AnswerController {...props} />
 }
