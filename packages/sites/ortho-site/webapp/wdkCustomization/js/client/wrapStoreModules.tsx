c9c7df34ae76753e2b0abce83affa671f17016fa
Jamie Long <jtlong3rd@gmail.com>
2020-11-18 15:37:10 -0500

    Group Record Pages: by default, have all sections closed (except for "TaxonCounts" and "GroupStatistics")

--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/wrapStoreModules.tsx
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/wrapStoreModules.tsx
@@ -1,10 +1,18 @@
 import storeModules from 'wdk-client/StoreModules';
 
+import * as record from 'ortho-client/store-modules/RecordStoreModule';
+
 // FIXME: Refine these types once EbrcWebsiteCommon's Redux has
 // been converted to TypeScript
 type EbrcStoreModules = typeof storeModules;
 type OrthoMclStoreModules = EbrcStoreModules;
 
 export function wrapStoreModules(ebrcStoreModules: EbrcStoreModules): OrthoMclStoreModules {
-  return ebrcStoreModules;
+  return {
+    ...ebrcStoreModules,
+    record: {
+      ...ebrcStoreModules.record,
+      ...record
+    }
+  };
 }
