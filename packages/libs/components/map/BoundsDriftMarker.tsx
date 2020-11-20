c9bdc0327b0be20b3e12389ec35fe13506c8df30
Brett Fox <brettmfox33@gmail.com>
2020-11-20 09:15:43 -0700

    Double clicking a marker zooms in on a marker's bounding box center

--- packages/libs/components/map/BoundsDriftMarker.tsx
+++ packages/libs/components/map/BoundsDriftMarker.tsx
@@ -1,20 +1,31 @@
-import { Rectangle } from "react-leaflet";
+import {Rectangle, useLeaflet} from "react-leaflet";
 import React, { useState } from "react";
 import { DriftMarker } from "leaflet-drift-marker";
 import { MarkerProps, Bounds } from './Types';
+import {LatLngBounds} from "leaflet";
 
 export interface BoundsDriftMarkerProps extends MarkerProps {
   bounds: Bounds,
-  duration: number
+  duration: number,
+  zoomLevel: number
 }
 
 
-export default function BoundsDriftMarker({position, bounds, icon, duration}: BoundsDriftMarkerProps) {
+export default function BoundsDriftMarker({position, bounds, icon, duration, zoomLevel}: BoundsDriftMarkerProps) {
   const [displayBounds, setDisplayBounds] = useState<boolean>(false)
+  const { map } = useLeaflet();
+  const boundingBox = new LatLngBounds([[bounds.southWest.lat, bounds.southWest.lng], [bounds.northEast.lat, bounds.northEast.lng]])
 
   // DriftMarker misbehaves if icon=undefined is provided
   // is this the most elegant way?
   const optionalIconProp = icon ? { icon } : { };
+
+  const HandleDoubleClick = () => {
+    const boundingBoxCenter = boundingBox.getCenter()
+    if (map) {
+      map.setView([boundingBoxCenter.lat, boundingBoxCenter.lng], zoomLevel+2)
+    }
+  }
 
   return (<DriftMarker
     duration={duration}
@@ -22,11 +33,12 @@ export default function BoundsDriftMarker({position, bounds, icon, duration}: Bo
     {...optionalIconProp}
     onMouseOver={() => setDisplayBounds(true)} // Display bounds rectangle
     onMouseOut={() => setDisplayBounds(false)} // Remove bounds rectangle
+    onDblClick={() => HandleDoubleClick()}
   >
     {
       displayBounds
           ? <Rectangle
-              bounds={[[bounds.southWest.lat, bounds.southWest.lng], [bounds.northEast.lat, bounds.northEast.lng]]}
+              bounds={boundingBox}
               color={"gray"}
               weight={1}
             >
