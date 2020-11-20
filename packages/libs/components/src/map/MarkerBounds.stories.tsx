99318315963894d015f695829add9673d26e99d7
Brett Fox <brettmfox33@gmail.com>
2020-11-20 09:15:43 -0700

    Double clicking a marker zooms in on a marker's bounding box center

--- packages/libs/components/src/map/MarkerBounds.stories.tsx
+++ packages/libs/components/src/map/MarkerBounds.stories.tsx
@@ -29,14 +29,15 @@ const getMarkerElements = ({ bounds, zoomLevel }: BoundsViewport, duration : num
   return buckets.map((bucket : any) => {
     if (bucket.val.length == geohashLevel) {
       return (
-        <BoundsDriftMarker
-          duration={duration}
-          bounds={{ southWest: { lat: bucket.ltMin, lng: bucket.lnMin }, northEast: { lat: bucket.ltMax, lng: bucket.lnMax }}} 
-	  position={{ lat: bucket.ltAvg, lng: bucket.lnAvg }}
-          id={bucket.val}
-          key={bucket.val}
-        />
-        )
+      <BoundsDriftMarker
+        duration={duration}
+        bounds={{ southWest: { lat: bucket.ltMin, lng: bucket.lnMin }, northEast: { lat: bucket.ltMax, lng: bucket.lnMax }}}
+        position={{ lat: bucket.ltAvg, lng: bucket.lnAvg }}
+        id={bucket.val}
+        key={bucket.val}
+        zoomLevel={zoomLevel}
+      />
+      )
     }
   })
 };
@@ -64,3 +65,27 @@ export const MarkerBounds = () => {
       />
   );
 };
+
+export const DoubleClick = () => {
+  const [ markerElements, setMarkerElements ] = useState<ReactElement<MarkerProps>[]>([]);
+  const duration = defaultAnimationDuration;
+
+  const handleViewportChanged = useCallback((bvp: BoundsViewport) => {
+    setMarkerElements(getMarkerElements(bvp, duration));
+  }, [setMarkerElements]);
+
+  return (
+      <MapVEuMap
+          viewport={{center: [ 10, -3 ], zoom: 5}}
+          height="96vh" width="98vw"
+          onViewportChanged={handleViewportChanged}
+          markers={markerElements}
+          animation={{
+            method: "geohash",
+            duration: defaultAnimationDuration,
+            animationFunction: geohashAnimation
+          }}
+          showGrid={true}
+      />
+  );
+}
