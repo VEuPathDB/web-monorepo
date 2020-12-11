9fead9a511dedcd80708cd3ebf847a4f78f3fb39
Mark Hickman <markhick@sas.upenn.edu>
2020-12-11 00:34:50 -0500

    add help to proteome summary columns

--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/utils/dataSummary.tsx
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/utils/dataSummary.tsx
@@ -39,49 +39,58 @@ export const RELEASE_SUMMARY_COLUMNS: DataTableColumns<
   core_peripheral: {
     key: 'core_peripheral',
     name: 'Core/Peripheral',
+    helpText: 'There are 150 Core species whose proteomes were used to form the initial core groups. The remaining species on the site are Peripheral species. The Peripheral proteins were mapped into core groups if they met sequence similarity thresholds. Proteins that did not map into core groups were collected and used to form Residual groups.',
     sortable: true
   },
   name: {
     key: 'name',
     name: 'Name',
+    helpText: 'Genus, species and strain name',
     sortable: true
   },
   root_taxon: {
     key: 'root_taxon',
     name: 'Category',
+    helpText: 'The taxonomic category of the organism. ALVE: Alveolata, AMOE: Amoeba, ARCH: Archaea, EUGL: Eugelnozoa, FUNG: Fungi, META: Metazoa, OBAC: Other Bacteria, OEUK: Other Eukaryota, PROT: Proteobactera, VIRI: Viridiplantae',
     sortable: true
   },
   three_letter_abbrev: {
     key: 'three_letter_abbrev',
     name: 'Abbreviation',
+    helpText: 'Usually, this a four-letter abbreviation for this organism at OrthoMCL. Sometimes, the name follows the format abcd-old, in which case it is a core organism that also has an updated proteome which has been added to OrthoMCL as a peripheral organism. The updated name in this case is abcd.',
     sortable: true
   },
   clustered_sequences: {
     key: 'clustered_sequences',
-    name: '# Clustered Sequences',
+    name: '# Grouped Sequences',
+    helpText: 'Number of proteins that are part of an ortholog grooup. A protein would not be part of a group if it was poor quality (e.g., many stop codons or very few amino acids).',
     sortable: true,
     makeOrder: ({ clustered_sequences }) => Number(clustered_sequences)
   },
   groups: {
     key: 'groups',
     name: '# Groups',
+    helpText: 'Number of ortholog groups in which the organism proteins are found.',
     sortable: true,
     makeOrder: ({ groups }) => Number(groups)
   },
   sequences: {
     key: 'sequences',
     name: '# Sequences',
+    helpText: 'Number of proteins in the organism that have been processed by OrthoMCL. This will depend on whether the gene was structurally annotated to encode a protein.',
     sortable: true,
     makeOrder: ({ sequences }) => Number(sequences)
   },
   resource_name: {
     key: 'resource_name',
     name: 'Resource',
+    helpText: 'The source of the organism proteome.',
     sortable: true
   },
   resource_url: {
     key: 'resource_url',
     name: 'URL',
+    helpText: 'The URL of the site where the organism proteome was obtained.',
     sortable: true
   },
   resource_version: {
