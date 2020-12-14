31b21d2f6ba30661324b92acdf5605dc8d663874
Dave Falke <dmfalke@gmail.com>
2020-12-13 23:27:21 -0500

    Promisify read function

--- packages/libs/eda/scripts/start.js
+++ packages/libs/eda/scripts/start.js
@@ -1,45 +1,45 @@
 #!/usr/bin/env node
 
+const util = require('util');
 const https = require('https');
 const querystring = require('querystring');
 const setCookie = require('set-cookie-parser');
 const childProcess = require('child_process');
 const path = require('path');
-const read = require('read')
+const read = util.promisify(require('read'));
 
-read({ prompt: 'username: '}, (err, username) => {
-  if (err) throw err;
+main();
 
-  read({ prompt: 'password: ', silent: true }, (err, password) => {
-    if (err) throw err;
-  
-    const postData = querystring.stringify({username, password});
-    const req = https.request(
-      'https://veupathdb.org/auth/bin/login',
-      {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Content-Length': Buffer.byteLength(postData),
-          'Cookie': 'auth_probe=1'
-        }
-      },
-      res => {
-        res
-          .on('end', () => {
-            const { auth_tkt: authCookie } = setCookie.parse(res, {
-              map: true,
-              decodeValues: true
-            });
-            if (authCookie == null) throw new Error("Could not get auth_tkt cookie value.");
-            const env = { ...process.env, VEUPATHDB_AUTH_TKT: authCookie.value };
-            childProcess.spawn(path.resolve(__dirname, '../node_modules/.bin/react-scripts'), [ 'start' ], {env, stdio: 'inherit'});
-          })
-          .resume()
+async function main() {
+  const username = await read({ prompt: 'Username: ' });
+  const password = await read({ prompt: 'Password: ', silent: true });
+  const postData = querystring.stringify({username, password});
+  const req = https.request(
+    'https://veupathdb.org/auth/bin/login',
+    {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(postData),
+        'Cookie': 'auth_probe=1'
       }
-    );
-    
-    req.write(postData);
-    req.end();
-  });
-})
+    },
+    res => {
+      res
+        .on('end', () => {
+          const { auth_tkt: authCookie } = setCookie.parse(res, {
+            map: true,
+            decodeValues: true
+          });
+          if (authCookie == null) throw new Error("Could not get auth_tkt cookie value.");
+          const env = { ...process.env, VEUPATHDB_AUTH_TKT: authCookie.value };
+          childProcess.spawn(path.resolve(__dirname, '../node_modules/.bin/react-scripts'), [ 'start' ], {env, stdio: 'inherit'});
+        })
+        .resume()
+    }
+  );
+  
+  req.write(postData);
+  req.end();
+
+}
