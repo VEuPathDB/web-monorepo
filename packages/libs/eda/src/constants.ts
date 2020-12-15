9b85d7c9afb06d34af5164a8bc426f9973a2b7c1
Dave Falke <dmfalke@gmail.com>
2020-12-14 22:17:43 -0500

    Preserve cookies and use constants file for proxy

--- packages/libs/eda/src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const rootUrl = '/';
-export const rootElement ='#root';
-export const endpoint = '/service';
--- packages/libs/eda/src/setupProxy.js
+++ packages/libs/eda/src/setupProxy.js
@@ -1,18 +1,25 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const { endpoint } = require('./constants');
 
 module.exports = function(app) {
   app.use(
-    '/service',
+    endpoint,
     createProxyMiddleware({
       target: process.env.WDK_SERVICE_URL,
-      pathRewrite: { "^/service": "" },
+      pathRewrite: { [`^${endpoint}`]: "" },
       secure: false,
       changeOrigin: true,
       followRedirects: true,
-      headers: {
-        'Cookie': `auth_tkt=${process.env.VEUPATHDB_AUTH_TKT}`
-      },
       logLevel: 'debug',
+      onProxyReq(proxyReq, req, res) {
+        if (proxyReq._isRedirect) return;
+        const authCookie = `auth_tkt=${process.env.VEUPATHDB_AUTH_TKT}`;
+        const cookieRaw = proxyReq.getHeader('cookie');
+        const cookies = cookieRaw == null ? authCookie
+          ? Array.isArray(cookieRaw) : [...cookieRaw, authCookie]
+          : [cookieRaw, authCookie];
+        proxyReq.setHeader('cookie', cookies);
+      }
     })
   );
 };
