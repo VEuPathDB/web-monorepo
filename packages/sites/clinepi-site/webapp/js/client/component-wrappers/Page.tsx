b6e10c29e3cbc7faacbfb9aeb9a088d797a3c2fe
Jamie Long <jtlong3rd@gmail.com>
2020-12-16 14:17:04 -0500

    "useAttemptActionClickHandler" in the ClinEpiPage

--- packages/sites/clinepi-site/webapp/js/client/component-wrappers/Page.tsx
+++ packages/sites/clinepi-site/webapp/js/client/component-wrappers/Page.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 import { Props } from 'wdk-client/Components/Layout/Page';
 
+import { useAttemptActionClickHandler } from 'ebrc-client/hooks/dataRestriction';
+
 export function Page(DefaultComponent: React.ComponentType<Props>) {
   return function ClinEpiPage(props: Props) {
+    useAttemptActionClickHandler();
+
     return <DefaultComponent {...props} />;
   };
 }
