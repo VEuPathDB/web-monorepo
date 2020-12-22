708c35232d812b3b5fde8426e32b9a1008070b47
Jamie Long <jtlong3rd@gmail.com>
2020-12-22 14:22:59 -0500

    Initialize project using Create React App

---
+++ packages/libs/multi-blast/src/Header.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export default function Header() {
+  return (
+    <h1 style={{ background: 'black', color: 'whitesmoke' }}>
+      <code>/// ========================== \\\</code>
+      <br />
+      <code>||| VEUPATHDB DEVELOPMENT SITE |||</code>
+      <br />
+      <code>\\\ ========================== ///</code>
+    </h1>
+  );
+}
