04243ece42d0d09997789020b9d962c4e117b42d
Michael <bigfatpanda@nd.edu>
2021-01-06 11:11:21 -0500

    Update docs.

--- packages/libs/components/src/types/plots.ts
+++ packages/libs/components/src/types/plots.ts
@@ -1,3 +1,3 @@
-// Type / Interface definitions for plots.
+// Commonly used type definitions for plots.
 export type BarLayoutOptions = 'overlay' | 'stack' | 'group';
 export type OrientationOptions = 'vertical' | 'horizontal';
