11a8c263ee5cd040c6fd092a40903b22a53a9574
Dave Falke <dmfalke@gmail.com>
2021-01-07 08:32:21 -0500

    Handle response errors in a better way.

--- packages/libs/wdk-client/src/StoreModules/BlastSummaryViewStoreModule.ts
+++ packages/libs/wdk-client/src/StoreModules/BlastSummaryViewStoreModule.ts
@@ -2,13 +2,14 @@ import { get } from 'lodash/fp';
 import { combineEpics } from 'redux-observable';
 import { Observable } from 'rxjs';
 import { Action } from 'wdk-client/Actions';
-import { fulfillBlastSummaryReport, requestBlastSummaryReport } from 'wdk-client/Actions/SummaryView/BlastSummaryViewActions';
+import { fulfillBlastSummaryReport, requestBlastSummaryReport, rejectBlastSummaryReport } from 'wdk-client/Actions/SummaryView/BlastSummaryViewActions';
 import { RootState } from 'wdk-client/Core/State/Types';
 import { EpicDependencies } from 'wdk-client/Core/Store';
 import { InferAction, mergeMapRequestActionsToEpic } from 'wdk-client/Utils/ActionCreatorUtils';
 import { BlastSummaryViewReport } from 'wdk-client/Utils/WdkModel';
 import { indexByActionProperty, IndexedState } from 'wdk-client/Utils/ReducerUtils';
 import {getCustomReport} from 'wdk-client/Utils/WdkResult';
+import { isServiceError } from 'wdk-client/Service/ServiceError';
 
 
 
@@ -18,6 +19,7 @@ export const reduce = indexByActionProperty(reduceView, get(['payload', 'viewId'
 
 type ViewState = {
   blastSummaryData?: BlastSummaryViewReport,
+  errorMessage?: string;
 };
 
 const initialState: ViewState = {
@@ -32,17 +34,27 @@ function reduceView(state: ViewState = initialState, action: Action): ViewState 
     case fulfillBlastSummaryReport.type: {
       return { ...state, blastSummaryData: action.payload.blastInfo }
     }
+    case rejectBlastSummaryReport.type: {
+      return { ...state, errorMessage: action.payload.message }
+    }
     default: {
       return state;
     }
   }
 }
 
-async function getBlastSummaryViewReport([requestAction]:  [InferAction<typeof requestBlastSummaryReport>], state$: Observable<RootState>, { wdkService }: EpicDependencies) : Promise<InferAction<typeof fulfillBlastSummaryReport>> {
+async function getBlastSummaryViewReport([requestAction]:  [InferAction<typeof requestBlastSummaryReport>], state$: Observable<RootState>, { wdkService }: EpicDependencies) : Promise<InferAction<typeof fulfillBlastSummaryReport> | InferAction<typeof rejectBlastSummaryReport>> {
   const { resultType, viewId } = requestAction.payload;
   let formatting = { format: 'blastSummaryView', formatConfig: { attributes: ['summary', 'alignment']} };
-  let report = await getCustomReport<BlastSummaryViewReport>(wdkService, resultType, formatting)
-  return fulfillBlastSummaryReport(viewId, resultType, report);
+  try {
+    let report = await getCustomReport<BlastSummaryViewReport>(wdkService, resultType, formatting)
+    return fulfillBlastSummaryReport(viewId, resultType, report);
+  }
+  catch (error) {
+    wdkService.submitErrorIfNot500(error);
+    const message = isServiceError(error) ? error.response : String(error);
+    return rejectBlastSummaryReport(viewId, message);
+  }
 }
 
 export const observe =
