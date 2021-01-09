2f56ab5521c472b46431458cd53684eec454672b
Michael <bigfatpanda@nd.edu>
2021-01-08 19:17:23 -0500

    Consolidate some additional type definitions.

--- packages/libs/components/src/types/plots.ts
+++ packages/libs/components/src/types/plots.ts
@@ -1,3 +1,25 @@
 // Commonly used type definitions for plots.
 export type BarLayoutOptions = 'overlay' | 'stack' | 'group';
 export type OrientationOptions = 'vertical' | 'horizontal';
+
+export type UnionOfPlotDataTypes = HistogramData;
+
+// Histogram Type Definitions
+export type HistogramData = Array<HistogramDataSeries>;
+export type HistogramDataSeries = {
+  /** The name of the series. */
+  name: string;
+  /** The color of the series. Optional. */
+  color?: string;
+  /** Bins of data in the series. */
+  bins: HistogramBin[];
+};
+
+export type HistogramBin = {
+  /** The starting value of the bin.  */
+  binStart: number | string;
+  /** A label for the bin. Optional. */
+  binLabel?: string;
+  /** The count of values in the bin. */
+  count: number;
+};
