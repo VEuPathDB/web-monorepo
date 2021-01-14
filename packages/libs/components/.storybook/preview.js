e604c173f97a6f3a5d9bfb3191ec9d064c9bf5f1
dkwon <dkwon@localhost.localdomain>
2021-01-14 11:03:12 -0500

    [DK] removing default margin for full screen

--- packages/libs/components/.storybook/preview.js
+++ packages/libs/components/.storybook/preview.js
@@ -1,1 +1,5 @@
 import '@storybook/addon-console';
+//DKDK storybook v6 seems to have default margin so remove it - https://github.com/storybookjs/storybook/issues/12109
+export const parameters = {
+    layout: 'fullscreen',
+};
