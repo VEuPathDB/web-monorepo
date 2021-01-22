199781e4cb57f323751c680e1513eabd455e03ad
Jamie Long <jtlong3rd@gmail.com>
2021-01-22 16:23:43 -0500

    Add link to BLAST workspace from landing page

--- packages/libs/multi-blast/src/Home.tsx
+++ packages/libs/multi-blast/src/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { Link } from '@veupathdb/wdk-client/lib/Components';
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the VEuPathDB development environment.</h1>
-      <div>
-        To get started, follow these steps:
+      <div style={{ fontSize: '1.2em' }}>
+        <p>To get started, follow these steps:</p>
         <ul>
           <li>
             Put your feature code in <code>src/lib</code>
@@ -15,6 +17,12 @@ export default function Home() {
           </li>
           <li>
             Configure external services in <code>src/setupProxy.js</code>
+          </li>
+        </ul>
+        <p>Work under development:</p>
+        <ul>
+          <li>
+            <Link to="/workspace/blast">Newfangled BLAST workspace</Link>
           </li>
         </ul>
       </div>
