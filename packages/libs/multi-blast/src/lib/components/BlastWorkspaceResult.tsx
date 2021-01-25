e99a79667d5a3e944f4996369052a6b13d7dc9f1
Jamie Long <jtlong3rd@gmail.com>
2021-01-25 15:21:32 -0500

    Initial commit of "wait" page content (sans job polling)

--- packages/libs/multi-blast/src/lib/components/BlastWorkspaceResult.tsx
+++ packages/libs/multi-blast/src/lib/components/BlastWorkspaceResult.tsx
@@ -1,7 +1,38 @@
+import { Link, Loading } from '@veupathdb/wdk-client/lib/Components';
+
+import { blastWorkspaceCx } from './BlastWorkspace';
+
+import './BlastWorkspaceResult.scss';
+
 interface Props {
   jobId: string;
 }
 
 export function BlastWorkspaceResult(props: Props) {
-  return <p style={{ fontSize: '1.2em' }}>Under Construction</p>;
+  return <LoadingBlastResult {...props} />;
+}
+
+function LoadingBlastResult(props: Props) {
+  return (
+    <div className={blastWorkspaceCx('Result', 'Loading')}>
+      <h1>BLAST Job - pending</h1>
+      <p className="JobId">
+        <span className="InlineHeader">Job:</span> {props.jobId}
+      </p>
+      <Loading>
+        <div className="Caption">
+          <p className="Status">
+            <span className="InlineHeader">Status:</span> running
+          </p>
+          <p className="Instructions">
+            This job could take some time to run. You may leave this page and
+            access the result from your{' '}
+            <Link to="/workspace/blast/all">jobs list</Link> later, or{' '}
+            <Link to="/workspace/blast/new">submit another BLAST job</Link>{' '}
+            while you wait.
+          </p>
+        </div>
+      </Loading>
+    </div>
+  );
 }
