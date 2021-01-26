e4062cd194635b5585585ef71da65420bdbf1fcf
Jamie Long <jtlong3rd@gmail.com>
2021-01-26 15:04:59 -0500

    Initial commit of organism utilities

---
+++ packages/libs/multi-blast/src/lib/utils/organisms.ts
@@ -0,0 +1,44 @@
+import { WdkService } from '@veupathdb/wdk-client/lib/Core';
+
+const ORGANISMS_URL_SEGMENT = 'GenomeDataTypes';
+
+const ORGANISM_NAME_ATTRIBUTE_NAME = 'organism_name';
+const NAME_FOR_FILENAMES_ATTRIBUTE_NAME = 'name_for_filenames';
+
+export async function fetchOrganismFilenameMap(wdkService: WdkService) {
+  const answer = await wdkService.getAnswerJson(
+    {
+      searchName: ORGANISMS_URL_SEGMENT,
+      searchConfig: {
+        parameters: {},
+      },
+    },
+    {
+      attributes: [
+        ORGANISM_NAME_ATTRIBUTE_NAME,
+        NAME_FOR_FILENAMES_ATTRIBUTE_NAME,
+      ],
+    }
+  );
+
+  return answer.records.reduce((memo, { attributes }) => {
+    const organismName = attributes[ORGANISM_NAME_ATTRIBUTE_NAME];
+    const nameForFileNames = attributes[NAME_FOR_FILENAMES_ATTRIBUTE_NAME];
+
+    if (typeof organismName !== 'string') {
+      throw new Error(
+        `The '${ORGANISM_NAME_ATTRIBUTE_NAME}' attribute should be a string`
+      );
+    }
+
+    if (typeof nameForFileNames !== 'string') {
+      throw new Error(
+        `The '${NAME_FOR_FILENAMES_ATTRIBUTE_NAME}' attribute should be a string`
+      );
+    }
+
+    memo[organismName] = nameForFileNames;
+
+    return memo;
+  }, {} as Record<string, string>);
+}
