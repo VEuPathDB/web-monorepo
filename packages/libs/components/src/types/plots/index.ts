9cf37b40359ae6909fc6f1826a1ab7f1d41624fd
Michael <bigfatpanda@nd.edu>
2021-02-02 08:44:39 -0500

    Create a new type extension to capture the logic that if `availableUnits` is specified in data returned from the backend, `selectedUnit` must also be present.

--- packages/libs/components/src/types/plots/index.ts
+++ packages/libs/components/src/types/plots/index.ts
@@ -8,6 +8,19 @@ import { PiePlotData } from './piePlot';
 // Commonly used type definitions for plots.
 export type BarLayoutOptions = 'overlay' | 'stack' | 'group';
 export type OrientationOptions = 'vertical' | 'horizontal';
+export type AvailableUnitsAddon =
+  | {
+      /** What units does the backend support switching between? */
+      availableUnits: Array<string>;
+      /** Currently selected unit. */
+      selectedUnit: string;
+    }
+  | {
+      /** What units does the backend support switching between? */
+      availableUnits?: never;
+      /** Currently selected unit. */
+      selectedUnit?: never;
+    };
 
 export type UnionOfPlotDataTypes = HistogramData | PiePlotData | LinePlotData;
 
