9f08cb611b44fea5962a439061a329d473fdb2a5
Dave Falke <dmfalke@gmail.com>
2021-02-02 10:31:20 -0500

    Basic information about selected entity and variable, counts, and filters.

--- packages/libs/eda/src/lib/Variables.tsx
+++ packages/libs/eda/src/lib/Variables.tsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
-import { StudyVariable, useStudy } from '@veupathdb/eda-workspace-core';
+import {
+  StudyVariable,
+  useAnalysis,
+  useEdaApi,
+  useStudy,
+  Distribution,
+} from '@veupathdb/eda-workspace-core';
 import { preorder } from '@veupathdb/wdk-client/lib/Utils/TreeUtils';
 import { startCase } from 'lodash';
 import { cx } from './Utils';
+import { usePromise } from '@veupathdb/eda-workspace-core/lib/hooks/usePromise';
 
 const variableKeys: (keyof StudyVariable)[] = [
   // 'displayName',
@@ -15,11 +22,25 @@ export function Variables() {
   const [selectedEntity, setSelectedEntity] = useState<number>(0);
   const [selectedVariable, setSelectedVariable] = useState<number>(0);
   const { studyMetadata } = useStudy();
+  const edaApi = useEdaApi();
   const entities = Array.from(
     preorder(studyMetadata.rootEntity, (e) => e.children || [])
   );
+  const {
+    history: { current: analysis },
+    setFilters,
+  } = useAnalysis();
   const entity = entities[selectedEntity];
   const variable = entity.variables[selectedVariable];
+  const filters = analysis?.filters ?? [];
+  const entityCount = usePromise(
+    () => edaApi.getEntityCount(studyMetadata.id, entity.id, []),
+    [studyMetadata.id, entity.id]
+  );
+  const filteredCount = usePromise(
+    () => edaApi.getEntityCount(studyMetadata.id, entity.id, filters),
+    [studyMetadata.id, entity.id, filters]
+  );
 
   return (
     <div className={cx('-Variables')}>
@@ -43,13 +64,41 @@ export function Variables() {
           value={selectedVariable}
           onChange={(e) => setSelectedVariable(Number(e.target.value))}
         >
-          {entity.variables.map((variable, idx) => (
-            <option value={idx}>{variable.displayName}</option>
-          ))}
+          {entity.variables.map(
+            (variable, idx) =>
+              variable.dataShape && (
+                <option value={idx}>
+                  {variable.displayName} ({variable.dataShape})
+                </option>
+              )
+          )}
         </select>
       </div>
       <div>
-        <h3>{entity.displayName}</h3>
+        <h3>Filters</h3>
+        {filters ? (
+          filters.map((f) => (
+            <div>
+              <button
+                type="button"
+                onClick={() => setFilters(filters.filter((_f) => _f !== f))}
+              >
+                Remove
+              </button>
+              <code>{JSON.stringify(f)}</code>
+            </div>
+          ))
+        ) : (
+          <div>
+            <i>No filters</i>
+          </div>
+        )}
+      </div>
+      <div>
+        <h3>
+          {entity.displayName} ({filteredCount.value?.count.toLocaleString()} of{' '}
+          {entityCount.value?.count.toLocaleString()})
+        </h3>
         <h4>{variable.displayName}</h4>
         <dl>
           {variableKeys.map((key) => (
@@ -59,6 +108,14 @@ export function Variables() {
             </div>
           ))}
         </dl>
+        <h4>Distribution</h4>
+        <div className="filter-param">
+          <Distribution
+            studyMetadata={studyMetadata}
+            entity={entity}
+            variable={variable}
+          />
+        </div>
       </div>
     </div>
   );
