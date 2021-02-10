b0bc01b265393f811974c607a0996924d5ea7943
Bob <uncoolbob@gmail.com>
2021-02-10 14:59:03 +0000

    oops, added some junk accidentally

--- packages/libs/components/src/stories/api/getMarkersFromFixtureData.tsx
+++ packages/libs/components/src/stories/api/getMarkersFromFixtureData.tsx
@@ -11,7 +11,6 @@ import { LeafletMouseEvent } from 'leaflet';
 
 function sleep(ms: number): Promise<() => void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
-  fet;
 }
 
 export const getSpeciesDonuts = async (
