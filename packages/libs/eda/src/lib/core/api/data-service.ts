d14684f80f7edc4ce80b201dd5e5e0af273eb187
Bob <uncoolbob@gmail.com>
2021-02-25 18:16:29 +0000

    request params and validator sketched out, not tested yet

--- packages/libs/eda/src/lib/core/api/data-service.ts
+++ packages/libs/eda/src/lib/core/api/data-service.ts
@@ -2,34 +2,57 @@ import {
   createJsonRequest,
   FetchClient,
 } from '@veupathdb/web-common/lib/util/api';
-import { number, record, string, type, TypeOf } from 'io-ts';
+import { TypeOf, string, number, array, type, tuple } from 'io-ts';
 import { Filter } from '../types/filter';
 import { ioTransformer } from './ioTransformer';
 
 export interface HistogramRequestParams {
+  studyId: string;
   filters: Filter[];
+  //  derivedVariables:  // TO DO
+  config: {
+    numBins: number;
+    entityId: string;
+    xAxisVariable: {
+      entityId: string;
+      variableId: string;
+    };
+  };
 }
 
-//////////////// TO CHANGE!!
+// TO DO: handle other three histogram queries and their responses
 export type HistogramResponse = TypeOf<typeof HistogramResponse>;
-
-export const HistogramResponse = type({
-  entitiesCount: number,
-  distribution: record(string, number),
-});
+export const HistogramResponse = tuple([
+  array(
+    type({
+      binLabel: array(string),
+      binStart: array(string),
+      value: array(number),
+    })
+  ),
+  type({
+    incompleteCases: array(number),
+    binSlider: type({
+      min: array(number),
+      max: array(number),
+      step: array(number),
+    }),
+    numBins: array(number),
+    xVariableDetails: type({
+      variableId: array(string),
+      entityId: array(string),
+    }),
+  }),
+]);
 
 export class DataClient extends FetchClient {
   getNumericHistogramNumBins(
-    studyId: string,
-    entityId: string,
-    variableId: string,
     params: HistogramRequestParams
   ): Promise<HistogramResponse> {
     return this.fetch(
       createJsonRequest({
         method: 'POST',
-        // CHANGE ME vvvvvvvvvvvvvvvvvvvvvvvvvvvv
-        path: `/studies/${studyId}/entities/${entityId}/variables/${variableId}/distribution`,
+        path: '/analyses/numeric-histogram-num-bins',
         body: params,
         transformResponse: ioTransformer(HistogramResponse),
       })
