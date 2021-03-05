f15a51b3989fdd08b26f26943a41eed05feca2dd
Dave Falke <dmfalke@gmail.com>
2021-03-05 16:39:52 -0500

    Add option to clear previous value or usePromise invocation.

--- packages/libs/eda/src/lib/core/hooks/promise.ts
+++ packages/libs/eda/src/lib/core/hooks/promise.ts
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
+import { stubFalse } from 'lodash/fp';
 
 export type PromiseHookState<T> = {
   value?: T;
@@ -9,15 +10,36 @@ export type PromiseHookState<T> = {
 export function usePromise<T>(
   promiseFactory: () => Promise<T>,
   deps?: unknown[]
+): PromiseHookState<T>;
+export function usePromise<T>(
+  promiseFactory: () => Promise<T>,
+  retainPreviousValue: (prevState?: T) => boolean,
+  deps?: unknown[]
+): PromiseHookState<T>;
+export function usePromise<T>(
+  promiseFactory: () => Promise<T>,
+  retainPreviousValueOrDeps?: unknown[] | ((preState?: T) => boolean),
+  optionalDeps?: unknown[]
 ): PromiseHookState<T> {
   const [state, setState] = useState<PromiseHookState<T>>({
     pending: true,
   });
+  const deps = Array.isArray(retainPreviousValueOrDeps)
+    ? retainPreviousValueOrDeps
+    : optionalDeps;
+  const retainPreviousValue =
+    typeof retainPreviousValueOrDeps === 'function'
+      ? retainPreviousValueOrDeps
+      : stubFalse;
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const callback = useCallback(promiseFactory, deps ?? [promiseFactory]);
   useEffect(() => {
     let ignoreResolve = false;
-    setState({ pending: true });
+    setState((prev) => ({
+      pending: true,
+      value: retainPreviousValue(prev.value) ? prev.value : undefined,
+      error: undefined,
+    }));
     callback().then(
       (value) => {
         if (ignoreResolve) return;
@@ -37,6 +59,6 @@ export function usePromise<T>(
     return function cleanup() {
       ignoreResolve = true;
     };
-  }, [callback]);
+  }, [callback, retainPreviousValue]);
   return state;
 }
