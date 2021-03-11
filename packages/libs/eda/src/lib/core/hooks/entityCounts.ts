64a810d01529ca442fbe159415a6a8e9364faf3a
Dave Falke <dmfalke@gmail.com>
2021-03-11 11:34:05 -0500

    Add fallback value for when count request fails

--- packages/libs/eda/src/lib/core/hooks/entityCounts.ts
+++ packages/libs/eda/src/lib/core/hooks/entityCounts.ts
@@ -11,11 +11,17 @@ export function useEntityCounts(filters?: Filter[]) {
     useCallback(async () => {
       const counts: Record<string, number> = {};
       for (const entity of preorder(rootEntity, (e) => e.children ?? [])) {
-        const { count } = await subsettingClient.getEntityCount(
-          id,
-          entity.id,
-          filters ?? []
-        );
+        const { count } = await subsettingClient
+          .getEntityCount(id, entity.id, filters ?? [])
+          .catch((error) => {
+            console.warn(
+              'Could not load count for entity',
+              entity.id,
+              entity.displayName
+            );
+            console.error(error);
+            return { count: 0 };
+          });
         counts[entity.id] = count;
       }
       return counts;
