9ceaf62aab65e7ad3e481e403586c56e8f9dc8f8
Dave Falke <dmfalke@gmail.com>
2021-03-15 11:41:52 -0400

    handle histogram filtering and retain bin selection

--- packages/libs/eda/src/lib/core/components/filter/HistogramFilter.tsx
+++ packages/libs/eda/src/lib/core/components/filter/HistogramFilter.tsx
@@ -4,15 +4,20 @@ import Histogram, {
   HistogramProps,
 } from '@veupathdb/components/lib/plots/Histogram';
 import {
+  DateRange,
   NumberOrTimeDelta,
   NumberOrTimeDeltaRange,
+  NumberRange,
 } from '@veupathdb/components/lib/types/general';
 import {
   HistogramData,
   HistogramDataSeries,
 } from '@veupathdb/components/lib/types/plots';
 import { Loading } from '@veupathdb/wdk-client/lib/Components';
-import React, { useCallback } from 'react';
+import { getOrElse } from 'fp-ts/lib/Either';
+import { pipe } from 'fp-ts/lib/function';
+import { number, partial, TypeOf } from 'io-ts';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import {
   DataClient,
   DateHistogramBinWidthResponse,
@@ -23,8 +28,8 @@ import {
 import { usePromise } from '../../hooks/promise';
 import { SessionState } from '../../hooks/session';
 import { useDataClient } from '../../hooks/workspace';
-import { Filter } from '../../types/filter';
-import { StudyEntity, StudyMetadata, StudyVariable } from '../../types/study';
+import { DateRangeFilter, Filter, NumberRangeFilter } from '../../types/filter';
+import { StudyEntity, StudyMetadata } from '../../types/study';
 import { PromiseType } from '../../types/utility';
 import { gray, red } from './colors';
 import { HistogramVariable } from './types';
@@ -42,9 +47,22 @@ type GetDataParams = {
   selectedUnit?: string;
 };
 
+const UIState = partial({
+  binWidth: number,
+});
+
 export function HistogramFilter(props: Props) {
   const { variable, entity, sessionState, studyMetadata } = props;
   const { id: studyId } = studyMetadata;
+  const { setFilters } = sessionState;
+  const filters = sessionState.session?.filters;
+  const uiStateKey = `${entity.id}/${variable.id}`;
+  const uiState = useMemo(() => {
+    return pipe(
+      UIState.decode(sessionState.session?.variableUISettings[uiStateKey]),
+      getOrElse((): TypeOf<typeof UIState> => ({}))
+    );
+  }, [sessionState.session?.variableUISettings, uiStateKey]);
   const dataClient = useDataClient();
   const getData = useCallback(
     async (
@@ -61,7 +79,7 @@ export function HistogramFilter(props: Props) {
         {
           entityId: entity.id,
           variableId: variable.id,
-          filters: sessionState.session?.filters,
+          filters,
         },
         (filters) => {
           const params = getRequestParams(
@@ -110,9 +128,72 @@ export function HistogramFilter(props: Props) {
         entityId: entity.id,
       };
     },
-    [dataClient, entity, sessionState, studyId, variable]
+    [dataClient, entity, filters, studyId, variable]
   );
-  const data = usePromise(getData);
+  const data = usePromise(
+    useCallback(() => getData(uiState), [getData, uiState])
+  );
+
+  const filter = filters?.find(
+    (f): f is NumberRangeFilter | DateRangeFilter =>
+      f.variableId === variable.id &&
+      f.entityId === entity.id &&
+      (f.type === 'dateRange' || f.type === 'numberRange')
+  );
+
+  const updateFilter = useCallback(
+    (selectedRange?: NumberRange | DateRange) => {
+      const otherFilters = filters?.filter((f) => f !== filter) ?? [];
+      if (selectedRange == null) {
+        if (otherFilters.length === 0 || filter == null) return;
+        setFilters(otherFilters);
+      } else {
+        if (
+          filter &&
+          (filter.type === 'dateRange' || filter.type === 'numberRange') &&
+          filter.min === selectedRange.min &&
+          filter.max === selectedRange.max
+        )
+          return;
+        setFilters(
+          otherFilters.concat([
+            variable.type === 'date'
+              ? {
+                  variableId: variable.id,
+                  entityId: entity.id,
+                  type: 'dateRange',
+                  min: (selectedRange as DateRange).min.toISOString(),
+                  max: (selectedRange as DateRange).max.toISOString(),
+                }
+              : {
+                  variableId: variable.id,
+                  entityId: entity.id,
+                  type: 'numberRange',
+                  ...(selectedRange as NumberRange),
+                },
+          ])
+        );
+      }
+      console.log('new selected range', selectedRange);
+    },
+    [entity.id, filters, filter, setFilters, variable.id, variable.type]
+  );
+
+  const updateUIState = useCallback(
+    (newUiState: TypeOf<typeof UIState>) => {
+      if (uiState.binWidth === newUiState.binWidth) return;
+      sessionState.setVariableUISettings({
+        [uiStateKey]: {
+          ...uiState,
+          ...newUiState,
+        },
+      });
+    },
+    [sessionState, uiStateKey, uiState]
+  );
+
+  // Note use of `key` used with HistogramPlotWithControls. This is a little hack to force
+  // the range to be reset if the filter is removed.
   return (
     <div style={{ position: 'relative' }}>
       {data.pending && (
@@ -123,12 +204,16 @@ export function HistogramFilter(props: Props) {
         data.value.variableId === variable.id &&
         data.value.entityId === entity.id && (
           <HistogramPlotWithControls
+            key={filters?.length ?? 0}
+            filter={filter}
             data={data.value}
             getData={getData}
             width={1000}
             height={600}
             orientation={'horizontal'}
             barLayout={'overlay'}
+            updateFilter={updateFilter}
+            updateUIState={updateUIState}
           />
         )}
     </div>
@@ -137,11 +222,17 @@ export function HistogramFilter(props: Props) {
 
 type HistogramPlotWithControlsProps = HistogramProps & {
   getData: (params?: GetDataParams) => Promise<HistogramData>;
+  updateFilter: (selectedRange?: NumberRange | DateRange) => void;
+  updateUIState: (uiState: TypeOf<typeof UIState>) => void;
+  filter?: DateRangeFilter | NumberRangeFilter;
 };
 
 function HistogramPlotWithControls({
   data,
   getData,
+  updateFilter,
+  updateUIState,
+  filter,
   ...histogramProps
 }: HistogramPlotWithControlsProps) {
   const plotControls = usePlotControls<HistogramData>({
@@ -150,13 +241,60 @@ function HistogramPlotWithControls({
       binWidthRange: data.binWidthRange,
       binWidthStep: data.binWidthStep,
       onBinWidthChange: getData,
+      displaySelectedRangeControls: true,
+      selectedRange:
+        filter &&
+        (filter.type === 'dateRange'
+          ? {
+              min: new Date(filter.min),
+              max: new Date(filter.max),
+            }
+          : {
+              min: filter.min,
+              max: filter.max,
+            }),
     },
     // onSelectedUnitChange: getData
   });
 
+  useEffect(() => {
+    if (
+      plotControls.histogram.selectedRange &&
+      plotControls.histogram.selectedRangeBounds
+    ) {
+      if (
+        plotControls.histogram.selectedRangeBounds.min ===
+          plotControls.histogram.selectedRange.min &&
+        plotControls.histogram.selectedRangeBounds.max ===
+          plotControls.histogram.selectedRange.max
+      ) {
+        updateFilter();
+      } else {
+        updateFilter(plotControls.histogram.selectedRange);
+      }
+    }
+  }, [
+    plotControls.histogram.selectedRange,
+    plotControls.histogram.selectedRangeBounds,
+    updateFilter,
+  ]);
+
+  useEffect(() => {
+    if (data.binWidth == null) return;
+    const newBinWidth =
+      typeof data.binWidth === 'number' ? data.binWidth : data.binWidth[0];
+    updateUIState({
+      binWidth: newBinWidth,
+    });
+  }, [data.binWidth, updateUIState]);
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Histogram {...histogramProps} {...plotControls} />
+      <Histogram
+        {...histogramProps}
+        {...plotControls}
+        {...plotControls.histogram}
+      />
       <HistogramControls
         label="Histogram Controls"
         {...plotControls}
