eae4a7f0080c1b4b9620676c506106d5f9861fb4
Connor Howington <connor.howington@gmail.com>
2021-03-24 17:10:08 -0400

    Add mostly-working unified entity diagram component

--- packages/libs/components/src/EntityDiagram/EntityDiagram.stories.tsx
+++ packages/libs/components/src/EntityDiagram/EntityDiagram.stories.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import MiniDiagram from './MiniDiagram';
 import ExpandedDiagram from './ExpandedDiagram';
+import EntityDiagram from './EntityDiagram';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { StudyData, ShadingData } from './Types';
 import './diagram.css';
@@ -9,166 +10,169 @@ export default {
   title: 'Entity Diagram',
 };
 
-export const EntityDiagram = () => {
+const rootEntity: StudyData = {
+  id: 'GEMS_House',
+  displayName: 'Household',
+  description: 'Households from the study area',
+  children: [
+    {
+      id: 'GEMS_HouseObs',
+      displayName: 'Household Observation',
+      description: '',
+      children: [],
+      variables: [
+        {
+          id: 'var-19',
+          providerLabel: '_watersupply',
+          displayName: 'Water supply',
+          type: 'string',
+          isMultiValued: false,
+        },
+      ],
+    },
+    {
+      id: 'GEMS_Part',
+      displayName: 'Participant',
+      description: 'Participants in the study',
+      children: [
+        {
+          id: 'GEMS_PartObs',
+          displayName: 'Participant Observation',
+          description: '',
+          children: [
+            {
+              id: 'GEMS_Sample',
+              displayName: 'Sample',
+              description: '',
+              children: [],
+              variables: [],
+            },
+            {
+              id: 'GEMS_Treat',
+              displayName: 'Treatment',
+              description: '',
+              children: [],
+              variables: [],
+            },
+          ],
+          variables: [
+            {
+              id: 'var-12',
+              providerLabel: '_weight',
+              displayName: 'Weight',
+              type: 'number',
+              isContinuous: true,
+              precision: 2,
+              units: 'pounds',
+              isMultiValued: true,
+            },
+            {
+              id: 'var-13',
+              providerLabel: '_favnumber',
+              displayName: 'Favorite number',
+              type: 'number',
+              isContinuous: false,
+              precision: 0,
+              units: '',
+              isMultiValued: false,
+            },
+            {
+              id: 'var-14',
+              providerLabel: '_startdate',
+              displayName: 'Start date',
+              type: 'date',
+              isContinuous: false,
+              isMultiValued: false,
+            },
+            {
+              id: 'var-15',
+              providerLabel: '_visitdate',
+              displayName: 'Visit date',
+              type: 'number',
+              isContinuous: false,
+              precision: 0,
+              isMultiValued: false,
+            },
+            {
+              id: 'var-16',
+              providerLabel: '_mood',
+              displayName: 'Mood',
+              type: 'string',
+              isMultiValued: false,
+            },
+          ],
+        },
+      ],
+      variables: [
+        {
+          id: 'var-10',
+          providerLabel: '_networth',
+          displayName: 'Net worth',
+          type: 'number',
+          isContinuous: true,
+          precision: 2,
+          units: 'dollars',
+          isMultiValued: true,
+        },
+        {
+          id: 'var-11',
+          providerLabel: '_shoesize',
+          displayName: 'Shoe size',
+          type: 'number',
+          isContinuous: true,
+          precision: 1,
+          units: 'shoe size',
+          isMultiValued: true,
+        },
+        {
+          id: 'var-17',
+          providerLabel: '_haircolor',
+          displayName: 'Hair color',
+          type: 'string',
+          isMultiValued: false,
+        },
+        {
+          id: 'var-20',
+          providerLabel: '_name',
+          displayName: 'Name',
+          type: 'string',
+          isMultiValued: false,
+        },
+      ],
+    },
+  ],
+  variables: [
+    {
+      id: 'var-18',
+      providerLabel: '_address',
+      displayName: 'City',
+      type: 'string',
+      isMultiValued: false,
+    },
+  ],
+};
+
+const studyData = {
+  id: 'DS12385',
+  rootEntity: rootEntity,
+};
+
+const shadingData: ShadingData = {
+  GEMS_House: 0.05,
+  GEMS_HouseObs: 0.2,
+  GEMS_Part: 0.41,
+  GEMS_PartObs: 0.66,
+  GEMS_Treat: 0.98,
+};
+
+const miniSize = { height: 300, width: 150 };
+const expandedSize = { height: 500, width: 600 };
+
+export const EntityDiagramSeparate = () => {
   const [orientation, setOrientation] = useState<'horizontal' | 'vertical'>(
     'vertical'
   );
   const [expanded, setExpanded] = useState<boolean>(false);
-
-  const rootEntity: StudyData = {
-    id: 'GEMS_House',
-    displayName: 'Household',
-    description: 'Households from the study area',
-    children: [
-      {
-        id: 'GEMS_HouseObs',
-        displayName: 'Household Observation',
-        description: '',
-        children: [],
-        variables: [
-          {
-            id: 'var-19',
-            providerLabel: '_watersupply',
-            displayName: 'Water supply',
-            type: 'string',
-            isMultiValued: false,
-          },
-        ],
-      },
-      {
-        id: 'GEMS_Part',
-        displayName: 'Participant',
-        description: 'Participants in the study',
-        children: [
-          {
-            id: 'GEMS_PartObs',
-            displayName: 'Participant Observation',
-            description: '',
-            children: [
-              {
-                id: 'GEMS_Sample',
-                displayName: 'Sample',
-                description: '',
-                children: [],
-                variables: [],
-              },
-              {
-                id: 'GEMS_Treat',
-                displayName: 'Treatment',
-                description: '',
-                children: [],
-                variables: [],
-              },
-            ],
-            variables: [
-              {
-                id: 'var-12',
-                providerLabel: '_weight',
-                displayName: 'Weight',
-                type: 'number',
-                isContinuous: true,
-                precision: 2,
-                units: 'pounds',
-                isMultiValued: true,
-              },
-              {
-                id: 'var-13',
-                providerLabel: '_favnumber',
-                displayName: 'Favorite number',
-                type: 'number',
-                isContinuous: false,
-                precision: 0,
-                units: '',
-                isMultiValued: false,
-              },
-              {
-                id: 'var-14',
-                providerLabel: '_startdate',
-                displayName: 'Start date',
-                type: 'date',
-                isContinuous: false,
-                isMultiValued: false,
-              },
-              {
-                id: 'var-15',
-                providerLabel: '_visitdate',
-                displayName: 'Visit date',
-                type: 'number',
-                isContinuous: false,
-                precision: 0,
-                isMultiValued: false,
-              },
-              {
-                id: 'var-16',
-                providerLabel: '_mood',
-                displayName: 'Mood',
-                type: 'string',
-                isMultiValued: false,
-              },
-            ],
-          },
-        ],
-        variables: [
-          {
-            id: 'var-10',
-            providerLabel: '_networth',
-            displayName: 'Net worth',
-            type: 'number',
-            isContinuous: true,
-            precision: 2,
-            units: 'dollars',
-            isMultiValued: true,
-          },
-          {
-            id: 'var-11',
-            providerLabel: '_shoesize',
-            displayName: 'Shoe size',
-            type: 'number',
-            isContinuous: true,
-            precision: 1,
-            units: 'shoe size',
-            isMultiValued: true,
-          },
-          {
-            id: 'var-17',
-            providerLabel: '_haircolor',
-            displayName: 'Hair color',
-            type: 'string',
-            isMultiValued: false,
-          },
-          {
-            id: 'var-20',
-            providerLabel: '_name',
-            displayName: 'Name',
-            type: 'string',
-            isMultiValued: false,
-          },
-        ],
-      },
-    ],
-    variables: [
-      {
-        id: 'var-18',
-        providerLabel: '_address',
-        displayName: 'City',
-        type: 'string',
-        isMultiValued: false,
-      },
-    ],
-  };
-
-  const studyData = {
-    id: 'DS12385',
-    rootEntity: rootEntity,
-  };
-
-  const shadingData: ShadingData = {
-    GEMS_House: 0.05,
-    GEMS_HouseObs: 0.2,
-    GEMS_Part: 0.41,
-    GEMS_PartObs: 0.66,
-    GEMS_Treat: 0.98,
-  };
 
   return (
     <div
@@ -177,7 +181,6 @@ export const EntityDiagram = () => {
         marginTop: 40,
         width: 1000,
         height: 700,
-        border: '1px black solid',
       }}
     >
       <button
@@ -194,13 +197,13 @@ export const EntityDiagram = () => {
       >
         Switch Size
       </button>
-      <div style={{ height: 300, width: 150, border: '1px solid' }}>
+      <div style={{ ...miniSize, border: '1px solid' }}>
         <MiniDiagram
           treeData={studyData.rootEntity}
           orientation={orientation}
           highlightedEntityID={'Sample'}
           shadingData={shadingData}
-          size={{ height: 300, width: 150 }}
+          size={miniSize}
         />
       </div>
       <TransitionGroup>
@@ -210,18 +213,62 @@ export const EntityDiagram = () => {
             timeout={1000}
             classNames="expanded-diagram"
           >
-            <div style={{ height: 500, width: 600, border: '1px solid' }}>
+            <div style={{ ...expandedSize, border: '1px solid' }}>
               <ExpandedDiagram
                 treeData={studyData.rootEntity}
                 orientation={orientation}
                 highlightedEntityID={'Sample'}
                 shadingData={shadingData}
-                size={{ height: 500, width: 600 }}
+                size={expandedSize}
               />
             </div>
           </CSSTransition>
         )}
       </TransitionGroup>
+    </div>
+  );
+};
+
+export const EntityDiagramUnified = () => {
+  const [orientation, setOrientation] = useState<'horizontal' | 'vertical'>(
+    'vertical'
+  );
+  const [expanded, setExpanded] = useState<boolean>(false);
+  const size = expanded ? expandedSize : miniSize;
+
+  return (
+    <div
+      style={{
+        marginLeft: 40,
+        marginTop: 40,
+        width: 1000,
+        height: 700,
+      }}
+    >
+      <button
+        onClick={() =>
+          orientation == 'horizontal'
+            ? setOrientation('vertical')
+            : setOrientation('horizontal')
+        }
+      >
+        Switch Orientation
+      </button>
+      <button
+        onClick={() => (expanded ? setExpanded(false) : setExpanded(true))}
+      >
+        Switch Size
+      </button>
+      <div style={{ ...size, border: '1px solid' }}>
+        <EntityDiagram
+          treeData={studyData.rootEntity}
+          orientation={orientation}
+          isExpanded={expanded}
+          highlightedEntityID={'Sample'}
+          shadingData={shadingData}
+          size={size}
+        />
+      </div>
     </div>
   );
 };
---
+++ packages/libs/components/src/EntityDiagram/EntityDiagram.tsx
@@ -0,0 +1,262 @@
+import React, { useState } from 'react';
+import { hierarchy, Tree } from '@visx/hierarchy';
+import { Group } from '@visx/group';
+import { HierarchyPointNode } from '@visx/hierarchy/lib/types';
+import OffsetLine from './OffsetLine';
+import { Tooltip } from '@visx/tooltip';
+import { LinearGradient } from '@visx/gradient';
+
+interface CustomNode {
+  node: HierarchyPointNode<StudyData>;
+}
+
+// Todo: There MUST be a smarter way to center the text
+function CalculateDYSize(nodeLength: number) {
+  switch (nodeLength) {
+    case 1:
+      return '.33em';
+    case 2:
+      return '.80em';
+    case 3:
+      return '1.35em';
+    case 4:
+      return '1.8em';
+    case 5:
+      return '1.8em';
+  }
+}
+
+export type VariableType =
+  | 'category'
+  | 'string'
+  | 'number'
+  | 'date'
+  | 'longitude';
+
+export interface Variables {
+  id: string;
+  providerLabel: string;
+  displayName: string;
+  type: VariableType;
+  isContinuous?: boolean;
+  precision?: number;
+  units?: string;
+  isMultiValued: boolean;
+}
+
+export interface StudyData {
+  id: string;
+  displayName: string;
+  description: string;
+  children?: this[];
+  variables: Variables[];
+}
+
+export interface ShadingData {
+  /** The key is the entity ID and the value is a decimal representing the
+   * fraction of the node to shade */
+  [index: string]: number;
+}
+
+export interface EntityDiagramProps {
+  /** Data that defines the tree structure */
+  treeData: StudyData;
+  /** Which direction the tree is oriented */
+  orientation: 'horizontal' | 'vertical';
+  /** Whether the diagram is expanded */
+  isExpanded: boolean;
+  /** The tree's dimensions. If the tree is horizontal, it may not take up the
+   * whole height; if it's vertical, it may not take up the full width. */
+  size: {
+    height: number;
+    width: number;
+  };
+  /** Which entity to highlight */
+  highlightedEntityID: string;
+  /** Data defining the background shading of each node */
+  shadingData: ShadingData;
+  /** An optional function returning the element to render for a node given its
+   * data */
+  renderNode?: (
+    node: StudyData,
+    children?: Array<React.ReactElement>
+  ) => React.ReactElement | null;
+}
+
+export default function EntityDiagram({
+  treeData,
+  orientation,
+  isExpanded,
+  highlightedEntityID,
+  shadingData,
+  renderNode,
+  size,
+}: EntityDiagramProps) {
+  const data = hierarchy(treeData);
+  const [tooltipOpen, setTooltipOpen] = useState<boolean>(false);
+  const [tooltipLeft, setTooltipLeft] = useState<number>(0);
+  const [tooltipTop, setTooltipTop] = useState<number>(0);
+  const [tooltipNode, setTooltipNode] = useState<null | HierarchyPointNode<
+    StudyData
+  >>(null);
+
+  const nodeWidth = isExpanded ? 120 : 30;
+  const nodeHeight = isExpanded ? 70 : 20;
+  // Node border width
+  const nodeStrokeWidth = 1;
+  // Width of the highlight border around the highlighted node
+  const nodeHighlightWidth = 3;
+  // treeHeight is always from root to furthest leaf, regardless of orientation
+  // (it's not always vertical on screen)
+  const treeHeight =
+    (orientation === 'horizontal'
+      ? size.width - nodeWidth
+      : size.height - nodeHeight) -
+    nodeHighlightWidth * 2;
+  // Likewise for treeWidth (it's not always horizontal on screen)
+  const treeWidth = orientation === 'horizontal' ? size.height : size.width;
+  // The tree's edge is in the middle of the boundary nodes, so we shift it by
+  // half a node dimension
+  const treeLeft =
+    orientation === 'horizontal' ? nodeWidth / 2 + nodeHighlightWidth : 0;
+  const treeTop =
+    (orientation === 'horizontal' ? 0 : nodeHeight / 2) + nodeHighlightWidth; // Where the baby rocks
+
+  function CustomNode({ node }: CustomNode) {
+    let displayText: string;
+    const isHighlighted = highlightedEntityID == node.data.displayName;
+
+    if (isExpanded) {
+      displayText = node.data.displayName;
+    } else {
+      // get acronym of displayName
+      const matches = node.data.displayName.match(/\b(\w)/g) as string[];
+      displayText = matches.join('');
+    }
+
+    const handleTooltipOpen = () => {
+      const topOffset = orientation == 'horizontal' ? 30 : 80; // offsets are based on margins
+      const leftOffset = orientation == 'horizontal' ? 95 : 55; // offsets are based on margins
+      setTooltipOpen(true);
+      setTooltipLeft(
+        (orientation == 'horizontal' ? node.y : node.x) + leftOffset
+      );
+      setTooltipTop(
+        (orientation == 'horizontal' ? node.x : node.y) + topOffset
+      );
+      setTooltipNode(node);
+    };
+
+    const rectangle = (
+      <rect
+        // These props don't account for stroke width, so we shrink them
+        // accordingly to make sure the node is exactly the dimensions we want
+        height={nodeHeight - nodeStrokeWidth * 2}
+        width={nodeWidth - nodeStrokeWidth * 2}
+        y={-nodeHeight / 2}
+        x={-nodeWidth / 2}
+        fill={
+          shadingData[node.data.id]
+            ? `url('#rect-gradient-${node.data.id}')`
+            : 'white'
+        }
+        stroke={'black'}
+        strokeWidth={nodeStrokeWidth}
+        style={{
+          outline: isHighlighted
+            ? `yellow ${nodeHighlightWidth}px solid`
+            : undefined,
+          overflowWrap: isExpanded ? 'normal' : undefined,
+        }}
+        onMouseEnter={isExpanded ? undefined : handleTooltipOpen}
+        onMouseLeave={isExpanded ? undefined : () => setTooltipOpen(false)}
+      />
+    );
+
+    const text = (
+      <text
+        fontSize={12}
+        textAnchor="middle"
+        style={{ cursor: 'default' }}
+        dy={
+          isExpanded
+            ? CalculateDYSize(node.data.displayName.split(' ').length)
+            : '.33em'
+        }
+        width={isExpanded ? 100 : undefined}
+        onMouseEnter={isExpanded ? undefined : handleTooltipOpen}
+        onMouseLeave={isExpanded ? undefined : () => setTooltipOpen(false)}
+      >
+        {displayText}
+      </text>
+    );
+
+    return (
+      <Group
+        top={orientation == 'horizontal' ? node.x : node.y}
+        left={orientation == 'horizontal' ? node.y : node.x}
+        key={node.x + node.y}
+      >
+        {renderNode?.(node.data, [rectangle, text]) ?? [rectangle, text]}
+      </Group>
+    );
+  }
+
+  return (
+    <div className={isExpanded ? '' : 'mini-diagram'}>
+      <svg width={size.width} height={size.height}>
+        <defs>
+          <marker
+            id="arrow"
+            viewBox="0 -5 10 10"
+            markerWidth={isExpanded ? '18' : '10'}
+            markerHeight={isExpanded ? '18' : '10'}
+            orient="auto"
+            fill="black"
+            style={{ opacity: 0.7 }}
+          >
+            <path d="M0,-5L10,0L0,5" />
+          </marker>
+        </defs>
+        {
+          // Node background shading definitions
+          Object.keys(shadingData).map((key, index) => (
+            <LinearGradient
+              key={index}
+              vertical={false}
+              x1={0}
+              x2={shadingData[key]}
+              fromOffset={1}
+              id={`rect-gradient-${key}`}
+              from="#e4c8c8"
+              to="white"
+            />
+          ))
+        }
+        <Tree root={data} size={[treeWidth, treeHeight]}>
+          {(tree) => (
+            <Group left={treeLeft} top={treeTop}>
+              {tree.links().map((link, i) => {
+                return (
+                  <OffsetLine
+                    link={link}
+                    orientation={orientation}
+                    key={isExpanded ? `expanded-link-${i}` : `link-${i}`}
+                  />
+                );
+              })}
+              {tree.descendants().map((node, i) => (
+                <CustomNode node={node} key={`node-${i}`} />
+              ))}
+            </Group>
+          )}
+        </Tree>
+      </svg>
+      {tooltipOpen && tooltipNode && (
+        <Tooltip left={tooltipLeft} top={tooltipTop}>
+          <div>{tooltipNode.data.displayName}</div>
+        </Tooltip>
+      )}
+    </div>
+  );
+}
