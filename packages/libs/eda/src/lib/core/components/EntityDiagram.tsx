3f119df2a0ec4c5143a2e1c5ef48018f1875f587
Dave Falke <dmfalke@gmail.com>
2021-04-17 11:14:35 -0400

    Compute dimensions based on tree size

--- packages/libs/eda/src/lib/core/components/EntityDiagram.tsx
+++ packages/libs/eda/src/lib/core/components/EntityDiagram.tsx
@@ -1,18 +1,15 @@
 import { useStudyMetadata } from '../hooks/workspace';
-import EntityDiagramComponent from '@veupathdb/components/lib/EntityDiagram/EntityDiagram';
+import EntityDiagramComponent, {
+  Orientation,
+} from '@veupathdb/components/lib/EntityDiagram/EntityDiagram';
 import { StudyEntity } from '../types/study';
 import { VariableLink } from './VariableLink';
+import { preorder } from '@veupathdb/wdk-client/lib/Utils/TreeUtils';
+import { reduce } from '@veupathdb/wdk-client/lib/Utils/IterableUtils';
 
 interface Props {
   expanded: boolean;
   orientation: 'horizontal' | 'vertical';
-  /** The tree's dimensions. If the tree is horizontal, it may not take up
-   * the whole height; if it's vertical, it may not take up the full
-   * width. */
-  size: {
-    height: number;
-    width: number;
-  };
   selectedEntity: string;
   entityCounts?: Record<string, number>;
   filteredEntityCounts?: Record<string, number>;
@@ -50,16 +47,89 @@ export function EntityDiagram(props: Props) {
     );
   };
 
-  const diagramProps = {
-    treeData: studyMetadata.rootEntity,
-    highlightedEntityID: props.selectedEntity,
-    orientation: props.orientation,
-    size: props.size,
-    shadingData: shadingData,
-    renderNode: renderNode,
-  };
+  const dimensions = getDimensions(studyMetadata.rootEntity, props.orientation);
 
   return (
-    <EntityDiagramComponent isExpanded={props.expanded} {...diagramProps} />
+    <EntityDiagramComponent
+      isExpanded={props.expanded}
+      treeData={studyMetadata.rootEntity}
+      highlightedEntityID={props.selectedEntity}
+      orientation={props.orientation}
+      shadingData={shadingData}
+      renderNode={renderNode}
+      selectedHighlightColor="#069"
+      shadowDispersion={3}
+      shadowOpacity={0.3}
+      {...dimensions}
+    />
+  );
+}
+
+interface Dimensions {
+  size: {
+    height: number;
+    width: number;
+  };
+  expandedNodeHeight: number;
+  expandedNodeWidth: number;
+}
+
+function getDimensions(
+  tree: StudyEntity,
+  orientation: Orientation
+): Dimensions {
+  switch (orientation) {
+    case 'horizontal': {
+      const treeWidth = getTreeWidth(tree);
+      const treeHeight = getTreeHeight(tree);
+      const expandedNodeHeight = 30;
+      const expandedNodeWidth = 200;
+      const height = treeWidth * (expandedNodeHeight + expandedNodeHeight / 2);
+      const width = treeHeight * (expandedNodeWidth + expandedNodeWidth / 3);
+      return {
+        size: {
+          height,
+          width,
+        },
+        expandedNodeHeight,
+        expandedNodeWidth,
+      };
+    }
+    case 'vertical': {
+      const treeWidth = getTreeWidth(tree);
+      const treeHeight = getTreeHeight(tree);
+      const expandedNodeHeight = 30;
+      const expandedNodeWidth = 200;
+      const height = treeHeight * (expandedNodeHeight + expandedNodeHeight / 2);
+      const width = treeWidth * (expandedNodeWidth + expandedNodeWidth / 3);
+      return {
+        size: {
+          height,
+          width,
+        },
+        expandedNodeHeight,
+        expandedNodeWidth,
+      };
+    }
+    default: {
+      throw new Error('No defaults allowed!');
+    }
+  }
+}
+
+function getTreeHeight(tree: StudyEntity): number {
+  if (tree.children == null || tree.children.length === 0) return 1;
+  const heights = tree.children.map(getTreeHeight);
+  return 1 + Math.max(...heights);
+}
+
+function getTreeWidth(tree: StudyEntity): number {
+  return reduce(
+    (width, node) =>
+      node.children == null || node.children.length === 0
+        ? width
+        : node.children.length - 1 + width,
+    1,
+    preorder(tree, (node) => node.children ?? [])
   );
 }
