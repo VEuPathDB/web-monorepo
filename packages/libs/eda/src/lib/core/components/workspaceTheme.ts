14a2d14aaa77f55ed8b94d3b62b0f39ebb0f134a
Dave Falke <dmfalke@gmail.com>
2021-04-17 11:16:01 -0400

    Initial attempt to expand use of mui theming

---
+++ packages/libs/eda/src/lib/core/components/workspaceTheme.ts
@@ -0,0 +1,16 @@
+import { ThemeOptions } from '@material-ui/core';
+
+export const workspaceTheme: ThemeOptions = {
+  typography: {
+    fontSize: 12,
+    fontFamily: 'Roboto',
+  },
+  palette: {
+    primary: {
+      light: '#eef1f2',
+      main: '#069',
+      dark: '#084e71',
+      contrastText: '#fff',
+    },
+  },
+};
