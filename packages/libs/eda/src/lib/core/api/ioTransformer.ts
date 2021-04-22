cf6886142dd6f32b16e14f65e0bf2396d65ad97c
Connor Howington <connor.howington@gmail.com>
2021-04-22 15:05:22 -0400

    Improve io-ts decoder error messages

--- packages/libs/eda/src/lib/core/api/ioTransformer.ts
+++ packages/libs/eda/src/lib/core/api/ioTransformer.ts
@@ -6,7 +6,18 @@ export function ioTransformer<I, A>(decoder: Decoder<I, A>) {
   return async function decodeOrThrow(value: I): Promise<A> {
     const result = decoder.decode(value);
     if (isLeft(result)) {
-      const message = PathReporter.report(result).join('\n');
+      const message = result.left.reduce((message, error) => {
+        const context = Array.from(error.context)
+          .reverse()
+          .map(
+            (context) =>
+              `${context.key || '[root]'} (type: ${context.type.name})`
+          )
+          .join('\n  of ');
+        return (message += `Invalid value ${JSON.stringify(
+          error.value
+        )} supplied to ${context}\n\n`);
+      }, '');
       throw new Error(message);
     }
     return result.right;
