62fccf1e483582ce0f4b51133bb893e852e0c7e6
Connor Howington <connor.howington@gmail.com>
2021-04-27 11:58:58 -0400

    Calculate mosaic column widths from data

--- packages/libs/components/src/plots/MosaicPlot.tsx
+++ packages/libs/components/src/plots/MosaicPlot.tsx
@@ -1,48 +1,64 @@
 import React from 'react';
 import PlotlyPlot, { PlotProps, ModebarDefault } from './PlotlyPlot';
+import _ from 'lodash';
 
-interface Props extends PlotProps {
-  // N columns/exposure, M rows/outcome
+export interface Props extends Omit<PlotProps, 'width' | 'height'> {
+  // N columns, M rows
   data: Array<Array<number>>; // MxN (M = outerLength; N = innerLength)
-  exposureValues: Array<string>; // N
-  outcomeValues: Array<string>; // M
-  exposureLabel: string;
-  outcomeLabel: string;
-  widths: Array<number>; // N
+  xValues: Array<string>; // N
+  yValues: Array<string>; // M
+  xLabel: string;
+  yLabel: string;
   colors?: Array<string>; // M
   showLegend?: boolean;
   showModebar?: boolean;
+  width?: number | string;
+  height?: number | string;
 }
 
 export default function MosaicPlot(props: Props) {
-  const column_centers = props.widths.map((width, i) => {
+  const column_widths = _.unzip(props.data).map((arr) => _.sum(arr));
+  const sum_column_widths = _.sum(column_widths);
+
+  const column_centers = column_widths.map((width, i) => {
     // Sum of the widths of previous columns
-    let column_start = props.widths.slice(0, i).reduce((a, b) => a + b, 0);
+    let column_start = _.sum(column_widths.slice(0, i));
     return column_start + width / 2;
   });
 
   const layout = {
     xaxis: {
-      title: props.exposureLabel,
+      title: props.xLabel,
       // Must expliticly define range for it to work consistently
-      range: [0, props.widths.reduce((a, b) => a + b, 0)] as number[],
+      range: [0, sum_column_widths] as number[],
       tickvals: column_centers,
-      ticktext: props.exposureValues,
+      ticktext: props.xValues,
+      // Shows x-axis counts similar to y-axis hover labels
+      // ticktext: props.xValues.map((value, i) => `${value}: ${(column_widths[i]/sum_column_widths*100).toFixed(1)}% (${column_widths[i]})`),
     },
     yaxis: {
-      title: props.outcomeLabel,
+      title: props.yLabel + ' (%)',
     },
     barmode: 'stack',
+    barnorm: 'percent',
   } as const;
 
   const data = props.data
     .map(
-      (vals, i) =>
+      (counts, i) =>
         ({
           x: column_centers,
-          y: vals,
-          name: props.outcomeValues[i],
-          width: props.widths,
+          y: counts,
+          name: props.yValues[i],
+          hoverinfo: 'text',
+          hovertext: counts.map(
+            (count, j) =>
+              `${props.yValues[i]}: ${(
+                (count / column_widths[j]) *
+                100
+              ).toFixed(1)}% (${count})`
+          ),
+          width: column_widths,
           type: 'bar',
           marker: {
             line: {
@@ -59,9 +75,8 @@ export default function MosaicPlot(props: Props) {
   return (
     <PlotlyPlot
       data={data}
+      style={{ width: props.width, height: props.height }}
       layout={Object.assign(layout, {
-        width: props.width,
-        height: props.height,
         margin: props.margin,
         showlegend: props.showLegend,
       })}
--- packages/libs/components/src/stories/plots/MosaicPlot.stories.tsx
+++ packages/libs/components/src/stories/plots/MosaicPlot.stories.tsx
@@ -10,13 +10,12 @@ export const TwoByTwo = () => (
   <MosaicPlot
     data={[
       [40, 15],
-      [10, 35],
+      [10, 25],
     ]}
-    exposureValues={['Men', 'Women']}
-    outcomeValues={['Died', 'Survived']}
-    exposureLabel={'Sex'}
-    outcomeLabel={'Status'}
-    widths={[40, 10]}
+    xValues={['Men', 'Women']}
+    yValues={['Died', 'Survived']}
+    xLabel={'Sex'}
+    yLabel={'Status'}
     colors={['orange', 'blue']}
   />
 );
@@ -24,14 +23,13 @@ export const TwoByTwo = () => (
 export const TwoByThree = () => (
   <MosaicPlot
     data={[
-      [40, 15, 30],
-      [10, 35, 20],
+      [45, 15, 20],
+      [10, 45, 20],
     ]}
-    exposureValues={['Rabbit', 'Cat', 'Dog']}
-    outcomeValues={['Positive', 'Negative']}
-    exposureLabel={'Animal'}
-    outcomeLabel={'Rabies'}
-    widths={[40, 10, 25]}
+    xValues={['Rabbit', 'Cat', 'Dog']}
+    yValues={['Positive', 'Negative']}
+    xLabel={'Animal'}
+    yLabel={'Rabies'}
     width={400}
     height={300}
     showLegend={false}
@@ -42,15 +40,14 @@ export const TwoByThree = () => (
 export const FourByThree = () => (
   <MosaicPlot
     data={[
-      [50, 15, 35],
-      [10, 40, 50],
-      [20, 15, 5],
-      [20, 30, 10],
+      [52, 15, 35],
+      [15, 40, 50],
+      [20, 15, 7],
+      [22, 30, 10],
     ]}
-    exposureValues={['Mercury', 'Venus', 'Mars']}
-    outcomeValues={['Nitrogen', 'Oxygen', 'Hydrogen', 'Other']}
-    exposureLabel={'Planet'}
-    outcomeLabel={'Atmospheric Makeup (%)'}
-    widths={[10, 40, 50]}
+    xValues={['Mercury', 'Venus', 'Mars']}
+    yValues={['Nitrogen', 'Oxygen', 'Hydrogen', 'Other']}
+    xLabel={'Planet'}
+    yLabel={'Atmospheric Makeup'}
   />
 );
