207d8780ed4f88be91e4248e1ea5bb85ccd1be5f
Dae Kun (DK) Kwon <dkwon@nd.edu>
2021-05-19 12:23:54 -0400

    implementing Barplot component with storybook controls

---
+++ packages/libs/components/src/plots/Barplot.tsx
@@ -0,0 +1,177 @@
+import { useEffect, useMemo, useState } from 'react';
+import { PlotParams } from 'react-plotly.js';
+
+// Definitions
+import { DARK_GRAY } from '../constants/colors';
+
+// Components
+import PlotlyPlot, { PlotProps } from './PlotlyPlot';
+import { Layout } from 'plotly.js';
+
+// in this example, the main variable is 'country'
+export interface BarplotProps extends PlotProps {
+  // probably should "extends PlotProps" like in Mosaic plot)
+  /** the data series - e.g. one per overlay variable value (sex: 'male', 'female') */
+  // data: Array<BarplotDataSeries>;
+  data: any;
+  /** The width of the plot in pixels (if number), or CSS length. */
+  // width?: number | string;
+  /** The height of the plot in pixels (if number), or CSS length. */
+  // height?: number | string;
+  /** The orientation of the plot. Defaults to `vertical`  (--> general PlotProp?) */
+  orientation: 'vertical' | 'horizontal';
+  /** How bars are displayed when there are multiple series. */
+  barLayout: 'overlay' | 'stack' | 'group';
+  /** Opacity of bars. Range is a decimal between 0 and 1.   */
+  opacity?: number;
+  /** Title of plot. (should probably move to general PlotProps) */
+  title?: string; // compulsory, unlike Histogram
+  /** Label for independent axis. e.g. 'Country' */ independentAxisLabel?: string;
+  /** Label for dependent axis. Defaults to 'Count' */
+  dependentAxisLabel?: string;
+  /** Show value for each bar */
+  showBarValues?: boolean;
+  /** Should plot legend be displayed? */
+  displayLegend?: boolean;
+  /** Should plotting library controls be displayed? Ex. Plot.ly */
+  displayLibraryControls?: boolean;
+  /** Fill color of the title, axes labels, tick marks, and legend.
+   * Defaults to DARK_GRAY. Note that textColor can be overridden
+   * for the legend if `legendOptions` is provided. */
+  textColor?: string;
+  /** Color of the gridlines. Use Plotly defaults if not specified. */
+  gridColor?: string;
+  /** Control of background color. Defaults to transparent.  */
+  backgroundColor?: string;
+}
+
+export type BarplotDataSeries = {
+  series: [
+    {
+      /** The name of the data. e.g. 'male' or 'female' */
+      name: string;
+      /** The color of the data. Optional. */
+      color?: string;
+      /** Array of labels */
+      // counts: Array<{
+      //   value: number;
+      //   label: string; // e.g. India, Pakistan, Mali
+      // }>;
+      value: number;
+      label: string; // e.g. India, Pakistan, Mali
+    }
+  ];
+};
+
+/** A Plotly-based Barplot component. */
+export default function Barplot({
+  data,
+  width,
+  height,
+  orientation = 'vertical',
+  title,
+  //DKDK do not set default values for axisLabels
+  independentAxisLabel,
+  dependentAxisLabel,
+  showBarValues = false,
+  textColor = DARK_GRAY,
+  gridColor,
+  opacity = 1,
+  barLayout = 'overlay',
+  backgroundColor = 'transparent',
+  displayLegend = true,
+  displayLibraryControls = true,
+  ...props
+}: BarplotProps) {
+  // Transform `data` into a Plot.ly friendly format.
+  const plotlyFriendlyData: PlotParams['data'] = useMemo(
+    () =>
+      data.series.map((el: any) => {
+        let calculatedOpacity = 1;
+
+        //DKDK set opacity less than unity for overlay & multiple data
+        if (barLayout === 'overlay' && data.series.length > 1)
+          calculatedOpacity = opacity * 0.75;
+
+        //DKDK check data exist
+        if (el.label && el.value) {
+          return {
+            x: orientation === 'vertical' ? el.label : el.value,
+            y: orientation === 'vertical' ? el.value : el.label,
+            name: el.name, //DKDK legend name
+            orientation: orientation === 'vertical' ? 'v' : 'h',
+            opacity: calculatedOpacity,
+            type: 'bar',
+          };
+        } else {
+          return {};
+        }
+      }),
+    [data, barLayout, orientation]
+  );
+
+  // Quirk of Plotly library. If you don't do this, the
+  // plot will not refresh on barLayout changes.
+  const [revision, setRevision] = useState(0);
+  useEffect(() => {
+    setRevision(revision + 1);
+  }, [barLayout]);
+
+  const independentAxisLayout: Layout['xaxis'] | Layout['yaxis'] = {
+    // type: data?.valueType === 'date' ? 'date' : 'linear',
+    automargin: true,
+    title: {
+      text: independentAxisLabel ? independentAxisLabel : '',
+      font: {
+        family: 'Arial, Helvetica, sans-serif',
+        size: 14,
+      },
+    },
+  };
+
+  const dependentAxisLayout: Layout['yaxis'] | Layout['xaxis'] = {
+    automargin: true,
+    title: {
+      text: dependentAxisLabel ? dependentAxisLabel : '',
+      font: {
+        family: 'Arial, Helvetica, sans-serif',
+        size: 14,
+      },
+    },
+    color: textColor,
+    gridcolor: gridColor,
+  };
+
+  const layout: Partial<Layout> = {
+    xaxis:
+      orientation === 'vertical' ? independentAxisLayout : dependentAxisLayout,
+    yaxis:
+      orientation === 'vertical' ? dependentAxisLayout : independentAxisLayout,
+    title: {
+      text: title ? title : '',
+    },
+    barmode: barLayout,
+  };
+
+  return (
+    <PlotlyPlot
+      data={plotlyFriendlyData}
+      revision={revision}
+      layout={{
+        ...layout,
+        ...{
+          autosize: true,
+          width: width,
+          height: height,
+          margin: props.margin ? props.margin : undefined,
+          showlegend: displayLegend,
+          selectdirection: orientation === 'vertical' ? 'h' : 'v',
+        },
+      }}
+      config={{
+        displayModeBar: displayLibraryControls ? 'hover' : false,
+        staticPlot: props.staticPlot ? props.staticPlot : false,
+      }}
+    />
+  );
+}
---
+++ packages/libs/components/src/stories/plots/Barplot.stories.tsx
@@ -0,0 +1,71 @@
+import { useMemo } from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Barplot, { BarplotProps, BarplotDataSeries } from '../../plots/Barplot';
+// import usePlotControls from '../../hooks/usePlotControls';
+// import HistogramControls from '../../components/plotControls/HistogramControls';
+// import { HistogramData } from '../../types/plots';
+
+export default {
+  title: 'Plots/Barplot',
+  component: Barplot,
+} as Meta;
+
+const dataSet = {
+  series: [
+    {
+      label: ['dogs', 'cats', 'monkeys'],
+      value: [20, 14, 23],
+      name: 'Yes',
+    },
+    {
+      label: ['dogs', 'cats', 'monkeys'],
+      value: [12, 18, 29],
+      name: 'No',
+    },
+  ],
+};
+
+//DKDK set initial props
+const plotWidth = 1000;
+const plotHeight = 600;
+// let plotWidth = 350;
+// let plotHeight = 250;
+const plotTitle = 'Barplot';
+const orientation = 'vertical';
+const barLayout = 'overlay';
+
+export const Basic = () => {
+  return (
+    <Barplot
+      data={dataSet}
+      width={plotWidth}
+      height={plotHeight}
+      // title={plotTitle}
+      orientation={orientation}
+      //DKDK check this option later
+      barLayout={barLayout}
+      independentAxisLabel={'Independent axis name'}
+      dependentAxisLabel={'Dependent axis name'}
+      staticPlot={false}
+      displayLegend={true}
+      displayLibraryControls={true}
+      // margin={{l: 50, r: 10, b: 20, t: 10}}
+    />
+  );
+};
+
+//DKDK adding storybook control
+const Template = (args: any) => <Barplot {...args} />;
+export const WithStorybookControl: Story<any> = Template.bind({});
+
+//DKDK set default values for args that use default storybook control
+WithStorybookControl.args = {
+  data: dataSet,
+  width: plotWidth,
+  height: plotHeight,
+  orientation: orientation,
+  barLayout: barLayout,
+  independentAxisLabel: 'Independent axis name',
+  dependentAxisLabel: 'Dependent axis name',
+};
