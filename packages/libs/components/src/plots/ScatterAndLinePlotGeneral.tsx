83d8cf28f0a35f7d55c346dfd4ead7f707750f8e
Dae Kun (DK) Kwon <dkwon@nd.edu>
2021-05-25 13:00:31 -0400

    scatterplot props descriptions

--- packages/libs/components/src/plots/ScatterAndLinePlotGeneral.tsx
+++ packages/libs/components/src/plots/ScatterAndLinePlotGeneral.tsx
@@ -1,38 +1,62 @@
+/**
+ * This component handles several plots such as marker, line, confidence interval,
+ * density, and combinations of plots like marker + line + confidence interval
+ */
 import PlotlyPlot, { PlotProps } from './PlotlyPlot';
 // import Layout for typing layout, especially with sliders
 import { Layout } from 'plotly.js';
 
-// change interface a bit more: this could avoid error on data type
-// export interface ScatterplotProps<T extends keyof PlotData> extends PlotProps {
 export interface ScatterplotProps extends PlotProps {
-  // data: any,
+  /** Data for the scatter plotplot */
   data: Array<{
+    /** x/y data */
     x: number[] | Date[];
     y: number[] | Date[];
+    /** legend text */
     name?: string;
-    mode?: string;
-    // type?: string,
+    /** plot style */
+    mode?: 'markers' | 'lines' | 'lines+markers';
+    /** plot with marker: scatter plot with raw data */
     marker?: {
+      /** marker color */
       color?: string;
+      /** marker size: no unit */
       size?: number;
+      /** marker's perimeter setting */
+      line?: {
+        /** marker's perimeter color */
+        color?: string;
+        /** marker's perimeter color: no unit */
+        width?: number;
+      };
     };
+    /** plot with marker: scatter plot with smoothedMean and bestfitline; line and density plots */
     line?: {
+      /** line color */
       color?: string;
-      shape?: string;
+      /** line style */
+      shape?: 'spline' | 'linear';
+      /** line width: no unit */
       width?: number;
     };
-    fill?: string;
+    /** filling plots: tozerox - scatter plot's confidence interval; toself - density plot */
+    fill?: 'tozerox' | 'toself';
+    /** filling plots: color */
     fillcolor?: string;
   }>;
+  /** x-axis label */
   xLabel?: string;
+  /** y-axis label */
   yLabel?: string;
-  // plotTitle?: string;
+  /** plot title */
   title?: string;
-  // involving CI, x & y range may need to be set
+  /** x-axis range: required for confidence interval */
   xRange?: number[] | Date[];
+  /** y-axis range: required for confidence interval */
   yRange?: number[] | Date[];
-  // add enable/disable legend and built-in controls
+  /** show plot legend */
   displayLegend?: boolean;
+  /** show plotly's built-in controls */
   displayLibraryControls?: boolean;
 }
 
