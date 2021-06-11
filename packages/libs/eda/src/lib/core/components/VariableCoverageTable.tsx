3ef36c798c46b29091e2acf9bb31b1c21edfd7ad
Jamie Long <jtlong3rd@gmail.com>
2021-06-10 21:41:22 -0400

    Introduce useVariableCoverageTableRows hook

--- packages/libs/eda/src/lib/core/components/VariableCoverageTable.tsx
+++ packages/libs/eda/src/lib/core/components/VariableCoverageTable.tsx
@@ -1,26 +1,26 @@
 import { CompleteCasesTable } from '../api/data-api';
 import { Variable } from '../types/variable';
 
-interface Props {
+export interface Props {
   completeCases?: CompleteCasesTable;
   filteredEntityCounts?: Record<string, number>;
   variables: VariableSpec[];
 }
 
-type VariableSpec =
+export type VariableSpec =
   | { selected: true; spec: SelectedVariableSpec }
   | { selected: false; spec: UnselectedVariableSpec };
 
-interface UnselectedVariableSpec {
+export interface UnselectedVariableSpec {
   role: VariableRole;
 }
 
-interface SelectedVariableSpec extends UnselectedVariableSpec, Variable {
+export interface SelectedVariableSpec extends UnselectedVariableSpec, Variable {
   display: VariableDisplay;
 }
 
 export interface VariableCoverageTableRow {
-  type: VariableRole;
+  role: VariableRole;
   display?: VariableDisplay;
   /* count of entities which have the variable */
   complete?: number;
@@ -31,10 +31,10 @@ export interface VariableCoverageTableRow {
 }
 
 /* A short description of the variable's role; e.g., "x-axis", "y-axis", "overlay" */
-type VariableRole = string;
+export type VariableRole = string;
 
 /* A descriptive string for the variable; e.g., "floor material" */
-type VariableDisplay = string;
+export type VariableDisplay = string;
 
 export function VariableCoverageTable(_: Props) {
   return null;
--- packages/libs/eda/src/lib/core/hooks/variableCoverage.ts
+++ packages/libs/eda/src/lib/core/hooks/variableCoverage.ts
@@ -1,13 +1,75 @@
 import { useMemo } from 'react';
 
 import { CompleteCasesTable } from '../api/data-api';
+import {
+  VariableCoverageTableRow,
+  VariableSpec,
+} from '../components/VariableCoverageTable';
+
+/**
+ * Returns an array of data to be rendered in a VariableCoverageTable
+ * @param completeCases
+ * @param filteredEntityCounts
+ * @param variables
+ * @returns A VariableCoverageTableRow[]
+ */
+export function useVariableCoverageTableRows(
+  completeCases: CompleteCasesTable,
+  filteredEntityCounts: Record<string, number> = {},
+  variables: VariableSpec[]
+): VariableCoverageTableRow[] {
+  const completeCasesMap = useCompleteCasesMap(completeCases);
+
+  return useMemo(
+    () =>
+      variables.map((variable) => {
+        if (!variable.selected) {
+          return {
+            role: variable.spec.role,
+          };
+        }
+
+        const variableCompleteCases =
+          completeCasesMap[
+            `${variable.spec.entityId}.${variable.spec.variableId}`
+          ];
+
+        if (variableCompleteCases == null) {
+          return {
+            role: variable.spec.role,
+            display: variable.spec.display,
+          };
+        }
+
+        const baseRowWithCounts = {
+          role: variable.spec.role,
+          display: variable.spec.display,
+          complete: variableCompleteCases,
+        };
+
+        const variableFilteredEntityCount =
+          filteredEntityCounts[variable.spec.entityId];
+
+        return variableFilteredEntityCount == null ||
+          baseRowWithCounts.complete > variableFilteredEntityCount
+          ? baseRowWithCounts
+          : {
+              ...baseRowWithCounts,
+              total: variableFilteredEntityCount,
+              incomplete:
+                variableFilteredEntityCount - baseRowWithCounts.complete,
+            };
+      }),
+    [completeCasesMap, filteredEntityCounts, variables]
+  );
+}
 
 /**
  * Transforms a CompleteCasesTable array into a map
  * @param completeCases
  * @returns A map with key-value pairs of the form {entityId}.{variableId} -> {completeCaseCount}
  */
-export function useCompleteCases(
+export function useCompleteCasesMap(
   completeCases: CompleteCasesTable = []
 ): Record<string, number> {
   return useMemo(
