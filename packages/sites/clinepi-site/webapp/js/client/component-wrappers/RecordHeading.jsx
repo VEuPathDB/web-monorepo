8e298b61fa1249969996020a15941360fc521281
Dave Falke <dmfalke@gmail.com>
2021-07-23 15:58:14 -0400

    Show "new analysis" button on study page and hide basket/favorite buttons

--- packages/sites/clinepi-site/webapp/js/client/component-wrappers/RecordHeading.jsx
+++ packages/sites/clinepi-site/webapp/js/client/component-wrappers/RecordHeading.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useEda } from '@veupathdb/web-common/lib/config';
 
 import { withPermissions } from '@veupathdb/web-common/lib/components/Permissions';
 import StudyRecordHeading from '@veupathdb/web-common/lib/component-wrappers/StudyRecordHeading';
+import { EdaRecordHeading } from '@veupathdb/web-common/lib/component-wrappers/EdaRecordHeading';
 
 const ClinEpiStudyRecordHeading = withPermissions(StudyRecordHeading);
+const ClinEpiEdaRecordHeading = withPermissions(EdaRecordHeading);
 
 export default RecordHeading => props => (
   props.recordClass.urlSegment === 'dataset'
-    ? <ClinEpiStudyRecordHeading {...props} DefaultComponent={RecordHeading} showSearches showDownload />
+    ? useEda
+      ? <ClinEpiEdaRecordHeading {...props} DefaultComponent={RecordHeading}/>
+      : <ClinEpiStudyRecordHeading {...props} DefaultComponent={RecordHeading} showSearches showDownload />
     : <RecordHeading {...props} />
 )
