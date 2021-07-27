4a8202e65fe4ec1920980e65220df089f6f78e19
Dave Falke <dmfalke@gmail.com>
2021-07-27 16:36:04 -0400

    Move EDA integration into ebrc client

--- packages/sites/clinepi-site/webapp/js/client/routes.jsx
+++ packages/sites/clinepi-site/webapp/js/client/routes.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import AccessRequestController from './controllers/AccessRequestController';
-import { WorkspaceRouter } from '@veupathdb/eda/lib/workspace/WorkspaceRouter';
-
-const edaServiceUrl = '/eda-data';
 
 export const wrapRoutes = ebrcRoutes => { 
   return [
@@ -10,15 +7,6 @@ export const wrapRoutes = ebrcRoutes => {
     {
       path: '/request-access/:datasetId',
       component: props => <AccessRequestController {...props.match.params}/>
-    },
-
-    {
-      path: '/eda',
-      exact: false,
-      component: () => <WorkspaceRouter
-        dataServiceUrl={edaServiceUrl}
-        subsettingServiceUrl={edaServiceUrl}
-      />
     },
 
     ...ebrcRoutes
