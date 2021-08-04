0055467292a13d6a062c1df77377aea60d452fb3
Dave Falke <dmfalke@gmail.com>
2021-08-03 23:35:31 -0400

    use new props in StudyRecordHeading

--- packages/sites/clinepi-site/webapp/js/client/component-wrappers/RecordHeading.jsx
+++ packages/sites/clinepi-site/webapp/js/client/component-wrappers/RecordHeading.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
 import { useEda } from '@veupathdb/web-common/lib/config';
 
 import { withPermissions } from '@veupathdb/web-common/lib/components/Permissions';
 import StudyRecordHeading from '@veupathdb/web-common/lib/component-wrappers/StudyRecordHeading';
-import { EdaRecordHeading } from '@veupathdb/web-common/lib/component-wrappers/EdaRecordHeading';
 
 const ClinEpiStudyRecordHeading = withPermissions(StudyRecordHeading);
-const ClinEpiEdaRecordHeading = withPermissions(EdaRecordHeading);
 
-export default RecordHeading => props => (
-  props.recordClass.urlSegment === 'dataset'
-    ? useEda
-      ? <ClinEpiEdaRecordHeading {...props} DefaultComponent={RecordHeading}/>
-      : <ClinEpiStudyRecordHeading {...props} DefaultComponent={RecordHeading} showSearches showDownload />
-    : <RecordHeading {...props} />
-)
+export default RecordHeading => props => {
+  const location = useLocation();
+  const isRecordRoute = location.pathname.startsWith('/record');
+  return (
+    props.recordClass.urlSegment === 'dataset'
+      ? <ClinEpiStudyRecordHeading
+          {...props}
+          DefaultComponent={RecordHeading}
+          showSearches={!useEda}
+          showDownload={!useEda}
+          showAnalyzeLink={useEda && isRecordRoute}
+        />
+      : <RecordHeading {...props} />
+  )
+}
