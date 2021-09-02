5fd4684f3a91f5e3c8b704f7a86ea853139f17f2
Michael <bigfatpanda@nd.edu>
2021-09-02 16:31:35 -0400

    Fix import bug.

--- packages/libs/coreui/src/index.ts
+++ packages/libs/coreui/src/index.ts
@@ -1,8 +1,8 @@
 export { default as H1 } from './components/headers/H1';
 export { default as H2 } from './components/headers/H2';
 export { default as H3 } from './components/headers/H3';
-export { default as H4 } from './components/headers/H3';
-export { default as H5 } from './components/headers/H3';
+export { default as H4 } from './components/headers/H4';
+export { default as H5 } from './components/headers/H5';
 
 export { default as SwissArmyButton } from './components/buttons/SwissArmyButton';
 
