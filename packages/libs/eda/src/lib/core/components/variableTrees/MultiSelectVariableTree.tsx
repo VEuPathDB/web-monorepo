b70935b88cd589d3067e042bae098dff391e7bc6
Michael <bigfatpanda@nd.edu>
2021-09-09 12:22:24 -0400

    Extract common functionality from components.

--- packages/libs/eda/src/lib/core/components/variableTrees/MultiSelectVariableTree.tsx
+++ packages/libs/eda/src/lib/core/components/variableTrees/MultiSelectVariableTree.tsx
@@ -8,9 +8,15 @@ import { cx } from '../../../workspace/Utils';
 import { StudyEntity } from '../../types/study';
 import { VariableDescriptor } from '../../types/variable';
 import { edaVariableToWdkField } from '../../utils/wdk-filter-param-adapter';
-import VariableList from '../VariableList';
+import VariableList from './VariableList';
 import './VariableTree.scss';
 import { useStudyEntities } from '../../hooks/study';
+import {
+  useFieldTree,
+  useFlattenedFields,
+  useFlattenFieldsByTerm,
+  useValuesMap,
+} from './hooks';
 
 export interface MultiSelectVariableTreeProps {
   /** The root (lowest level) */
@@ -38,113 +44,10 @@ export default function MultiSelectVariableTree({
   onChange,
 }: MultiSelectVariableTreeProps) {
   const entities = useStudyEntities(rootEntity);
-
-  /**
-   * Generates a flattened representation of the possible values for
-   * all entity/variable combinations.
-   *
-   * An example entry would look something like this.
-   * "PCO_0000024/ENVO_00000004": "Bangladesh India Kenya Mali Mozambique Pakistan The Gambia",
-   *
-   * Object keys are `entityID/variableID` and object values are the possible values for that
-   * entity/variable combination.
-   *
-   * This is used by the search functionality of FieldList.
-   * It should be a map from field term to string.
-   * In WDK searches, this is a concatenated string of values
-   * for categorical-type variables.
-   *
-   */
-
-  const valuesMap = useMemo(() => {
-    const valuesMap: Record<string, string> = {};
-    for (const entity of entities) {
-      for (const variable of entity.variables) {
-        if (variable.type !== 'category' && variable.vocabulary) {
-          valuesMap[`${entity.id}/${variable.id}`] = variable.vocabulary.join(
-            ' '
-          );
-        }
-      }
-    }
-    return valuesMap;
-  }, [entities]);
-
-  const fields = useMemo(() => {
-    return entities.flatMap((entity) => {
-      // Create a Set of variableId so we can lookup parentIds
-      const variableIds = new Set(entity.variables.map((v) => v.id));
-      return [
-        // Create a non-filterable field for the entity.
-        // Note that we're prefixing the term. This avoids
-        // collisions with variables using the same term.
-        // This situation shouldn't happen in production,
-        // but there is nothing preventing it, so we need to
-        // handle the case.
-        {
-          term: `entity:${entity.id}`,
-          display: entity.displayName,
-        },
-        ...entity.variables
-          // Before handing off to edaVariableToWdkField, we will
-          // change the id of the variable to include the entityId.
-          // This will make the id unique across the tree and prevent
-          // duplication across entity subtrees.
-          .map((variable) => ({
-            ...variable,
-            id: `${entity.id}/${variable.id}`,
-            parentId:
-              // Use entity as parent under the following conditions:
-              // - if parentId is null
-              // - if the parentId is the same as the entityId
-              // - if the parentId does not exist in the provided list of variables
-              //
-              // Variables that meet any of these conditions will serve
-              // as the root nodes of the variable subtree, which will
-              // become the children of the entity node in the final tree.
-              variable.parentId == null ||
-              variable.parentId === entity.id ||
-              !variableIds.has(variable.parentId)
-                ? `entity:${entity.id}`
-                : `${entity.id}/${variable.parentId}`,
-          }))
-          .map(edaVariableToWdkField),
-      ];
-    });
-  }, [entities]);
-
-  const featuredFields = useMemo(() => {
-    return entities.flatMap((entity) =>
-      entity.variables
-        .filter(
-          (variable) => variable.type !== 'category' && variable.isFeatured
-        )
-        .map((variable) => ({
-          ...variable,
-          id: `${entity.id}/${variable.id}`,
-          displayName: `<span class="Entity">${entity.displayName}</span>: ${variable.displayName}`,
-        }))
-        .map(edaVariableToWdkField)
-    );
-  }, [entities]);
-
-  // Construct the fieldTree using the fields defined above.
-  const fieldTree = useMemo(() => {
-    const initialTree = getTree(fields, { hideSingleRoot: false });
-    const tree = pruneDescendantNodes(
-      (node) => node.field.type != null || node.children.length > 0,
-      initialTree
-    );
-    return tree;
-  }, [fields]);
-
-  // const disabledFields = useMemo(
-  //   () => disabledVariables?.map((v) => `${v.entityId}/${v.variableId}`),
-  //   [disabledVariables]
-  // );
-
-  // Used to lookup a field by entityId and variableId.
-  const fieldsByTerm = useMemo(() => keyBy(fields, (f) => f.term), [fields]);
+  const valuesMap = useValuesMap(entities);
+  const flattenedFields = useFlattenedFields(entities);
+  const fieldsByTerm = useFlattenFieldsByTerm(flattenedFields);
+  const fieldTree = useFieldTree(flattenedFields);
 
   const onActiveFieldChange = useCallback(
     (term?: string) => {
@@ -158,14 +61,8 @@ export default function MultiSelectVariableTree({
     [onChange]
   );
 
-  // Lookup activeField
-  // const activeField =
-  //   entityId && variableId
-  //     ? fieldsByTerm[`${entityId}/${variableId}`]
-  //     : undefined;
-
   return (
-    // TODO: @dmfalke This is strip down to the minimum temporary for MVP.
+    // TODO: @dmfalke This is striped down to the minimum temporarily for MVP.
     <VariableList
       onActiveFieldChange={onActiveFieldChange}
       // featuredFields={featuredFields}
--- packages/libs/eda/src/lib/core/components/variableTrees/VariableTree.tsx
+++ packages/libs/eda/src/lib/core/components/variableTrees/VariableTree.tsx
@@ -1,18 +1,20 @@
-import PopoverButton from '@veupathdb/components/lib/components/widgets/PopoverButton';
-import { Button } from '@material-ui/core';
-import { getTree } from '@veupathdb/wdk-client/lib/Components/AttributeFilter/AttributeFilterUtils';
-import { pruneDescendantNodes } from '@veupathdb/wdk-client/lib/Utils/TreeUtils';
 import { keyBy } from 'lodash';
-import { useCallback, useMemo, useState } from 'react';
-import { cx } from '../../../workspace/Utils';
+import { useCallback, useMemo } from 'react';
+
 import { StudyEntity } from '../../types/study';
 import { VariableDescriptor } from '../../types/variable';
-import { edaVariableToWdkField } from '../../utils/wdk-filter-param-adapter';
-import VariableList from '../VariableList';
+import VariableList from './VariableList';
 import './VariableTree.scss';
 import { useStudyEntities } from '../../hooks/study';
+import {
+  useValuesMap,
+  useFlattenedFields,
+  useFeaturedFields,
+  useFieldTree,
+  useFlattenFieldsByTerm,
+} from './hooks';
 
-export interface Props {
+export interface VariableTreeProps {
   rootEntity: StudyEntity;
   starredVariables?: string[];
   toggleStarredVariable: (targetVariableId: string) => void;
@@ -24,113 +26,29 @@ export interface Props {
   hideDisabledFields?: boolean;
   setHideDisabledFields?: (hide: boolean) => void;
 }
-export function VariableTree(props: Props) {
-  const {
-    rootEntity,
-    disabledVariables,
-    starredVariables,
-    toggleStarredVariable,
-    entityId,
-    variableId,
-    onChange,
-    hideDisabledFields = false,
-    setHideDisabledFields = () => {},
-  } = props;
+
+export default function VariableTree({
+  rootEntity,
+  disabledVariables,
+  starredVariables,
+  toggleStarredVariable,
+  entityId,
+  variableId,
+  onChange,
+  hideDisabledFields = false,
+  setHideDisabledFields = () => {},
+}: VariableTreeProps) {
   const entities = useStudyEntities(rootEntity);
-
-  // This is used by the search functionality of FieldList.
-  // It should be a map from field term to string.
-  // In WDK searches, this is a concatenated string of values
-  // for categorical-type variables.
-  const valuesMap = useMemo(() => {
-    const valuesMap: Record<string, string> = {};
-    for (const entity of entities) {
-      for (const variable of entity.variables) {
-        if (variable.type !== 'category' && variable.vocabulary) {
-          valuesMap[`${entity.id}/${variable.id}`] = variable.vocabulary.join(
-            ' '
-          );
-        }
-      }
-    }
-    return valuesMap;
-  }, [entities]);
-
-  const fields = useMemo(() => {
-    return entities.flatMap((entity) => {
-      // Create a Set of variableId so we can lookup parentIds
-      const variableIds = new Set(entity.variables.map((v) => v.id));
-      return [
-        // Create a non-filterable field for the entity.
-        // Note that we're prefixing the term. This avoids
-        // collisions with variables using the same term.
-        // This situation shouldn't happen in production,
-        // but there is nothing preventing it, so we need to
-        // handle the case.
-        {
-          term: `entity:${entity.id}`,
-          display: entity.displayName,
-        },
-        ...entity.variables
-          // Before handing off to edaVariableToWdkField, we will
-          // change the id of the variable to include the entityId.
-          // This will make the id unique across the tree and prevent
-          // duplication across entity subtrees.
-          .map((variable) => ({
-            ...variable,
-            id: `${entity.id}/${variable.id}`,
-            parentId:
-              // Use entity as parent under the following conditions:
-              // - if parentId is null
-              // - if the parentId is the same as the entityId
-              // - if the parentId does not exist in the provided list of variables
-              //
-              // Variables that meet any of these conditions will serve
-              // as the root nodes of the variable subtree, which will
-              // become the children of the entity node in the final tree.
-              variable.parentId == null ||
-              variable.parentId === entity.id ||
-              !variableIds.has(variable.parentId)
-                ? `entity:${entity.id}`
-                : `${entity.id}/${variable.parentId}`,
-          }))
-          .map(edaVariableToWdkField),
-      ];
-    });
-  }, [entities]);
-
-  const featuredFields = useMemo(() => {
-    return entities.flatMap((entity) =>
-      entity.variables
-        .filter(
-          (variable) => variable.type !== 'category' && variable.isFeatured
-        )
-        .map((variable) => ({
-          ...variable,
-          id: `${entity.id}/${variable.id}`,
-          displayName: `<span class="Entity">${entity.displayName}</span>: ${variable.displayName}`,
-        }))
-        .map(edaVariableToWdkField)
-    );
-  }, [entities]);
-
-  // Construct the fieldTree using the fields defined above.
-  const fieldTree = useMemo(() => {
-    const initialTree = getTree(fields, { hideSingleRoot: false });
-    const tree = pruneDescendantNodes(
-      (node) => node.field.type != null || node.children.length > 0,
-      initialTree
-    );
-    return tree;
-  }, [fields]);
+  const valuesMap = useValuesMap(entities);
+  const flattenedFields = useFlattenedFields(entities);
+  const fieldsByTerm = useFlattenFieldsByTerm(flattenedFields);
+  const featuredFields = useFeaturedFields(entities);
+  const fieldTree = useFieldTree(flattenedFields);
 
   const disabledFields = useMemo(
     () => disabledVariables?.map((v) => `${v.entityId}/${v.variableId}`),
     [disabledVariables]
   );
-
-  // Used to lookup a field by entityId and variableId.
-  const fieldsByTerm = useMemo(() => keyBy(fields, (f) => f.term), [fields]);
 
   const onActiveFieldChange = useCallback(
     (term?: string) => {
@@ -164,42 +82,5 @@ export function VariableTree(props: Props) {
       hideDisabledFields={hideDisabledFields}
       setHideDisabledFields={setHideDisabledFields}
     />
-  );
-}
-
-export function VariableTreeDropdown(props: Props) {
-  const { rootEntity, entityId, variableId, onChange } = props;
-  const [hideDisabledFields, setHideDisabledFields] = useState(false);
-  const entities = useStudyEntities(rootEntity);
-  const variable = entities
-    .find((e) => e.id === entityId)
-    ?.variables.find((v) => v.id === variableId);
-  const label = variable?.displayName ?? 'Select a variable';
-  return (
-    <div className={cx('-VariableTreeDropdown')}>
-      <PopoverButton label={label} key={`${entityId}/${variableId}`}>
-        {variable && (
-          <div style={{ textAlign: 'center', padding: '.75em 0.25em 0.25em' }}>
-            <Button
-              type="button"
-              style={{ width: '90%' }}
-              variant="contained"
-              color="default"
-              size="small"
-              onClick={() => onChange()}
-            >
-              Clear selection
-            </Button>
-          </div>
-        )}
-        <div className={cx('-VariableTreeDropdownTreeContainer')}>
-          <VariableTree
-            {...props}
-            hideDisabledFields={hideDisabledFields}
-            setHideDisabledFields={setHideDisabledFields}
-          />
-        </div>
-      </PopoverButton>
-    </div>
   );
 }
