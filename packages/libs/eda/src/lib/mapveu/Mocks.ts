92ab745490795f0ee689604d79ac6e72c11fe1b6
Jamie Long <jtlong3rd@gmail.com>
2021-09-20 14:17:08 -0400

    Update mockAnalysisStore to mirror the user service API

--- packages/libs/eda/src/lib/mapveu/Mocks.ts
+++ packages/libs/eda/src/lib/mapveu/Mocks.ts
@@ -1,25 +1,39 @@
-import { Analysis, NewAnalysis } from '../core';
-import localforage from 'localforage';
-import { AnalysisClient } from '../core/api/analysis-api';
-import { isRight } from 'fp-ts/lib/Either';
+import { isLeft, isRight } from 'fp-ts/lib/Either';
 import { PathReporter } from 'io-ts/lib/PathReporter';
+import localforage from 'localforage';
+import {
+  Analysis,
+  NewAnalysis,
+  AnalysisClient,
+  AnalysisPreferences,
+  NewAnalysisClient,
+  AnalysisSummary,
+  AnalysisDetails,
+  NewAnalysisDetails,
+  AnalysisDescriptor,
+} from '../core';
+import { max } from 'lodash';
 
-const localStore = localforage.createInstance({
+const analysisStore = localforage.createInstance({
   name: 'mockAnalysisStore',
+});
+
+const preferenceStore = localforage.createInstance({
+  name: 'mockPreferencesStore',
 });
 
 export const mockAnalysisStore: AnalysisClient = {
   async getAnalyses() {
     const records: Analysis[] = [];
-    await localStore.iterate((value) => {
+    await analysisStore.iterate((value) => {
       records.push(value as Analysis);
     });
     return records;
   },
   async createAnalysis(newAnalysis: NewAnalysis) {
-    const id = String((await localStore.keys()).length + 1);
+    const id = String((await analysisStore.keys()).length + 1);
     const now = new Date().toISOString();
-    await localStore.setItem<Analysis>(id, {
+    await analysisStore.setItem<Analysis>(id, {
       ...newAnalysis,
       id,
       created: now,
@@ -28,7 +42,7 @@ export const mockAnalysisStore: AnalysisClient = {
     return { id };
   },
   async getAnalysis(id: string) {
-    const analysis = await localStore.getItem(id);
+    const analysis = await analysisStore.getItem(id);
     if (analysis) {
       const result = Analysis.decode(analysis);
       if (isRight(result)) return result.right;
@@ -38,9 +52,107 @@ export const mockAnalysisStore: AnalysisClient = {
   },
   async updateAnalysis(analysis: Analysis) {
     const now = new Date().toISOString();
-    await localStore.setItem(analysis.id, { ...analysis, modified: now });
+    await analysisStore.setItem(analysis.id, { ...analysis, modified: now });
   },
   async deleteAnalysis(id: string) {
-    await localStore.removeItem(id);
+    await analysisStore.removeItem(id);
   },
 } as AnalysisClient;
+
+export const newMockAnalysisStore: NewAnalysisClient = {
+  async getPreferences() {
+    const prefs = await preferenceStore.getItem('preferences');
+    const result = AnalysisPreferences.decode(prefs);
+    if (isRight(result)) return result.right;
+    throw new Error(PathReporter.report(result).join('\n'));
+  },
+  async setPreferences(preferences: AnalysisPreferences) {
+    await preferenceStore.setItem<AnalysisPreferences>(
+      'preferences',
+      preferences
+    );
+  },
+  async getAnalyses() {
+    const records: AnalysisSummary[] = [];
+    await analysisStore.iterate((value) => {
+      const result = AnalysisDetails.decode(value);
+      if (isLeft(result)) {
+        throw new Error(PathReporter.report(result).join('\n'));
+      }
+      const { descriptor, ...analysisSummary } = result.right;
+      records.push(analysisSummary);
+    });
+    return records;
+  },
+  async createAnalysis(newAnalysis: NewAnalysisDetails) {
+    const usedIds = await analysisStore.keys();
+    const analysisId = String((max(usedIds.map((x) => Number(x))) ?? 0) + 1);
+    const now = new Date().toISOString();
+    await analysisStore.setItem<AnalysisDetails>(analysisId, {
+      ...newAnalysis,
+      ...computeSummaryCounts(newAnalysis.descriptor),
+      analysisId,
+      creationTime: now,
+      modificationTime: now,
+    });
+    return { analysisId };
+  },
+  async getAnalysis(id: string) {
+    const analysis = await analysisStore.getItem(id);
+    if (analysis) {
+      const result = AnalysisDetails.decode(analysis);
+      if (isRight(result)) return result.right;
+      throw new Error(PathReporter.report(result).join('\n'));
+    }
+    throw new Error(`Could not find analysis with id "${id}".`);
+  },
+  async updateAnalysis(
+    analysisId: string,
+    analysisPatch: Partial<NewAnalysisDetails>
+  ) {
+    const analysis = await analysisStore.getItem(analysisId);
+    if (analysis == null) {
+      throw new Error(
+        `Tried to update a nonexistent analysis with id "${analysisId}".`
+      );
+    }
+
+    const result = AnalysisDetails.decode(analysis);
+    if (isLeft(result)) {
+      throw new Error(PathReporter.report(result).join('\n'));
+    }
+
+    const decodedAnalysis = result.right;
+    const now = new Date().toISOString();
+    await analysisStore.setItem<AnalysisDetails>(analysisId, {
+      ...decodedAnalysis,
+      ...analysisPatch,
+      modificationTime: now,
+    });
+  },
+  async deleteAnalysis(id: string) {
+    await analysisStore.removeItem(id);
+  },
+  async deleteAnalyses(ids: Iterable<string>) {
+    const deletionPromises = Array.from(ids).map((id) =>
+      analysisStore.removeItem(id)
+    );
+
+    await Promise.allSettled(deletionPromises);
+  },
+} as NewAnalysisClient;
+
+function computeSummaryCounts(
+  descriptor: AnalysisDescriptor
+): Pick<
+  AnalysisSummary,
+  'numComputations' | 'numFilters' | 'numVisualizations'
+> {
+  return {
+    numFilters: descriptor.subset.descriptor.length,
+    numComputations: descriptor.computations.length,
+    numVisualizations: descriptor.computations
+      .map(({ visualizations }) => visualizations.length)
+      .reduce((memo, visualizationCount) => memo + visualizationCount, 0),
+  };
+}
--- packages/libs/eda/src/lib/workspace/Mocks.ts
+++ packages/libs/eda/src/lib/workspace/Mocks.ts
@@ -1,4 +1,4 @@
-import { isRight } from 'fp-ts/lib/Either';
+import { isLeft, isRight } from 'fp-ts/lib/Either';
 import { PathReporter } from 'io-ts/lib/PathReporter';
 import localforage from 'localforage';
 import {
@@ -6,6 +6,11 @@ import {
   NewAnalysis,
   AnalysisClient,
   AnalysisPreferences,
+  NewAnalysisClient,
+  AnalysisSummary,
+  AnalysisDetails,
+  NewAnalysisDetails,
+  AnalysisDescriptor,
 } from '../core';
 import { max } from 'lodash';
 
@@ -66,3 +71,101 @@ export const mockAnalysisStore: AnalysisClient = {
     for (const id of ids) await analysisStore.removeItem(id);
   },
 } as AnalysisClient;
+
+export const newMockAnalysisStore: NewAnalysisClient = {
+  async getPreferences() {
+    const prefs = await preferenceStore.getItem('preferences');
+    const result = AnalysisPreferences.decode(prefs);
+    if (isRight(result)) return result.right;
+    throw new Error(PathReporter.report(result).join('\n'));
+  },
+  async setPreferences(preferences: AnalysisPreferences) {
+    await preferenceStore.setItem<AnalysisPreferences>(
+      'preferences',
+      preferences
+    );
+  },
+  async getAnalyses() {
+    const records: AnalysisSummary[] = [];
+    await analysisStore.iterate((value) => {
+      const result = AnalysisDetails.decode(value);
+      if (isLeft(result)) {
+        throw new Error(PathReporter.report(result).join('\n'));
+      }
+      const { descriptor, ...analysisSummary } = result.right;
+      records.push(analysisSummary);
+    });
+    return records;
+  },
+  async createAnalysis(newAnalysis: NewAnalysisDetails) {
+    const usedIds = await analysisStore.keys();
+    const analysisId = String((max(usedIds.map((x) => Number(x))) ?? 0) + 1);
+    const now = new Date().toISOString();
+    await analysisStore.setItem<AnalysisDetails>(analysisId, {
+      ...newAnalysis,
+      ...computeSummaryCounts(newAnalysis.descriptor),
+      analysisId,
+      creationTime: now,
+      modificationTime: now,
+    });
+    return { analysisId };
+  },
+  async getAnalysis(id: string) {
+    const analysis = await analysisStore.getItem(id);
+    if (analysis) {
+      const result = AnalysisDetails.decode(analysis);
+      if (isRight(result)) return result.right;
+      throw new Error(PathReporter.report(result).join('\n'));
+    }
+    throw new Error(`Could not find analysis with id "${id}".`);
+  },
+  async updateAnalysis(
+    analysisId: string,
+    analysisPatch: Partial<NewAnalysisDetails>
+  ) {
+    const analysis = await analysisStore.getItem(analysisId);
+    if (analysis == null) {
+      throw new Error(
+        `Tried to update a nonexistent analysis with id "${analysisId}".`
+      );
+    }
+
+    const result = AnalysisDetails.decode(analysis);
+    if (isLeft(result)) {
+      throw new Error(PathReporter.report(result).join('\n'));
+    }
+
+    const decodedAnalysis = result.right;
+    const now = new Date().toISOString();
+    await analysisStore.setItem<AnalysisDetails>(analysisId, {
+      ...decodedAnalysis,
+      ...analysisPatch,
+      modificationTime: now,
+    });
+  },
+  async deleteAnalysis(id: string) {
+    await analysisStore.removeItem(id);
+  },
+  async deleteAnalyses(ids: Iterable<string>) {
+    const deletionPromises = Array.from(ids).map((id) =>
+      analysisStore.removeItem(id)
+    );
+
+    await Promise.allSettled(deletionPromises);
+  },
+} as NewAnalysisClient;
+
+function computeSummaryCounts(
+  descriptor: AnalysisDescriptor
+): Pick<
+  AnalysisSummary,
+  'numComputations' | 'numFilters' | 'numVisualizations'
+> {
+  return {
+    numFilters: descriptor.subset.descriptor.length,
+    numComputations: descriptor.computations.length,
+    numVisualizations: descriptor.computations
+      .map(({ visualizations }) => visualizations.length)
+      .reduce((memo, visualizationCount) => memo + visualizationCount, 0),
+  };
+}
