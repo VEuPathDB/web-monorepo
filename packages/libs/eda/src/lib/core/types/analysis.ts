700b7a30415a866b05c512b1c534da7eefdb7f75
Jamie Long <jtlong3rd@gmail.com>
2021-09-20 00:53:23 -0400

    Introduce AnalysisDescriptor, NewAnalysisDetails, and AnalysisDetails types based on the user service interface; fix typo in the name of an existing type

--- packages/libs/eda/src/lib/core/types/analysis.ts
+++ packages/libs/eda/src/lib/core/types/analysis.ts
@@ -2,15 +2,15 @@
 import * as t from 'io-ts';
 import { Filter } from './filter';
 import { VariableDescriptor } from './variable';
-import { Computation, Visualization } from './visualization';
+import { Computation, NewComputation, Visualization } from './visualization';
 
 export type AnalysisPreferences = t.TypeOf<typeof AnalysisPreferences>;
 export const AnalysisPreferences = t.partial({
   pinnedAnalyses: t.array(t.string),
 });
 
-export type DerviedVariable = t.TypeOf<typeof DerviedVariable>;
-export const DerviedVariable = t.unknown;
+export type DerivedVariable = t.TypeOf<typeof DerivedVariable>;
+export const DerivedVariable = t.unknown;
 
 export type VariableUISetting = t.TypeOf<typeof VariableUISetting>;
 export const VariableUISetting = t.UnknownRecord;
@@ -47,7 +47,7 @@ export const NewAnalysis = t.type({
   name: t.string,
   studyId: t.string,
   filters: t.array(Filter),
-  derivedVariables: t.array(DerviedVariable),
+  derivedVariables: t.array(DerivedVariable),
   starredVariables: t.array(VariableDescriptor),
   variableUISettings: t.record(t.string, VariableUISetting),
   visualizations: t.array(Visualization),
@@ -61,6 +61,39 @@ export const Analysis = t.intersection([
     id: t.string,
     created: t.string,
     modified: t.string,
+  }),
+]);
+
+export type AnalysisDescriptor = t.TypeOf<typeof AnalysisDescriptor>;
+export const AnalysisDescriptor = t.type({
+  subset: t.type({
+    descriptor: t.array(Filter),
+    uiSettings: t.record(t.string, VariableUISetting),
+  }),
+  computations: t.array(NewComputation),
+  starredVariables: t.array(VariableDescriptor),
+  dataTableColumns: t.array(VariableDescriptor),
+  derivedVariables: t.array(DerivedVariable),
+});
+
+export type NewAnalysisDetails = t.TypeOf<typeof NewAnalysisDetails>;
+export const NewAnalysisDetails = t.intersection([
+  AnalysisBase,
+  t.type({
+    descriptor: AnalysisDescriptor,
+  }),
+]);
+
+export type AnalysisDetails = t.TypeOf<typeof AnalysisDetails>;
+export const AnalysisDetails = t.intersection([
+  NewAnalysisDetails,
+  t.type({
+    analysisId: t.string,
+    creationTime: t.string,
+    modificationTime: t.string,
+    numFilters: t.number,
+    numComputations: t.number,
+    numVisualizations: t.number,
   }),
 ]);
 
