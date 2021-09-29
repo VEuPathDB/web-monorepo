37bcf1417fef77205cdf06dbc4a069c6d80ebc93
Ryan Doherty <rdoherty@upenn.edu>
2021-09-29 12:17:52 -0400

    code cleanup

--- packages/libs/eda/src/lib/workspace/Utils.ts
+++ packages/libs/eda/src/lib/workspace/Utils.ts
@@ -10,21 +10,21 @@ export function findFirstVariable(
 ): Variable | undefined {
   // look for featured variables and return the first if any
   const featuredVariable = variables.find(
-    (v) => v.type !== 'category' && v.isFeatured
+    (v): v is Variable => v.type !== 'category' && v.isFeatured
   );
-  if (featuredVariable) return featuredVariable as Variable;
+  if (featuredVariable) return featuredVariable;
 
   // no featured vars; find first var with passed parentId (category id or enclosing entity id)
   const variable = variables.find((v) => v.parentId === parentId);
 
-  // if no nodes have this as parent, tree is broken because entity or category has no children
+  // if no nodes have the specified parent, tree is broken because entity or category has no children
   if (variable == null) {
     // TODO: return undefined here instead?
     throw 'Tree is broken; no variables with parent ' + parentId;
   }
 
   // if first var is a category, find first var beneath; otherwise return var
-  return variable.type === 'category'
-    ? findFirstVariable(variables, variable.id)
-    : (variable as Variable);
+  return variable.type !== 'category'
+    ? variable
+    : findFirstVariable(variables, variable.id);
 }
