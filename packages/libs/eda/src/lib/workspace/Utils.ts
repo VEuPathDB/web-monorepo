db249876c98deee9936dab8994ff5cc7588e4021
Dave Falke <dmfalke@gmail.com>
2021-09-30 13:58:04 -0400

    Handle cases where variable tree is not rooted by entity

--- packages/libs/eda/src/lib/workspace/Utils.ts
+++ packages/libs/eda/src/lib/workspace/Utils.ts
@@ -1,4 +1,5 @@
 import { makeClassNameHelper } from '@veupathdb/wdk-client/lib/Utils/ComponentUtils';
+import { groupBy } from 'lodash';
 import { Variable, VariableTreeNode } from '../core/types/study';
 
 import './EDAWorkspace.scss';
@@ -8,27 +9,55 @@ export const cx = makeClassNameHelper('EDAWorkspace');
  * Returns the first featured var; if none exist, then returns the
  * first non-category var in the tree using depth first search.
  */
-export function findFirstVariable(
-  variables: VariableTreeNode[],
-  parentId: string
-): Variable | undefined {
+export function findFirstVariable(variables: VariableTreeNode[]): Variable {
   // look for featured variables and return the first if any
   const featuredVariable = variables.find(
     (v): v is Variable => v.type !== 'category' && v.isFeatured
   );
+
   if (featuredVariable) return featuredVariable;
 
-  // no featured vars; find first var with passed parentId (category id or enclosing entity id)
-  const variable = variables.find((v) => v.parentId === parentId);
+  // Find root variables. These are variables whose parent is not present in the provided array.
+  const roots = findRootVariables(variables);
+
+  if (roots.length === 0)
+    throw new Error('Tree is broken: cannot determine root nodes.');
+
+  // Traverse first branch of tree and find first non-category node
+  const variable = findFirstNonCategory(
+    groupBy(variables, (v) => v.parentId),
+    roots[0]
+  );
 
   // if no nodes have the specified parent, tree is broken because entity or category has no children
   if (variable == null) {
     // TODO: return undefined here instead?
-    throw 'Tree is broken; no variables with parent ' + parentId;
+    throw new Error(
+      'Tree is broken: could not find a non-category node in first branch.'
+    );
   }
 
-  // if first var is a category, find first var beneath; otherwise return var
-  return variable.type !== 'category'
-    ? variable
-    : findFirstVariable(variables, variable.id);
+  return variable;
+}
+
+function findRootVariables(variables: VariableTreeNode[]): VariableTreeNode[] {
+  // A variable is a root if its parent variable is not in the provided array.
+  // This can happen if parentId == null, or if the parentId refers to a variable
+  // that is not included.
+  const variableIds = new Set(variables.map((v) => v.id));
+  return variables.filter(
+    (v) => v.parentId == null || !variableIds.has(v.parentId)
+  );
+}
+
+function findFirstNonCategory(
+  variablesByParentId: Record<string, VariableTreeNode[] | undefined>,
+  root: VariableTreeNode
+): Variable | undefined {
+  const children = variablesByParentId[root.id];
+  const firstChild = children && children[0];
+  if (firstChild == null) return undefined;
+  if (firstChild.type === 'category')
+    return findFirstNonCategory(variablesByParentId, firstChild);
+  return firstChild;
 }
