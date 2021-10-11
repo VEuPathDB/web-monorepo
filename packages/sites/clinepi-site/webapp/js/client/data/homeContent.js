f6d1e9f7774fde35da25f91859e4518f0709b50d
Dave Falke <dmfalke@gmail.com>
2021-10-11 16:34:32 -0400

    Add analysis cards section to homepage, replacing public stragies and visualizations

--- packages/sites/clinepi-site/webapp/js/client/data/homeContent.js
+++ packages/sites/clinepi-site/webapp/js/client/data/homeContent.js
@@ -1,6 +1,5 @@
 import { StudyCard } from '@veupathdb/web-common/lib/App/Studies';
-import { SearchCard } from '@veupathdb/web-common/lib/App/Searches';
-import { ImageCard } from '@veupathdb/web-common/lib/App/ImageCard';
+import { AnalysisCard } from '@veupathdb/web-common/lib/App/Analyses';
 
 import { studyMatchPredicate, studyFilters } from '@veupathdb/web-common/lib/util/homeContent';
 
@@ -23,26 +22,34 @@ export default permissionsValue => ({ studies, searches, visualizations }) => ([
     permissions: permissionsValue.permissions
   },
   {
-    title: 'Explore Example Searches',
-    description: 'ClinEpiDB can be used to employ a sophisticated search strategy system to explore study data. Use the example searches below to jump to saved strategies, view their results and get acquainted with ClinEpiDB capabilities.',
-    viewAllAppUrl: '/app/workspace/strategies/public',
-    contentType: 'SearchCardList',
-    contentNamePlural: 'searches',
+    title: 'Explore Example Analyses',
+    description: 'ClinEpiDB can be used to explore and visualize study data. Use the example analyses below to see some examples.',
+    viewAllAppUrl: '/app/workspace/analyses/public',
+    contentType: 'AnalysisCardList',
+    contentNamePlural: 'analyses',
     items: searches.entities,
     isLoading: searches.loading,
-    cardComponent: SearchCard,
+    cardComponent: AnalysisCard,
     getSearchStringForItem: item =>
-      item.name + ' ' + item.description
+      item.name + ' ' + item.description,
+    loadItems,
   },
-  {
-    title: 'Explore Visualization Tools',
-    description: 'Gain clear insights into your data and illustrate powerful connections using our visualization and analysis tools. Use the brief examples below to get learn how to get started exploring data with these resources.',
-    contentType: 'ImageCardList',
-    contentNamePlural: 'visualization tools',
-    items: visualizations.entities,
-    isLoading: visualizations.loading,
-    cardComponent: ImageCard,
-    getSearchStringForItem: item =>
-      item.title + ' ' + item.description
-  }
 ]);
+
+async function loadItems({ analysisClient, wdkService }) {
+  const overviews = await analysisClient.getPublicAnalyses();
+  const studies = await wdkService.getStudies();
+  return overviews.flatMap(overview => {
+    const study = studies.records.find(study => study.attributes.dataset_id === overview.studyId);
+    if (study == null) return [];
+    return [{
+      displayName: overview.displayName,
+      studyDisplayName: study.displayName,
+      description: overview.description,
+      studyId: overview.studyId,
+      analysisId: overview.analysisId,
+      ownerUserId: overview.userId,
+      ownerName: overview.userName,
+    }];
+  });
+}
