f4c048dc54078fe55a72bd2bee86c8b9f59ee55a
Michael <bigfatpanda@nd.edu>
2021-10-14 15:14:22 -0400

    User the backend service to store analysis state.

--- packages/libs/eda/src/lib/core/hooks/analysisClient.ts
+++ packages/libs/eda/src/lib/core/hooks/analysisClient.ts
@@ -1,14 +1,9 @@
 import { useNonNullableContext } from '@veupathdb/wdk-client/lib/Hooks/NonNullableContext';
 import { WdkDepdendenciesContext } from '@veupathdb/wdk-client/lib/Hooks/WdkDependenciesEffect';
-import { mockAnalysisStore } from '../../workspace/Mocks';
 
 import { AnalysisClient } from '../api/analysis-api';
 
 export function useConfiguredAnalysisClient(userServiceUrl: string) {
-  /* FIXME Uncomment this when the backend is updated with the correct data table settings format
   const { wdkService } = useNonNullableContext(WdkDepdendenciesContext);
-
   return AnalysisClient.getClient(userServiceUrl, wdkService);
-  */
-  return mockAnalysisStore;
 }
