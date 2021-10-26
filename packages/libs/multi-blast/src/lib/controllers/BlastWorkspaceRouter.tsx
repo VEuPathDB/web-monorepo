4db503f302b58a366f992451d4a1ff71820bee98
Jamie Long <jtlong3rd@gmail.com>
2021-10-26 17:53:09 -0400

    Don't unmount/remount BlastWorkspaceResult when switching tabs

--- packages/libs/multi-blast/src/lib/controllers/BlastWorkspaceRouter.tsx
+++ packages/libs/multi-blast/src/lib/controllers/BlastWorkspaceRouter.tsx
@@ -1,15 +1,9 @@
-import {
-  Route,
-  RouteComponentProps,
-  Switch,
-  useRouteMatch,
-} from 'react-router';
+import { Route, Switch, useRouteMatch } from 'react-router';
 
 import { NotFoundController } from '@veupathdb/wdk-client/lib/Controllers';
 
 import { BlastWorkspace } from '../components/BlastWorkspace';
 import { BlastWorkspaceResult } from '../components/BlastWorkspaceResult';
-import { parseBlastResultSubpath } from '../utils/routes';
 
 export function BlastWorkspaceRouter() {
   const { path } = useRouteMatch();
@@ -22,28 +16,49 @@ export function BlastWorkspaceRouter() {
         component={BlastWorkspace}
       />
       <Route
-        path={`${path}/result/:jobId/:subPath(combined|individual/\\d+)?`}
+        path={`${path}/result/:jobId/combined`}
         exact
-        component={(
-          props: RouteComponentProps<{
-            jobId: string;
-            subPath: string | undefined;
-          }>
-        ) => {
-          const selectedResult = parseBlastResultSubpath(
-            props.match.params.subPath
-          );
+        render={(props) => {
+          const jobId = props.match.params.jobId as string;
 
-          return selectedResult != null && selectedResult.type === 'unknown' ? (
-            <NotFoundController />
-          ) : (
+          return (
             <BlastWorkspaceResult
-              jobId={props.match.params.jobId}
-              selectedResult={selectedResult}
+              jobId={jobId}
+              selectedResult={{ type: 'combined' }}
             />
           );
         }}
       />
+      <Route
+        path={`${path}/result/:jobId/individual/:resultIndex(\\d+)`}
+        exact
+        render={(props) => {
+          const jobId = props.match.params.jobId as string;
+          const resultIndex = Number(props.match.params.resultIndex as string);
+
+          return (
+            <BlastWorkspaceResult
+              jobId={jobId}
+              selectedResult={{
+                type: 'individual',
+                resultIndex,
+              }}
+            />
+          );
+        }}
+      />
+      <Route
+        path={`${path}/result/:jobId`}
+        exact
+        render={(props) => {
+          const jobId = props.match.params.jobId as string;
+
+          return (
+            <BlastWorkspaceResult jobId={jobId} selectedResult={undefined} />
+          );
+        }}
+      />
+      <Route component={NotFoundController} />
     </Switch>
   );
 }
