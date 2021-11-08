077c6923f8877642946c924ef1eb5942b02b0ad6
Jamie Long <jtlong@upenn.edu>
2021-11-08 15:22:41 -0500

    FacetedPlot: expose the refs of the rendered PlotlyPlot components

--- packages/libs/components/src/plots/FacetedPlot.tsx
+++ packages/libs/components/src/plots/FacetedPlot.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { Ref, forwardRef, useImperativeHandle, useRef } from 'react';
+
+import { memoize } from 'lodash';
+
 import { isFaceted } from '../types/guards';
 import { FacetedData } from '../types/plots';
-import { PlotProps } from './PlotlyPlot';
+import { ComponentWithPlotRef, PlotProps, PlotRef } from './PlotlyPlot';
 
 export interface FacetedPlotProps<D, P extends PlotProps<D>> {
   data?: FacetedData<D>;
-  component: React.ElementType<P>;
+  component: ComponentWithPlotRef<P>;
   props: P;
 }
 
-export default function FacetedPlot<D, P extends PlotProps<D>>(
-  props: FacetedPlotProps<D, P>
-) {
-  const { data, component, props: componentProps } = props;
+export type FacetedPlotRef = PlotRef[];
 
-  const Component = component as React.ElementType; // casting seems to be needed if using component: React.ElementType<P>; above
+function renderFacetedPlot<D, P extends PlotProps<D>>(
+  props: FacetedPlotProps<D, P>,
+  ref: Ref<FacetedPlotRef>
+) {
+  const { data, component: Component, props: componentProps } = props;
+  const plotRefs = useRef<FacetedPlotRef>([]);
+
+  useImperativeHandle<FacetedPlotRef, FacetedPlotRef>(
+    ref,
+    () => {
+      const plotRefsLength = data?.facets.length ?? 0;
+
+      plotRefs.current = plotRefs.current.slice(0, plotRefsLength);
+
+      return plotRefs.current;
+    },
+    [data?.facets]
+  );
 
   // return a regular plot component if the data isn't faceted.
   if (!isFaceted(data)) {
@@ -27,6 +44,13 @@ export default function FacetedPlot<D, P extends PlotProps<D>>(
           {data?.facets.map(({ data, label }, index) => (
             <Component
               {...componentProps}
+              ref={(plotRef) => {
+                if (plotRef == null) {
+                  delete plotRefs.current[index];
+                } else {
+                  plotRefs.current[index] = plotRef;
+                }
+              }}
               key={index}
               data={data}
               title={label}
@@ -43,4 +67,27 @@ export default function FacetedPlot<D, P extends PlotProps<D>>(
       </div>
     );
   }
+}
+
+const makeFacetedPlotComponent = memoize(function <D, P extends PlotProps<D>>(
+  UnfacetedPlotComponent: ComponentWithPlotRef<P>
+) {
+  const FacetedPlotComponent = forwardRef<
+    FacetedPlotRef,
+    FacetedPlotProps<D, P>
+  >(renderFacetedPlot);
+
+  FacetedPlotComponent.displayName = `FacetedPlotComponent(${
+    UnfacetedPlotComponent.displayName || UnfacetedPlotComponent.name
+  })`;
+
+  return FacetedPlotComponent;
+});
+
+export default function FacetedPlot<D, P extends PlotProps<D>>(
+  props: FacetedPlotProps<D, P> & { facetedPlotRef?: Ref<FacetedPlotRef> }
+) {
+  const FacetedPlotComponent = makeFacetedPlotComponent<D, P>(props.component);
+
+  return <FacetedPlotComponent {...props} ref={props.facetedPlotRef} />;
 }
