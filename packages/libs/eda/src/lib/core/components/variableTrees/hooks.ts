8670f31daa72e205cf6030b9f3e3d140049d5c06
Dave Falke <dfalke@uga.edu>
2021-11-16 22:33:37 -0500

    Remove duplicate code

--- packages/libs/eda/src/lib/core/components/variableTrees/hooks.ts
+++ packages/libs/eda/src/lib/core/components/variableTrees/hooks.ts
@@ -5,7 +5,10 @@ import { getTree } from '@veupathdb/wdk-client/lib/Components/AttributeFilter/At
 import { pruneDescendantNodes } from '@veupathdb/wdk-client/lib/Utils/TreeUtils';
 
 import { StudyEntity } from '../../types/study';
-import { edaVariableToWdkField } from '../../utils/wdk-filter-param-adapter';
+import {
+  edaVariableToWdkField,
+  entitiesToFields,
+} from '../../utils/wdk-filter-param-adapter';
 import { keyBy } from 'lodash';
 
 /**
@@ -40,72 +43,10 @@ export const useValuesMap = (entities: StudyEntity[]) =>
   }, [entities]);
 
 /**
- * Generate a flat array of objects where each object represents
- * a given entity/variable category/variable in the entity hierarchy.
- * 
- * An example entry will generally look like this:
- * {
-    "display": "Country",
-    "isRange": false,
-    "parent": "PCO_0000024/GAZ_00000448",
-    "precision": 1,
-    "term": "PCO_0000024/ENVO_00000004",
-    "type": "string",
-    "variableName": "[\"SITE\"]"
-  }
- *
- * Of note, `parent` will be a reference to one of the following:
- * 1. An entity.
- * 2. A "variable category" - which isn't really a variable, but a organizational
- *    grouping of related variables. 
- * 
- * `Term` is a reference to the item itself and can be either an 
- * entity, variable category, or variable itself.
+ * Memoized hook that delegates to {@link entitiesToFields}
  */
 export const useFlattenedFields = (entities: StudyEntity[]) =>
-  useMemo(() => {
-    return entities.flatMap((entity) => {
-      // Create a Set of variableId so we can lookup parentIds
-      const variableIds = new Set(entity.variables.map((v) => v.id));
-      return [
-        // Create a non-filterable field for the entity.
-        // Note that we're prefixing the term. This avoids
-        // collisions with variables using the same term.
-        // This situation shouldn't happen in production,
-        // but there is nothing preventing it, so we need to
-        // handle the case.
-        {
-          term: `entity:${entity.id}`,
-          display: entity.displayName,
-        },
-        ...entity.variables
-          // Before handing off to edaVariableToWdkField, we will
-          // change the id of the variable to include the entityId.
-          // This will make the id unique across the tree and prevent
-          // duplication across entity subtrees.
-          .map((variable) => ({
-            ...variable,
-            id: `${entity.id}/${variable.id}`,
-            parentId:
-              // Use entity as parent under the following conditions:
-              // - if parentId is null
-              // - if the parentId is the same as the entityId
-              // - if the parentId does not exist in the provided list of variables
-              //
-              // Variables that meet any of these conditions will serve
-              // as the root nodes of the variable subtree, which will
-              // become the children of the entity node in the final tree.
-              variable.parentId == null ||
-              variable.parentId === entity.id ||
-              !variableIds.has(variable.parentId)
-                ? `entity:${entity.id}`
-                : `${entity.id}/${variable.parentId}`,
-          }))
-
-          .map((variable) => edaVariableToWdkField(variable)),
-      ];
-    });
-  }, [entities]);
+  useMemo(() => entitiesToFields(entities), [entities]);
 
 /**
  * Identity "fields" from the entity hierarchy which have been marked
