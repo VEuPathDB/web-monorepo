e12cf29d4bd6780c394278e129c19812ad9b94e2
Dave Falke <dmfalke@gmail.com>
2021-11-22 14:43:14 -0500

    remove unnecessary name property

--- packages/libs/eda/src/lib/core/components/visualizations/InputVariables.tsx
+++ packages/libs/eda/src/lib/core/components/visualizations/InputVariables.tsx
@@ -68,46 +68,41 @@ export interface Props {
   outputEntity?: StudyEntity;
 }
 
-const useStyles = makeStyles(
-  {
-    inputs: {
-      display: 'flex',
-      flexWrap: 'nowrap', // if it didn't wrap so aggressively, it would be good to allow wrapping
-      // perhaps after the Material UI capitalization is removed.
-      marginLeft: '0.5em', // this indent is only needed because the wdk-SaveableTextEditor above it is indented
-      alignItems: 'flex-start',
-    },
-    inputGroup: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    input: {
-      display: 'flex',
-      alignItems: 'center',
-      marginBottom: '0.5em', // in case they end up stacked vertically on a narrow screen
-      marginRight: '2em',
-    },
-    label: {
-      marginRight: '1ex',
-      fontWeight: 500,
-    },
-    dataLabel: {
-      textAlign: 'right',
-      marginTop: '2em',
-      fontSize: '1.35em',
-      fontWeight: 500,
-    },
-    fullRow: {
-      flexBasis: '100%',
-    },
-    primary: {},
-    stratification: {},
-    showMissingness: {},
+const useStyles = makeStyles({
+  inputs: {
+    display: 'flex',
+    flexWrap: 'nowrap', // if it didn't wrap so aggressively, it would be good to allow wrapping
+    // perhaps after the Material UI capitalization is removed.
+    marginLeft: '0.5em', // this indent is only needed because the wdk-SaveableTextEditor above it is indented
+    alignItems: 'flex-start',
   },
-  {
-    name: 'InputVariables',
-  }
-);
+  inputGroup: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  input: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '0.5em', // in case they end up stacked vertically on a narrow screen
+    marginRight: '2em',
+  },
+  label: {
+    marginRight: '1ex',
+    fontWeight: 500,
+  },
+  dataLabel: {
+    textAlign: 'right',
+    marginTop: '2em',
+    fontSize: '1.35em',
+    fontWeight: 500,
+  },
+  fullRow: {
+    flexBasis: '100%',
+  },
+  primary: {},
+  stratification: {},
+  showMissingness: {},
+});
 
 export function InputVariables(props: Props) {
   const {
