d7486d4cfb2dbca1f66272497adf42238b2009f6
Michael <bigfatpanda@nd.edu>
2021-11-29 12:39:17 -0500

    Refresh icon components.

--- packages/libs/coreui/src/components/icons/CheckIcon.tsx
+++ packages/libs/coreui/src/components/icons/CheckIcon.tsx
@@ -1,25 +1,36 @@
 import * as React from "react";
+import { SVGProps } from "react";
 
-function SvgCheckIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      fillRule="evenodd"
-      clipRule="evenodd"
-      strokeLinejoin="round"
-      strokeMiterlimit={2}
-      width="1em"
-      height="1em"
-      {...props}
-    >
-      <path fill="none" d="M0 0h24v24H0z" />
-      <path
-        d="M10.054 14.619l-5.43-4.897-2.292 2.531 7.97 7.19L21.668 6.84l-2.53-2.282-9.084 10.06z"
-        fillRule="nonzero"
-      />
-    </svg>
-  );
-}
+const SvgCheckIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    style={{
+      fillRule: "evenodd",
+      clipRule: "evenodd",
+      strokeLinejoin: "round",
+      strokeMiterlimit: 2,
+    }}
+    width="1em"
+    height="1em"
+    {...props}
+  >
+    <path
+      style={{
+        fill: "none",
+        fillRule: "nonzero",
+      }}
+      d="M0 0h24v24H0z"
+    />
+    <path
+      d="M9 14.17 3.83 9l-2.42 2.41L9 19 21 7l-2.41-2.41L9 14.17Z"
+      style={{
+        fillRule: "nonzero",
+      }}
+      transform="rotate(-2.951 18.236 -6.055)"
+    />
+  </svg>
+);
 
 export default SvgCheckIcon;
