6ca5eb50831756f2a272fb8ab01ba007f6324712
Jamie Long <jtlong@upenn.edu>
2021-12-06 13:11:27 -0500

    Fix typo in comment

--- packages/libs/wdk-client/src/Hooks/RefinedContext.ts
+++ packages/libs/wdk-client/src/Hooks/RefinedContext.ts
@@ -3,7 +3,7 @@ import { Context, useContext } from 'react';
 /**
  * Ensures the value of React Context is of a specified subtype.
  * This is useful to prevent a consumer from accessing context which
- * has not been properply configured.
+ * has not been properly configured.
  */
 export function makeUseRefinedContext<T, U extends T>(
   isRefinedValue: (v: T) => v is U,
