ecefa86c47b0c00922d44caed06578f7c1209a1f
Jamie Long <jtlong@upenn.edu>
2021-12-07 16:27:32 -0500

    Include studyAccessApi in WdkDependencies

--- packages/sites/clinepi-site/webapp/js/client/main.js
+++ packages/sites/clinepi-site/webapp/js/client/main.js
@@ -1,3 +1,8 @@
+import { partial } from 'lodash';
+
+import { reduxMiddleware } from '@veupathdb/study-data-access/lib/data-restriction/DataRestrictionUtils';
+import { wrapWdkDependencies } from '@veupathdb/study-data-access/lib/shared/wrapWdkDependencies';
+
 import { initialize } from '@veupathdb/web-common/lib/bootstrap';
 
 import '@veupathdb/web-common/lib/styles/client.scss';
@@ -5,12 +10,12 @@ import '@veupathdb/web-common/lib/styles/client.scss';
 import componentWrappers from './component-wrappers';
 import wrapStoreModules from './wrapStoreModules';
 import { wrapRoutes } from './routes';
-import { reduxMiddleware } from '@veupathdb/study-data-access/lib/data-restriction/DataRestrictionUtils'
 
 import 'site/css/ClinEpiSite.scss';
 
 initialize({
   componentWrappers,
+  wrapWdkDependencies: partial(wrapWdkDependencies, '/eda'),
   wrapStoreModules,
   wrapRoutes,
   additionalMiddleware: [ reduxMiddleware ]
