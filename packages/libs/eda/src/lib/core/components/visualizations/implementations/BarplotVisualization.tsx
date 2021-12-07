c97ed0ce254680f96f228198b85a2d1f01c235c7
Dae Kun (DK) Kwon <dkwon@nd.edu>
2021-12-06 19:52:08 -0500

    keep the state of checked legend items

--- packages/libs/eda/src/lib/core/components/visualizations/implementations/BarplotVisualization.tsx
+++ packages/libs/eda/src/lib/core/components/visualizations/implementations/BarplotVisualization.tsx
@@ -14,7 +14,8 @@ import { preorder } from '@veupathdb/wdk-client/lib/Utils/TreeUtils';
 import { getOrElse } from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/function';
 import * as t from 'io-ts';
-import { useCallback, useEffect, useMemo } from 'react';
+// add useState
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import PluginError from '../PluginError';
 
 // need to set for Barplot
@@ -371,10 +372,29 @@ function BarplotViz(props: VisualizationProps) {
       : [];
   }, [data]);
 
-  // use this to set all checked
+  // set this useState to determine whether a new checkedLegendItems should be made
+  const [newLegendList, setNewLegendList] = useState<boolean>(false);
+
+  // run once when rendering and check if vizConfig.checkedLegendItmes is defined
+  // this will handle the case of pre-existing plot
   useEffect(() => {
-    if (data != null) {
-      onCheckedLegendItemsChange(legendItems.map((item) => item.label));
+    if (vizConfig.checkedLegendItems != null) {
+      onCheckedLegendItemsChange(vizConfig.checkedLegendItems);
+    } else {
+      // vizConfig.checkedLegendItems is undefined thus a new legend list needs to be made in the below useEffect()
+      setNewLegendList(true);
+    }
+  }, []);
+
+  // check whether new legend list is required. this will handle the cases of new Viz and changing variables within the Viz
+  useEffect(() => {
+    // separate data.value and newLegendList conditions so that newLegendList is not updated before executing onCheckedLegendItemsChange
+    if (data.value != null) {
+      if (newLegendList)
+        // set checked legend items with new legend list
+        onCheckedLegendItemsChange(legendItems.map((item) => item.label));
+      // this handles the case after re-entering the plot from thumbnail, subsetting, etc.
+      else setNewLegendList(true);
     }
   }, [data, legendItems]);
 
@@ -383,6 +403,10 @@ function BarplotViz(props: VisualizationProps) {
     plotContainerStyles,
     [data, vizConfig.checkedLegendItems]
   );
+
+  console.log('checkedLegendItems =', vizConfig.checkedLegendItems);
+  // console.log('legendItems =', legendItems)
+  console.log('newLegendList =', newLegendList);
 
   // these props are passed to either a single plot
   // or by FacetedPlot to each individual facet plot (where some will be overridden)
