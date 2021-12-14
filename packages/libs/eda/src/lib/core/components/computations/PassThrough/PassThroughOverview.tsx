2b5528d3b2a8f639ff8ee242d07fb636d47a69cc
Dave Falke <dfalke@uga.edu>
2021-12-14 16:12:33 -0500

    checkpoint

---
+++ packages/libs/eda/src/lib/core/components/computations/PassThrough/PassThroughOverview.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function PassThroughOverview() {
+  return <div>Hello</div>;
+}
--- packages/libs/eda/src/lib/core/components/computations/Types.ts
+++ packages/libs/eda/src/lib/core/components/computations/Types.ts
@@ -1,11 +1,28 @@
 import { AnalysisState } from '../../hooks/analysis';
 import { EntityCounts } from '../../hooks/entityCounts';
 import { PromiseHookState } from '../../hooks/promise';
-import { ComputationAppOverview } from '../../types/visualization';
+import { Computation, ComputationAppOverview } from '../../types/visualization';
 
 export interface ComputationProps {
   analysisState: AnalysisState;
   computationAppOverview: ComputationAppOverview;
   totalCounts: PromiseHookState<EntityCounts>;
   filteredCounts: PromiseHookState<EntityCounts>;
+}
+
+export interface ComputationOverviewProps extends ComputationProps {}
+
+export interface ComputationInstanceProps extends ComputationProps {
+  computation: Computation;
+}
+
+export interface ComputationComponents {
+  /** Screen that shows details of app and existing instances */
+  overview: React.ComponentType<ComputationOverviewProps>;
+  /** Screen that allows user to configure an app instance */
+  create: React.ComponentType<ComputationOverviewProps>;
+  /** Screen that allow user to update configuration of an app instance */
+  edit: React.ComponentType<ComputationInstanceProps>;
+  /** Screen that allows user to add visuzliations to an app instance */
+  instance: React.ComponentType<ComputationInstanceProps>;
 }
