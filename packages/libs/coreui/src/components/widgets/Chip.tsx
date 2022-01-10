bf6cd73c60f286523ffb3b624f1d387229473f3f
Michael <bigfatpanda@nd.edu>
2022-01-10 16:56:55 -0500

    Increase border radius on `Chip` component.

--- packages/libs/coreui/src/components/widgets/Chip.tsx
+++ packages/libs/coreui/src/components/widgets/Chip.tsx
@@ -67,7 +67,7 @@ export default function Chip({
       border: {
         width: 1,
         color: gray[400],
-        radius: 8,
+        radius: 10,
       },
     },
     hover: {
@@ -76,7 +76,7 @@ export default function Chip({
       border: {
         width: 1,
         color: blue[500],
-        radius: 8,
+        radius: 10,
       },
     },
     pressed: {
@@ -85,7 +85,7 @@ export default function Chip({
       border: {
         width: 2,
         color: blue[500],
-        radius: 8,
+        radius: 10,
       },
     },
   };
@@ -103,7 +103,7 @@ export default function Chip({
                 width: 1,
                 color:
                   theme.palette[themeRole].hue[theme.palette[themeRole].level],
-                radius: 8,
+                radius: 10,
               },
             },
             pressed: {
@@ -114,7 +114,7 @@ export default function Chip({
                 width: 2,
                 color:
                   theme.palette[themeRole].hue[theme.palette[themeRole].level],
-                radius: 8,
+                radius: 10,
               },
             },
           }
