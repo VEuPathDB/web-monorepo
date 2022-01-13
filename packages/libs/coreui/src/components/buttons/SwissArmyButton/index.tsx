2d48c438d3e8a4f9efe5b150bf7767c3ae8f0c48
Michael <bigfatpanda@nd.edu>
2022-01-13 13:55:36 -0500

    Updates to `MesaButton`
    * Change the default style to be more in-line with other buttons.
    * Add support for disabled prop.

--- packages/libs/coreui/src/components/buttons/SwissArmyButton/index.tsx
+++ packages/libs/coreui/src/components/buttons/SwissArmyButton/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import { ButtonStyleSpec, SwissArmyButtonVariantProps } from '..';
 import typography from '../../../styleDefinitions/typography';
@@ -12,6 +12,7 @@ export type SwissArmyButtonProps = Omit<
 export default function SwissArmyButton({
   text,
   onPress,
+  disabled = false,
   tooltip,
   size = 'medium',
   icon,
@@ -21,12 +22,11 @@ export default function SwissArmyButton({
     'default' | 'hover' | 'pressed'
   >('default');
 
-  /**
-   * If textColor is specified, use it. Otherwise if `type` is solid, use
-   * white. If `type` is outline, use `color` unless button is pressed, then
-   * use `onPressColor` if specified.
-   */
-  const calculatedTextColor = styleSpec[buttonState].textColor ?? 'white';
+  const styleState = useMemo<'default' | 'hover' | 'pressed' | 'disabled'>(
+    () => (disabled ? 'disabled' : buttonState),
+    [buttonState, disabled]
+  );
+
   const calculatedFontSize = size === 'large' ? '1rem' : '.80rem';
   const calculatedIconSize =
     size === 'large' ? '1.5rem' : size === 'medium' ? '1.25rem' : '1rem';
@@ -38,6 +38,7 @@ export default function SwissArmyButton({
   return (
     <div css={{ position: 'relative' }}>
       <button
+        disabled={disabled}
         css={[
           typography.primaryFont,
           {
@@ -46,25 +47,25 @@ export default function SwissArmyButton({
             alignItems: 'center',
             paddingLeft: horizontalPadding,
             paddingRight: horizontalPadding,
-            color: calculatedTextColor,
-            textTransform: styleSpec[buttonState].textTransform ?? 'uppercase',
-            fontWeight: styleSpec[buttonState].fontWeight ?? 600,
+            color: styleSpec[styleState].textColor,
+            textTransform: styleSpec[styleState].textTransform ?? 'uppercase',
+            fontWeight: styleSpec[styleState].fontWeight ?? 600,
             fontSize: calculatedFontSize,
             ...styleSpec.container,
             backgroundColor: `${
-              styleSpec[buttonState].color ?? 'transparent'
+              styleSpec[styleState].color ?? 'transparent'
             } !important`,
-            borderRadius: styleSpec[buttonState].border?.radius ?? 5,
-            outlineStyle: styleSpec[buttonState].border?.style ?? 'none',
-            outlineColor: styleSpec[buttonState].border?.color,
-            outlineWidth: styleSpec[buttonState].border?.width,
-            outlineOffset: styleSpec[buttonState].border?.width
-              ? -1 * styleSpec[buttonState].border?.width!
+            borderRadius: styleSpec[styleState].border?.radius ?? 5,
+            outlineStyle: styleSpec[styleState].border?.style ?? 'none',
+            outlineColor: styleSpec[styleState].border?.color,
+            outlineWidth: styleSpec[styleState].border?.width,
+            outlineOffset: styleSpec[styleState].border?.width
+              ? -1 * styleSpec[styleState].border?.width!
               : undefined,
             border: 'none',
           },
-          styleSpec[buttonState].dropShadow && {
-            boxShadow: `${styleSpec[buttonState].dropShadow?.offsetX} ${styleSpec[buttonState].dropShadow?.offsetY} ${styleSpec[buttonState].dropShadow?.blurRadius} ${styleSpec[buttonState].dropShadow?.color}`,
+          styleSpec[styleState].dropShadow && {
+            boxShadow: `${styleSpec[styleState].dropShadow?.offsetX} ${styleSpec[styleState].dropShadow?.offsetY} ${styleSpec[styleState].dropShadow?.blurRadius} ${styleSpec[styleState].dropShadow?.color}`,
           },
         ]}
         onMouseDown={() => {
@@ -80,7 +81,7 @@ export default function SwissArmyButton({
         {Icon && (
           <Icon
             fontSize={calculatedIconSize}
-            fill={calculatedTextColor}
+            fill={styleSpec[styleState].textColor}
             css={text && { marginRight: 10 }}
           />
         )}
--- packages/libs/coreui/src/stories/buttons/MesaButton.stories.tsx
+++ packages/libs/coreui/src/stories/buttons/MesaButton.stories.tsx
@@ -4,7 +4,7 @@ import { SwissArmyButtonVariantProps } from '../../components/buttons';
 import MesaButton from '../../components/buttons/MesaButton';
 import { TableDownload, Download } from '../../components/icons';
 import UIThemeProvider from '../../components/theming/UIThemeProvider';
-import { blue, orange } from '../../definitions/colors';
+import { blue, mutedCyan, orange } from '../../definitions/colors';
 
 export default {
   title: 'Controls/Buttons/MesaButton',
@@ -16,7 +16,7 @@ const Template: Story<SwissArmyButtonVariantProps> = (args) => {
     <UIThemeProvider
       theme={{
         palette: {
-          primary: { hue: blue, level: 500 },
+          primary: { hue: mutedCyan, level: 500 },
           secondary: { hue: orange, level: 500 },
         },
       }}
@@ -30,30 +30,51 @@ export const Default = Template.bind({});
 Default.args = {
   text: 'Hello Developer',
   size: 'medium',
-};
+  disabled: false,
+} as SwissArmyButtonVariantProps;
 
 export const WithIcon = Template.bind({});
 WithIcon.args = {
+  ...Default.args,
   text: 'Button With Icon',
   icon: TableDownload,
-};
+} as SwissArmyButtonVariantProps;
 
 export const IconOnly = Template.bind({});
 IconOnly.args = {
+  size: 'medium',
+  disabled: false,
   icon: TableDownload,
-};
+} as SwissArmyButtonVariantProps;
 
 export const Tooltip = Template.bind({});
 Tooltip.args = {
+  ...Default.args,
   text: 'Button With Tooltip',
   icon: TableDownload,
   tooltip: 'Hello there friend.',
-};
+} as SwissArmyButtonVariantProps;
 
 export const UseTheme = Template.bind({});
 UseTheme.args = {
+  ...Default.arts,
   text: 'Using Theme Styles',
   icon: Download,
   themeRole: 'primary',
   size: 'medium',
-};
+} as SwissArmyButtonVariantProps;
+
+export const StyleOverrides = Template.bind({});
+StyleOverrides.args = {
+  ...Default.arts,
+  text: 'Style Override Example',
+  icon: Download,
+  themeRole: 'primary',
+  size: 'medium',
+  styleOverrides: {
+    default: { textTransform: 'none' },
+    hover: { textTransform: 'none' },
+    pressed: { textTransform: 'none' },
+    disabled: { textTransform: 'none' },
+  },
+} as SwissArmyButtonVariantProps;
