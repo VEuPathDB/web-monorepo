40a86fa89979b360753b3748f1fca610af3d4d86
Jamie Long <jtlong@upenn.edu>
2022-01-18 17:46:47 -0500

    Upgrade webpack overrides to accommodate react-scripts 5.0.0

--- packages/configs/cra-template/template/config-overrides.js
+++ packages/configs/cra-template/template/config-overrides.js
@@ -10,11 +10,41 @@ module.exports = function override(config, env) {
     ...config,
     resolve: {
       ...config.resolve,
-      modules: [ path.join(__dirname, 'node_modules'), ...(config.resolve.modules || ['node_modules']) ]
+      modules: [
+        path.join(__dirname, 'node_modules'),
+        ...(config.resolve?.modules || ['node_modules']),
+      ],
+      fallback: {
+        "assert": require.resolve("assert/"),
+        "buffer": require.resolve("buffer/"),
+        "path": require.resolve("path-browserify"),
+        "querystring": require.resolve("querystring-es3"),
+        "fs": false,
+        "stream": require.resolve("stream-browserify")
+      },
     },
     resolveLoader: {
       ...config.resolveLoader,
-      modules: [ path.join(__dirname, 'node_modules'), ...(config.resolveLoader.modules || ['node_modules']) ]
-    }
-  }
+      modules: [
+        path.join(__dirname, 'node_modules'),
+        ...(config.resolveLoader?.modules || ['node_modules']),
+      ],
+    },
+    externals: [{ jquery: 'jQuery' }],
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module?.rules,
+        // The ify-loader is needed for some plotly features.
+        // This also needs the bubleify packages installed as
+        // a dev dependency.
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          include: /node_modules/,
+          loader: 'ify-loader',
+        },
+      ],
+    },
+    stats: 'errors-warnings',
+  };
 }
