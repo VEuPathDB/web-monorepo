172dcbb6e2ee57a2395250ac775ae8ee1f89a1b5
Bob <uncoolbob@gmail.com>
2022-01-26 14:56:07 +0000

    useGeoConfig ready for use, hopefully

--- packages/libs/eda/src/lib/core/hooks/geoConfig.ts
+++ packages/libs/eda/src/lib/core/hooks/geoConfig.ts
@@ -1,10 +1,59 @@
 import { useMemo } from 'react';
 import { StudyEntity } from '../types/study';
-
+import { findGeolocationNode } from '../utils/geoVariables';
 import { leafletZoomLevelToGeohashLevel } from '../utils/visualization';
+import { sortBy } from 'lodash';
 
-type GeoConfig = {};
+/**
+ * GeoConfig is a list that corresponds to (only) entities that have geo-variables
+ *
+ * Temporary implementation that looks for a specific combination of variables underneath a single variable tree node.
+ * Assumes that aggregationVariables are in ID order (e.g. geohash_1 has the lowest ID and geohash_6 the highest)
+ *
+ */
 
-export function useGeoConfig(entities: StudyEntity[]): GeoConfig {
-  return useMemo(() => entities.filter(() => true).map(() => ({})), [entities]);
+type GeoConfig = {
+  entity: StudyEntity;
+  zoomLevelToAggregationLevel: (zoomLevel: number) => number;
+  latitudeVariableId: string;
+  longitudeVariableId: string;
+  aggregationVariableIds: string[];
+};
+
+export function useGeoConfig(entities: StudyEntity[]): GeoConfig[] {
+  return useMemo(
+    () =>
+      entities
+        .map((entity) => {
+          const geolocationNode = findGeolocationNode(entity);
+          if (geolocationNode != null) {
+            const directChildren = entity.variables.filter(
+              (variable) => variable.parentId === geolocationNode.id
+            );
+            return {
+              entity,
+              zoomLevelToAggregationLevel: leafletZoomLevelToGeohashLevel, // default leaflet to geohash mapping
+              latitudeVariableId: directChildren.find(
+                (variable) => variable.type === 'number'
+              )?.id,
+              longitudeVariableId: directChildren.find(
+                (variable) => variable.type === 'longitude'
+              )?.id,
+              aggregationVariableIds: sortBy(
+                directChildren
+                  .filter(
+                    (variable) =>
+                      variable.type === 'string' &&
+                      variable.dataShape === 'continuous'
+                  )
+                  .map((variable) => variable.id)
+              ),
+            };
+          } else {
+            return undefined;
+          }
+        })
+        .filter((item): item is GeoConfig => item != null),
+    [entities]
+  );
 }
---
+++ packages/libs/eda/src/lib/core/utils/geoVariables.ts
@@ -0,0 +1,43 @@
+import { StudyEntity, VariableTreeNode } from '../types/study';
+
+/**
+ * Given a study, search its variable tree to find a node that has the following direct children
+ *
+ * a longitude variable
+ * a number variable
+ * six string categorical variables
+ * and nothing more!
+ *
+ * returns the VariableTreNode or null
+ *
+ * This is a placeholder implementation, until more direct variable annotations (displayType) are available for b57
+ */
+export function findGeolocationNode(
+  entity: StudyEntity
+): VariableTreeNode | undefined {
+  // first find the longitude variable
+  const longitudeVariables = entity.variables.filter(
+    (variable) => variable.type === 'longitude'
+  );
+  if (longitudeVariables.length === 1) {
+    const longitudeVariable = longitudeVariables[0];
+    const siblingVariables = entity.variables.filter(
+      (variable) => variable.parentId === longitudeVariable.parentId
+    );
+    const numberSiblings = siblingVariables.filter(
+      (variable) => variable.type === 'number'
+    );
+    if (numberSiblings.length === 1) {
+      const stringCategoricals = siblingVariables.filter(
+        (variable) =>
+          variable.type === 'string' && variable.dataShape === 'categorical'
+      );
+      if (stringCategoricals.length === 6) {
+        return entity.variables.find(
+          (variable) => variable.id === longitudeVariable.parentId
+        );
+      }
+    }
+  }
+  return undefined;
+}
