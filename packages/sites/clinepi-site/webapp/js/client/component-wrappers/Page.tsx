3f9b5bbfed31090ccc83dab7e4c90e56ed987c10
asizemore <annsize@seas.upenn.edu>
2022-01-28 15:06:01 -0500

    use core-components ui theming

--- packages/sites/clinepi-site/webapp/js/client/component-wrappers/Page.tsx
+++ packages/sites/clinepi-site/webapp/js/client/component-wrappers/Page.tsx
@@ -4,10 +4,24 @@ import { Props } from '@veupathdb/wdk-client/lib/Components/Layout/Page';
 
 import { useAttemptActionClickHandler } from '@veupathdb/study-data-access/lib/data-restriction/dataRestrictionHooks';
 
+import UIThemeProvider from '@veupathdb/core-components/dist/components/theming/UIThemeProvider';
+import { colors } from '@veupathdb/core-components';
+
 export function Page(DefaultComponent: React.ComponentType<Props>) {
   return function ClinEpiPage(props: Props) {
     useAttemptActionClickHandler();
 
-    return <DefaultComponent {...props} />;
+    return (
+           <UIThemeProvider
+              theme={{
+                palette: {
+                  primary: { hue: colors.mutedCyan, level: 600 },
+                  secondary: { hue: colors.mutedRed, level: 500 },
+                },
+              }}
+            >
+              <DefaultComponent {...props} />
+            </UIThemeProvider>
+    );
   };
 }
