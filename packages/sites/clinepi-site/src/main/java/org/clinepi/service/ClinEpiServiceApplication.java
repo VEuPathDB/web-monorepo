0b44cf8f5dbf62696395c8c87724b16832d429a5
Ryan Doherty <rdoherty@upenn.edu>
2022-01-28 21:33:21 -0500

    Add configuration value to show or hide unreleased datasets/studies and expose through project service

--- packages/sites/clinepi-site/src/main/java/org/clinepi/service/ClinEpiServiceApplication.java
+++ packages/sites/clinepi-site/src/main/java/org/clinepi/service/ClinEpiServiceApplication.java
@@ -3,12 +3,12 @@ package org.clinepi.service;
 import java.util.Set;
 
 import org.clinepi.service.accessRequest.AccessRequestService;
-import org.eupathdb.common.service.EuPathServiceApplication;
-import org.eupathdb.common.service.contact.ContactUsService;
-import org.gusdb.fgputil.SetBuilder;
-import org.gusdb.wdk.service.service.user.ProfileService;
 import org.clinepi.service.services.ShinyDataService;
 import org.clinepi.service.services.ShinyQueryService;
+import org.eupathdb.common.service.EuPathServiceApplication;
+import org.gusdb.fgputil.SetBuilder;
+import org.gusdb.wdk.service.service.ProjectService;
+import org.gusdb.wdk.service.service.user.ProfileService;
 
 public class ClinEpiServiceApplication extends EuPathServiceApplication {
 
@@ -16,22 +16,22 @@ public class ClinEpiServiceApplication extends EuPathServiceApplication {
   public Set<Class<?>> getClasses() {
     return new SetBuilder<Class<?>>()
 
-      // add all WDK classes (not using Ebrc!)
+      // add all Ebrc and WDK classes
       .addAll(super.getClasses())
 
-      // ... but we do need Ebrc's ContactUsService
-      .add(ContactUsService.class)
-
-      // replace ProfileService with custom version for ClinEpi
+      // custom profile endpoint that contains study access info
       .replace(ProfileService.class, CustomProfileService.class)
 
-      // add ClinEpi services
+      // custom project endpoint provides clinepi-specific site config
+      .replace(ProjectService.class,  CustomProjectService.class)
+
+      // ability to request study/dataset access
       .add(AccessRequestService.class)
 
-      // add web service to find shiny input files
+      // provide access to shiny input files
       .add(ShinyDataService.class)
 
-      // add service to let shiny apps query db
+      // let shiny apps query db
       .add(ShinyQueryService.class) 
 
       .toSet();
