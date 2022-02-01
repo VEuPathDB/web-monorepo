614dbf2d3911de0a9a6596a62cd358739c3126a6
Connor Howington <connor.howington@gmail.com>
2022-01-31 21:39:58 -0500

    Add two solutions for map base layer prop

--- packages/libs/components/src/map/MapVEuMap.tsx
+++ packages/libs/components/src/map/MapVEuMap.tsx
@@ -23,6 +23,132 @@ import MouseTools, { MouseMode } from './MouseTools';
 import { PlotRef } from '../types/plots';
 import { ToImgopts } from 'plotly.js';
 
+const baseLayers = {
+  Street: {
+    url:
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+    attribution:
+      'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
+  },
+  Terrain: {
+    url:
+      'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}',
+    attribution:
+      'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    // minZoom='0'
+    // maxZoom='18'
+    // ext='png'
+  },
+  Satellite: {
+    url:
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    attribution:
+      'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    // DKDK testing worldmap issue - with bounds props, message like 'map data not yet availalbe' is not shown
+    bounds: [
+      [-90, -180],
+      [90, 180],
+    ],
+    noWrap: true,
+  },
+  Light: {
+    url: 'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png',
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    // maxZoom='18'
+  },
+  Dark: {
+    url:
+      'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}{r}.png',
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+    subdomains: 'abcd',
+    // maxZoom='19'
+  },
+  OSM: {
+    url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    attribution:
+      '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    // minZoom='2'
+    // maxZoom='18'
+    // noWrap='0'
+  },
+};
+
+// const baseLayers = [
+//   {
+//     name: 'Street',
+//     url:
+//       'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+//     attribution:
+//       'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
+//   },
+//   {
+//     name: 'Terrain',
+//     url:
+//       'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}',
+//     attribution:
+//       'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+//     subdomains: 'abcd',
+//     // minZoom='0'
+//     // maxZoom='18'
+//     // ext='png'
+//   },
+//   {
+//     name: 'Satellite',
+//     url:
+//       'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+//     attribution:
+//       'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+//     // DKDK testing worldmap issue - with bounds props, message like 'map data not yet availalbe' is not shown
+//     bounds: [
+//       [-90, -180],
+//       [90, 180],
+//     ],
+//     noWrap: true,
+//   },
+//   {
+//     name: 'Light',
+//     url: 'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png',
+//     attribution:
+//       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+//     // maxZoom='18'
+//   },
+//   {
+//     name: 'Dark',
+//     url:
+//       'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}{r}.png',
+//     attribution:
+//       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+//     subdomains: 'abcd',
+//     // maxZoom='19'
+//   },
+//   {
+//     name: 'OSM',
+//     url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+//     attribution:
+//       '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//     // minZoom='2'
+//     // maxZoom='18'
+//     // noWrap='0'
+//   },
+// ] as const;
+
+// const test = [
+//   {
+//     name: 'hi',
+//   },
+//   {
+//     name: 'hello',
+//   }
+// ] as const;
+
+// type NameChoice = typeof test[number]['name'];
+
+type BaseLayerChoice = keyof typeof baseLayers;
+// type BaseLayerChoice = typeof baseLayers[number]['name'];
+
 /**
  * Renders a Leaflet map with semantic zooming markers
  *
@@ -65,6 +191,12 @@ export interface MapVEuMapProps {
    * Should the mouse-mode (regular/magnifying glass) icons be shown and active?
    **/
   showMouseToolbar?: boolean;
+  /**
+   * The name of the tile layer to use. If omitted, defaults to Street.
+   */
+  baseLayer?: BaseLayerChoice;
+  /** Callback for when the base layer has changed */
+  onBaseLayerChanged?: (newBaseLayer: BaseLayerChoice) => void;
 }
 
 function MapVEuMap(props: MapVEuMapProps, ref: Ref<PlotRef>) {
@@ -80,6 +212,8 @@ function MapVEuMap(props: MapVEuMapProps, ref: Ref<PlotRef>) {
     showGrid,
     zoomLevelToGeohashLevel,
     showMouseToolbar,
+    baseLayer,
+    onBaseLayerChanged,
   } = props;
 
   // this is the React Map component's onViewPortChanged handler
@@ -155,6 +289,9 @@ function MapVEuMap(props: MapVEuMapProps, ref: Ref<PlotRef>) {
       worldCopyJump={false}
       ondragstart={() => setIsDragging(true)}
       ondragend={() => setIsDragging(false)}
+      onbaselayerchange={(event) =>
+        onBaseLayerChanged && onBaseLayerChanged(event.name as BaseLayerChoice)
+      }
       ref={mapRef}
     >
       <TileLayer
@@ -178,58 +315,24 @@ function MapVEuMap(props: MapVEuMapProps, ref: Ref<PlotRef>) {
       ) : null}
 
       <LayersControl position="topright">
-        <BaseLayer checked name="street">
-          <TileLayer
-            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
-            attribution="Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012"
-          />
-        </BaseLayer>
-        <BaseLayer name="terrain">
-          <TileLayer
-            url="https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}"
-            attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            subdomains="abcd"
-            // minZoom='0'
-            // maxZoom='18'
-            // ext='png'
-          />
-        </BaseLayer>
-        <BaseLayer name="satellite">
-          <TileLayer
-            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-            attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-            // DKDK testing worldmap issue - with bounds props, message like 'map data not yet availalbe' is not shown
-            bounds={[
-              [-90, -180],
-              [90, 180],
-            ]}
-            noWrap={true}
-          />
-        </BaseLayer>
-        <BaseLayer name="light">
-          <TileLayer
-            url="http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            // maxZoom='18'
-          />
-        </BaseLayer>
-        <BaseLayer name="dark">
-          <TileLayer
-            url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-            subdomains="abcd"
-            // maxZoom='19'
-          />
-        </BaseLayer>
-        <BaseLayer name="OSM">
-          <TileLayer
-            url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
-            // minZoom='2'
-            // maxZoom='18'
-            // noWrap='0'
-          />
-        </BaseLayer>
+        {Object.entries(baseLayers).map(([name, layerProps], i) => (
+          <BaseLayer
+            name={name}
+            key={name}
+            checked={baseLayer ? name === baseLayer : i === 0}
+          >
+            <TileLayer {...layerProps} />
+          </BaseLayer>
+        ))}
+        {/* {baseLayers.map(({ name, ...layerProps }, i) => (
+          <BaseLayer
+            name={name}
+            key={name}
+            checked={baseLayer ? name === baseLayer : i === 0}
+          >
+            <TileLayer {...(layerProps as TileLayer['props'])} />
+          </BaseLayer>
+        ))} */}
       </LayersControl>
     </Map>
   );
