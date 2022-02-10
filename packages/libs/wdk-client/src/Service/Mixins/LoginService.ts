dcc561c8c4f515f9526453cccdb9669d7e33bc1e
Jamie Long <jtlong@upenn.edu>
2022-02-10 18:10:44 -0500

    WdkService: add "logout" method

--- packages/libs/wdk-client/src/Service/Mixins/LoginService.ts
+++ packages/libs/wdk-client/src/Service/Mixins/LoginService.ts
@@ -3,35 +3,41 @@ import * as Decode from 'wdk-client/Utils/Json';
 
 /** This is for POST requests */
 export type TryLoginResponse = {
-    success: true;
-    redirectUrl: string;
+  success: true;
+  redirectUrl: string;
 } | {
-    success: false;
-    message: string;
+  success: false;
+  message: string;
 }
 
 /** This is for POST requests */
 const tryLoginDecoder: Decode.Decoder<TryLoginResponse> =
-    Decode.oneOf(
-        Decode.combine(
-            Decode.field('success', Decode.constant(true)),
-            Decode.field('redirectUrl', Decode.string)
-        ),
-        Decode.combine(
-            Decode.field('success', Decode.constant(false)),
-            Decode.field('message', Decode.string)
-        )
+  Decode.oneOf(
+    Decode.combine(
+      Decode.field('success', Decode.constant(true)),
+      Decode.field('redirectUrl', Decode.string)
+    ),
+    Decode.combine(
+      Decode.field('success', Decode.constant(false)),
+      Decode.field('message', Decode.string)
     )
+  )
 
 export default (base: ServiceBase) => {
+  function tryLogin(email: string, password: string, redirectUrl: string) {
+    return base.sendRequest(tryLoginDecoder, {
+      method: 'post',
+      path: '/login',
+      body: JSON.stringify({ email, password, redirectUrl })
+    });
+  }
 
-    function tryLogin(email: string, password: string, redirectUrl: string) {
-        return base.sendRequest(tryLoginDecoder, {
-            method: 'post',
-            path: '/login',
-            body: JSON.stringify({ email, password, redirectUrl })
-        });
-    }
+  async function logout() {
+    return fetch(
+      `${base.serviceUrl}/logout`,
+      { credentials: 'include' }
+    );
+  }
 
-    return { tryLogin };
+  return { tryLogin, logout };
 }
