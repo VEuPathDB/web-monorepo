0338b4014ad6f9108d6629625a54add4c8e9b040
Bob <uncoolbob@gmail.com>
2022-02-23 10:12:30 +0000

    simplified useEntityCount debouncing

--- packages/libs/eda/src/lib/core/hooks/entityCounts.ts
+++ packages/libs/eda/src/lib/core/hooks/entityCounts.ts
@@ -1,9 +1,9 @@
 import { preorder } from '@veupathdb/wdk-client/lib/Utils/TreeUtils';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback } from 'react';
 import { Filter } from '../types/filter';
 import { usePromise } from './promise';
 import { useStudyMetadata, useSubsettingClient } from './workspace';
-import { debounce } from 'lodash';
+import { useDebounce } from '../hooks/debouncing';
 import { isStubEntity, STUB_ENTITY } from './study';
 
 export type EntityCounts = Record<string, number>;
@@ -15,18 +15,8 @@ export function useEntityCounts(filters: Filter[] = []) {
   const isStub = isStubEntity(rootEntity);
 
   // use JSON version in dependencies to prevent unnecessary recalculations
-  const filtersJSON = JSON.stringify(filters);
-
-  // debounce the dependencies of the useCallback below
-  const [counter, setCounter] = useState(0);
-  const debouncedSetCounter = useMemo(
-    () => debounce(setCounter, 2000, { leading: true, trailing: true }),
-    []
-  );
-  useEffect(() => debouncedSetCounter.cancel, [debouncedSetCounter.cancel]);
-  useEffect(() => {
-    debouncedSetCounter((count) => count + 1);
-  }, [rootEntity, subsettingClient, id, filtersJSON, debouncedSetCounter]);
+  // debounce to prevent a back end call for each click on a filter checkbox
+  const filtersJSON = useDebounce(JSON.stringify(filters), 2000);
 
   return usePromise(
     useCallback(async () => {
@@ -50,6 +40,6 @@ export function useEntityCounts(filters: Filter[] = []) {
         counts[entity.id] = count;
       }
       return counts;
-    }, [counter]) // debounced [rootEntity, subsettingClient, id, filtersJSON]
+    }, [rootEntity, isStub, subsettingClient, id, filtersJSON])
   );
 }
