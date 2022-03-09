f5e53ea560da607df4906bd27f30382032f2edda
Jeremy Myers <inernestmusic@gmail.com>
2022-03-09 14:46:48 -0500

    Create hook for localStorage

---
+++ packages/libs/wdk-client/src/Hooks/LocalBackedState.ts
@@ -0,0 +1,38 @@
+import { useCallback, useState } from 'react';
+
+type Encoder<T> = (t: T) => string;
+type Parser<T> = (s: string) => T;
+
+export function useLocalBackedState<T>(
+  defaultValue: T,
+  key: string,
+  encode: Encoder<T>,
+  parse: Parser<T>,
+): [ T, (newState: T) => void ] {
+  let initialValue = defaultValue;
+
+    try {
+      const storedStringValue = window.localStorage.getItem(key);
+
+    if (storedStringValue !== null) {
+      initialValue = parse(storedStringValue);
+    }
+  } catch (e) {
+    console.warn(
+      `Failed attempt to retrieve state value at local key ${key}: ${e}; falling back to component state`
+    );
+  }
+
+  const [ state, setState ] = useState(initialValue);
+
+  const setLocalBackedState = useCallback((newValue: T) => {
+    try {
+      window.localStorage.setItem(key, encode(newValue));
+    } catch {
+      console.warn(`Failed attempt to persist state value ${newValue} at local key ${key}; falling back to component state`);
+    }
+    setState(newValue);
+  }, [ encode ]);
+
+  return [ state, setLocalBackedState ];
+};
