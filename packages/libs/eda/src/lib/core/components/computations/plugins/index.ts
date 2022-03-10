0ed4967a6ec533acc1e2f4bd1cc68691c1bf6a50
asizemore <annsize@seas.upenn.edu>
2022-03-10 12:52:06 -0500

    add three non-computational apps to mbio eda

--- packages/libs/eda/src/lib/core/components/computations/plugins/index.ts
+++ packages/libs/eda/src/lib/core/components/computations/plugins/index.ts
@@ -1,8 +1,14 @@
 import { ComputationPlugin } from '../Types';
 import { plugin as alphadiv } from './alphaDiv';
 import { plugin as pass } from './pass';
+import { plugin as distributions } from './distributions';
+import { plugin as countsandproportions } from './countsandproportions';
+import { plugin as xyrelationships } from './xyrelationships';
 
 export const plugins: Record<string, ComputationPlugin> = {
   alphadiv,
+  countsandproportions,
+  distributions,
   pass,
+  xyrelationships,
 };
