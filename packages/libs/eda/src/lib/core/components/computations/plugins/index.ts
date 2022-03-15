83eafb2a6876f6cbbd41b5e63e43e8cd98ea62d4
asizemore <annsize@seas.upenn.edu>
2022-03-15 17:15:33 -0400

    make plugin file naming consistent

--- packages/libs/eda/src/lib/core/components/computations/plugins/index.ts
+++ packages/libs/eda/src/lib/core/components/computations/plugins/index.ts
@@ -2,9 +2,9 @@ import { ComputationPlugin } from '../Types';
 import { plugin as alphadiv } from './alphaDiv';
 import { plugin as pass } from './pass';
 import { plugin as distributions } from './distributions';
-import { plugin as countsandproportions } from './countsandproportions';
+import { plugin as countsandproportions } from './countsAndProportions';
 import { plugin as abundance } from './abundance';
-import { plugin as xyrelationships } from './xyrelationships';
+import { plugin as xyrelationships } from './xyRelationships';
 
 export const plugins: Record<string, ComputationPlugin> = {
   abundance,
