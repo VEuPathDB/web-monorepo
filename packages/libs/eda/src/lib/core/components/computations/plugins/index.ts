ed7151922af023fa573479d5d9740aed498b0d11
asizemore <annsize@seas.upenn.edu>
2022-03-15 17:02:30 -0400

    add ranked abundance plugin

--- packages/libs/eda/src/lib/core/components/computations/plugins/index.ts
+++ packages/libs/eda/src/lib/core/components/computations/plugins/index.ts
@@ -3,9 +3,11 @@ import { plugin as alphadiv } from './alphaDiv';
 import { plugin as pass } from './pass';
 import { plugin as distributions } from './distributions';
 import { plugin as countsandproportions } from './countsandproportions';
+import { plugin as abundance } from './abundance';
 import { plugin as xyrelationships } from './xyrelationships';
 
 export const plugins: Record<string, ComputationPlugin> = {
+  abundance,
   alphadiv,
   countsandproportions,
   distributions,
