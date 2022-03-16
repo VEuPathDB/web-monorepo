5e792380bef52bbc3b6d436ff4ec1d2e2d4c5a08
Jamie Long <jtlong@upenn.edu>
2022-03-16 00:23:02 -0400

    Remove unused bigwig utilities

--- packages/libs/user-datasets/src/lib/Components/UserDatasetUtils.jsx
+++ packages/libs/user-datasets/src/lib/Components/UserDatasetUtils.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export function makeClassifier(...classNames) {
   return (substyle = null) =>
     classNames
@@ -17,27 +15,6 @@ export function textCell(prop, transform) {
   const getValue =
     typeof transform === 'function' ? transform : (value) => value;
   return ({ row }) => (prop in row ? <span>{getValue(row[prop])}</span> : null);
-}
-
-export function getBigwigStatusUrl(datasetId) {
-  if (typeof datasetId !== 'number')
-    throw new TypeError(
-      `Can't build BigwigStatusUrl; invalid datasetId given (${datasetId}) [${typeof datasetId}]`
-    );
-  return `/service/users/current/user-datasets/${datasetId}/monitor-bigwig-tracks`;
-}
-
-export function getBigwigUploadUrl(datasetId, filename) {
-  if (typeof datasetId !== 'number')
-    throw new TypeError(
-      `Can't build BigwigUploadUrl; invalid datasetId given (${datasetId}) [${typeof datasetId}]`
-    );
-  if (typeof filename !== 'string')
-    throw new TypeError(
-      `Can't build BigwigUploadUrl; invalid filename given (${filename}) [${typeof filename}]`
-    );
-
-  return `/service/users/current/user-datasets/${datasetId}/upload-bigwig-track?datafileName=${filename}`;
 }
 
 export function getDownloadUrl(datasetId, filename) {
