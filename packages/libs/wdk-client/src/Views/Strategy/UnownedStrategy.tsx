8508fde2fe21c8ca324c6effbea55d88d591198e
Jeremy Myers <inernestmusic@gmail.com>
2022-03-21 13:08:36 -0400

    Remove filler onClose since the prop type is now optional

--- packages/libs/wdk-client/src/Views/Strategy/UnownedStrategy.tsx
+++ packages/libs/wdk-client/src/Views/Strategy/UnownedStrategy.tsx
@@ -9,17 +9,18 @@ export default function UnownedStrategy() {
     <Banner
       banner={{
         type: 'danger',
-        message: <div style={{ fontSize: '1.25em' }}>
-          To access one of your strategies, please make sure you are logged in. <br />
-          If you are already logged in, then either the requested strategy does not exist, or it belongs to another user. <br />
-          <br />
-          If another user is sharing a strategy with you, ask them to use the share button (<IconAlt fa={StrategyActions.share.iconName} />) in their panel,
-            to generate a valid URL that you may use to make a copy of their strategy.  <br />
-          <br />
-          <Link to="/workspace/strategies">Dismiss</Link>
-        </div>
+        message:
+          <div style={{ fontSize: '1.25em' }}>
+            To access one of your strategies, please make sure you are logged in. <br />
+            If you are already logged in, then either the requested strategy does not exist, or it belongs to another user. <br />
+            <br />
+            If another user is sharing a strategy with you, ask them to use the share button (<IconAlt fa={StrategyActions.share.iconName} />) in their panel,
+              to generate a valid URL that you may use to make a copy of their strategy.  <br />
+            <br />
+            <Link to="/workspace/strategies">Dismiss</Link>
+          </div>,
+        pinned: true
       }}
-      onClose={() => null}
     />
   );
 }
