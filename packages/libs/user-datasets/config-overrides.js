9acad1e74434bba5d718522f3c3290118ef69936
Jamie Long <jtlong@upenn.edu>
2022-03-25 15:51:47 -0400

    Remove old webpack config which was being used to support "yarn link"

--- packages/libs/user-datasets/config-overrides.js
+++ packages/libs/user-datasets/config-overrides.js
@@ -1,34 +1,16 @@
-const path = require('path');
-
-/*
- * The following gives preference to the project's root node_modules directory when loading modules and loaders.
- * This necessary when using modules that are symlinked into node_modules (e.g., using `yarn link` or `npm link`).
- * This should not have any impact on building npm artifacts, as it is only used by webpack.
- */
 module.exports = function override(config, env) {
   return {
     ...config,
     resolve: {
       ...config.resolve,
-      modules: [
-        path.join(__dirname, 'node_modules'),
-        ...(config.resolve?.modules || ['node_modules']),
-      ],
       fallback: {
-        "assert": require.resolve("assert/"),
-        "buffer": require.resolve("buffer/"),
-        "path": require.resolve("path-browserify"),
-        "querystring": require.resolve("querystring-es3"),
-        "fs": false,
-        "stream": require.resolve("stream-browserify")
+        assert: require.resolve('assert/'),
+        buffer: require.resolve('buffer/'),
+        path: require.resolve('path-browserify'),
+        querystring: require.resolve('querystring-es3'),
+        fs: false,
+        stream: require.resolve('stream-browserify'),
       },
-    },
-    resolveLoader: {
-      ...config.resolveLoader,
-      modules: [
-        path.join(__dirname, 'node_modules'),
-        ...(config.resolveLoader?.modules || ['node_modules']),
-      ],
     },
     externals: [{ jquery: 'jQuery' }],
     module: {
@@ -47,4 +29,4 @@ module.exports = function override(config, env) {
     },
     stats: 'errors-warnings',
   };
-}
+};
