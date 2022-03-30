a487b87208c63e9acba32660b42b35f4ad1cfc92
Jamie Long <jtlong@upenn.edu>
2022-03-30 14:43:06 -0400

    Header: display "Studies" loading indicator iff the "studies" OR "permissions" still need to be loaded

--- packages/sites/clinepi-site/webapp/js/client/data/headerMenuItems.jsx
+++ packages/sites/clinepi-site/webapp/js/client/data/headerMenuItems.jsx
@@ -3,129 +3,137 @@ import { StudyMenuItem, StudyMenuSearch } from '@veupathdb/web-common/lib/App/St
 import { menuItemsFromSocials, iconMenuItemsFromSocials } from '@veupathdb/web-common/lib/App/Utils/Utils';
 import { getStaticSiteData } from '../selectors/siteData';
 import { STATIC_ROUTE_PATH, makeEdaRoute } from '@veupathdb/web-common/lib/routes';
-import { withPermissions } from '@veupathdb/study-data-access/lib/data-restriction/Permissions';
 
-const ClinEpiStudyMenuItem = withPermissions(StudyMenuItem);
+export default function makeHeaderMenuItemsFactory(permissionsValue) {
+  return function makeHeaderMenuItems(state, props) {
+    const { siteConfig } = state.globalData;
+    const siteData = getStaticSiteData(state);
+    const { studies } = siteData;
+    const { youtubeUrl } = siteConfig;
+    const socialIcons = iconMenuItemsFromSocials(siteConfig);
+    const socialLinks = menuItemsFromSocials(siteConfig);
+    const searchTerm = props.searchTerm;
+    const setSearchTerm = props.setSearchTerm;
 
-export default function headerMenuItems (state, props) {
-  const { siteConfig } = state.globalData;
-  const siteData = getStaticSiteData(state);
-  const { studies } = siteData;
-  const { youtubeUrl } = siteConfig;
-  const socialIcons = iconMenuItemsFromSocials(siteConfig);
-  const socialLinks = menuItemsFromSocials(siteConfig);
-  const searchTerm = props.searchTerm;
-  const setSearchTerm = props.setSearchTerm;
+    const studyTableIconStyle = {
+      fontSize: '1.4em',
+      marginRight: '.25em',
+      position: 'relative',
+      bottom: '-.25em'
+    }
 
-  const studyTableIconStyle = {
-    fontSize: '1.4em',
-    marginRight: '.25em',
-    position: 'relative',
-    bottom: '-.25em'
-  }
-
-  return {
-    mainMenu: [
-      {
-        id: 'studies',
-        text: 'Studies',
-        children:[
-          {
-            text: (
-              <div style={{ padding: '0.5em 0' }}>
-                <i className="ebrc-icon-table" style={studyTableIconStyle}></i> Study summaries table
-              </div>
-            ),
-            route: '/search/dataset/Studies/result'
-          },
-          {
-            text: <StudyMenuSearch searchTerm={searchTerm} onSearchTermChange={setSearchTerm}/>
-          }
-        ].concat(  
-          studies.entities != null
-          ? studies.entities.filter(study => (study.name.toLowerCase().includes(searchTerm.toLowerCase()))).map(study => ({ text: <ClinEpiStudyMenuItem study={study} config={siteConfig} /> }))
-          : [{ text: <i style={{ fontSize: '13em' }} className="fa fa-align-justify"/> }])
-      },
-      {
-        id: 'workspace',
-        text: 'Workspace',
-        children: [
-          {
-            text: 'My Analyses',
-            route: makeEdaRoute()
-          },
-          {
-            text: 'Public Analyses',
-            route: `${makeEdaRoute()}/public`
-          },
-        ]
-      },
-      {
-        id: 'help',
-        text: 'Help',
-        children: [
-          { 
-            text: 'Tutorials',
-            route: `${STATIC_ROUTE_PATH}/ClinEpiDB/tutorials.html`
-          },
-          { 
-            text: 'Webinars',
-            route: `${STATIC_ROUTE_PATH}/ClinEpiDB/webinars.html`
-          },
-          {
-            text: 'Workshops',
-            route: `${STATIC_ROUTE_PATH}/ClinEpiDB/workshops.html`
-          },
-          {
-            text: 'External Resources',
-            route: `${STATIC_ROUTE_PATH}/ClinEpiDB/resources.html`
-          }
-        ]
-      },
-      {
-        id: 'about',
-        text: 'About',
-        children: [
-          {
-            text: 'About ClinEpiDB',
-            route: `${STATIC_ROUTE_PATH}/ClinEpiDB/about.html`
-          },
-          { 
-            text: 'Features coming soon',
-            route: `${STATIC_ROUTE_PATH}/ClinEpiDB/newfeatures.html`
-          },
-          {
-            text: 'News',
-            route: `${STATIC_ROUTE_PATH}/ClinEpiDB/news.html`
-          },
-          { 
-            text: 'FAQ',
-            route: `${STATIC_ROUTE_PATH}/ClinEpiDB/faq.html`
-          },
-          {
-            text: 'Data Access & Use Policy',
-            route: `${STATIC_ROUTE_PATH}/ClinEpiDB/access_and_use.html`
-          },
-          {
-            text: 'Publications about ClinEpiDB',
-            url: 'https://gatesopenresearch.org/articles/3-1661',
-            target: '_blank'
-          },
-          {
-            text: 'Publications that use our resource',
-            url: 'https://scholar.google.com/scholar?hl=en&q=ClinEpiDB',
-            target: '_blank'
-          },
-          ...socialLinks
-        ]
-      },
-      {
-        target: '_blank',
-        id: 'contactus',
-        text: 'Contact Us',
-        route: '/contact-us'
-      }
-    ],
-    iconMenu: [ ...socialIcons ]
-  }
-};
+    return {
+      mainMenu: [
+        {
+          id: 'studies',
+          text: 'Studies',
+          children:[
+            {
+              text: (
+                <div style={{ padding: '0.5em 0' }}>
+                  <i className="ebrc-icon-table" style={studyTableIconStyle}></i> Study summaries table
+                </div>
+              ),
+              route: '/search/dataset/Studies/result'
+            },
+            {
+              text: <StudyMenuSearch searchTerm={searchTerm} onSearchTermChange={setSearchTerm}/>
+            }
+          ].concat(
+            studies.entities != null && !permissionsValue.loading
+            ? studies.entities
+                .filter(
+                  study => (
+                    study.name.toLowerCase().includes(searchTerm.toLowerCase())
+                  )
+                ).map(
+                  study => ({
+                    text: <StudyMenuItem study={study} config={siteConfig} permissions={permissionsValue.permissions} />
+                  })
+                )
+            : [{ text: <i style={{ fontSize: '13em' }} className="fa fa-align-justify"/> }])
+        },
+        {
+          id: 'workspace',
+          text: 'Workspace',
+          children: [
+            {
+              text: 'My Analyses',
+              route: makeEdaRoute()
+            },
+            {
+              text: 'Public Analyses',
+              route: `${makeEdaRoute()}/public`
+            }
+          ]
+        },
+        {
+          id: 'help',
+          text: 'Help',
+          children: [
+            {
+              text: 'Tutorials',
+              route: `${STATIC_ROUTE_PATH}/ClinEpiDB/tutorials.html`
+            },
+            {
+              text: 'Webinars',
+              route: `${STATIC_ROUTE_PATH}/ClinEpiDB/webinars.html`
+            },
+            {
+              text: 'Workshops',
+              route: `${STATIC_ROUTE_PATH}/ClinEpiDB/workshops.html`
+            },
+            {
+              text: 'External Resources',
+              route: `${STATIC_ROUTE_PATH}/ClinEpiDB/resources.html`
+            }
+          ]
+        },
+        {
+          id: 'about',
+          text: 'About',
+          children: [
+            {
+              text: 'About ClinEpiDB',
+              route: `${STATIC_ROUTE_PATH}/ClinEpiDB/about.html`
+            },
+            {
+              text: 'Features coming soon',
+              route: `${STATIC_ROUTE_PATH}/ClinEpiDB/newfeatures.html`
+            },
+            {
+              text: 'News',
+              route: `${STATIC_ROUTE_PATH}/ClinEpiDB/news.html`
+            },
+            {
+              text: 'FAQ',
+              route: `${STATIC_ROUTE_PATH}/ClinEpiDB/faq.html`
+            },
+            {
+              text: 'Data Access & Use Policy',
+              route: `${STATIC_ROUTE_PATH}/ClinEpiDB/access_and_use.html`
+            },
+            {
+              text: 'Publications about ClinEpiDB',
+              url: 'https://gatesopenresearch.org/articles/3-1661',
+              target: '_blank'
+            },
+            {
+              text: 'Publications that use our resource',
+              url: 'https://scholar.google.com/scholar?hl=en&q=ClinEpiDB',
+              target: '_blank'
+            },
+            ...socialLinks
+          ]
+        },
+        {
+          target: '_blank',
+          id: 'contactus',
+          text: 'Contact Us',
+          route: '/contact-us'
+        }
+      ],
+      iconMenu: [ ...socialIcons ]
+    }
+  };
+}
