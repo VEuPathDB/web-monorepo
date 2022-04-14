607acce8d2cd0630a27bc0701455ab3068b1ef1b
Jeremy Myers <inernestmusic@gmail.com>
2022-04-14 15:24:17 -0400

    Remove UserComment files

--- packages/libs/wdk-client/src/Actions.ts
+++ packages/libs/wdk-client/src/Actions.ts
@@ -10,8 +10,6 @@ import * as FilterParamActions from 'wdk-client/Actions/FilterParamActions';
 import * as DatasetParamActions from 'wdk-client/Actions/DatasetParamActions';
 import * as TreeBoxEnumParamActions from 'wdk-client/Actions/TreeBoxEnumParamActions';
 import * as UserActions from 'wdk-client/Actions/UserActions';
-import * as UserCommentFormActions from 'wdk-client/Actions/UserCommentFormActions';
-import * as UserCommentShowActions from 'wdk-client/Actions/UserCommentShowActions';
 import * as RecordActions from 'wdk-client/Actions/RecordActions';
 import * as StaticDataActions from 'wdk-client/Actions/StaticDataActions';
 import * as SiteMapActions from 'wdk-client/Actions/SiteMapActions';
@@ -64,8 +62,6 @@ export {
   HistogramAnalysisActions,
   MatchedTranscriptsFilterActions,
   ResultPanelActions,
-  UserCommentFormActions,
-  UserCommentShowActions,
   UserSessionActions,
   UserDatasetUploadActions,
 };
@@ -93,8 +89,6 @@ export type Action =
   | StrategyListActions.Action
   | PublicStrategyActions.Action
   | UserDatasetsActions.Action
-  | UserCommentFormActions.Action
-  | UserCommentShowActions.Action
   | ResultTableSummaryViewActions.Action
   | BlastSummaryViewActions.Action
   | GenomeSummaryViewActions.Action
