9e89cda10c4a48b46d21ab2861a1fadcc34685b2
Jamie Long <jtlong@upenn.edu>
2022-04-20 17:26:40 -0400

    Add browserslist environments for our sites

--- packages/configs/browserslist-config/index.js
+++ packages/configs/browserslist-config/index.js
@@ -8,5 +8,18 @@ module.exports = {
     "last 1 chrome version",
     "last 1 firefox version",
     "last 1 safari version"
+  ],
+  modern: [
+    "last 2 chrome versions",
+    "last 2 firefox versions",
+    "last 2 safari versions",
+    "last 2 edge versions",
+    "last 2 ios versions"
+  ],
+  legacy: [
+    "> 0%"
+  ],
+  test: [
+    "current node"
   ]
 }
