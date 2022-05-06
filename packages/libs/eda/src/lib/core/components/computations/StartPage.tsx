a42e48aa5ccbb7308b83ff6cc71419e1e84c139e
Jeremy Myers <inernestmusic@gmail.com>
2022-05-05 21:48:47 -0400

    Disable apps without plugin and render them last

--- packages/libs/eda/src/lib/core/components/computations/StartPage.tsx
+++ packages/libs/eda/src/lib/core/components/computations/StartPage.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { ComputationAppOverview } from '../../types/visualization';
+import { ComputationPlugin } from './Types';
+import { orderBy } from 'lodash';
 
 interface Props {
   baseUrl: string;
   apps: ComputationAppOverview[];
+  plugins: Record<string, ComputationPlugin>;
 }
 
 export function StartPage(props: Props) {
-  const { apps, baseUrl } = props;
+  const { apps, baseUrl, plugins } = props;
   return (
     <div>
       <h3>To get started, select an app.</h3>
@@ -18,22 +20,35 @@ export function StartPage(props: Props) {
         variables.
       </p>
       <div style={{ display: 'flex', flexDirection: 'column', width: '50%' }}>
-        {apps.map((app) => (
-          <Link
-            to={`${baseUrl}/new/${app.name}`}
-            style={{
-              padding: '1em',
-              border: '1px solid',
-              borderRadius: '.5em',
-              margin: '1em 0',
-            }}
-          >
-            <div style={{ fontWeight: 'bold', fontSize: '1.2em' }}>
-              {app.displayName}
+        {/* orderBy renders available apps ahead of those in development */}
+        {orderBy(apps, [(app) => (plugins[app.name] ? 1 : 0)], ['desc']).map(
+          (app) => (
+            <div
+              style={{
+                padding: '1em',
+                border: '1px solid',
+                borderRadius: '.5em',
+                margin: '1em 0',
+                cursor: plugins[app.name] ? 'pointer' : 'not-allowed',
+                background: plugins[app.name] ? 'transparent' : '#eee',
+                opacity: plugins[app.name] ? '1' : '0.5',
+              }}
+            >
+              <Link
+                to={`${baseUrl}/new/${app.name}`}
+                style={{
+                  pointerEvents: plugins[app.name] ? 'auto' : 'none',
+                }}
+              >
+                <div style={{ fontWeight: 'bold', fontSize: '1.2em' }}>
+                  {app.displayName}
+                  {plugins[app.name] ? '' : <i> (Coming soon!)</i>}
+                </div>
+                <div>{app.description}</div>
+              </Link>
             </div>
-            <div>{app.description}</div>
-          </Link>
-        ))}
+          )
+        )}
       </div>
     </div>
   );
