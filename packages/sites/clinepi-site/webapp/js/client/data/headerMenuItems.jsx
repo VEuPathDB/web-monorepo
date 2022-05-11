98e74903b2003d2465b30014688ead7fbfa25df8
Jamie Long <jtlong@upenn.edu>
2022-05-11 13:10:17 -0400

    Use "My Studies" workspace title; use "legacy" help route for now

--- packages/sites/clinepi-site/webapp/js/client/data/headerMenuItems.jsx
+++ packages/sites/clinepi-site/webapp/js/client/data/headerMenuItems.jsx
@@ -66,7 +66,7 @@ export default function makeHeaderMenuItemsFactory(permissionsValue) {
               useUserDatasetsWorkspace
                 ? [
                     {
-                      text: 'My Data',
+                      text: 'My Studies',
                       route: '/workspace/datasets'
                     }
                   ]
