c2d8a27a844a0992d288e9b82c72e28859bcb5a1
Jeremy Myers <inernestmusic@gmail.com>
2022-05-27 13:20:06 -0400

    Create hook to derive plugins' default configs

---
+++ packages/libs/eda/src/lib/core/components/computations/getAppsDefaultConfigs.tsx
@@ -0,0 +1,40 @@
+import { useStudyMetadata } from '../..';
+import { useCollectionVariables } from '../../hooks/study';
+import { ComputationAppOverview } from '../../types/visualization';
+
+const defaultValues = {
+  abundance: {
+    name: 'RankedAbundanceComputation',
+    method: 'median',
+    methodPropertyName: 'rankingMethod',
+  },
+  alphadiv: {
+    name: 'AlphaDivComputation',
+    method: 'shannon',
+    methodPropertyName: 'alphaDivMethod',
+  },
+};
+
+export const useDefaultPluginConfiguration = (
+  apps: ComputationAppOverview[]
+) => {
+  const studyMetadata = useStudyMetadata();
+  const collections = useCollectionVariables(studyMetadata.rootEntity);
+  return apps.map((app) => {
+    // @ts-ignore
+    const defaultObject = defaultValues[app.name];
+    if (!defaultObject) return null;
+    return {
+      name: app.name,
+      configuration: {
+        name: defaultObject.name,
+        collectionVariable: {
+          variableId: collections[0].id,
+          entityId: collections[0].entityId,
+        },
+        [defaultObject.methodPropertyName]: defaultObject.method,
+        description: `Data: ${collections[0].entityDisplayName}: ${collections[0].displayName}; Method: ${defaultObject.method}`,
+      },
+    };
+  });
+};
