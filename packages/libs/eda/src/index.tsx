ad0c8292a95cb55c874673723808680d37f0c56c
Dave Falke <dfalke@uga.edu>
2022-05-31 15:09:16 -0400

    Remove @veupathdb/web-common devDependency and backfill some styling

--- packages/libs/eda/src/index.tsx
+++ packages/libs/eda/src/index.tsx
@@ -10,11 +10,13 @@ import React, {
 
 import { partial } from 'lodash';
 
-import { initialize } from '@veupathdb/web-common/lib/bootstrap';
+import {
+  initialize,
+  wrapComponents,
+} from '@veupathdb/wdk-client/lib/Core/main';
 import { Link } from '@veupathdb/wdk-client/lib/Components';
 import { RouteEntry } from '@veupathdb/wdk-client/lib/Core/RouteEntry';
 import '@veupathdb/wdk-client/lib/Core/Style/index.scss';
-import '@veupathdb/web-common/lib/styles/client.scss';
 import { Props } from '@veupathdb/wdk-client/lib/Components/Layout/Page';
 
 import { DataRestrictionDaemon } from '@veupathdb/study-data-access/lib/data-restriction';
@@ -37,7 +39,7 @@ import { useAttemptActionClickHandler } from '@veupathdb/study-data-access/lib/d
 import { useCoreUIFonts } from '@veupathdb/coreui/dist/hooks';
 
 // Definitions
-import { colors } from '@veupathdb/coreui';
+import { colors, H3 } from '@veupathdb/coreui';
 
 import './index.css';
 
@@ -64,6 +66,52 @@ export const DevLoginFormContext = createContext<DevLoginFormState>({
   setLoginFormVisible: () => {},
 });
 
+wrapComponents({
+  Header: () => Header,
+  Footer: () => () => null,
+  Page: (DefaultComponent: React.ComponentType<Props>) => {
+    return function ClinEpiPage(props: Props) {
+      const [loginFormVisible, setLoginFormVisible] = useState(false);
+      const loginFormContext = useMemo(
+        () => ({
+          loginFormVisible,
+          setLoginFormVisible,
+        }),
+        [loginFormVisible]
+      );
+
+      useEffect(() => {
+        if (process.env.REACT_APP_DISABLE_DATA_RESTRICTIONS === 'true') {
+          disableRestriction();
+        } else {
+          enableRestriction();
+        }
+      }, []);
+
+      useAttemptActionClickHandler();
+      useCoreUIFonts();
+
+      return (
+        <DevLoginFormContext.Provider value={loginFormContext}>
+          <DataRestrictionDaemon
+            makeStudyPageRoute={(id: string) => `/eda/${id}`}
+          />
+          <UIThemeProvider
+            theme={{
+              palette: {
+                primary: { hue: colors.mutedCyan, level: 600 },
+                secondary: { hue: colors.mutedRed, level: 500 },
+              },
+            }}
+          >
+            <DefaultComponent {...props} />
+          </UIThemeProvider>
+        </DevLoginFormContext.Provider>
+      );
+    };
+  },
+});
+
 initialize({
   rootUrl,
   rootElement,
@@ -72,7 +120,7 @@ initialize({
       path: '/',
       component: () => (
         <div>
-          <h1>EDA Links</h1>
+          <H3>EDA Links</H3>
           <ul>
             <li>
               <Link to="/eda">EDA Workspace</Link>
@@ -118,50 +166,6 @@ initialize({
     },
     ...routes,
   ],
-  componentWrappers: {
-    SiteHeader: () => Header,
-    Page: (DefaultComponent: React.ComponentType<Props>) => {
-      return function ClinEpiPage(props: Props) {
-        const [loginFormVisible, setLoginFormVisible] = useState(false);
-        const loginFormContext = useMemo(
-          () => ({
-            loginFormVisible,
-            setLoginFormVisible,
-          }),
-          [loginFormVisible]
-        );
-
-        useEffect(() => {
-          if (process.env.REACT_APP_DISABLE_DATA_RESTRICTIONS === 'true') {
-            disableRestriction();
-          } else {
-            enableRestriction();
-          }
-        }, []);
-
-        useAttemptActionClickHandler();
-        useCoreUIFonts();
-
-        return (
-          <DevLoginFormContext.Provider value={loginFormContext}>
-            <DataRestrictionDaemon
-              makeStudyPageRoute={(id: string) => `/eda/${id}`}
-            />
-            <UIThemeProvider
-              theme={{
-                palette: {
-                  primary: { hue: colors.mutedCyan, level: 600 },
-                  secondary: { hue: colors.mutedRed, level: 500 },
-                },
-              }}
-            >
-              <DefaultComponent {...props} />
-            </UIThemeProvider>
-          </DevLoginFormContext.Provider>
-        );
-      };
-    },
-  },
   wrapWdkDependencies: partial(wrapWdkDependencies, '/eda-dataset-access'),
   endpoint,
   additionalMiddleware: [reduxMiddleware],
