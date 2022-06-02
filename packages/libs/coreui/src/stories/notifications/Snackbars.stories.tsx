5d2e8ba87bd87c6e5896898b8c773b1ac3d66e47
Jamie Long <jtlong@upenn.edu>
2022-06-02 17:49:47 -0400

    Initial commit of stories for snackbar

---
+++ packages/libs/coreui/src/stories/notifications/Snackbars.stories.tsx
@@ -0,0 +1,76 @@
+import { ReactNode } from 'react';
+
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { OptionsObject } from 'notistack';
+
+import { FilledButton } from '../../components/buttons';
+
+import makeSnackbarProvider from '../../components/notifications/SnackbarProvider';
+import useSnackbar from '../../components/notifications/useSnackbar';
+
+const SnackbarProvider = makeSnackbarProvider();
+
+export default {
+  title: 'Notifications/Snackbars',
+  component: SnackbarProvider
+} as Meta;
+
+interface SnackbarStoryArgs {
+  message: ReactNode;
+  options?: OptionsObject;
+}
+
+const Template: Story<SnackbarStoryArgs> = (args) => {
+  return (
+    <SnackbarProvider styleProps={{}}>
+      <SnackbarContainer {...args} />
+    </SnackbarProvider>
+  );
+}
+
+function SnackbarContainer(props: SnackbarStoryArgs) {
+  const { enqueueSnackbar } = useSnackbar();
+
+  return (
+    <FilledButton
+      text="Open Snackbar"
+      onPress={() => {
+        enqueueSnackbar(
+          props.message,
+          props.options
+        );
+      }}
+    />
+  );
+}
+
+export const Success = Template.bind({});
+Success.args = {
+  message: 'This is a persisted "success" snackbar',
+  options: {
+    variant: 'success',
+    persist: true,
+  }
+} as SnackbarStoryArgs;
+
+export const Info = Template.bind({});
+Info.args = {
+  message: 'This is an "info" snackbar',
+  options: undefined,
+} as SnackbarStoryArgs;
+
+export const Error = Template.bind({});
+Error.args = {
+  message: 'This is an "error" snackbar',
+  options: {
+    variant: 'error',
+  }
+} as SnackbarStoryArgs;
+
+export const Warning = Template.bind({});
+Warning.args = {
+  message: 'This is a "warning" snackbar',
+  options: {
+    variant: 'warning',
+  }
+} as SnackbarStoryArgs;
