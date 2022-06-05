aa65c38c69a747bf5a4dfd15a19af62fea6bbb69
Jamie Long <jtlong@upenn.edu>
2022-06-05 14:12:23 -0400

    Introduce store module + hook for programmatic display of a snackbar in response to a redux action

---
+++ packages/libs/wdk-client/src/Actions/NotificationActions.ts
@@ -0,0 +1,50 @@
+import {
+  OptionsObject,
+  SnackbarMessage,
+  SnackbarKey,
+} from 'notistack';
+
+import { v4 as uuid } from 'uuid';
+
+import {
+  makeActionCreator,
+  InferAction,
+} from 'wdk-client/Utils/ActionCreatorUtils';
+
+/** Dispatch this action to programmatically enqueue a snackbar */
+export const enqueueSnackbar = makeActionCreator(
+  'notifications/enqueue-snackbar',
+  (message: SnackbarMessage, options?: OptionsObject) => {
+    return {
+      message,
+      options: {
+        ...options,
+        key: options?.key ?? uuid()
+      }
+    };
+  }
+);
+
+/** Used internally to indicate that an enqueued snackbar has been displayed */
+export const _displaySnackbar = makeActionCreator(
+  'notifications/display-snackbar',
+  (key: SnackbarKey) => ({ key })
+);
+
+/** Use this action to programmatically close a snackbar */
+export const closeSnackbar = makeActionCreator(
+  'notifications/close-snackbar',
+  (key?: SnackbarKey) => ({ key })
+);
+
+/** Used internally to remove closed snackbars from the queue */
+export const _dequeueSnackbar = makeActionCreator(
+  'notifications/dequeue-snackbar',
+  (key: SnackbarKey) => ({ key })
+);
+
+export type Action =
+  | InferAction<typeof enqueueSnackbar>
+  | InferAction<typeof _displaySnackbar>
+  | InferAction<typeof closeSnackbar>
+  | InferAction<typeof _dequeueSnackbar>;
