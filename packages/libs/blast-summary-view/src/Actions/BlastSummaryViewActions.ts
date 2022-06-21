28da2def04943845b135616cd51e762d7045c64f
Jamie Long <jtlong@upenn.edu>
2022-06-21 00:08:42 -0400

    Port BLAST summary view actions and types from ApiCommonWebsite

---
+++ packages/libs/blast-summary-view/src/Actions/BlastSummaryViewActions.ts
@@ -0,0 +1,30 @@
+import {
+  makeActionCreator,
+  InferAction,
+} from '@veupathdb/wdk-client/lib/Utils/ActionCreatorUtils';
+import { BlastSummaryViewReport } from '../Utils/types';
+import { ResultType } from '@veupathdb/wdk-client/lib/Utils/WdkResult';
+
+export const requestBlastSummaryReport = makeActionCreator(
+  'blastSummaryView/requestBlastSummaryReport',
+  (viewId: string, resultType: ResultType) => ({ viewId, resultType })
+);
+
+export const fulfillBlastSummaryReport = makeActionCreator(
+  'blastSummaryView/fulfillBlastSummaryReport',
+  (
+    viewId: string,
+    resultType: ResultType,
+    blastInfo: BlastSummaryViewReport
+  ) => ({ viewId, blastInfo, resultType })
+);
+
+export const rejectBlastSummaryReport = makeActionCreator(
+  'blastSummaryView/rejectBlastSummaryReport',
+  (viewId: string, message: string) => ({ viewId, message })
+);
+
+export type Action =
+  | InferAction<typeof requestBlastSummaryReport>
+  | InferAction<typeof fulfillBlastSummaryReport>
+  | InferAction<typeof rejectBlastSummaryReport>;
