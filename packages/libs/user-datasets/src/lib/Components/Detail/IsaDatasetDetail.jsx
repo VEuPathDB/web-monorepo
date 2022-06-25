42b0f1ed38e0f0468167b13464006b5c1350b066
Jamie Long <jtlong@upenn.edu>
2022-06-25 19:41:56 -0400

    Initial commit of IsaDatasetDetail

---
+++ packages/libs/user-datasets/src/lib/Components/Detail/IsaDatasetDetail.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+import UserDatasetDetail from './UserDatasetDetail';
+
+class IsaDatasetDetail extends UserDatasetDetail {
+  constructor(props) {
+    super(props);
+    this.renderEdaLinkout = this.renderEdaLinkout.bind(this);
+  }
+
+  renderEdaLinkout() {
+    const {
+      config: { displayName },
+      userDataset: { isInstalled },
+      edaWorkspaceUrl,
+    } = this.props;
+
+    return !isInstalled || !edaWorkspaceUrl ? null : (
+      <section id="eda-linkout">
+        <h2>
+          <Link to={edaWorkspaceUrl}>Explore in {displayName}</Link>
+        </h2>
+      </section>
+    );
+  }
+
+  getPageSections() {
+    const [headerSection, , fileSection] = super.getPageSections();
+
+    return [headerSection, this.renderEdaLinkout, fileSection];
+  }
+}
+
+export default IsaDatasetDetail;
