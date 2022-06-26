b2158a9035dfd1c94ab005878e34e51b50314f0f
Jamie Long <jtlong@upenn.edu>
2022-06-25 20:29:29 -0400

    Wire up ISA dataset details

--- packages/sites/clinepi-site/webapp/js/client/routes/userDatasetRoutes.tsx
+++ packages/sites/clinepi-site/webapp/js/client/routes/userDatasetRoutes.tsx
@@ -1,7 +1,16 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useMemo } from 'react';
 
 import { Loading } from '@veupathdb/wdk-client/lib/Components';
 import { RouteEntry } from '@veupathdb/wdk-client/lib/Core/RouteEntry';
+
+import { makeEdaRoute } from '@veupathdb/web-common/lib/routes';
+import { diyUserDatasetIdToWdkRecordId } from '@veupathdb/web-common/lib/util/diyDatasets';
+
+import IsaDatasetDetail from '@veupathdb/user-datasets/lib/Components/Detail/IsaDatasetDetail';
+
+import {
+  UserDatasetDetailProps
+} from '@veupathdb/user-datasets/lib/Controllers/UserDatasetDetailController';
 
 import {
   uploadTypeConfig
@@ -15,16 +24,32 @@ export const userDatasetRoutes: RouteEntry[] = [
   {
     path: '/workspace/datasets',
     exact: false,
-    component: () => (
-      <Suspense fallback={<Loading />}>
-        <UserDatasetRouter
-          availableUploadTypes={availableUploadTypes}
-          detailsPageTitle="My Data Set"
-          helpRoute="/workspace/datasets/help"
-          workspaceTitle="My Studies"
-          uploadTypeConfig={uploadTypeConfig}
-        />
-      </Suspense>
-    ),
+    component: function ClinEpiUserDatasetRouter() {
+      const detailComponentsByTypeName = useMemo(() => ({
+        ISA: function ClinEpiIsaDatasetDetail(props: UserDatasetDetailProps) {
+          const wdkDatasetId = diyUserDatasetIdToWdkRecordId(props.userDataset.id);
+
+          return (
+            <IsaDatasetDetail
+              {...props}
+              edaWorkspaceUrl={`${makeEdaRoute(wdkDatasetId)}/new`}
+            />
+          )
+        }
+      }), []);
+
+      return (
+        <Suspense fallback={<Loading />}>
+          <UserDatasetRouter
+            availableUploadTypes={availableUploadTypes}
+            detailsPageTitle="My User Study"
+            helpRoute="/workspace/datasets/help"
+            workspaceTitle="My User Studies"
+            uploadTypeConfig={uploadTypeConfig}
+            detailComponentsByTypeName={detailComponentsByTypeName}
+          />
+        </Suspense>
+      );
+    },
   }
 ];
