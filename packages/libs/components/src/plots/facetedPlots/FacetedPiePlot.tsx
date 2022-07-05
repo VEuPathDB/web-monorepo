d24130350cbb65ecea124887b8498f1e86191314
Jeremy Myers <inernestmusic@gmail.com>
2022-07-05 14:32:48 -0400

    Address modal bug and tweak styling in faceted plots

--- packages/libs/components/src/plots/facetedPlots/FacetedPiePlot.tsx
+++ packages/libs/components/src/plots/facetedPlots/FacetedPiePlot.tsx
@@ -7,7 +7,7 @@ import { select } from 'd3';
 export const defaultContainerStyles: PiePlotProps['containerStyles'] = {
   height: 300,
   width: 375,
-  marginLeft: '0.75rem',
+  // marginLeft: '0.75rem',
   marginBottom: '0.25rem',
   border: '1px solid #dedede',
   boxShadow: '1px 1px 4px #00000066',
