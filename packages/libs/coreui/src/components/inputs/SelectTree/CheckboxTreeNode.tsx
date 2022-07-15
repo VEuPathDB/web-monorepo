c9de8fe44a81033621e4d623268b821af1fb99c5
Jeremy Myers <inernestmusic@gmail.com>
2022-07-15 17:26:49 -0400

    Create internal components for SelectTree

---
+++ packages/libs/coreui/src/components/inputs/SelectTree/CheckboxTreeNode.tsx
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+import { isLeaf } from './Utils';
+import IndeterminateCheckbox, { IndeterminateCheckboxProps } from './IndeterminateCheckbox';
+
+const visibleElement = { display: '' };
+const hiddenElement = { display: 'none' };
+
+type TreeRadioProps<T> = {
+  name: string;
+  checked: boolean;
+  value: string;
+  node: T;
+  onChange: (node: T, checked: boolean) => void;
+}
+
+function TreeRadio<T>({
+    name,
+    checked,
+    value,
+    node,
+    onChange
+}: TreeRadioProps<T>) {
+    
+    const handleClick = () => {
+        if (!checked) {
+            onChange(node, false);
+        }
+    };
+
+    return (
+        <input 
+            type="radio"
+            name={name}
+            value={value}
+            checked={checked}
+            onChange={handleClick}
+        />
+    )
+}
+
+type NodeState = {
+  isSelected: boolean;
+  isVisible: boolean;
+  isIndeterminate?: boolean;
+  isExpanded?: boolean;
+}
+
+export type CustomCheckboxes<T> = {[index: string]: React.ComponentType<Partial<IndeterminateCheckboxProps<T>>>};
+
+type Props<T> = {
+  node: T;
+  name: string;
+  path: number[];
+  getNodeState: (node: T) => NodeState;
+  isSelectable: boolean;
+  isMultiPick: boolean;
+  isActiveSearch: boolean;
+  toggleExpansion: (node: T) => void;
+  toggleSelection: (node: T, checked: boolean) => void;
+  getNodeId: (node: T) => string;
+  getNodeChildren: (node: T) => T[];
+  renderNode: (node: T, path?: number[]) => React.ReactNode;
+  customCheckboxes?: CustomCheckboxes<T>;
+  shouldExpandOnClick: boolean;
+}
+
+export default function CheckboxTreeNode<T>({
+    name,
+    node,
+    path,
+    getNodeState,
+    isSelectable,
+    isMultiPick,
+    isActiveSearch,
+    toggleSelection,
+    toggleExpansion,
+    getNodeId,
+    getNodeChildren,
+    renderNode,
+    customCheckboxes,
+    shouldExpandOnClick
+  }: Props<T>
+) {
+    // We have to apply the generic type `T` to these child components. This is
+    // a known TypeScript issue and will likely be solved in the future.
+    // const IndeterminateCheckboxT = IndeterminateCheckbox as new () => IndeterminateCheckbox<T>;
+    // const TreeRadioT = TreeRadio as new () => TreeRadio<T>;
+
+    let { isSelected, isIndeterminate, isVisible, isExpanded } = getNodeState(node);
+    let isLeafNode = isLeaf(node, getNodeChildren);
+    let nodeVisibilityCss = isVisible ? visibleElement : hiddenElement;
+    let childrenVisibilityCss = isExpanded ? visibleElement : hiddenElement;
+    let nodeType = isLeafNode ? "leaf"
+                 : isExpanded ? "expanded"
+                 : "collapsed";
+    let inputName = isLeafNode ? name : '';
+    let nodeId = getNodeId(node);
+    const nodeElement = renderNode(node, path);
+    const commonInputProps = {
+      name: inputName,
+      checked: isSelected,
+      node,
+      value: nodeId,
+    };
+    const checkboxProps: IndeterminateCheckboxProps<T> = {...commonInputProps, indeterminate: !!isIndeterminate, toggleCheckbox: toggleSelection};
+    const CustomCheckbox = (customCheckboxes && (nodeId in customCheckboxes)) ? customCheckboxes[nodeId] : undefined;
+
+    return (
+      <li style={nodeVisibilityCss}>
+        <div>
+          {isLeafNode || isActiveSearch ? (
+            <i />
+          ) : (
+            <i
+              onClick={() => toggleExpansion(node)}
+            />
+          )}
+          {!isSelectable || (!isMultiPick && !isLeafNode) ? (
+            <div
+              onClick={shouldExpandOnClick ? () => toggleExpansion(node) : undefined}
+            >
+              {nodeElement}
+            </div>
+          ) : (
+            <label>
+              {CustomCheckbox ? <CustomCheckbox {...checkboxProps} /> : isMultiPick
+                  ? <IndeterminateCheckbox {...checkboxProps} />
+                  : <TreeRadio
+                      {...commonInputProps}
+                      onChange={toggleSelection}
+                    />
+              } {nodeElement}
+            </label>
+          )}
+        </div>
+        { !isLeafNode && isVisible && isExpanded &&
+          <ul style={childrenVisibilityCss}>
+            {getNodeChildren(node).map((child, index) =>
+              <CheckboxTreeNode
+                key={"node_" + getNodeId(child)}
+                name={name}
+                node={child}
+                path={path.concat(index)}
+                getNodeState={getNodeState}
+                isSelectable={isSelectable}
+                isMultiPick={isMultiPick}
+                isActiveSearch={isActiveSearch}
+                toggleSelection={toggleSelection}
+                toggleExpansion={toggleExpansion}
+                shouldExpandOnClick={shouldExpandOnClick}
+                getNodeId={getNodeId}
+                getNodeChildren={getNodeChildren}
+                renderNode={renderNode}
+                customCheckboxes={customCheckboxes} />
+            )}
+          </ul>
+        }
+      </li>
+    );
+}
---
+++ packages/libs/coreui/src/components/inputs/SelectTree/IndeterminateCheckbox.tsx
@@ -0,0 +1,40 @@
+import { useRef, FormEvent, useEffect } from 'react';
+
+export type IndeterminateCheckboxProps<T> = {
+  checked: boolean;
+  indeterminate: boolean;
+  name: string;
+  node: T;
+  toggleCheckbox: (node: T, selected: boolean) => void;
+  value: string;
+}
+
+/**
+ * React Component that provides a 3-state checkbox
+ */
+export default function IndeterminateCheckbox<T>({
+    checked,
+    indeterminate,
+    name,
+    node,
+    toggleCheckbox,
+    value
+}: IndeterminateCheckboxProps<T>) {
+    const nameProp = name ? { name } : {};
+    const nodeRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (!nodeRef.current) return;
+        nodeRef.current.indeterminate = indeterminate;
+    }, [indeterminate])
+
+    const handleChange = (e: FormEvent<HTMLInputElement>) => {
+        const selected = e.currentTarget.checked;
+        toggleCheckbox(node, selected);
+    };
+
+    return (
+        <input ref={nodeRef} type="checkbox" {...nameProp} value={value}
+            checked={checked} onChange={handleChange} />
+    )
+}
