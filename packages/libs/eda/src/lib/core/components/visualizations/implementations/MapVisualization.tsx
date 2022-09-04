886f9a997e539ca5ca16c555543d34fcac26870e
Bob <uncoolbob@gmail.com>
2022-09-04 14:56:37 +0100

    changed map viz back to normal behaviour

--- packages/libs/eda/src/lib/core/components/visualizations/implementations/MapVisualization.tsx
+++ packages/libs/eda/src/lib/core/components/visualizations/implementations/MapVisualization.tsx
@@ -218,7 +218,7 @@ function MapViz(props: VisualizationProps<Options>) {
     basicMarkerError,
     overlayError,
   } = useMapMarkers({
-    requireOverlay: false,
+    requireOverlay: true,
     boundsZoomLevel,
     vizConfig,
     geoConfig,
