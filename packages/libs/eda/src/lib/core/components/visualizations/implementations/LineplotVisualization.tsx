692e95b0a1ea0ddf0db2943d11b34d3ceeb02c43
Jeremy Myers <inernestmusic@gmail.com>
2023-01-26 14:39:18 -0500

    Assign n to denominator of y-axis proportion in marker tooltip

--- packages/libs/eda/src/lib/core/components/visualizations/implementations/LineplotVisualization.tsx
+++ packages/libs/eda/src/lib/core/components/visualizations/implementations/LineplotVisualization.tsx
@@ -2273,9 +2273,7 @@ function processInputData(
               extraTooltipText: categoricalMode
                 ? el.binSampleSize.map(
                     (bss) =>
-                      `n: ${(bss as { numeratorN: number }).numeratorN}/${
-                        (bss as { denominatorN: number }).denominatorN
-                      }`
+                      `n: ${(bss as { denominatorN: number }).denominatorN}`
                   )
                 : el.binSampleSize.map(
                     (bss) => `n: ${(bss as { N: number }).N}`
