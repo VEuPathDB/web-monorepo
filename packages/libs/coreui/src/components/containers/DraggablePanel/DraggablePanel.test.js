ed9d161608e55508f3f1270ddb7b7bb4c38954e7
Sam Kendrick <skendri2@upenn.edu>
2023-01-26 10:57:07 -0600

    Add stub tests

---
+++ packages/libs/coreui/src/components/containers/DraggablePanel/DraggablePanel.test.js
@@ -0,0 +1,7 @@
+describe("Draggable Panel", () => {
+  test("panels live somewhere initially.", () => {});
+  test("dragging a panel changes where it lives.", () => {});
+  test("when there are two draggables stacked on each other, only the panel the user drags will move.", () => {});
+  test("users can close draggable panels.", () => {});
+  test("users can open draggable panels.", () => {});
+});
