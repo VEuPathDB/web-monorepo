caacf1798cbb846f95eae6d57437a8f75e1fcee3
Sam Kendrick <skendri2@upenn.edu>
2023-01-30 09:53:41 -0600

    Add comment

--- packages/libs/coreui/src/components/containers/DraggablePanel/DraggablePanel.test.tsx
+++ packages/libs/coreui/src/components/containers/DraggablePanel/DraggablePanel.test.tsx
@@ -89,6 +89,20 @@ describe("Draggable Panels", () => {
   });
 });
 
+/**
+ * So we're pretty limited as regards js-dom and dragging. Here's what I would like to do:
+ * 1. Simulate dragging events on the draggable element.
+ * 2. Find the element, getBoundingClientRect for the element
+ * 3. Assert that the coordinates moved predictably.
+ *
+ * Here's the reality: jsdom doesn't do any rendering, so getBoundingClientRect() always
+ * returns 0,0,0,0. That won't change (even foreseeable long-term).
+ * You can try to mock the function to emulate the results you'd expect.
+ * https://github.com/jsdom/jsdom/issues/1590#issuecomment-243228840
+ *
+ * @param element
+ * @param destinationCoordinates
+ */
 function drag(
   element: HTMLElement,
   destinationCoordinates: DraggablePanelCoordinatePair
