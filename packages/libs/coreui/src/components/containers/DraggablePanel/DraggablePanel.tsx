952986bb9cab441536f7ed2d2792f12bad158408
Sam Kendrick <skendri2@upenn.edu>
2023-02-03 15:49:22 -0600

    Add button text to dismiss button for testing and accessibility

--- packages/libs/coreui/src/components/containers/DraggablePanel/DraggablePanel.tsx
+++ packages/libs/coreui/src/components/containers/DraggablePanel/DraggablePanel.tsx
@@ -1,10 +1,11 @@
 import { CSSProperties, ReactNode, useState } from "react";
 import Draggable, { DraggableEvent, DraggableData } from "react-draggable";
 import { css } from "@emotion/react";
-import { DragHandle } from "@material-ui/icons";
 import { gray } from "../../../definitions/colors";
-import { CloseCircle } from "../../icons";
 import { primaryFont } from "../../../styleDefinitions/typography";
+import { useUITheme } from "../../theming";
+import DismissButton from "../../notifications/DismissButton";
+import { H6 } from "../../typography";
 
 export type DraggablePanelCoordinatePair = {
   x: number;
@@ -13,6 +14,8 @@ export type DraggablePanelCoordinatePair = {
 
 export type DraggablePanelStyleOverrides = {
   boxShadow?: CSSProperties["boxShadow"];
+  margin?: CSSProperties["margin"];
+  padding?: CSSProperties["padding"];
   zIndex?: CSSProperties["zIndex"];
 };
 
@@ -23,14 +26,10 @@ export type DraggablePanelProps = {
   confineToParentContainer?: boolean;
   /** If provided, the panel will live here instead of where they originally live, as defined in the CSS & HTML. */
   defaultPosition?: DraggablePanelCoordinatePair;
-  /** This meaningful text is used to both render a title and ensure a more WAI-compliant experience.  */
-  panelTitle: string;
-  /** This allows you to specify how tall your panel should be. */
-  initialPanelHeight?: string;
-  /** This allows you to specify how wide your panel should be. */
-  initialPanelWidth?: string;
   /** This controls weather the panel is visible or not. */
   isOpen: boolean;
+  /** This meaningful text is used to both render a title and ensure a more WAI-compliant experience.  */
+  panelTitle: string;
   /** This allows developers to show or hide the panel title. */
   showPanelTitle: boolean;
   /** This surfaces configurable CSS properties for the <DraggablePanel /> */
@@ -41,25 +40,14 @@ export type DraggablePanelProps = {
   ) => void;
   /** This event fires when the user's drag begins. */
   onDragStart?: () => void;
-  /** This event fires when the user dismisses a visible panel. */
+  /** This event fires when the user dismisses a visible panel. If you supply nothing, then the close button will not render. */
   onPanelDismiss?: () => void;
 };
-
-const screenReaderOnly = css`
-  height: 1px;
-  left: -10000px;
-  overflow: hidden;
-  position: absolute;
-  top: auto;
-  width: 1px;
-`;
 
 export function DraggablePanel({
   confineToParentContainer,
   children,
   defaultPosition,
-  initialPanelHeight,
-  initialPanelWidth,
   isOpen,
   onDragComplete,
   onDragStart,
@@ -68,6 +56,8 @@ export function DraggablePanel({
   showPanelTitle,
   styleOverrides,
 }: DraggablePanelProps) {
+  const theme = useUITheme();
+
   const [wasDragged, setWasDragged] = useState<boolean>(false);
   const [isDragging, setIsDragging] = useState<boolean>(false);
 
@@ -108,16 +98,15 @@ export function DraggablePanel({
         css={css`
           background: white;
           border-radius: 7px;
-          box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
-            rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+          box-shadow: ${styleOverrides?.boxShadow ??
+          `rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+          rgba(0, 0, 0, 0.3) 0px 1px 3px -1px`};
           min-width: 250px;
           position: relative;
-          width: ${initialPanelWidth || "unset"};
-          ${isOpen === false &&
-          `
-          visibility: hidden;
-          `}
-          z-index: ${styleOverrides?.zIndex ?? "auto"}
+          width: fit-content;
+          visibility: ${isOpen === false ? "hidden" : "visible"};
+          z-index: ${styleOverrides?.zIndex ?? "auto"};
+          margin: ${styleOverrides?.margin ?? "margin"};
         `}
       >
         <div
@@ -125,67 +114,35 @@ export function DraggablePanel({
           css={css`
             align-items: center;
             border-radius: 7px 7px 0 0;
-            background: ${gray[100]};
+            background: ${theme?.palette.primary.hue[100] ?? gray[100]};
             cursor: ${isDragging ? "grabbing" : "grab"};
             display: flex;
             height: 2rem;
-            justify-content: space-between;
+            justify-content: center;
             position: relative;
             width: 100%;
           `}
         >
-          <div
-            aria-hidden
-            css={css`
-              margin-left: 1rem;
-            `}
-          >
-            <DragHandle />
-          </div>
-          <h2
-            css={css`
-              font-size: 15px;
-              font-weight: bold;
-              font-family: ${primaryFont};
-            `}
-          >
+          <H6 additionalStyles={{ fontWeight: "bold" }}>
             <span css={showPanelTitle ? null : screenReaderOnly}>
               {panelTitle}
             </span>
-          </h2>
+          </H6>
           {onPanelDismiss && (
             <div
               css={css`
-                height: 100%;
+                position: absolute;
+                right: 0;
               `}
             >
-              <button
-                css={css`
-                  all: unset;
-                  align-items: center;
-                  cursor: pointer;
-                  display: flex;
-                  height: 100%;
-                  justify-content: center;
-                  padding: 0 1rem;
-                  &:hover {
-                    opacity: 0.3;
-                  }
-                `}
-                onClick={onPanelDismiss}
-              >
-                <span css={screenReaderOnly}>Close {panelTitle}</span>
-                <CloseCircle fontSize="1.5rem" fill={gray[600]} aria-hidden />
-              </button>
+              <DismissButton onClick={onPanelDismiss} />
             </div>
           )}
         </div>
         <div
           css={css`
             border-radius: 7px;
-            height: ${initialPanelHeight || "unset"};
             overflow: scroll;
-            width: ${initialPanelWidth || "unset"};
           `}
         >
           {children}
--- packages/libs/coreui/src/stories/containers/DraggablePanel.stories.tsx
+++ packages/libs/coreui/src/stories/containers/DraggablePanel.stories.tsx
@@ -8,7 +8,7 @@ import {
   DraggablePanelProps,
 } from "../../components/containers/DraggablePanel/DraggablePanel";
 import UIThemeProvider from "../../components/theming/UIThemeProvider";
-import { orange, green } from "../../definitions/colors";
+import { mutedMagenta, gray } from "../../definitions/colors";
 
 export default {
   title: "Containers/DraggablePanel",
@@ -16,409 +16,165 @@ export default {
 } as Meta;
 
 const Template: Story<DraggablePanelProps> = (args) => {
-  const [panelOneIsOpen, setPanelOneIsOpen] = useState<boolean>(true);
-  const [panelOneCoordinates, setPanelOneCoordinates] =
-    useState<DraggablePanelCoordinatePair>({
-      x: 0,
-      y: 0,
-    });
-
-  const [panelTwoIsOpen, setPanelTwoIsOpen] = useState<boolean>(true);
-
-  // https://beta.reactjs.org/reference/react/Children
-  // https://beta.reactjs.org/reference/react/Children#accepting-an-array-of-objects-as-a-prop
   const panelDefinitionOjects: DraggablePanelProps[] = [
     "Panel 1",
     "Panel 2",
     "Panel 3",
-  ].map((panelTitle) => {
+    "Panel 4",
+  ].map((panelTitle, panelIndex) => {
     return {
       children: () => <p>Panel Contents</p>,
       panelTitle,
-      showPanelTitle: true,
       isOpen: true,
+      backgroundColorForStorybookOnly: mutedMagenta[`${panelIndex + 1}00`],
+      showPanelTitle: args.showPanelTitle,
+      includeDismissButton: args.includeDismissButton,
+      styleOverrides: args.styleOverrides,
     };
   });
-
-  return <StackOrderingKeeper draggablePanelProps={panelDefinitionOjects} />;
 
   return (
     <UIThemeProvider
       theme={{
         palette: {
-          primary: { hue: green, level: 600 },
-          secondary: { hue: orange, level: 500 },
+          primary: { hue: gray, level: 500 },
+          secondary: { hue: mutedMagenta, level: 500 },
         },
       }}
     >
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          height: 95vh;
-        `}
-      >
-        <div
-          css={css`
-            display: flex;
-            justify-content: space-around;
-            padding: 1rem 0;
-          `}
-        >
-          <button onClick={() => setPanelOneIsOpen((isOpen) => !isOpen)}>
-            {panelOneIsOpen ? "Close" : "Open"} <strong>Panel 1</strong> from up
-            here!
-          </button>
-          <button onClick={() => setPanelTwoIsOpen((isOpen) => !isOpen)}>
-            {panelTwoIsOpen ? "Close" : "Open"} <strong>Panel 2</strong> from up
-            here!
-          </button>
-        </div>
-        <DraggablePanel
-          confineToParentContainer
-          isOpen={panelOneIsOpen}
-          onPanelDismiss={() => setPanelOneIsOpen(false)}
-          panelTitle="Panel 1"
-          showPanelTitle
-          initialPanelWidth="700px"
-          initialPanelHeight="200px"
-          onDragComplete={setPanelOneCoordinates}
-        >
-          <div
-            css={css`
-              display: flex;
-              flex-direction: column;
-              justify-content: space-between;
-            `}
-          >
-            <pre
-              css={css`
-                padding-left: 1rem;
-              `}
-            >
-              Coordinates (updates when you complete your dragging):{" "}
-              {JSON.stringify(panelOneCoordinates)}
-            </pre>
-            {getHtmlTable()}
-          </div>
-        </DraggablePanel>
-        <br />
-        <DraggablePanel
-          confineToParentContainer
-          isOpen={panelTwoIsOpen}
-          onPanelDismiss={() => setPanelTwoIsOpen(false)}
-          panelTitle="Panel 2"
-          showPanelTitle
-          onDragComplete={() => {}}
-          initialPanelWidth="200px"
-        >
-          <div
-            css={css`
-              background: white;
-              display: flex;
-              flex-direction: column;
-              padding: 0rem 0.5rem;
-            `}
-          >
-            {getMenuHtml()}
-          </div>
-        </DraggablePanel>
-      </div>
+      <StackOrderingKeeper draggablePanelProps={panelDefinitionOjects} />;
     </UIThemeProvider>
   );
 };
 
 export const Default = Template.bind({});
+Default.args = {
+  showPanelTitle: true,
+  includeDismissButton: true,
+  styleOverrides: {},
+};
 
-function getMenuHtml() {
-  return (
-    <div
-      css={css`
-        font-family: sans-serif;
-        padding: 1rem;
-      `}
-    >
-      <form>
-        <h3>Complicated Menu</h3>
-        <div>
-          <p>Audio settings:</p>
-          <div>
-            <input type="range" id="volume" name="volume" min={0} max={11} />
-            <label htmlFor="volume">Volume</label>
-          </div>
-          <div>
-            <input
-              type="range"
-              id="cowbell"
-              name="cowbell"
-              min={0}
-              max={100}
-              defaultValue={90}
-              step={10}
-            />
-            <label htmlFor="cowbell">Cowbell</label>
-          </div>
-        </div>
-        <fieldset>
-          <legend>Select a maintenance drone:</legend>
-          <div>
-            <input
-              type="radio"
-              id="huey"
-              name="drone"
-              defaultValue="huey"
-              defaultChecked
-            />
-            <label htmlFor="huey">Huey</label>
-          </div>
-          <div>
-            <input type="radio" id="dewey" name="drone" defaultValue="dewey" />
-            <label htmlFor="dewey">Dewey</label>
-          </div>
-          <div>
-            <input type="radio" id="louie" name="drone" defaultValue="louie" />
-            <label htmlFor="louie">Louie</label>
-          </div>
-        </fieldset>
-      </form>
-    </div>
+type StackOrderingKeeperProps = { draggablePanelProps: DraggablePanelProps[] };
+
+function StackOrderingKeeper({
+  draggablePanelProps,
+}: StackOrderingKeeperProps) {
+  const [zIndicies, setZIndicies] = useState<string[]>(
+    draggablePanelProps.map((props) => props.panelTitle)
   );
-}
 
-function getHtmlTable() {
-  return (
-    <div
-      css={css`
-        background: white;
-        padding: 1rem;
-
-        caption {
-          font-weight: bold;
-          padding: 1rem 0;
-          font-size: 24px;
-        }
-
-        td,
-        th {
-          border: 1px solid #777;
-          padding: 0.5rem;
-          text-align: center;
-        }
-
-        table {
-          border-collapse: collapse;
-          font-family: monospace;
-        }
-
-        tbody tr:nth-child(odd) {
-          background: #eee;
-        }
-        caption {
-          font-size: 0.8rem;
-        }
-      `}
-    >
-      <table className="wikitable sortable mw-collapsible jquery-tablesorter mw-made-collapsible">
-        <caption>
-          Estimated water requirements for various foods
-          <sup id="cite_ref-48" className="reference">
-            <a href="#cite_note-48">[48]</a>
-          </sup>
-          <span
-            className="mw-collapsible-toggle mw-collapsible-toggle-default"
-            role="button"
-            tabIndex={0}
-            aria-expanded="true"
-          >
-            <a className="mw-collapsible-text">hide</a>
-          </span>
-        </caption>
-        <thead>
-          <tr>
-            <th
-              className="headerSort"
-              tabIndex={0}
-              role="columnheader button"
-              title="Sort ascending"
-            >
-              Food Types
-            </th>
-            <th
-              className="headerSort"
-              tabIndex={0}
-              role="columnheader button"
-              title="Sort ascending"
-            >
-              Litre per kilocalorie
-            </th>
-            <th
-              className="headerSort"
-              tabIndex={0}
-              role="columnheader button"
-              title="Sort ascending"
-            >
-              Litre per gram of protein
-            </th>
-            <th
-              className="headerSort"
-              tabIndex={0}
-              role="columnheader button"
-              title="Sort ascending"
-            >
-              Litre per kilogram
-            </th>
-            <th
-              className="headerSort"
-              tabIndex={0}
-              role="columnheader button"
-              title="Sort ascending"
-            >
-              Litre per gram of fat
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Sugar crops</td>
-            <td>0.69</td>
-            <td>0.0</td>
-            <td>197</td>
-            <td>0.0</td>
-          </tr>
-          <tr>
-            <td>Vegetables</td>
-            <td>1.34</td>
-            <td>26</td>
-            <td>322</td>
-            <td>154</td>
-          </tr>
-          <tr>
-            <td>Starchy roots</td>
-            <td>0.47</td>
-            <td>31</td>
-            <td>387</td>
-            <td>226</td>
-          </tr>
-          <tr>
-            <td>Fruits</td>
-            <td>2.09</td>
-            <td>180</td>
-            <td>962</td>
-            <td>348</td>
-          </tr>
-          <tr>
-            <td>Cereals</td>
-            <td>0.51</td>
-            <td>21</td>
-            <td>1644</td>
-            <td>112</td>
-          </tr>
-          <tr>
-            <td>Oil crops</td>
-            <td>0.81</td>
-            <td>16</td>
-            <td>2364</td>
-            <td>11</td>
-          </tr>
-          <tr>
-            <td>Pulses</td>
-            <td>1.19</td>
-            <td>19</td>
-            <td>4055</td>
-            <td>180</td>
-          </tr>
-          <tr>
-            <td>Nuts</td>
-            <td>3.63</td>
-            <td>139</td>
-            <td>9063</td>
-            <td>47</td>
-          </tr>
-          <tr>
-            <td>Milk</td>
-            <td>1.82</td>
-            <td>31</td>
-            <td>1020</td>
-            <td>33</td>
-          </tr>
-          <tr>
-            <td>Eggs</td>
-            <td>2.29</td>
-            <td>29</td>
-            <td>3265</td>
-            <td>33</td>
-          </tr>
-          <tr>
-            <td>Chicken meat</td>
-            <td>3.00</td>
-            <td>34</td>
-            <td>4325</td>
-            <td>43</td>
-          </tr>
-          <tr>
-            <td>Butter</td>
-            <td>0.72</td>
-            <td>0.0</td>
-            <td>5553</td>
-            <td>6.4</td>
-          </tr>
-          <tr>
-            <td>Pig meat</td>
-            <td>2.15</td>
-            <td>57</td>
-            <td>5988</td>
-            <td>23</td>
-          </tr>
-          <tr>
-            <td>Sheep/goat meat</td>
-            <td>4.25</td>
-            <td>63</td>
-            <td>8763</td>
-            <td>54</td>
-          </tr>
-          <tr>
-            <td>Bovine meat</td>
-            <td>10.19</td>
-            <td>112</td>
-            <td>15415</td>
-            <td>153</td>
-          </tr>
-        </tbody>
-        <tfoot />
-      </table>
-    </div>
+  const [panelOpenMap, setPanelOpenMap] = useState(
+    draggablePanelProps.reduce((acc, props) => {
+      acc[props.panelTitle] = true;
+      return acc;
+    }, {})
   );
-}
 
-type StackOrderingKeeper = { draggablePanelProps: DraggablePanelProps[] };
-function StackOrderingKeeper({ draggablePanelProps }: StackOrderingKeeper) {
-  const [zIndicies, setZIndicies] = useState<string[]>([]);
+  function togglePanelOpen(panelTitle) {
+    setPanelOpenMap((currentMap) => ({
+      ...currentMap,
+      [panelTitle]: !currentMap[panelTitle],
+    }));
+  }
+
+  function movePanelToTopLayer(panelTitleToMove: string) {
+    setZIndicies((currentList) => {
+      return currentList
+        .filter((panelTitle) => panelTitle !== panelTitleToMove)
+        .concat(panelTitleToMove);
+    });
+  }
 
   return (
     <div>
-      {draggablePanelProps.map((props) => {
+      <ul
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          listStyle: "none",
+        }}
+      >
+        {draggablePanelProps.map((props) => {
+          const isOpen = panelOpenMap[props.panelTitle];
+          return (
+            <li key={props.panelTitle}>
+              <button
+                onClick={() => {
+                  movePanelToTopLayer(props.panelTitle);
+                  togglePanelOpen(props.panelTitle);
+                }}
+                style={{ backgroundColor: isOpen ? "tomato" : "lightgreen" }}
+              >
+                <span
+                  style={{
+                    fontSize: 16,
+                    fontWeight: "bold",
+                    padding: "0.25rem 0.5rem",
+                  }}
+                >
+                  {isOpen ? "Close" : "Open"} {props.panelTitle}
+                </span>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      {draggablePanelProps.map((props, propsIndex) => {
         const indexOfelement = zIndicies.findIndex(
           (panelTitle) => panelTitle === props.panelTitle
         );
         const zIndex = indexOfelement > 0 ? indexOfelement : 0;
+
+        function handleOnPanelDismiss() {
+          movePanelToTopLayer(props.panelTitle);
+          togglePanelOpen(props.panelTitle);
+        }
+
         return (
           <DraggablePanel
-            isOpen
-            panelTitle={props.panelTitle}
-            showPanelTitle
+            defaultPosition={{ x: 20 * propsIndex, y: 1 * propsIndex }}
+            confineToParentContainer
             key={props.panelTitle}
-            onDragStart={() => {
-              setZIndicies((currentList) => {
-                return currentList
-                  .filter((panelTitle) => panelTitle !== props.panelTitle)
-                  .concat(props.panelTitle);
-              });
-            }}
+            isOpen={panelOpenMap[props.panelTitle]}
+            onPanelDismiss={
+              props.includeDismissButton ? handleOnPanelDismiss : undefined
+            }
+            panelTitle={props.panelTitle}
+            showPanelTitle={props.showPanelTitle}
+            onDragStart={() => movePanelToTopLayer(props.panelTitle)}
             styleOverrides={{
               zIndex,
+              margin: "0 0 1rem 0",
+              ...props.styleOverrides,
             }}
           >
-            content... my z: {zIndex}
+            {/* This is just nonsense to fill the panel with content */}
+            <div
+              style={{
+                padding: "1rem",
+                maxHeight: 150,
+                maxWidth: 500,
+                fontFamily: "sans-serif",
+              }}
+            >
+              <h2>{props.panelTitle} Content</h2>
+              <div
+                style={{
+                  height: 25,
+                  width: "100%",
+                  backgroundColor: props.backgroundColorForStorybookOnly,
+                }}
+              ></div>
+              <p>
+                <code>z-index</code>: {zIndex}
+              </p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe
+                labore ut quia harum expedita distinctio eius deserunt, officiis
+                inventore velit. Voluptatibus unde eum animi alias, illum
+                eligendi ullam facilis consectetur?
+              </p>
+            </div>
           </DraggablePanel>
         );
       })}
