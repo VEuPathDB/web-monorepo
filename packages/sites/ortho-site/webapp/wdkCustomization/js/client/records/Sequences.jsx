8545476a0fc3ae61c810689e36c921682f6c5e85
Dave Falke <dfalke@uga.edu>
2023-03-02 13:08:30 -0500

    Convert to function component to better satisfy typescript

--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/records/Sequences.jsx
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/records/Sequences.jsx
@@ -1,54 +1,46 @@
 import lodash from 'lodash';
-import React from 'react';
+import React, { useMemo } from 'react';
 import ReactDOM from 'react-dom';
 
-class SortKeyTable extends React.Component {
+// function RecordTable_TaxonCounts({ value }: WrappedComponentProps<RecordTableProps>) {
+export function RecordTable_Sequences(props) {
+  const formRef = useRef(null);
 
-      constructor(props) {
-          super(props);
-	  // Memoize the sorting. Without this, the DataTable widget will think is
-  	  // is a new table and reset the sorting. This is bad if a user has already
-  	  // sorted the table.
-	  this.sortValue = lodash.memoize(value => lodash.sortBy(value, 'sort_key'));
-      }
+  function toggleAll(checked) {
+    const node = formRef.current;
+    if (node == null) return;
+    for (const input of node.querySelectorAll('input[name="msa_full_ids"]')) {
+      input.checked = checked;
+    }
+  }
 
-      render() {
-      	return <this.props.DefaultComponent {...this.props} value={this.sortValue(this.props.value)}/>
-      }
+  const sortedValue = useSortedValue(props.value);
+
+    return (
+      <form ref={formRef} action="/cgi-bin/msaOrthoMCL" target="_blank" method="post">
+        <input type="hidden" name="project_id" value="OrthoMCL"/>
+
+        <props.DefaultComponent {...props} value={sortedValue}/>
+        <input type="button" name="CheckAll" value="Check All" onClick={() => toggleAll(true)}/> 
+        <input type="button" name="UnCheckAll" value="Uncheck All" onClick={() => toggleAll(false)}/>
+        <br/>
+        <p>Please note: selecting a large number of proteins will take several minutes to align.</p>
+        <div id="userOptions" >
+          <p>Output format: &nbsp;
+            <select name='clustalOutFormat'>
+              <option value="clu">Mismatches highlighted</option>
+              <option value="fasta">FASTA</option>
+              <option value="phy">PHYLIP</option>
+              <option value="st">STOCKHOLM</option>
+              <option value="vie">VIENNA</option>
+            </select></p>
+          <input type="submit" value="Run Clustal Omega for selected proteins"/>
+        </div>
+      </form>
+    );
 }
 
 
-export class RecordTable_Sequences extends SortKeyTable {
-
-       toggleAll(checked) {
-               const node = ReactDOM.findDOMNode(this);
-	       for (const input of node.querySelectorAll('input[name="msa_full_ids"]')) {
-		    input.checked = checked;
-	        }
-       }
-
-       render () {
-       	      return (
-       	      	     <form action="/cgi-bin/msaOrthoMCL" target="_blank" method="post">
-		     	   <input type="hidden" name="project_id" value="OrthoMCL"/>
-
-			   <this.props.DefaultComponent {...this.props} value={this.sortValue(this.props.value)}/>
-			   <input type="button" name="CheckAll" value="Check All" onClick={() => this.toggleAll(true)}/> 
-			   <input type="button" name="UnCheckAll" value="Uncheck All" onClick={() => this.toggleAll(false)}/>
-			   <br/>
-			   <p>Please note: selecting a large number of proteins will take several minutes to align.</p>
-			   <div id="userOptions" >
-			   	<p>Output format: &nbsp;
-				<select name='clustalOutFormat'>
-					<option value="clu">Mismatches highlighted</option>
-					<option value="fasta">FASTA</option>
-					<option value="phy">PHYLIP</option>
-					<option value="st">STOCKHOLM</option>
-					<option value="vie">VIENNA</option>
-				</select></p>
-				<input type="submit" value="Run Clustal Omega for selected proteins"/>
-			   </div>
-		    </form>
-	      );
-       }
+function useSortedValue(value) {
+  return useMemo(() => lodash.sortBy(value, 'sort_key'), [value])
 }
