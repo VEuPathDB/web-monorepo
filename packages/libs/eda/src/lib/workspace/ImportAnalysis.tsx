f49dc985689d5a3185fabe3802df9e1387eeb3bf
Jeremy Myers <inernestmusic@gmail.com>
2023-03-28 17:24:55 -0400

    Add error messaging for forbidden analysis imports

--- packages/libs/eda/src/lib/workspace/ImportAnalysis.tsx
+++ packages/libs/eda/src/lib/workspace/ImportAnalysis.tsx
@@ -1,21 +1,32 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import Path from 'path';
 
-import { useSetDocumentTitle } from '@veupathdb/wdk-client/lib/Utils/ComponentUtils';
 import { Task } from '@veupathdb/wdk-client/lib/Utils/Task';
-
 import { AnalysisClient } from '../core';
+import { showLoginForm } from '@veupathdb/wdk-client/lib/Actions/UserSessionActions';
+
+import { useSetDocumentTitle } from '@veupathdb/wdk-client/lib/Utils/ComponentUtils';
+import { useWdkService } from '@veupathdb/wdk-client/lib/Hooks/WdkServiceHook';
 
 interface Props {
   analysisClient: AnalysisClient;
   analysisId: string;
 }
 
+type ImportAnalysisError = {
+  status: string;
+  message: string;
+};
+
 export function ImportAnalysis({ analysisClient, analysisId }: Props) {
   const history = useHistory();
-  const [error, setError] = useState<string | undefined>();
+  const user = useWdkService((wdkService) => wdkService.getCurrentUser(), []);
+  const dispatch = useDispatch();
+  const [error, setError] =
+    useState<string | ImportAnalysisError | undefined>();
 
   useSetDocumentTitle(
     error == null ? 'Importing Analysis' : 'Import Unsuccessful'
@@ -44,15 +55,64 @@ export function ImportAnalysis({ analysisClient, analysisId }: Props) {
           };
           history.replace(newLocation);
         },
-        (error) =>
-          setError(error instanceof Error ? error.message : String(error))
+        (error) => {
+          const enforcedErrorObject =
+            error instanceof Error ? error : new Error(String(error));
+          const errorMessageList = enforcedErrorObject.message.split('\n');
+          try {
+            setError(JSON.parse(errorMessageList[1]));
+          } catch {
+            setError(enforcedErrorObject.message);
+          }
+        }
       );
   }, [analysisClient, history, analysisId]);
+
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+
+      if (user === undefined || user.isGuest) {
+        dispatch(showLoginForm());
+      }
+    },
+    [history, user, dispatch]
+  );
+
+  const errorMessage =
+    error && typeof error === 'object' ? (
+      error.status === 'forbidden' ? (
+        <>
+          <p>
+            Unable to import this analysis. You do not have access to the study.
+          </p>
+          <p>
+            {user === undefined || user.isGuest ? (
+              <>
+                <button className="link" onClick={handleClick}>
+                  Please login
+                </button>{' '}
+                or{' '}
+              </>
+            ) : (
+              <>Please</>
+            )}{' '}
+            request access from the owner of the study.
+          </p>
+        </>
+      ) : (
+        <pre>
+          status: {error.status}; message: {error.message}
+        </pre>
+      )
+    ) : (
+      <pre>{error}</pre>
+    );
 
   return (
     <div>
       <h1>{error == null ? 'Importing Analysis...' : 'Import Unsuccessful'}</h1>
-      {error && <pre>{error}</pre>}
+      {errorMessage}
     </div>
   );
 }
