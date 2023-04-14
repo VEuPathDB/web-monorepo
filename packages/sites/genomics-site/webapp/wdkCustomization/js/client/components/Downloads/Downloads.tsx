bd863939859eaf11e9d341b7f6dca592e14c85d8
Dave Falke <dfalke@uga.edu>
2023-04-14 18:27:48 -0400

    Updates to UI based on feedback

--- packages/sites/genomics-site/webapp/wdkCustomization/js/client/components/Downloads/Downloads.tsx
+++ packages/sites/genomics-site/webapp/wdkCustomization/js/client/components/Downloads/Downloads.tsx
@@ -1,9 +1,4 @@
 import React, { useState } from 'react';
-import { colors, ExpandablePanel } from '@veupathdb/coreui';
-import {
-  ExpandablePanelStyleSpec,
-  EXPANDABLE_PANEL_PRESET_STYLES,
-} from '@veupathdb/coreui/dist/components/containers/ExpandablePanel/stylePresets';
 import { SearchConfig } from '@veupathdb/wdk-client/lib/Utils/WdkModel';
 import { DownloadsFilter } from './DownloadsFilter';
 import { DownloadsTable } from './DownloadsTable';
@@ -13,37 +8,23 @@ const RECORD_NAME = 'dfile';
 const TABLE_QUESTION_NAME = 'GetAllFileRecords';
 const BULK_QUESTION_NAME = 'GetFileRecordsByID';
 
-const expandablePanelStyleOverride: Partial<ExpandablePanelStyleSpec> = {
-  open: {
-    ...EXPANDABLE_PANEL_PRESET_STYLES.default.open,
-    content: {
-      divider: {
-        color: colors.gray[400],
-        thickness: 2,
-      },
-      backgroundColor: colors.white,
-    },
-  },
-};
-
 export function Downloads() {
   const [searchConfig, setSearchConfig] = useState<SearchConfig>();
 
   return (
     <div className="Downloads">
       <h1>Download Data Files</h1>
-      <ExpandablePanel
-        state={'open'}
-        title="Filter files"
-        subTitle="Filter the files displayed in the table below"
-        styleOverrides={expandablePanelStyleOverride}
-      >
+      <p className="Downloads-Instructions">
+        Use the filters to reduce the table below. Use the table to select the
+        files to download.
+      </p>
+      <div className="Downloads-Filter-Container">
         <DownloadsFilter
           recordName={RECORD_NAME}
           questionName={TABLE_QUESTION_NAME}
           onChange={setSearchConfig}
         />
-      </ExpandablePanel>
+      </div>
       {searchConfig && (
         <DownloadsTable
           tableSearchName={TABLE_QUESTION_NAME}
