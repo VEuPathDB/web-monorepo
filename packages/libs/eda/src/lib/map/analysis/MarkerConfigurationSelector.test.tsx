18395cdf3e0f04d135d1a5a9d7ee0ca3929d8f78
Sam Kendrick <skendrick366@gmail.com>
2023-04-25 14:38:32 -0500

    Implement <MarkerConfigurationSelector />

--- packages/libs/eda/src/lib/map/analysis/MarkerConfigurationSelector.test.tsx
+++ packages/libs/eda/src/lib/map/analysis/MarkerConfigurationSelector.test.tsx
@@ -1,113 +1,152 @@
-import { render, screen } from '@testing-library/react';
-import { MarkerConfigurationSelector } from './MarkerConfigurationSelector';
+import { fireEvent, render, screen } from '@testing-library/react';
+import {
+  MarkerConfigurationSelector,
+  MarkerConfiguration,
+} from './MarkerConfigurationSelector';
 import { useState } from 'react';
 
-const markerTypes = [
+const PlaceholderIcon = () => <svg />;
+
+const markConfigurations: MarkerConfiguration[] = [
   {
     name: 'Donuts',
-    icon: () => {},
-    renderConfigurationMenu: function DonutConfigurationMenu() {
-      const [frostingSelection, setFrostingSelection] = useState('');
-      return (
-        <div>
-          {frostingSelection.length > 0 ? (
-            <p>A donut with {frostingSelection} frosting.</p>
-          ) : (
-            <></>
-          )}
-          <label htmlFor="frostingFlavor">
-            Frosting flavor
-            <select
-              onChange={(event) => setFrostingSelection(event.target.value)}
-              name="frostingFlavor"
-              id="frostingFlavor"
-            >
-              <option value="">Unfrosted</option>
-              <option value="strawberry">Strawberry</option>
-              <option value="chocolate">Chocolate</option>
-            </select>
-          </label>
-        </div>
-      );
-    },
+    icon: <PlaceholderIcon />,
+    renderConfigurationMenu: <DonutConfigurationMenu />,
   },
   {
     name: 'Bar plots',
-    icon: () => {},
-    renderConfigurationMenu: function BarPlotConfigurationMenu() {
-      const [barSelection, setBarSelection] = useState('Cheers');
-      const [plotSelection, setPlotSelection] = useState('Novel');
-
-      function handleSelection(event: React.ChangeEvent<HTMLSelectElement>) {
-        const { name, value } = event.target;
-        name === 'bar' ? setBarSelection(value) : setPlotSelection(value);
-      }
-
-      return (
-        <div>
-          <p>
-            You will be plotting a {plotSelection} at {barSelection}.
-          </p>
-
-          <label htmlFor="bar">
-            Bar options
-            <select
-              onChange={handleSelection}
-              value={barSelection}
-              name="bar"
-              id="bar"
-            >
-              <option value="Cheers">Cheers (Bostom, MA)</option>
-              <option value="The Blue Bar">The Blue Bar (NYC)</option>
-              <option value="Harry's Bar">Harry's Bar (Venice, IT)</option>
-            </select>
-          </label>
-
-          <label htmlFor="plot">
-            Plot options
-            <select
-              onChange={handleSelection}
-              value={plotSelection}
-              name="plot"
-              id="plot"
-            >
-              <option value="Novel">Novel</option>
-              <option value="Bank Heist">Bank Heist</option>
-              <option value="Points">Points on a graph</option>
-            </select>
-          </label>
-        </div>
-      );
-    },
+    icon: <PlaceholderIcon />,
+    renderConfigurationMenu: <BarPlotConfigurationMenu />,
   },
 ];
 
 describe('<MarkerConfigurationMenu />', () => {
+  test('users can select a marker', async () => {
+    render(<ParentComponent />);
+
+    expect(screen.getByText('No marker configuration selected.')).toBeVisible();
+    screen.getByText('Donuts').click();
+    screen.getByText('Selected marker configuration: Donuts.');
+
+    screen.getByText('Bar plots').click();
+    screen.getByText('Selected marker configuration: Bar plots.');
+  });
+
   test('after a user selects a marker type, a marker-specific configuration panel appears', async () => {
-    render(<MarkerConfigurationSelector markerTypes={markerTypes} />);
+    render(<ParentComponent />);
 
     screen.getByText('Donuts').click();
-    screen.getByText('Frosting flavor');
-    screen.getByText('Strawberry').click();
 
-    expect(screen.getByText('A donut with strawberry frosting')).toBeVisible();
+    const select = screen.getByLabelText('Frosting flavor');
+    fireEvent.change(select, { target: { value: 'strawberry' } });
+
+    expect(screen.getByText("You've got one strawberry donut.")).toBeVisible();
 
     screen.getByText('Bar plots').click();
 
     expect(
-      screen.getByText('You will be plotting a novel at Cheers.')
+      screen.getByText('You will be plotting a Novel at Cheers.')
     ).toBeVisible();
   });
 
   test('users can configure their marker selection', async () => {
-    render(<MarkerConfigurationSelector markerTypes={markerTypes} />);
+    render(<ParentComponent />);
 
     screen.getByText('Bar plots').click();
-    screen.getByLabelText('Bar options').click();
-    screen.getByLabelText('The Blue Bar').click();
+
+    const select = screen.getByLabelText('Bar options');
+    fireEvent.change(select, { target: { value: 'The Blue Bar' } });
 
     expect(
-      screen.getByText('You will be plotting a novel at The Blue Bar.')
+      screen.getByText('You will be plotting a Novel at The Blue Bar.')
     ).toBeVisible();
   });
 });
+
+function ParentComponent() {
+  const [selectedMarkerConfigurationName, setSelectedMarkerConfigurationName] =
+    useState('');
+
+  return (
+    <>
+      {selectedMarkerConfigurationName.length ? (
+        <p>Selected marker configuration: {selectedMarkerConfigurationName}.</p>
+      ) : (
+        <p>No marker configuration selected.</p>
+      )}
+      <MarkerConfigurationSelector
+        markerConfigurations={markConfigurations}
+        selectedMarkerConfigurationName={selectedMarkerConfigurationName}
+        setSelectedMarkerConfigurationName={setSelectedMarkerConfigurationName}
+      />
+    </>
+  );
+}
+
+function DonutConfigurationMenu() {
+  const [frostingSelection, setFrostingSelection] = useState('Unfrosted');
+  return (
+    <div>
+      <p>You've got one {frostingSelection} donut.</p>
+      <label htmlFor="frostingFlavor">
+        Frosting flavor
+        <select
+          onChange={(event) => setFrostingSelection(event.target.value)}
+          name="frostingFlavor"
+          id="frostingFlavor"
+          value={frostingSelection}
+        >
+          <option value="Unfrosted">Unfrosted</option>
+          <option value="strawberry">Strawberry</option>
+          <option value="chocolate">Chocolate</option>
+        </select>
+      </label>
+    </div>
+  );
+}
+
+function BarPlotConfigurationMenu() {
+  const [barSelection, setBarSelection] = useState('Cheers');
+  const [plotSelection, setPlotSelection] = useState('Novel');
+
+  function handleSelection(event: React.ChangeEvent<HTMLSelectElement>) {
+    const { name, value } = event.target;
+    name === 'bar' ? setBarSelection(value) : setPlotSelection(value);
+  }
+
+  return (
+    <div>
+      <p>
+        You will be plotting a {plotSelection} at {barSelection}.
+      </p>
+
+      <label htmlFor="bar">
+        Bar options
+        <select
+          onChange={handleSelection}
+          value={barSelection}
+          name="bar"
+          id="bar"
+        >
+          <option value="Cheers">Cheers (Bostom, MA)</option>
+          <option value="The Blue Bar">The Blue Bar (NYC)</option>
+          <option value="Harry's Bar">Harry's Bar (Venice, IT)</option>
+        </select>
+      </label>
+
+      <label htmlFor="plot">
+        Plot options
+        <select
+          onChange={handleSelection}
+          value={plotSelection}
+          name="plot"
+          id="plot"
+        >
+          <option value="Novel">Novel</option>
+          <option value="Bank Heist">Bank Heist</option>
+          <option value="Points">Points on a graph</option>
+        </select>
+      </label>
+    </div>
+  );
+}
--- packages/libs/eda/src/lib/map/analysis/MarkerConfigurationSelector.tsx
+++ packages/libs/eda/src/lib/map/analysis/MarkerConfigurationSelector.tsx
@@ -1,3 +1,47 @@
-export function MarkerConfigurationSelector() {
-  return <p>Hello</p>;
+import { H6 } from '@veupathdb/coreui';
+
+export interface MarkerConfiguration {
+  name: string;
+  icon?: JSX.Element;
+  renderConfigurationMenu: JSX.Element;
+}
+
+interface Props {
+  markerConfigurations: MarkerConfiguration[];
+  selectedMarkerConfigurationName: string;
+  setSelectedMarkerConfigurationName: (name: string) => void;
+}
+
+export function MarkerConfigurationSelector({
+  markerConfigurations,
+  selectedMarkerConfigurationName: selectedMarkerConfiguration,
+  setSelectedMarkerConfigurationName: setSelectedMarkerConfiguration,
+}: Props) {
+  const activeMarkerConfigurationMenu = markerConfigurations.find(
+    ({ name }) => name === selectedMarkerConfiguration
+  )?.renderConfigurationMenu;
+
+  return (
+    <div>
+      <H6>Choose marker type:</H6>
+      <ul>
+        {markerConfigurations.map(({ name, icon }) => {
+          return (
+            <li
+              style={{
+                background:
+                  selectedMarkerConfiguration === name ? 'blue' : 'inherit',
+              }}
+              key={name}
+            >
+              <button onClick={() => setSelectedMarkerConfiguration(name)}>
+                {name} <span aria-hidden>{icon}</span>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <div>{activeMarkerConfigurationMenu}</div>
+    </div>
+  );
 }
