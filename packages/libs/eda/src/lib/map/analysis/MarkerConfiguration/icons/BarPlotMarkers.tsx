58a0deeb0d20ee3ca66794cccb1dc010ea82f6ae
asizemore <annsize@seas.upenn.edu>
2023-05-05 16:48:51 -0400

    swap bar marker svg colors

--- packages/libs/eda/src/lib/map/analysis/MarkerConfiguration/icons/BarPlotMarkers.tsx
+++ packages/libs/eda/src/lib/map/analysis/MarkerConfiguration/icons/BarPlotMarkers.tsx
@@ -12,24 +12,93 @@ export function BarPlotMarkers(props: SVGProps<SVGSVGElement>) {
     >
       <style>
         {
-          '.st0{fill:none;stroke:#3f3e3e;stroke-miterlimit:10}.st1{fill:#262626}.st2{fill:#4d4d4e}.st3{fill:#7f7f7f}.st4{fill:#dbd9d9}'
+          '.st0{fill:none;stroke:#262626;stroke-miterlimit:10;}.st2{fill:#DBD9D9;}.st3{fill:#7F7F7F;}.st1{fill:#4D4D4E;}.st4{fill:#262626;}'
         }
       </style>
-      <path d="M5.28 6.001v24.824h26.927" className="st0" />
-      <path d="M8.562 8.178h4.131v20.024H8.562z" className="st1" />
-      <path d="M14.667 15.995h4.131v12.206h-4.131z" className="st2" />
-      <path d="M20.771 12.263h4.131v15.939h-4.131z" className="st3" />
-      <path d="M26.876 9.238h4.131v18.964h-4.131z" className="st4" />
-      <path d="M42.115 6.001v24.824h27.197" className="st0" />
-      <path d="M45.665 23.641h4.131v4.561h-4.131z" className="st1" />
-      <path d="M51.77 15.995h4.131v12.206H51.77z" className="st2" />
-      <path d="M57.874 7.112h4.131v21.09h-4.131z" className="st3" />
-      <path d="M63.979 16.104h4.131v12.098h-4.131z" className="st4" />
-      <path d="M78.814 6.001v24.824h27.179" className="st0" />
-      <path d="M82.009 6.706h4.131v21.495h-4.131z" className="st1" />
-      <path d="M88.114 11.678h4.131v16.523h-4.131z" className="st2" />
-      <path d="M94.218 20.463h4.131v7.739h-4.131z" className="st3" />
-      <path d="M100.323 16.104h4.131v12.098h-4.131z" className="st4" />
+      <polyline
+        className="st0"
+        points="5.28,6.001 5.28,30.825 32.207,30.825 "
+      />
+      <rect x="8.562" y="8.178" className="st1" width="4.131" height="20.024" />
+      <rect
+        x="14.667"
+        y="15.995"
+        className="st2"
+        width="4.131"
+        height="12.206"
+      />
+      <rect
+        x="20.771"
+        y="12.263"
+        className="st3"
+        width="4.131"
+        height="15.939"
+      />
+      <rect
+        x="26.876"
+        y="9.238"
+        className="st4"
+        width="4.131"
+        height="18.964"
+      />
+      <polyline
+        className="st0"
+        points="42.115,6.001 42.115,30.825 69.312,30.825 "
+      />
+      <rect
+        x="45.665"
+        y="23.641"
+        className="st1"
+        width="4.131"
+        height="4.561"
+      />
+      <rect
+        x="51.77"
+        y="15.995"
+        className="st2"
+        width="4.131"
+        height="12.206"
+      />
+      <rect x="57.874" y="7.112" className="st3" width="4.131" height="21.09" />
+      <rect
+        x="63.979"
+        y="16.104"
+        className="st4"
+        width="4.131"
+        height="12.098"
+      />
+      <polyline
+        className="st0"
+        points="78.814,6.001 78.814,30.825 105.993,30.825 "
+      />
+      <rect
+        x="82.009"
+        y="6.706"
+        className="st1"
+        width="4.131"
+        height="21.495"
+      />
+      <rect
+        x="88.114"
+        y="11.678"
+        className="st2"
+        width="4.131"
+        height="16.523"
+      />
+      <rect
+        x="94.218"
+        y="20.463"
+        className="st3"
+        width="4.131"
+        height="7.739"
+      />
+      <rect
+        x="100.323"
+        y="16.104"
+        className="st4"
+        width="4.131"
+        height="12.098"
+      />
     </svg>
   );
 }
