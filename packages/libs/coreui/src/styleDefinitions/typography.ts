b1d1089b96db2f49f9f9f44be7ad4703186fc61c
Sam Kendrick <skendrick366@gmail.com>
2023-05-09 10:28:51 -0500

    Add default margin of 0 for coreui headings and paragraphs

--- packages/libs/coreui/src/styleDefinitions/typography.ts
+++ packages/libs/coreui/src/styleDefinitions/typography.ts
@@ -1,89 +1,95 @@
-import { css } from "@emotion/react";
-import { gray } from "../definitions/colors";
+import { css } from '@emotion/react';
+import { gray } from '../definitions/colors';
+import { CSSInterpolation } from '@emotion/serialize';
 
 export const primaryFont = "'Inter', sans-serif";
 export const secondaryFont = '"Roboto", sans-serif';
+const defaultHeadingStyles: CSSInterpolation = {
+  fontFamily: primaryFont,
+  margin: 0,
+};
 
 export const h1 = css([
-  { fontFamily: primaryFont },
+  defaultHeadingStyles,
   {
     fontSize: 72,
     fontWeight: 700,
-    MozOsxFontSmoothing: "auto",
-    WebkitFontSmoothing: "auto",
+    MozOsxFontSmoothing: 'auto',
+    WebkitFontSmoothing: 'auto',
   },
 ]);
 export const h2 = css([
-  { fontFamily: primaryFont },
+  defaultHeadingStyles,
   {
     fontSize: 56,
     fontWeight: 700,
-    MozOsxFontSmoothing: "auto",
-    WebkitFontSmoothing: "auto",
+    MozOsxFontSmoothing: 'auto',
+    WebkitFontSmoothing: 'auto',
   },
 ]);
 export const h3 = css([
-  { fontFamily: primaryFont },
+  defaultHeadingStyles,
   {
     fontSize: 42,
     fontWeight: 600,
-    MozOsxFontSmoothing: "auto",
-    WebkitFontSmoothing: "auto",
+    MozOsxFontSmoothing: 'auto',
+    WebkitFontSmoothing: 'auto',
   },
 ]);
 export const h4 = css([
-  { fontFamily: primaryFont },
+  defaultHeadingStyles,
   {
     fontSize: 32,
     fontWeight: 600,
-    MozOsxFontSmoothing: "auto",
-    WebkitFontSmoothing: "auto",
+    MozOsxFontSmoothing: 'auto',
+    WebkitFontSmoothing: 'auto',
   },
 ]);
 export const h5 = css([
-  { fontFamily: primaryFont },
+  defaultHeadingStyles,
   {
     fontSize: 21,
     fontWeight: 500,
-    MozOsxFontSmoothing: "auto",
-    WebkitFontSmoothing: "auto",
+    MozOsxFontSmoothing: 'auto',
+    WebkitFontSmoothing: 'auto',
   },
 ]);
 export const h6 = css([
-  { fontFamily: primaryFont },
+  defaultHeadingStyles,
   {
     fontSize: 16,
     fontWeight: 500,
-    MozOsxFontSmoothing: "auto",
-    WebkitFontSmoothing: "auto",
+    MozOsxFontSmoothing: 'auto',
+    WebkitFontSmoothing: 'auto',
   },
 ]);
 
 export const p = css([
   { fontFamily: secondaryFont },
   {
-    fontSize: "0.8rem",
+    fontSize: '0.8rem',
     fontWeight: 400,
-    MozOsxFontSmoothing: "auto",
-    WebkitFontSmoothing: "auto",
+    margin: 0,
+    MozOsxFontSmoothing: 'auto',
+    WebkitFontSmoothing: 'auto',
   },
 ]);
 export const pre = css([
   { fontFamily: secondaryFont },
   {
-    fontSize: ".80rem",
+    fontSize: '.80rem',
     fontWeight: 400,
-    MozOsxFontSmoothing: "auto",
-    WebkitFontSmoothing: "auto",
+    MozOsxFontSmoothing: 'auto',
+    WebkitFontSmoothing: 'auto',
   },
 ]);
 export const label = css([
   { fontFamily: secondaryFont },
   {
-    fontSize: ".75rem",
+    fontSize: '.75rem',
     fontWeight: 400,
-    MozOsxFontSmoothing: "auto",
-    WebkitFontSmoothing: "auto",
+    MozOsxFontSmoothing: 'auto',
+    WebkitFontSmoothing: 'auto',
     color: gray[400],
   },
 ]);
@@ -91,31 +97,31 @@ export const label = css([
 export const metaData = css([
   { fontFamily: secondaryFont },
   {
-    fontSize: ".70rem",
+    fontSize: '.70rem',
     fontWeight: 400,
-    textTransform: "uppercase",
+    textTransform: 'uppercase',
     color: gray[300],
-    MozOsxFontSmoothing: "auto",
-    WebkitFontSmoothing: "auto",
+    MozOsxFontSmoothing: 'auto',
+    WebkitFontSmoothing: 'auto',
   },
 ]);
 
 export const th = css([
   { fontFamily: secondaryFont },
   {
-    fontSize: ".90rem",
+    fontSize: '.90rem',
     fontWeight: 500,
-    MozOsxFontSmoothing: "auto",
-    WebkitFontSmoothing: "auto",
+    MozOsxFontSmoothing: 'auto',
+    WebkitFontSmoothing: 'auto',
   },
 ]);
 export const td = css([
   { fontFamily: secondaryFont },
   {
-    fontSize: ".90rem",
+    fontSize: '.90rem',
     fontWeight: 400,
-    MozOsxFontSmoothing: "auto",
-    WebkitFontSmoothing: "auto",
+    MozOsxFontSmoothing: 'auto',
+    WebkitFontSmoothing: 'auto',
   },
 ]);
 
@@ -125,15 +131,15 @@ export const screenReaderOnly = css([
   // https://gomakethings.com/revisting-aria-label-versus-a-visually-hidden-class/#:~:text=In%20the%20posts%2C%20I%20recommended,visually%20shown%20to%20sighted%20users.
   {
     border: 0,
-    clip: "rect(0 0 0 0)",
-    clipPath: "inset(50%)",
-    display: "inline-block", // new - for reading order in macOS VO
-    height: "1px",
-    margin: "-1px",
-    overflow: "hidden",
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    display: 'inline-block', // new - for reading order in macOS VO
+    height: '1px',
+    margin: '-1px',
+    overflow: 'hidden',
     padding: 0,
-    position: "relative", // different - for reading order in macOS VO
-    width: "1px",
+    position: 'relative', // different - for reading order in macOS VO
+    width: '1px',
   },
 ]);
 
