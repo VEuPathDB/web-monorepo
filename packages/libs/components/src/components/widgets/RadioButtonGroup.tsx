3c28b9c329e56b79ca1e1ae3f8300054c710fd24
Dae Kun (DK) Kwon <dkwon@nd.edu>
2023-05-10 09:28:37 -0400

    set default buttonColor to be primary

--- packages/libs/components/src/components/widgets/RadioButtonGroup.tsx
+++ packages/libs/components/src/components/widgets/RadioButtonGroup.tsx
@@ -50,7 +50,7 @@ export default function RadioButtonGroup({
   containerStyles = {},
   labelPlacement,
   minWidth,
-  buttonColor,
+  buttonColor = 'primary',
   margins,
   itemMarginRight,
   disabledList,
