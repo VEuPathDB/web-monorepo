edbf0bcb67a9644ae18b6faf05519001d3196a23
Dave Falke <dfalke@uga.edu>
2023-05-15 10:51:00 -0400

    Add missing dependency

--- packages/sites/genomics-site/webapp/wdkCustomization/js/client/components/homepage/VEuPathDBHomePage.tsx
+++ packages/sites/genomics-site/webapp/wdkCustomization/js/client/components/homepage/VEuPathDBHomePage.tsx
@@ -352,45 +352,46 @@ const useHeaderMenuItems = (
 
   const showInteractiveMaps = Boolean(useEda && projectId === 'VectorBase');
 
-  const mapMenuItems = useWdkService(async (wdkService): Promise<
-    HeaderMenuItem[]
-  > => {
-    if (!showInteractiveMaps) return [];
-    try {
-      const anwser = await wdkService.getAnswerJson(
-        {
-          searchName: 'AllDatasets',
-          searchConfig: {
-            parameters: {},
+  const mapMenuItems = useWdkService(
+    async (wdkService): Promise<HeaderMenuItem[]> => {
+      if (!showInteractiveMaps) return [];
+      try {
+        const anwser = await wdkService.getAnswerJson(
+          {
+            searchName: 'AllDatasets',
+            searchConfig: {
+              parameters: {},
+            },
           },
-        },
-        {
-          attributes: ['eda_study_id'],
-        }
-      );
-      return anwser.records
-        .filter((record) => record.attributes.eda_study_id != null)
-        .map((record) => ({
-          key: `map-${record.id[0].value}`,
-          display: record.displayName,
-          type: 'reactRoute',
-          url: `/maps/${record.id[0].value}/new`,
-        }));
-    } catch (error) {
-      console.error(error);
-      return [
-        {
-          key: 'maps-error',
-          display: (
-            <>
-              <Warning /> Could not load map data
-            </>
-          ),
-          type: 'custom',
-        },
-      ];
-    }
-  });
+          {
+            attributes: ['eda_study_id'],
+          }
+        );
+        return anwser.records
+          .filter((record) => record.attributes.eda_study_id != null)
+          .map((record) => ({
+            key: `map-${record.id[0].value}`,
+            display: record.displayName,
+            type: 'reactRoute',
+            url: `/maps/${record.id[0].value}/new`,
+          }));
+      } catch (error) {
+        console.error(error);
+        return [
+          {
+            key: 'maps-error',
+            display: (
+              <>
+                <Warning /> Could not load map data
+              </>
+            ),
+            type: 'custom',
+          },
+        ];
+      }
+    },
+    [showInteractiveMaps]
+  );
 
   // type: reactRoute, webAppRoute, externalLink, subMenu, custom
   const fullMenuItemEntries: HeaderMenuItemEntry[] = [
