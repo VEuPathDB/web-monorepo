22f9d9cbe54c83896c846289921629f5cc6b7f9a
Sam Kendrick <skendri2@upenn.edu>
2023-05-25 11:00:42 -0500

    142: Add loading spinner to map legend (#232)

--- packages/libs/eda/src/lib/map/analysis/MapLegend.tsx
+++ packages/libs/eda/src/lib/map/analysis/MapLegend.tsx
@@ -1,33 +1,41 @@
-import React from 'react';
+import Spinner from '@veupathdb/components/lib/components/Spinner';
 import PlotLegend from '@veupathdb/components/lib/components/plotControls/PlotLegend';
 import { LegendItemsProps } from '@veupathdb/components/lib/components/plotControls/PlotListLegend';
 
 interface Props {
   legendItems: LegendItemsProps[];
+  isLoading: boolean;
   title?: string;
   showCheckbox?: boolean;
 }
 
 export function MapLegend(props: Props) {
-  const { legendItems, title = 'Legend', showCheckbox } = props;
+  const { legendItems, isLoading, title = 'Legend', showCheckbox } = props;
+
   return (
     <>
       <div>
         <strong>{title}</strong>
       </div>
-      <PlotLegend
-        type="list"
-        legendItems={legendItems}
-        showOverlayLegend
-        containerStyles={{
-          border: 'none',
-          boxShadow: 'none',
-          padding: 0,
-          width: 'auto',
-          maxWidth: 400,
-        }}
-        showCheckbox={showCheckbox}
-      />
+      {isLoading ? (
+        <div style={{ marginTop: '1em', height: 50, position: 'relative' }}>
+          <Spinner size={20} />
+        </div>
+      ) : (
+        <PlotLegend
+          type="list"
+          legendItems={legendItems}
+          showOverlayLegend
+          containerStyles={{
+            border: 'none',
+            boxShadow: 'none',
+            padding: 0,
+            width: 'auto',
+            maxWidth: 400,
+          }}
+          showCheckbox={showCheckbox}
+        />
+      )}
     </>
   );
 }
