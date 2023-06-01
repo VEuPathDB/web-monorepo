1551e756848e2cb57dc742a64fbb5cdd48ae49d0
Dave Falke <dfalke@uga.edu>
2023-05-31 23:46:05 -0400

    Add missing node polyfills

--- packages/configs/site-webpack-config/index.js
+++ packages/configs/site-webpack-config/index.js
@@ -15,6 +15,10 @@ module.exports = function configure(additionalConfig) {
           fs: false,
           path: require.resolve('path-browserify'),
           stream: require.resolve('stream-browserify'),
+          crypto: require.resolve('crypto-browserify'),
+          http: require.resolve('stream-http'),
+          https: require.resolve('https-browserify'),
+          os: require.resolve('os-browserify/browser'),
         },
       },
 
