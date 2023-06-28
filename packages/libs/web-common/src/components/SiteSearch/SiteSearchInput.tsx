752cbfdc6062b9eaa6b91b23133c58974e363a32
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2023-06-28 09:13:19 -0400

    minor style updates

--- packages/libs/web-common/src/components/SiteSearch/SiteSearchInput.tsx
+++ packages/libs/web-common/src/components/SiteSearch/SiteSearchInput.tsx
@@ -3,22 +3,13 @@ import React, { useCallback, useEffect, useRef } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { Tooltip } from '@veupathdb/wdk-client/lib/Components';
 import { useSessionBackedState } from '@veupathdb/wdk-client/lib/Hooks/SessionBackedState';
-import {
-  makeClassNameHelper,
-  wrappable,
-} from '@veupathdb/wdk-client/lib/Utils/ComponentUtils';
-import {
-  SITE_SEARCH_ROUTE,
-  SEARCH_TERM_PARAM,
-  DOCUMENT_TYPE_PARAM,
-  ORGANISM_PARAM,
-  FILTERS_PARAM,
-} from './SiteSearchConstants';
+import { makeClassNameHelper, wrappable } from '@veupathdb/wdk-client/lib/Utils/ComponentUtils';
+import { SITE_SEARCH_ROUTE, SEARCH_TERM_PARAM, DOCUMENT_TYPE_PARAM, ORGANISM_PARAM, FILTERS_PARAM } from './SiteSearchConstants';
 
 import './SiteSearch.scss';
 import { TypeAheadInput } from './TypeAheadInput';
 
-const cx = makeClassNameHelper('SiteSearch');
+const cx = makeClassNameHelper("SiteSearch");
 
 const preventEventWith = (callback: () => void) => (event: React.FormEvent) => {
   event.preventDefault();
@@ -39,24 +30,11 @@ export const SiteSearchInput = wrappable(function ({
   const inputRef = useRef<HTMLInputElement>(null);
   const formRef = useRef<HTMLFormElement>(null);
   const searchParams = new URLSearchParams(location.search);
-  const searchString =
-    (location.pathname === SITE_SEARCH_ROUTE &&
-      searchParams.get(SEARCH_TERM_PARAM)) ||
-    '';
-  const docType =
-    (location.pathname === SITE_SEARCH_ROUTE &&
-      searchParams.get(DOCUMENT_TYPE_PARAM)) ||
-    '';
-  const organisms =
-    (location.pathname === SITE_SEARCH_ROUTE &&
-      searchParams.getAll(ORGANISM_PARAM)) ||
-    [];
-  const fields =
-    (location.pathname === SITE_SEARCH_ROUTE &&
-      searchParams.getAll(FILTERS_PARAM)) ||
-    [];
-  const hasFilters =
-    !isEmpty(docType) || !isEmpty(organisms) || !isEmpty(fields);
+  const searchString = (location.pathname === SITE_SEARCH_ROUTE && searchParams.get(SEARCH_TERM_PARAM)) || '';
+  const docType = (location.pathname === SITE_SEARCH_ROUTE && searchParams.get(DOCUMENT_TYPE_PARAM)) || '';
+  const organisms = (location.pathname === SITE_SEARCH_ROUTE && searchParams.getAll(ORGANISM_PARAM)) || [];
+  const fields = (location.pathname === SITE_SEARCH_ROUTE && searchParams.getAll(FILTERS_PARAM)) || [];
+  const hasFilters = !isEmpty(docType) || !isEmpty(organisms) || !isEmpty(fields);
 
   const onSearch = useCallback(
     (queryString: string) => {
