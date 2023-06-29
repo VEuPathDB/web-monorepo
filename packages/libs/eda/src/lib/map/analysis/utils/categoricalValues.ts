8338e5a582a3a353cd6029caffbb09486b85629f
Jeremy Myers <inernestmusic@gmail.com>
2023-06-29 11:33:28 -0400

    Simplify logic and improve documentation

--- packages/libs/eda/src/lib/map/analysis/utils/categoricalValues.ts
+++ packages/libs/eda/src/lib/map/analysis/utils/categoricalValues.ts
@@ -14,10 +14,10 @@ export async function getCategoricalValues({
 }: CategoricalValuesProps) {
   if (overlayEntity && overlayVariable) {
     /**
-     * The goal of this function is to return all possible values and a count for each value when a categorical variable is selected. This is acheived by:
+     * The goal of this function is to return all possible values and a count for each value when a categorical variable is selected. This is acheived as follows:
      *  1. Get all the values and counts by applying no filters to the distribution request
      *  2. If no filters are applied, we can just return unfilteredValues (the processed response) from this function
-     *  3. If filters are applied, we send a new request to get the filtered counts a convert it into filteredValues. Then, we
+     *  3. If filters are applied, we send a new request to get the filtered counts and convert it into filteredValues. Then, we
      *      1. map over unfilteredValues while checking against the filteredValues data
      *      2. if the data exists on filteredValues, we'll use its count; otherwise, we assign it a count of 0
      *      3. return the mapped unfilteredValues, which should include a count for all values now (even if 0)
@@ -50,21 +50,13 @@ export async function getCategoricalValues({
       const filteredValues = filteredDistributionResponse.histogram.map(
         (bin) => ({ label: bin.binLabel, count: bin.value })
       );
-      const filteredSet = new Set(filteredValues.map((v) => v.label));
-      return unfilteredValues.map((uv) => {
-        if (filteredSet.has(uv.label)) {
-          // NOTE: to please TS, this falls back to an empty count although .find() should
-          // always return a match in this condition
-          return (
-            filteredValues.find((fv) => fv.label === uv.label) ?? {
-              ...uv,
-              count: 0,
-            }
-          );
-        } else {
-          return { ...uv, count: 0 };
-        }
-      });
+      return unfilteredValues.map(
+        (uv) =>
+          filteredValues.find((fv) => fv.label === uv.label) ?? {
+            ...uv,
+            count: 0,
+          }
+      );
     } else {
       return unfilteredValues;
     }
