4a0e29212f605c58d02c610e64f55ee8868444aa
Jeremy Myers <inernestmusic@gmail.com>
2023-07-07 16:47:30 -0400

    Style disabled textarea state and add title attribute

--- packages/sites/clinepi-site/webapp/js/client/controllers/AccessRequestTextArea.jsx
+++ packages/sites/clinepi-site/webapp/js/client/controllers/AccessRequestTextArea.jsx
@@ -5,21 +5,23 @@ const AccessRequestTextArea = ({
   rows,
   cols,
   required,
+  disabled,
   ...otherProps
 }) => (
   <tr>
     <td colSpan={4}>
       <strong>
-        <label htmlFor={mykey}>
-          {label}
-        </label>
+        <label htmlFor={mykey}>{label}</label>
       </strong>
       <br />
       <br />
       <textarea
         id={mykey}
+        className={disabled ? 'disabled-access-request-textarea' : null}
         rows={8}
         cols={150}
+        title={disabled ? 'Pending requests cannot be edited.' : null}
+        disabled
         required
         {...otherProps}
       />
