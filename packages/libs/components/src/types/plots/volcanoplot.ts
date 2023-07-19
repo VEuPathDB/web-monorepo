7001a4403d77079ed1f611d421f20775ef146bae
asizemore <annsize@seas.upenn.edu>
2023-07-19 07:05:47 -0400

    fix required type error

--- packages/libs/components/src/types/plots/volcanoplot.ts
+++ packages/libs/components/src/types/plots/volcanoplot.ts
@@ -1,14 +1,14 @@
 // Can remove the | undefined from most of these after some other backend work is merged
 export type VolcanoPlotDataPoint = {
   // log2foldChange becomes the x axis. Also used for coloring points
-  log2foldChange: string | undefined;
+  log2foldChange?: string;
   // pValue will be negative log transformed for the y axis. Also
   // needed as is (untransformed) in the tooltip and when coloring points
-  pValue: string | undefined;
+  pValue?: string;
   // Used for thresholding and tooltip
-  adjustedPValue: string | undefined;
+  adjustedPValue?: string;
   // Used for tooltip
-  pointID: string | undefined;
+  pointID?: string;
 };
 
 export type VolcanoPlotData = Array<VolcanoPlotDataPoint>;
