3a8a3deed54b1b85acbc38b9e7d02e49909a3572
asizemore <annsize@seas.upenn.edu>
2023-08-02 14:35:44 -0400

    updated bubble marker

--- packages/libs/eda/src/lib/map/analysis/MarkerConfiguration/icons/BubbleMarker.tsx
+++ packages/libs/eda/src/lib/map/analysis/MarkerConfiguration/icons/BubbleMarker.tsx
@@ -16,36 +16,14 @@ export function BubbleMarker(props: SVGProps<SVGSVGElement>) {
       {...props}
     >
       <style type="text/css">
-        .st0&#123;fill:none;stroke:#262626;stroke-miterlimit:10;&#125;
-        .st1&#123;fill:#C3C2C2;&#125; .st2&#123;fill:#7F7F7F;&#125;
-        .st3&#123;fill:#4D4D4E;&#125; .st4&#123;fill:#262626;&#125;
-        .st5&#123;fill:none;stroke:#C3C2C2;stroke-width:5.4029;stroke-miterlimit:10;&#125;
-        .st6&#123;fill:none;stroke:#7F7F7F;stroke-width:5.4029;stroke-miterlimit:10;&#125;
-        .st7&#123;fill:none;stroke:#262626;stroke-width:5.4029;stroke-miterlimit:10;&#125;
-        .st8&#123;fill:none;stroke:#4D4D4E;stroke-width:5.4029;stroke-miterlimit:10;&#125;
-        .st9&#123;fill:#F2F2F3;&#125; .st10&#123;fill:#DAD9D9;&#125;
-        .st11&#123;fill:#BFBFBF;&#125; .st12&#123;fill:#D9D8D8;&#125;
-        .st13&#123;fill:#D8D7D7;&#125; .st14&#123;fill:#D7D6D6;&#125;
-        .st15&#123;fill:#D6D6D5;&#125; .st16&#123;fill:#D5D5D4;&#125;
-        .st17&#123;fill:#D4D4D3;&#125; .st18&#123;fill:#D3D3D3;&#125;
-        .st19&#123;fill:#D3D2D2;&#125; .st20&#123;fill:#D2D1D1;&#125;
-        .st21&#123;fill:#D1D0D0;&#125; .st22&#123;fill:#D0D0CF;&#125;
-        .st23&#123;fill:#CFCFCE;&#125; .st24&#123;fill:#CECECD;&#125;
-        .st25&#123;fill:#CECDCD;&#125; .st26&#123;fill:#CDCCCC;&#125;
-        .st27&#123;fill:#CCCBCB;&#125; .st28&#123;fill:#CBCACA;&#125;
-        .st29&#123;fill:#CACAC9;&#125; .st30&#123;fill:#C9C9C8;&#125;
-        .st31&#123;fill:#C9C8C8;&#125; .st32&#123;fill:#C8C7C7;&#125;
-        .st33&#123;fill:#C7C6C6;&#125; .st34&#123;fill:#C6C5C5;&#125;
-        .st35&#123;fill:#C5C5C4;&#125; .st36&#123;fill:#C4C4C4;&#125;
-        .st37&#123;fill:#C3C3C3;&#125; .st38&#123;fill:#C2C1C1;&#125;
-        .st39&#123;fill:#C1C0C0;&#125; .st40&#123;fill:#C0C0BF;&#125;
-        .st41&#123;fill:#666766;&#125; .st42&#123;fill:#4C4C4D;&#125;
-        .st43&#123;fill:#333333;&#125;
-        .st44&#123;fill:#DAD9D9;stroke:#333333;stroke-miterlimit:10;&#125;
+        .bubble-1&#123;fill:#C3C2C2;&#125; .bubble-2&#123;fill:#666766;&#125;
+        .bubble-3&#123;fill:#262626;&#125;
       </style>
-      <circle className="st44" cx="13.485" cy="17.771" r="10.862" />
-      <circle className="st44" cx="24.705" cy="10.487" r="5.115" />
-      <circle className="st44" cx="19.149" cy="6.367" r="2.558" />
+      <g>
+        <circle className="bubble-1" cx="12.004" cy="19.253" r="9.381" />
+        <circle className="bubble-2" cx="24.787" cy="10.631" r="5.034" />
+        <circle className="bubble-3" cx="17.171" cy="6.391" r="2.582" />
+      </g>
     </svg>
   );
 }
