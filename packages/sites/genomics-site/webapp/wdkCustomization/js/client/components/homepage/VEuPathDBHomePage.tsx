be4c7195b64aba9aa4230d53a9dc86f7baf8f674
Dave Falke <dfalke@uga.edu>
2023-09-07 14:18:36 -0400

    Fix map menu entry (#483)
    * Fix placement of beta icon
    
    * Open map in new tab

--- packages/sites/genomics-site/webapp/wdkCustomization/js/client/components/homepage/VEuPathDBHomePage.tsx
+++ packages/sites/genomics-site/webapp/wdkCustomization/js/client/components/homepage/VEuPathDBHomePage.tsx
@@ -601,11 +601,12 @@ const useHeaderMenuItems = (
           type: 'reactRoute',
           display: (
             <>
-              <img alt="BETA" src={betaImage} /> Multi-study Interactive Map
+              Multi-study Interactive Map <img alt="BETA" src={betaImage} />
             </>
           ),
           key: 'map--mega-study',
           url: '/workspace/maps/DS_480c976ef9/new',
+          target: '_blank',
           metadata: {
             test: () => showInteractiveMaps,
           },
