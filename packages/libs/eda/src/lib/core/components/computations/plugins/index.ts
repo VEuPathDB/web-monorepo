276b8dce0e3bc2b81cc5c587329eb8254d10b53d
asizemore <annsize@seas.upenn.edu>
2023-09-25 10:51:25 -0400

    added correlation plugin

--- packages/libs/eda/src/lib/core/components/computations/plugins/index.ts
+++ packages/libs/eda/src/lib/core/components/computations/plugins/index.ts
@@ -7,12 +7,13 @@ import { plugin as countsandproportions } from './countsAndProportions';
 import { plugin as abundance } from './abundance';
 import { plugin as differentialabundance } from './differentialabundance';
 import { plugin as xyrelationships } from './xyRelationships';
-
+import { plugin as correlationassaymetadata } from './correlationAssayMetadata';
 export const plugins: Record<string, ComputationPlugin> = {
   abundance,
   alphadiv,
   betadiv,
   differentialabundance,
+  correlationassaymetadata,
   countsandproportions,
   distributions,
   pass,
