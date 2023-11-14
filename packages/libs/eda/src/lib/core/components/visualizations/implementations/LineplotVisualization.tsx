91f51769a5137672df81c1619b3932154c1171ef
Bob <uncoolbob@gmail.com>
2023-11-14 23:18:26 +0000

    revert removal of code from AggregationInputs

--- packages/libs/eda/src/lib/core/components/visualizations/implementations/LineplotVisualization.tsx
+++ packages/libs/eda/src/lib/core/components/visualizations/implementations/LineplotVisualization.tsx
@@ -2738,6 +2738,7 @@ type AggregationConfig<F extends string, P extends Array<string>> =
       options: P;
     };
 
+// TO DO: move outside of this source file
 export function AggregationInputs<F extends string, P extends Array<string>>(
   props: AggregationConfig<F, P>
 ) {
@@ -2797,6 +2798,9 @@ export function AggregationInputs<F extends string, P extends Array<string>>(
           >
             <ValuePicker
               allowedValues={props.options}
+              disallowedValues={props.numeratorValues.filter(
+                (value) => !props.options.includes(value)
+              )}
               selectedValues={props.numeratorValues}
               onSelectedValuesChange={props.onNumeratorChange}
             />
@@ -2810,6 +2814,9 @@ export function AggregationInputs<F extends string, P extends Array<string>>(
           >
             <ValuePicker
               allowedValues={props.options}
+              disallowedValues={props.numeratorValues.filter(
+                (value) => !props.options.includes(value)
+              )}
               selectedValues={props.denominatorValues}
               onSelectedValuesChange={props.onDenominatorChange}
             />
