82cd4e162a8acf8cc4624c08cc55afe96d39f514
aurreco-uga <aurreco@uga.edu>
2023-12-01 16:45:02 -0500

    add menu link now that we have a page in the portal

--- packages/sites/genomics-site/webapp/wdkCustomization/js/client/components/homepage/VEuPathDBHomePage.tsx
+++ packages/sites/genomics-site/webapp/wdkCustomization/js/client/components/homepage/VEuPathDBHomePage.tsx
@@ -744,9 +744,6 @@ const useHeaderMenuItems = (
           display: <>Download data files</>,
           type: 'reactRoute',
           url: '/downloads',
-          metadata: {
-            exclude: [EuPathDB],
-          },
         },
         {
           key: 'mahpic-data',
