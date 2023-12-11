47e259170c1398aafa600a99b1b24156edaecda4
Dae Kun (DK) Kwon <dkwon@nd.edu>
2023-12-11 16:11:41 -0500

    add scale to EDA's display labels

--- packages/libs/eda/src/lib/core/utils/variable-display.ts
+++ packages/libs/eda/src/lib/core/utils/variable-display.ts
@@ -1,9 +1,29 @@
 import { NumberVariable, VariableTreeNode } from '../types/study';
 
+//DKDK add scale in conjunction with
 export function variableDisplayWithUnit(
   variable: VariableTreeNode | undefined
 ): string | undefined {
-  if (NumberVariable.is(variable) && variable.units && variable.units != null)
-    return variable.displayName + ' (' + variable.units + ')';
-  else return variable?.displayName;
+  if (NumberVariable.is(variable)) {
+    if (variable.units && variable.units != null) {
+      if (variable.scale && variable.scale != null) {
+        return (
+          variable.displayName +
+          ' (' +
+          variable.units +
+          ', ' +
+          variable.scale +
+          ')'
+        );
+      } else {
+        return variable.displayName + ' (' + variable.units + ')';
+      }
+    } else {
+      if (variable.scale && variable.scale != null) {
+        return variable.displayName + ' (' + variable.scale + ')';
+      } else {
+        return variable?.displayName;
+      }
+    }
+  } else return variable?.displayName;
 }
