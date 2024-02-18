0ecb5d35b7493d26e8a69c5d45be01258513b8ed
Bob <uncoolbob@gmail.com>
2024-02-18 00:29:07 +0000

    configure ts and storybook/babel for emotion css tag

--- packages/libs/components/.storybook/main.js
+++ packages/libs/components/.storybook/main.js
@@ -11,6 +11,17 @@ module.exports = {
     '@storybook/addon-viewport/register',
     './redmine-addon/register.js',
   ],
+  babel: async (options) => {
+    return {
+      ...options,
+      presets: [...options.presets, '@emotion/babel-preset-css-prop'],
+      // See https://stackoverflow.com/questions/70406632/typescript-parameter-properties-not-working-with-storybook-rollup-in-developm
+      plugins: options.plugins.filter(
+        (x) =>
+          !(typeof x === 'string' && x.includes('plugin-transform-classes'))
+      ),
+    };
+  },
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
