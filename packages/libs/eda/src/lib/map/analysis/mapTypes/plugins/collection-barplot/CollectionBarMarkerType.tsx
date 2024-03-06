e3b2c89d2e7750630e2c27ee0604ab30795f12ff
Dave Falke <dfalke@uga.edu>
2024-03-06 17:06:48 -0500

    WIP collection bar marker config

--- packages/libs/eda/src/lib/map/analysis/mapTypes/plugins/collection-barplot/CollectionBarMarkerType.tsx
+++ packages/libs/eda/src/lib/map/analysis/mapTypes/plugins/collection-barplot/CollectionBarMarkerType.tsx
@@ -1,30 +1,57 @@
 import { preorder } from '@veupathdb/wdk-client/lib/Utils/TreeUtils';
-import { VariableCollectionDescriptor } from '../../../../../core/types/variable';
-import { MapTypeConfigPanelProps, MapTypePlugin } from '../../types';
+import {
+  MapTypeConfigPanelProps,
+  MapTypeMapLayerProps,
+  MapTypePlugin,
+} from '../../types';
+import {
+  CollectionVariableTreeNode,
+  StudyEntity,
+  useFindEntityAndVariableCollection,
+  useStudyEntities,
+} from '../../../../../core';
+import { VariableCollectionSelectList } from '../../../../../core/components/variableSelectors/VariableCollectionSingleSelect';
+import { CheckboxList, SelectList } from '@veupathdb/coreui';
+import { Item } from '@veupathdb/coreui/lib/components/inputs/checkboxes/CheckboxList';
+import { DraggablePanel } from '@veupathdb/coreui/lib/components/containers';
+import { DraggableLegendPanel } from '../../../DraggableLegendPanel';
+import { MapLegend } from '../../../MapLegend';
+import { LegendItemsProps } from '@veupathdb/components/lib/components/plotControls/PlotListLegend';
+import { BarPlotMarkerIcon } from '../../MarkerConfiguration/icons';
 
 const displayName = 'Bar plots';
 
 interface CollectionBarMarkerConfiguration {
   type: 'collection-barplot';
-  selectedCollection: VariableCollectionDescriptor;
+  entityId: string;
+  collectionId: string;
+  selectedVariableIds: string[];
 }
 
 export const plugin: MapTypePlugin<CollectionBarMarkerConfiguration> = {
+  type: 'collection-barplot',
+  IconComponent: BarPlotMarkerIcon,
   displayName,
   getDefaultConfig({ study }) {
     const firstCollection = Array.from(
       preorder(study.rootEntity, (e) => e.children ?? [])
     )
-      .flatMap((e) => e.collections?.map((c) => [e, c]) ?? [])
+      .flatMap(
+        (e) =>
+          e.collections?.map((c): [StudyEntity, CollectionVariableTreeNode] => [
+            e,
+            c,
+          ]) ?? []
+      )
       .at(0);
     if (firstCollection == null)
       throw new Error('This study does not have any collections.');
+    const [entity, collection] = firstCollection;
     return {
       type: 'collection-barplot',
-      selectedCollection: {
-        entityId: firstCollection[0].id,
-        collectionId: firstCollection[1].id,
-      },
+      entityId: entity.id,
+      collectionId: collection.id,
+      selectedVariableIds: collection.memberVariableIds,
     };
   },
   ConfigPanelComponent,
@@ -36,10 +63,59 @@ export const plugin: MapTypePlugin<CollectionBarMarkerConfiguration> = {
 function ConfigPanelComponent(
   props: MapTypeConfigPanelProps<CollectionBarMarkerConfiguration>
 ) {
+  const configuration = props.configuration as CollectionBarMarkerConfiguration;
+  const { updateConfiguration } = props;
+  const findEntityAndCollection = useFindEntityAndVariableCollection();
+  const { entity, variableCollection } =
+    findEntityAndCollection({
+      entityId: configuration.entityId,
+      collectionId: configuration.collectionId,
+    }) ?? {};
+  const memberVariableIdSet = new Set(variableCollection?.memberVariableIds);
+  const variableCheckboxListItems = entity?.variables
+    .filter((v) => memberVariableIdSet.has(v.id))
+    .map(
+      (variable): Item<string> => ({
+        display: variable.displayName,
+        value: variable.id,
+      })
+    );
   return (
     <div css={{ padding: '2em' }}>
       <p>I am a config component</p>
-      <p>Selected collection: {JSON.stringify(props.configuration)}</p>
+      <VariableCollectionSelectList
+        value={{
+          entityId: configuration.entityId,
+          collectionId: configuration.collectionId,
+        }}
+        onSelect={function (
+          value?: { entityId: string; collectionId: string } | undefined
+        ): void {
+          if (value == null) return;
+          const { entityId, collectionId } = value;
+          updateConfiguration({
+            type: 'collection-barplot',
+            entityId,
+            collectionId,
+            selectedVariableIds: [],
+          });
+        }}
+      />
+      <SelectList
+        defaultButtonDisplayContent="Select variables"
+        items={variableCheckboxListItems ?? []}
+        value={configuration.selectedVariableIds}
+        onChange={function (selectedVariableIds: string[]): void {
+          updateConfiguration({
+            ...configuration,
+            selectedVariableIds,
+          });
+        }}
+      />
+      <p>
+        Selected collection:{' '}
+        <pre>{JSON.stringify(props.configuration, null, 2)}</pre>
+      </p>
     </div>
   );
 }
@@ -48,8 +124,47 @@ function MapLayerComponent() {
   return <div>I am a map layer component</div>;
 }
 
-function MapOverlayComponent() {
-  return <div>I am a map overlay component</div>;
+function MapOverlayComponent(
+  props: MapTypeMapLayerProps<CollectionBarMarkerConfiguration>
+) {
+  const configuration = props.configuration as CollectionBarMarkerConfiguration;
+  const { headerButtons } = props;
+
+  const findEntityAndCollection = useFindEntityAndVariableCollection();
+  const { entity, variableCollection } =
+    findEntityAndCollection(configuration) ?? {};
+
+  const noDataError = null;
+
+  const legendItems = entity?.variables
+    .filter((variable) =>
+      configuration.selectedVariableIds.includes(variable.id)
+    )
+    .map(
+      (variable): LegendItemsProps => ({
+        label: variable.displayName,
+        marker: 'square',
+        hasData: true,
+      })
+    );
+
+  return (
+    <DraggableLegendPanel
+      panelTitle={variableCollection?.displayName}
+      zIndex={3}
+      headerButtons={headerButtons}
+    >
+      <div style={{ padding: '5px 10px' }}>
+        {noDataError ?? (
+          <MapLegend
+            isLoading={false}
+            plotLegendProps={{ type: 'list', legendItems: legendItems ?? [] }}
+            showCheckbox={false}
+          />
+        )}
+      </div>
+    </DraggableLegendPanel>
+  );
 }
 
 function MapTypeHeaderDetails() {
---
+++ packages/libs/eda/src/lib/map/analysis/mapTypes/plugins/collection-barplot/CollectionBarPlotMarkerConfigurationMenu.tsx
@@ -0,0 +1,336 @@
+import { useCallback } from 'react';
+import {
+  InputVariables,
+  Props as InputVariablesProps,
+} from '../../../../../core/components/visualizations/InputVariables';
+import RadioButtonGroup from '@veupathdb/components/lib/components/widgets/RadioButtonGroup';
+import { VariablesByInputName } from '../../../../../core/utils/data-element-constraints';
+import {
+  usePromise,
+  AllValuesDefinition,
+  OverlayConfig,
+  Variable,
+  Filter,
+} from '../../../../../core';
+import { CategoricalMarkerConfigurationTable } from '../../MarkerConfiguration/CategoricalMarkerConfigurationTable';
+import { CategoricalMarkerPreview } from '../../MarkerConfiguration/CategoricalMarkerPreview';
+import Barplot from '@veupathdb/components/lib/plots/Barplot';
+import { SubsettingClient } from '../../../../../core/api';
+import { Toggle } from '@veupathdb/coreui';
+import { useUncontrolledSelections } from '../../../hooks/uncontrolledSelections';
+import {
+  BinningMethod,
+  SelectedCountsOption,
+  SelectedValues,
+} from '../../../appState';
+import { gray } from '@veupathdb/coreui/lib/definitions/colors';
+import { SharedMarkerConfigurations } from '../../shared';
+
+interface MarkerConfiguration<T extends string> {
+  type: T;
+}
+
+export interface BarPlotMarkerConfiguration
+  extends MarkerConfiguration<'collection-barplot'>,
+    SharedMarkerConfigurations {
+  selectedPlotMode: 'count' | 'proportion';
+  dependentAxisLogScale: boolean;
+  binningMethod: BinningMethod;
+  selectedValues: SelectedValues;
+  selectedCountsOption: SelectedCountsOption;
+}
+
+interface Props
+  extends Omit<
+    InputVariablesProps,
+    'onChange' | 'selectedVariables' | 'selectedPlotMode' | 'onPlotSelected'
+  > {
+  onChange: (configuration: BarPlotMarkerConfiguration) => void;
+  configuration: BarPlotMarkerConfiguration;
+  overlayConfiguration: OverlayConfig | undefined;
+  overlayVariable: Variable | undefined;
+  subsettingClient: SubsettingClient;
+  studyId: string;
+  filters: Filter[] | undefined;
+  continuousMarkerPreview: JSX.Element | undefined;
+  /**
+   * Always used for categorical marker preview. Also used in categorical table if selectedCountsOption is 'filtered'
+   */
+  allFilteredCategoricalValues: AllValuesDefinition[] | undefined;
+  /**
+   * Only defined and used in categorical table if selectedCountsOption is 'visible'
+   */
+  allVisibleCategoricalValues: AllValuesDefinition[] | undefined;
+}
+
+// TODO: generalize this and PieMarkerConfigMenu into MarkerConfigurationMenu. Lots of code repetition...
+
+export function BarPlotMarkerConfigurationMenu({
+  entities,
+  onChange,
+  starredVariables,
+  toggleStarredVariable,
+  configuration,
+  constraints,
+  overlayConfiguration,
+  overlayVariable,
+  subsettingClient,
+  studyId,
+  filters,
+  continuousMarkerPreview,
+  allFilteredCategoricalValues,
+  allVisibleCategoricalValues,
+}: Props) {
+  /**
+   * Used to track the CategoricalMarkerConfigurationTable's selection state, which allows users to
+   * select more than the allowable limit. Doing so results in a message to the user that they've selected
+   * too many values. The state is lifted up (versus living in CategoricalMarkerConfigurationTable) in order
+   * to pass its length to CategoricalMarkerPreview.
+   */
+  const { uncontrolledSelections, setUncontrolledSelections } =
+    useUncontrolledSelections(
+      overlayConfiguration?.overlayType === 'categorical'
+        ? overlayConfiguration?.overlayValues
+        : undefined
+    );
+
+  const barplotData = usePromise(
+    useCallback(async () => {
+      if (
+        !overlayVariable ||
+        overlayConfiguration?.overlayType !== 'continuous' ||
+        !('distributionDefaults' in overlayVariable)
+      )
+        return;
+      const binSpec = {
+        displayRangeMin:
+          overlayVariable.distributionDefaults.rangeMin +
+          (overlayVariable.type === 'date' ? 'T00:00:00Z' : ''),
+        displayRangeMax:
+          overlayVariable.distributionDefaults.rangeMax +
+          (overlayVariable.type === 'date' ? 'T00:00:00Z' : ''),
+        binWidth: overlayVariable.distributionDefaults.binWidth ?? 1,
+        binUnits:
+          'binUnits' in overlayVariable.distributionDefaults
+            ? overlayVariable.distributionDefaults.binUnits
+            : undefined,
+      };
+      const distributionResponse = await subsettingClient.getDistribution(
+        studyId,
+        configuration.selectedVariable.entityId,
+        configuration.selectedVariable.variableId,
+        {
+          valueSpec: 'count',
+          filters: filters ?? [],
+          binSpec,
+        }
+      );
+      return {
+        name: '',
+        value: distributionResponse.histogram.map((d) => d.value),
+        label: distributionResponse.histogram.map((d) => d.binLabel),
+        showValues: false,
+        color: '#333',
+      };
+    }, [
+      studyId,
+      overlayVariable,
+      overlayConfiguration?.overlayType,
+      subsettingClient,
+      filters,
+      configuration.selectedVariable,
+    ])
+  );
+
+  function handleInputVariablesOnChange(selection: VariablesByInputName) {
+    if (!selection.overlayVariable) {
+      console.error(
+        `Expected overlay to defined but got ${typeof selection.overlayVariable}`
+      );
+      return;
+    }
+
+    onChange({
+      ...configuration,
+      selectedVariable: selection.overlayVariable,
+      selectedValues: undefined,
+    });
+  }
+  function handlePlotModeSelection(option: string) {
+    onChange({
+      ...configuration,
+      selectedPlotMode:
+        option as BarPlotMarkerConfiguration['selectedPlotMode'],
+    });
+  }
+  function handleBinningMethodSelection(option: string) {
+    onChange({
+      ...configuration,
+      binningMethod: option as BarPlotMarkerConfiguration['binningMethod'],
+    });
+  }
+  function handleLogScaleChange(option: boolean) {
+    onChange({
+      ...configuration,
+      dependentAxisLogScale: option,
+    });
+  }
+
+  return (
+    <div>
+      <p
+        style={{
+          margin: '5px 0 0 0',
+          fontWeight: 'bold',
+        }}
+      >
+        Color:
+      </p>
+      {/* limit inputVariables width */}
+      <div style={{ maxWidth: '350px' }}>
+        <InputVariables
+          inputs={[
+            {
+              name: 'overlayVariable',
+              label: 'Variable',
+              titleOverride: ' ',
+              isNonNullable: true,
+            },
+          ]}
+          entities={entities}
+          selectedVariables={{
+            overlayVariable: configuration.selectedVariable,
+          }}
+          onChange={handleInputVariablesOnChange}
+          starredVariables={starredVariables}
+          toggleStarredVariable={toggleStarredVariable}
+          constraints={constraints}
+        />
+      </div>
+      <div style={{ margin: '5px 0 0 0' }}>
+        <div style={{ fontWeight: 'bold', marginBottom: '0.5em' }}>
+          Summary marker (all filtered data)
+        </div>
+        {overlayConfiguration?.overlayType === 'categorical' ? (
+          <>
+            <CategoricalMarkerPreview
+              overlayConfiguration={overlayConfiguration}
+              allFilteredCategoricalValues={allFilteredCategoricalValues}
+              mapType="barplot"
+              numberSelected={uncontrolledSelections.size}
+              isDependentAxisLogScaleActive={
+                configuration.dependentAxisLogScale
+              }
+            />
+          </>
+        ) : (
+          continuousMarkerPreview
+        )}
+      </div>
+      <div style={{ maxWidth: '360px', marginTop: '1em' }}>
+        <div
+          style={{
+            color: gray[900],
+            fontWeight: 500,
+            fontSize: '1.2em',
+            marginBottom: '0.5em',
+          }}
+        >
+          Marker X-axis controls
+        </div>
+        <RadioButtonGroup
+          containerStyles={
+            {
+              // marginTop: 20,
+            }
+          }
+          label="Binning method"
+          selectedOption={configuration.binningMethod ?? 'equalInterval'}
+          options={['equalInterval', 'quantile', 'standardDeviation']}
+          optionLabels={['Equal interval', 'Quantile (10)', 'Std. dev.']}
+          buttonColor={'primary'}
+          // margins={['-1em', '0', '0', '0em']}
+          onOptionSelected={handleBinningMethodSelection}
+          disabledList={
+            overlayConfiguration?.overlayType === 'continuous'
+              ? []
+              : ['equalInterval', 'quantile', 'standardDeviation']
+          }
+        />
+      </div>
+      <div style={{ maxWidth: '360px', marginTop: '1em', marginBottom: '1em' }}>
+        <div
+          style={{
+            color: gray[900],
+            fontWeight: 500,
+            fontSize: '1.2em',
+            marginBottom: '0.5em',
+          }}
+        >
+          Marker Y-axis controls
+        </div>
+        <RadioButtonGroup
+          containerStyles={
+            {
+              // marginTop: 20,
+            }
+          }
+          label="Plot mode"
+          selectedOption={configuration.selectedPlotMode || 'count'}
+          options={['count', 'proportion']}
+          optionLabels={['Count', 'Proportion']}
+          buttonColor={'primary'}
+          // margins={['-1em', '0', '0', '1em']}
+          onOptionSelected={handlePlotModeSelection}
+        />
+        <Toggle
+          label="Log scale"
+          themeRole="primary"
+          value={configuration.dependentAxisLogScale}
+          onChange={handleLogScaleChange}
+        />
+      </div>
+      {overlayConfiguration?.overlayType === 'categorical' && (
+        <div style={{ maxWidth: '360px', marginTop: '1em' }}>
+          <CategoricalMarkerConfigurationTable
+            overlayValues={overlayConfiguration.overlayValues}
+            configuration={configuration}
+            onChange={onChange}
+            uncontrolledSelections={uncontrolledSelections}
+            setUncontrolledSelections={setUncontrolledSelections}
+            allCategoricalValues={
+              configuration.selectedCountsOption === 'filtered'
+                ? allFilteredCategoricalValues
+                : allVisibleCategoricalValues
+            }
+            selectedCountsOption={configuration.selectedCountsOption}
+          />
+        </div>
+      )}
+      {overlayConfiguration?.overlayType === 'continuous' && barplotData.value && (
+        <div style={{ margin: '5px 0 0 0' }}>
+          <span style={{ fontWeight: 'bold' }}>
+            Raw distribution of overall filtered data
+          </span>
+          <Barplot
+            data={{ series: [barplotData.value] }}
+            barLayout="overlay"
+            showValues={false}
+            showIndependentAxisTickLabel={false}
+            spacingOptions={{
+              padding: 0,
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 0,
+              marginBottom: 0,
+            }}
+            containerStyles={{
+              height: '300px',
+              maxWidth: '360px',
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
