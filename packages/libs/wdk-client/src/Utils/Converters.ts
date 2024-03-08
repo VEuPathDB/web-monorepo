0c28e2b1c359d9fd50cf4c36d0b19942f5a6f1e3
Dave Falke <dfalke@uga.edu>
2024-03-08 12:26:52 -0500

    Add support for max file size

--- packages/libs/wdk-client/src/Utils/Converters.ts
+++ packages/libs/wdk-client/src/Utils/Converters.ts
@@ -1,7 +1,7 @@
 const scales = ['B', 'K', 'M', 'G'];
 
 export function bytesToHuman(size: number, scale = 0): string {
-  return size < 1024 || scale === scales.length - 1
+  return size < 1000 || scale === scales.length - 1
     ? `${size.toFixed(2)} ${scales[scale]}`
-    : bytesToHuman(size / 1024, scale + 1);
+    : bytesToHuman(size / 1000, scale + 1);
 }
