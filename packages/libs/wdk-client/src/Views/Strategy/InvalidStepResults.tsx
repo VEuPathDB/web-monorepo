1a7c71fa9b37cc17ce9fc70cf25b0ba2c5050982
Jeremy Myers <inernestmusic@gmail.com>
2024-03-08 14:56:08 -0500

    Add link to contact form

--- packages/libs/wdk-client/src/Views/Strategy/InvalidStepResults.tsx
+++ packages/libs/wdk-client/src/Views/Strategy/InvalidStepResults.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from '../../Components';
 
 interface Props {}
 
@@ -27,7 +28,8 @@ export default function InvalidStepResults(props: Props) {
           <strong>Warning</strong>: If a step contains an obsolete search, you
           will not be able to revise the step; you can only delete the step.
           Currently, such a strategy cannot be modified. Instead, you will have
-          to recreate the strategy. Please contact us if we can help with this.
+          to recreate the strategy. Please{' '}
+          <Link to="/contact-us">contact us</Link> if we can help with this.
         </em>
       </p>
     </div>
