1e22d36b30deb894e67844ef0d08585a7f2bde77
Dave Falke <dfalke@uga.edu>
2024-03-11 13:15:11 -0400

    Add support for max file size (#922)
    * Add support for max file size
    
    * formatting
    
    * verbiage

--- packages/libs/wdk-client/src/Utils/Converters.ts
+++ packages/libs/wdk-client/src/Utils/Converters.ts
@@ -1,7 +1,16 @@
-const scales = ['B', 'K', 'M', 'G'];
+const scales = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
 
-export function bytesToHuman(size: number, scale = 0): string {
-  return size < 1024 || scale === scales.length - 1
-    ? `${size.toFixed(2)} ${scales[scale]}`
-    : bytesToHuman(size / 1024, scale + 1);
+/**
+ * Convert bytes to a human readable string
+ */
+export function bytesToHuman(numBytes: number) {
+  return recurse(numBytes);
+}
+
+function recurse(size: number, scale = 0): string {
+  return size < 1000 || scale === scales.length - 1
+    ? `${size.toLocaleString(undefined, { maximumFractionDigits: 2 })} ${
+        scales[scale]
+      }`
+    : recurse(size / 1000, scale + 1);
 }
