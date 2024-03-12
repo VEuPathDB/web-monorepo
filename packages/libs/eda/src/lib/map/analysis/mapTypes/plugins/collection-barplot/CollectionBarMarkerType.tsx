2772780a5f6a775d4371817f09ad991351264c19
Dave Falke <dfalke@uga.edu>
2024-03-12 13:49:05 -0400

    WIP config menu

--- packages/libs/eda/src/lib/map/analysis/mapTypes/plugins/collection-barplot/CollectionBarMarkerType.tsx
+++ packages/libs/eda/src/lib/map/analysis/mapTypes/plugins/collection-barplot/CollectionBarMarkerType.tsx
@@ -7,17 +7,18 @@ import {
 import {
   CollectionVariableTreeNode,
   StudyEntity,
+  Variable,
   useFindEntityAndVariableCollection,
-  useStudyEntities,
 } from '../../../../../core';
 import { VariableCollectionSelectList } from '../../../../../core/components/variableSelectors/VariableCollectionSingleSelect';
-import { CheckboxList, SelectList } from '@veupathdb/coreui';
+import { SelectList } from '@veupathdb/coreui';
 import { Item } from '@veupathdb/coreui/lib/components/inputs/checkboxes/CheckboxList';
-import { DraggablePanel } from '@veupathdb/coreui/lib/components/containers';
 import { DraggableLegendPanel } from '../../../DraggableLegendPanel';
 import { MapLegend } from '../../../MapLegend';
 import { LegendItemsProps } from '@veupathdb/components/lib/components/plotControls/PlotListLegend';
 import { BarPlotMarkerIcon } from '../../MarkerConfiguration/icons';
+import { difference, noop, union, uniq } from 'lodash';
+import { Mesa } from '@veupathdb/coreui';
 
 const displayName = 'Bar plots';
 
@@ -26,6 +27,7 @@ interface CollectionBarMarkerConfiguration {
   entityId: string;
   collectionId: string;
   selectedVariableIds: string[];
+  selectedValues: string[];
 }
 
 export const plugin: MapTypePlugin<CollectionBarMarkerConfiguration> = {
@@ -47,11 +49,17 @@ export const plugin: MapTypePlugin<CollectionBarMarkerConfiguration> = {
     if (firstCollection == null)
       throw new Error('This study does not have any collections.');
     const [entity, collection] = firstCollection;
+    const selectedValues = entity.variables
+      .filter((variable): variable is Variable =>
+        collection.memberVariableIds.includes(variable.id)
+      )
+      .flatMap((variable) => variable.vocabulary ?? []);
     return {
       type: 'collection-barplot',
       entityId: entity.id,
       collectionId: collection.id,
-      selectedVariableIds: collection.memberVariableIds,
+      selectedVariableIds: collection.memberVariableIds.slice(0, 8),
+      selectedValues: union(selectedValues),
     };
   },
   ConfigPanelComponent,
@@ -72,50 +80,154 @@ function ConfigPanelComponent(
       collectionId: configuration.collectionId,
     }) ?? {};
   const memberVariableIdSet = new Set(variableCollection?.memberVariableIds);
-  const variableCheckboxListItems = entity?.variables
-    .filter((v) => memberVariableIdSet.has(v.id))
-    .map(
-      (variable): Item<string> => ({
-        display: variable.displayName,
-        value: variable.id,
-      })
-    );
+  const variables =
+    entity?.variables.filter((v): v is Variable =>
+      memberVariableIdSet.has(v.id)
+    ) ?? [];
+  const valueCheckboxListItems = union(
+    variables?.flatMap((v) => v.vocabulary ?? [])
+  ).map(
+    (value): Item<string> => ({
+      display: value,
+      value,
+    })
+  );
   return (
     <div css={{ padding: '2em' }}>
-      <p>I am a config component</p>
-      <VariableCollectionSelectList
-        value={{
-          entityId: configuration.entityId,
-          collectionId: configuration.collectionId,
+      <p
+        style={{
+          margin: '5px 0 0 0',
+          fontWeight: 'bold',
         }}
-        onSelect={function (
-          value?: { entityId: string; collectionId: string } | undefined
-        ): void {
-          if (value == null) return;
-          const { entityId, collectionId } = value;
-          updateConfiguration({
-            type: 'collection-barplot',
-            entityId,
-            collectionId,
-            selectedVariableIds: [],
-          });
-        }}
-      />
-      <SelectList
-        defaultButtonDisplayContent="Select variables"
-        items={variableCheckboxListItems ?? []}
-        value={configuration.selectedVariableIds}
-        onChange={function (selectedVariableIds: string[]): void {
-          updateConfiguration({
-            ...configuration,
-            selectedVariableIds,
-          });
-        }}
-      />
-      <p>
-        Selected collection:{' '}
-        <pre>{JSON.stringify(props.configuration, null, 2)}</pre>
+      >
+        Marker preview:
       </p>
+      <div>TBD</div>
+
+      <p
+        style={{
+          margin: '5px 0 0 0',
+          fontWeight: 'bold',
+        }}
+      >
+        Color:
+      </p>
+      <div>
+        <div css={{ margin: '1em 0' }}>
+          <label>Grouped Variable:</label>
+          <VariableCollectionSelectList
+            value={{
+              entityId: configuration.entityId,
+              collectionId: configuration.collectionId,
+            }}
+            onSelect={function (
+              value?: { entityId: string; collectionId: string } | undefined
+            ): void {
+              if (value == null) return;
+              const { entity, variableCollection } =
+                findEntityAndCollection(value) ?? {};
+              if (entity == null || variableCollection == null) return;
+              const selectedVariableIds =
+                variableCollection.memberVariableIds.slice(0, 8);
+              const selectedValues = uniq(
+                entity.variables
+                  .filter((v): v is Variable =>
+                    selectedVariableIds.includes(v.id)
+                  )
+                  .flatMap((v) => v.vocabulary ?? [])
+              );
+
+              updateConfiguration({
+                type: 'collection-barplot',
+                entityId: entity.id,
+                collectionId: variableCollection.id,
+                selectedVariableIds,
+                selectedValues,
+              });
+            }}
+          />
+        </div>
+        <div
+          css={{
+            width: '30em',
+            margin: '1em 0',
+          }}
+        >
+          <Mesa
+            state={{
+              rows: variables,
+              columns: [
+                {
+                  key: 'displayName',
+                  name: 'Variable',
+                  sortable: true,
+                },
+              ],
+              options: {
+                title: 'Choose variables',
+                useStickyHeader: true,
+                tableBodyMaxHeight: '20em',
+                isRowSelected: (row) =>
+                  configuration.selectedVariableIds.includes(row.id),
+              },
+              eventHandlers: {
+                onSearch: noop,
+                onSort: noop,
+                onRowSelect: (row) => {
+                  updateConfiguration({
+                    ...configuration,
+                    selectedVariableIds: union(
+                      configuration.selectedVariableIds,
+                      [row.id]
+                    ),
+                  });
+                },
+                onRowDeselect: (row) => {
+                  updateConfiguration({
+                    ...configuration,
+                    selectedVariableIds: difference(
+                      configuration.selectedVariableIds,
+                      [row.id]
+                    ),
+                  });
+                },
+                onMultipleRowDeselect: (rows) => {
+                  updateConfiguration({
+                    ...configuration,
+                    selectedVariableIds: difference(
+                      configuration.selectedVariableIds,
+                      rows.map((r) => r.id)
+                    ),
+                  });
+                },
+                onMultipleRowSelect: (rows) => {
+                  updateConfiguration({
+                    ...configuration,
+                    selectedVariableIds: union(
+                      configuration.selectedVariableIds,
+                      rows.map((r) => r.id)
+                    ),
+                  });
+                },
+              },
+            }}
+          />
+        </div>
+        <div css={{ margin: '1em 0' }}>
+          <label>Select values:</label>
+          <SelectList
+            defaultButtonDisplayContent="Select values"
+            items={valueCheckboxListItems ?? []}
+            value={configuration.selectedValues ?? []}
+            onChange={function (selectedValues: string[]): void {
+              updateConfiguration({
+                ...configuration,
+                selectedValues,
+              });
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
