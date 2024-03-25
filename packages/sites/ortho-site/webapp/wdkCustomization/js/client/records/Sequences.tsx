f137fa2b98bf339bd5f7d12182810c96a6ce7125
Bob <uncoolbob@gmail.com>
2024-03-25 23:04:34 +0000

    added pfam legend - needs wiring still

--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/records/Sequences.tsx
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/records/Sequences.tsx
@@ -14,6 +14,8 @@ import { PfamDomainArchitecture } from 'ortho-client/components/pfam-domains/Pfa
 import { extractPfamDomain } from 'ortho-client/records/utils';
 import Banner from '@veupathdb/coreui/lib/components/banners/Banner';
 import RadioButtonGroup from '@veupathdb/components/lib/components/widgets/RadioButtonGroup';
+import Mesa from '../../../../../../../libs/coreui/lib/components/Mesa';
+import { PfamDomain } from 'ortho-client/components/pfam-domains/PfamDomain';
 
 type RowType = Record<string, AttributeValue>;
 const CorePeripheralFilterStates = ['both', 'core', 'peripheral'] as const;
@@ -41,6 +43,8 @@ export function RecordTable_Sequences(
 
   const [corePeripheralFilterState, setCorePeripheralFilterState] =
     useState<CorePeripheralFilterState>('both');
+
+  const [pfamFilterIds, setPfamFilterIds] = useState<string[]>([]);
 
   const groupName = props.record.id.find(
     ({ name }) => name === 'group_name'
@@ -71,6 +75,17 @@ export function RecordTable_Sequences(
   // deal with Pfam domain architectures
   const proteinPfams = props.record.tables['ProteinPFams'];
   const rowsByAccession = groupBy(proteinPfams, 'full_id');
+
+  const accessionToPfamIds = useMemo(
+    () =>
+      proteinPfams.reduce((map, row) => {
+        const full_id = row['full_id'] as string;
+        if (!map.has(full_id)) map.set(full_id, new Set<string>());
+        map.set(full_id, map.get(full_id)!.add(row['accession'] as string));
+        return map;
+      }, new Map<string, Set<string>>()),
+    [proteinPfams]
+  );
 
   mesaColumns.unshift({
     key: 'pfamArchitecture',
@@ -209,8 +224,59 @@ export function RecordTable_Sequences(
   const clustalDisabled =
     highlightedNodes == null || highlightedNodes.length < 2;
 
+  const pfamMesaState = {
+    options: {
+      isRowSelected: (row: RowType) =>
+        pfamFilterIds.includes(row.accession as string),
+    },
+    rows: props.record.tables['PFams'],
+    columns: [
+      {
+        key: 'accession',
+        name: 'PFam accession',
+      },
+      {
+        key: 'symbol',
+        name: 'Symbol',
+      },
+      {
+        key: 'description',
+        name: 'Description',
+      },
+      {
+        key: 'num_proteins',
+        name: 'Count',
+        helpText: 'Number of proteins that contain this domain',
+      },
+      {
+        key: 'legend',
+        name: 'Legend',
+        renderCell: (cellProps: { row: RowType }) => {
+          const pfamId = cellProps.row.accession as string;
+          const symbol = cellProps.row.symbol as string;
+          return <PfamDomain pfamId={pfamId} title={`${pfamId} (${symbol})`} />;
+        },
+      },
+    ],
+    eventHandlers: {
+      onRowSelect: (row: RowType) =>
+        setPfamFilterIds((prev) => [...prev, row.accession as string]),
+      onRowDeselect: (row: RowType) =>
+        setPfamFilterIds((prev) => prev.filter((id) => id !== row.accession)),
+    },
+  };
+
   return (
     <>
+      <div
+        style={{
+          marginLeft: treeWidth,
+          display: 'flex',
+          flexDirection: 'row-reverse',
+        }}
+      >
+        <Mesa state={pfamMesaState} />
+      </div>
       <div
         style={{
           marginLeft: treeWidth,
