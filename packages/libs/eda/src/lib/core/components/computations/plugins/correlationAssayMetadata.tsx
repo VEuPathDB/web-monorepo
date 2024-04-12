f97fa9f9af2e634c3e783c24471de7d1f88659e2
asizemore <annsize@seas.upenn.edu>
2024-04-12 07:09:43 -0400

    move node actions to correlation plugin only and update column names

--- packages/libs/eda/src/lib/core/components/computations/plugins/correlationAssayMetadata.tsx
+++ packages/libs/eda/src/lib/core/components/computations/plugins/correlationAssayMetadata.tsx
@@ -70,39 +70,6 @@ export const plugin: ComputationPlugin = {
           return { partition1Name, partition2Name };
         }
       },
-      makeGetNodeMenuActions(studyMetadata) {
-        const entities = entityTreeToArray(studyMetadata.rootEntity);
-        const variables = entities.flatMap((e) => e.variables);
-        const collections = entities.flatMap(
-          (entity) => entity.collections ?? []
-        );
-        const hostCollection = collections.find(
-          (c) => c.id === 'EUPATH_0005050'
-        );
-        const parasiteCollection = collections.find(
-          (c) => c.id === 'EUPATH_0005051'
-        );
-        return function getNodeActions(nodeId: string) {
-          const [, variableId] = nodeId.split('.');
-          const variable = variables.find((v) => v.id === variableId);
-          if (variable == null) return [];
-
-          const href = parasiteCollection?.memberVariableIds.includes(
-            variable.id
-          )
-            ? `https://qa.plasmodb.org/plasmo/app/search/transcript/GenesByRNASeqpfal3D7_Lee_Gambian_ebi_rnaSeq_RSRCWGCNAModules?param.wgcnaParam=${variable.displayName.toLowerCase()}&autoRun=1`
-            : hostCollection?.memberVariableIds.includes(variable.id)
-            ? `https://qa.hostdb.org/hostdb/app/search/transcript/GenesByRNASeqhsapREF_Lee_Gambian_ebi_rnaSeq_RSRCWGCNAModules?param.wgcnaParam=${variable.displayName.toLowerCase()}&autoRun=1`
-            : undefined;
-          if (href == null) return [];
-          return [
-            {
-              label: 'See list of genes',
-              href,
-            },
-          ];
-        };
-      },
     }), // Must match name in data service and in visualization.tsx
   },
   isEnabledInPicker: isEnabledInPicker,
--- packages/libs/eda/src/lib/core/components/visualizations/implementations/BipartiteNetworkVisualization.tsx
+++ packages/libs/eda/src/lib/core/components/visualizations/implementations/BipartiteNetworkVisualization.tsx
@@ -284,7 +284,7 @@ function BipartiteNetworkViz(props: VisualizationProps<Options>) {
         {`Showing links with an absolute correlation coefficient above ${vizConfig.correlationCoefThreshold?.toString()} and a p-value below ${vizConfig.significanceThreshold?.toString()}`}
       </p>
       <p>
-        Click a node to hightlight it and its edges.
+        Click on a node to highlight its edges.
         {getNodeMenuActions &&
           ' A dropdown menu will appear on mouseover, if additional actions are available.'}
       </p>
