1b5eef8f41128c6a4288bcbff0607075ee304d02
Dave Falke <dfalke@uga.edu>
2024-07-30 12:16:31 -0400

    Convert to function component

--- packages/libs/user-datasets/src/lib/Components/Sharing/UserDatasetCommunityModal.jsx
+++ packages/libs/user-datasets/src/lib/Components/Sharing/UserDatasetCommunityModal.jsx
@@ -1,184 +1,148 @@
 import React from 'react';
 
 import { IconAlt as Icon, Loading } from '@veupathdb/wdk-client/lib/Components';
-import { WdkDependenciesContext } from '@veupathdb/wdk-client/lib/Hooks/WdkDependenciesEffect';
 import { Modal } from '@veupathdb/coreui';
 import { FilledButton } from '@veupathdb/coreui';
 
 import './UserDatasetSharingModal.scss';
 
-class UserDatasetSharingModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // Can be one of 'pending' | 'success' | 'error'
-      status: 'pending',
-    };
-    this.isMyDataset = this.isMyDataset.bind(this);
-    this.getDatasetNoun = this.getDatasetNoun.bind(this);
+export default function UserDatasetSharingModal(props) {
+  const {
+    datasets,
+    onClose,
+    dataNoun,
+    updateDatasetCommunityVisibility,
+    updateSuccessful,
+    updateError,
+    context,
+    updatePending,
+    user,
+  } = props;
 
-    this.renderViewContent = this.renderViewContent.bind(this);
-    this.isDatasetShareable = this.isDatasetShareable.bind(this);
-  }
+  const totalSelectedDatasets = datasets.length;
 
-  isMyDataset(dataset) {
-    const { user } = this.props;
-    return dataset && dataset.ownerUserId && dataset.ownerUserId === user.id;
-  }
+  const totalOwnedDatasets = datasets.filter(
+    (dataset) =>
+      dataset && dataset.ownerUserId && dataset.ownerUserId === user.id
+  ).length;
 
-  getDatasetNoun() {
-    return this.props.datasets.length === 1
-      ? `this ${this.props.dataNoun.singular.toLowerCase()}`
-      : `these ${this.props.dataNoun.plural.toLowerCase()}`;
-  }
+  const totalCommunityDatasets = datasets.filter(
+    (dataset) => dataset.meta.visibility === 'public'
+  ).length;
 
-  isDatasetShareable(dataset = {}) {
-    return dataset.ownerUserId === this.props.user.id;
-  }
+  const totalNotOwnedDatasets = totalSelectedDatasets - totalOwnedDatasets;
 
-  renderViewContent() {
-    const {
-      datasets,
-      onClose,
-      dataNoun,
-      updateDatasetCommunityVisibility,
-      updateSuccessful,
-      updateError,
-      context,
-    } = this.props;
-    const datasetNoun = this.getDatasetNoun();
+  const targetNoun =
+    totalSelectedDatasets === 1 ? dataNoun.singular : dataNoun.plural;
+  const targetNounLower = targetNoun.toLowerCase();
 
-    const CloseButton = () => (
-      <button className="btn" onClick={() => onClose()}>
-        Close this window.
-      </button>
-    );
+  const datasetNoun =
+    (totalSelectedDatasets === 1 ? 'this ' : 'these ') + targetNounLower;
 
-    if (updateError) {
-      return (
-        <div className="UserDataset-SharingModal-StatusView">
-          <Icon fa="times-circle danger" />
-          <h2>Error Sharing {dataNoun.plural}.</h2>
+  const CloseButton = () => (
+    <button className="btn" onClick={() => onClose()}>
+      Close this window.
+    </button>
+  );
+
+  const content = updatePending ? (
+    <Loading />
+  ) : updateError ? (
+    <div className="UserDataset-SharingModal-StatusView">
+      <Icon fa="times-circle danger" />
+      <h2>Error Sharing {targetNoun}.</h2>
+      <p>
+        An error occurred while sharing your {targetNounLower}. Please try
+        again.
+      </p>
+      <CloseButton />
+    </div>
+  ) : updateSuccessful ? (
+    <div className="UserDataset-SharingModal-StatusView">
+      <Icon fa="check-circle success" />
+      <h2>Community access updated successfully.</h2>
+      <CloseButton />
+    </div>
+  ) : (
+    <div className="UserDataset-SharingModal-FormView">
+      <div className="UserDataset-SharingModal-VisibilitySection">
+        <p className="UserDataset-SharingModal-Subtitle">
+          <em>
+            Community {dataNoun.plural} can be viewed and downloaded by all
+            users.
+          </em>
+        </p>
+        <div>
           <p>
-            An error occurred while sharing your {dataNoun.plural.toLowerCase()}
-            . Please try again.
+            {totalSelectedDatasets} selected ({totalCommunityDatasets}{' '}
+            {isAre(totalCommunityDatasets)} already in Community{' '}
+            {dataNoun.plural}{' '}
+            {totalNotOwnedDatasets > 0
+              ? `; ${totalNotOwnedDatasets} ${isAre(
+                  totalNotOwnedDatasets
+                )} owned by someone else`
+              : ''}
+            ).
           </p>
-          <CloseButton />
+          <p>
+            <strong>
+              {totalOwnedDatasets > 0
+                ? `Change Community access for ${totalOwnedDatasets} selected ${targetNounLower} that you own:`
+                : `You do not own any of the selected datsets.`}
+            </strong>
+          </p>
+          <FilledButton
+            disabled={totalOwnedDatasets === 0}
+            themeRole="primary"
+            styleOverrides={{
+              container: {
+                margin: '1em 0',
+              },
+            }}
+            text={`Grant access to ${totalOwnedDatasets} ${targetNounLower}`}
+            onPress={() =>
+              updateDatasetCommunityVisibility(
+                datasets.map((d) => d.id),
+                true,
+                context
+              )
+            }
+          />
+          <FilledButton
+            disabled={totalOwnedDatasets === 0}
+            themeRole="primary"
+            styleOverrides={{
+              container: {
+                margin: '1em 0',
+              },
+            }}
+            text={`Revoke access to ${totalOwnedDatasets} ${targetNounLower}`}
+            onPress={() =>
+              updateDatasetCommunityVisibility(
+                datasets.map((d) => d.id),
+                false,
+                context
+              )
+            }
+          />
         </div>
-      );
-    } else if (updateSuccessful) {
-      return (
-        <div className="UserDataset-SharingModal-StatusView">
-          <Icon fa="check-circle success" />
-          <h2>Community access updated successfully.</h2>
-          <CloseButton />
-        </div>
-      );
-    } else {
-      const totalSelectedDatasets = datasets.length;
-      const totalOwnedDatasets = datasets.filter((dataset) =>
-        this.isMyDataset(dataset)
-      ).length;
-      const totalCommunityDatasets = datasets.filter(
-        (dataset) => dataset.meta.visibility === 'public'
-      ).length;
-      const targetNoun = (
-        totalSelectedDatasets === 1 ? dataNoun.singular : dataNoun.plural
-      ).toLowerCase();
-      const totalNotOwnedDatasets = totalSelectedDatasets - totalOwnedDatasets;
-      return (
-        <div className="UserDataset-SharingModal-FormView">
-          <div className="UserDataset-SharingModal-VisibilitySection">
-            <p className="UserDataset-SharingModal-Subtitle">
-              <em>
-                Community {dataNoun.plural} can be viewed and downloaded by all
-                users.
-              </em>
-            </p>
-            <div>
-              <p>
-                {totalSelectedDatasets} selected ({totalCommunityDatasets}{' '}
-                {isAre(totalCommunityDatasets)} already in Community{' '}
-                {dataNoun.plural}{' '}
-                {totalNotOwnedDatasets > 0
-                  ? `; ${totalNotOwnedDatasets} ${isAre(
-                      totalNotOwnedDatasets
-                    )} owned by someone else`
-                  : ''}
-                ).
-              </p>
-              <p>
-                <strong>
-                  {totalOwnedDatasets > 0
-                    ? `Change Community access for ${totalOwnedDatasets} selected ${targetNoun}:`
-                    : `You do not own any of the selected datsets.`}
-                </strong>
-              </p>
-              <FilledButton
-                disabled={totalOwnedDatasets === 0}
-                themeRole="primary"
-                styleOverrides={{
-                  container: {
-                    margin: '1em 0',
-                  },
-                }}
-                text={`Grant access to ${totalOwnedDatasets} ${targetNoun}`}
-                onPress={() =>
-                  updateDatasetCommunityVisibility(
-                    datasets.map((d) => d.id),
-                    true,
-                    context
-                  )
-                }
-              />
-              <FilledButton
-                disabled={totalOwnedDatasets === 0}
-                themeRole="primary"
-                styleOverrides={{
-                  container: {
-                    margin: '1em 0',
-                  },
-                }}
-                text={`Revoke access to ${totalOwnedDatasets} ${targetNoun}`}
-                onPress={() =>
-                  updateDatasetCommunityVisibility(
-                    datasets.map((d) => d.id),
-                    false,
-                    context
-                  )
-                }
-              />
-            </div>
-          </div>
-        </div>
-      );
-    }
-  }
+      </div>
+    </div>
+  );
 
-  render() {
-    const { onClose, updatePending } = this.props;
-    const ViewContent = this.renderViewContent;
-
-    return (
-      <Modal
-        title={`Manage Community Access to ${this.getDatasetNoun()}`}
-        themeRole="primary"
-        includeCloseButton
-        toggleVisible={onClose}
-        visible
-        titleSize="small"
-      >
-        <div className="UserDataset-SharingModal">
-          {updatePending ? <Loading /> : <ViewContent />}
-        </div>
-      </Modal>
-    );
-  }
+  return (
+    <Modal
+      title={`Manage Community Access to ${datasetNoun}`}
+      themeRole="primary"
+      includeCloseButton
+      toggleVisible={onClose}
+      visible
+      titleSize="small"
+    >
+      <div className="UserDataset-SharingModal">{content}</div>
+    </Modal>
+  );
 }
-
-UserDatasetSharingModal.contextType = WdkDependenciesContext;
-
-export default UserDatasetSharingModal;
 
 function isAre(total) {
   return total === 1 ? 'is' : 'are';
