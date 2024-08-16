15f0df31274a9a8b67ab3427e7f38c5d3413e326
Bob <uncoolbob@gmail.com>
2024-08-16 13:07:23 +0100

    static histogram image now shows, with elaborate width-setting for caption below

--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/records/GroupStats.tsx
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/records/GroupStats.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { RecordTable } from './SequenceRecordClasses.SequenceRecordClass';
 import { RecordTableProps, WrappedComponentProps } from './Types';
+
+import eval_hist_img from '../../../images/eval-hist.png';
 
 export function RecordTable_GroupStats(
   props: WrappedComponentProps<RecordTableProps>
 ) {
   const regularRecordTable = RecordTable(props);
+
+  const imgRef = useRef<HTMLImageElement | null>(null);
+  const [imgWidth, setImgWidth] = useState(0);
+
+  useEffect(() => {
+    setImgWidth(imgRef.current?.offsetWidth ?? 0);
+  }, [imgRef]);
 
   return (
     <div
@@ -15,28 +24,24 @@ export function RecordTable_GroupStats(
         gap: 10,
       }}
     >
-      <div
+      {regularRecordTable}
+      <figure
         style={{
-          backgroundColor: '#eee',
-          height: 300,
-          width: 300,
-          textAlign: 'center',
-          paddingTop: 75,
-          paddingLeft: 20,
-          paddingRight: 20,
+          width: imgWidth || '100%',
         }}
       >
-        <p>This could be an image.</p>
-        <p>
-          Though I don't know how we will programmatically obtain the image{' '}
-          <code>src</code>.
-        </p>
-        <p>
-          I don't know where <code>images/eval-hist.png</code> is or what it
-          looks like.
-        </p>
-      </div>
-      {regularRecordTable}
+        <img
+          alt="Histogram of median inter-group e-values for both core-only and core+peripheral proteins. The distributions of both peak at around 1e-20 to 1e-60 with a substantial tail out to e-values of 1e-300."
+          src={eval_hist_img}
+          ref={imgRef}
+          onLoad={() => setImgWidth(imgRef.current?.offsetWidth ?? 0)}
+        />
+        <figcaption style={{ fontSize: '90%' }}>
+          This histogram is provided to aid the interpretation of E-values in
+          the adjoining table. E-values have been transformed using a negative
+          logarithm, so higher significance is represented further to the right.
+        </figcaption>
+      </figure>
     </div>
   );
 }
