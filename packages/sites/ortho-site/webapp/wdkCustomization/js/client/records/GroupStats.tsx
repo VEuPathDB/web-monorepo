65b065dd8ae65d85c2e56507be8c8804f44b4833
Bob <uncoolbob@gmail.com>
2024-08-16 15:56:39 +0100

    simplified to hardcoded dimensions

--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/records/GroupStats.tsx
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/records/GroupStats.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { RecordTable } from './SequenceRecordClasses.SequenceRecordClass';
 import { RecordTableProps, WrappedComponentProps } from './Types';
 
@@ -8,13 +8,6 @@ export function RecordTable_GroupStats(
   props: WrappedComponentProps<RecordTableProps>
 ) {
   const regularRecordTable = RecordTable(props);
-
-  const imgRef = useRef<HTMLImageElement | null>(null);
-  const [imgWidth, setImgWidth] = useState(0);
-
-  useEffect(() => {
-    setImgWidth(imgRef.current?.offsetWidth ?? 0);
-  }, [imgRef]);
 
   return (
     <div
@@ -27,27 +20,24 @@ export function RecordTable_GroupStats(
       {regularRecordTable}
       <figure
         style={{
-          width: imgWidth || undefined,
+          width: 480,
         }}
       >
         <img
           alt="Histogram of median inter-group e-values for both core-only and core+peripheral proteins. The distributions of both peak at around 1e-20 to 1e-60 with a substantial tail out to e-values of 1e-300."
+          width={480}
+          height={480}
           src={eval_hist_img}
-          ref={imgRef}
-          onLoad={() => setImgWidth(imgRef.current?.offsetWidth ?? 0)}
         />
-        {imgWidth ? ( // only render the caption after the image is fully loaded so it doesn't shape-shift
-          <figcaption
-            style={{
-              fontSize: '90%',
-            }}
-          >
-            This histogram is provided to aid the interpretation of E-values in
-            the adjoining table. E-values have been transformed using a negative
-            logarithm, so higher significance is represented further to the
-            right.
-          </figcaption>
-        ) : null}
+        <figcaption
+          style={{
+            fontSize: '90%',
+          }}
+        >
+          This histogram is provided to aid the interpretation of E-values in
+          the adjoining table. E-values have been transformed using a negative
+          logarithm, so higher significance is represented further to the right.
+        </figcaption>
       </figure>
     </div>
   );
