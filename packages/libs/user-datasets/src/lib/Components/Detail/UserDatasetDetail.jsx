c1d3a3a25c5836f78aa217c60da62470e6a80419
Jeremy Myers <inernestmusic@gmail.com>
2024-08-23 15:07:51 -0500

    Adjust styling and position of UD attributes
    (cherry picked from commit 5f30f997c39032f2823da576f003ccd529f21183)

--- packages/libs/user-datasets/src/lib/Components/Detail/UserDatasetDetail.jsx
+++ packages/libs/user-datasets/src/lib/Components/Detail/UserDatasetDetail.jsx
@@ -267,6 +267,30 @@ class UserDatasetDetail extends React.Component {
               quotaSize
             )}`,
           },
+      {
+        attribute: 'Summary',
+        value: (
+          <SaveableTextEditor
+            multiLine={true}
+            value={meta.summary}
+            readOnly={!isOwner}
+            onSave={onMetaSave('summary')}
+            emptyText="No Summary"
+          />
+        ),
+      },
+      {
+        attribute: 'Description',
+        value: (
+          <SaveableTextEditor
+            value={meta.description}
+            multiLine={true}
+            readOnly={!isOwner}
+            onSave={this.onMetaSave('description')}
+            emptyText="No Description"
+          />
+        ),
+      },
       !questions || !questions.length || !isInstalled
         ? null
         : {
