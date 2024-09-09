6e27cd2ee7b2d56559b8bcfd7f40f4d28e93c648
Dave Falke <dfalke@uga.edu>
2024-09-09 13:32:19 -0400

    Handle cases w/ guest user

--- packages/libs/user-datasets/src/lib/Controllers/UserDatasetDetailController.tsx
+++ packages/libs/user-datasets/src/lib/Controllers/UserDatasetDetailController.tsx
@@ -211,6 +211,7 @@ class UserDatasetDetailController extends PageController<MergedProps> {
     const entry = userDatasetsById[id];
     const isOwner = !!(
       user &&
+      entry &&
       entry.resource &&
       entry.resource.ownerUserId === user.id
     );
@@ -237,8 +238,8 @@ class UserDatasetDetailController extends PageController<MergedProps> {
       sharingSuccess,
       shareSuccessful,
       updateSharingModalState,
-      userDataset: entry.resource,
-      fileListing: entry.fileListing,
+      userDataset: entry?.resource,
+      fileListing: entry?.fileListing,
       getQuestionUrl: this.getQuestionUrl,
       questionMap: keyBy(questions, 'fullName'),
       workspaceTitle,
@@ -254,7 +255,7 @@ class UserDatasetDetailController extends PageController<MergedProps> {
     };
 
     const DetailView = this.getDetailView(
-      typeof entry.resource === 'object' ? entry.resource.type : null
+      typeof entry?.resource === 'object' ? entry.resource.type : null
     );
     return user && user.isGuest ? (
       this.renderGuestView()
