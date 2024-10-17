cd3e13ef474cc15f5fa94aca76a450d0aaa445f9
Bob <uncoolbob@gmail.com>
2024-10-17 23:00:40 +0100

    cancel variable works and old data is flushed when appropriate

--- packages/libs/eda/src/lib/core/hooks/cachedPromise.ts
+++ packages/libs/eda/src/lib/core/hooks/cachedPromise.ts
@@ -3,13 +3,17 @@ import { PromiseHookState } from './promise';
 import { useMemo } from 'react';
 
 // This is a wrapper around `useQuery` to replace our `usePromise` hook.
-// This will provide full client-side cacheing (cache size > 1!)
-// It is not quite a drop-in replacement as the caching uses serialisable keys.
 //
-
-// Note: the task may not return undefined.
+// It provides full client-side cacheing (cache size > 1!)
+// It is not quite a drop-in replacement as the caching uses serialisable keys.
+// But on the plus side, it's not necessary to memoize the task function.
+//
 // The task will not be executed while all items in the queryKey array are nullish
 // (empty queryKey array not allowed)
+//
+// Note: the task may not return undefined, but the hook will return `value: undefined`
+// when disabled.
+//
 export function useCachedPromise<T>(
   task: () => Promise<T>,
   queryKey: [any, ...any[]]
@@ -20,17 +24,18 @@ export function useCachedPromise<T>(
     queryKey: ['useCachedPromise', ...(queryKey ?? [])],
     queryFn: task,
     enabled,
+    keepPreviousData: enabled,
   });
 
   // Mapping the state from useQuery to PromiseHookState<T>
   // and return something stable
   const state: PromiseHookState<T> = useMemo(
     () => ({
-      value: data,
+      value: enabled ? data : undefined,
       pending: enabled && (isLoading || isFetching),
       error: error,
     }),
-    [data, enabled, isLoading, isFetching, error]
+    [data, enabled, isLoading || isFetching, error]
   );
 
   return state;
