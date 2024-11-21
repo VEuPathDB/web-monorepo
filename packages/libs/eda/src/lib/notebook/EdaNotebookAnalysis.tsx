e162ad7caa8ec5c9a6b76738b04df866f7ff08c8
Dave Falke <dfalke@uga.edu>
2024-11-21 14:07:26 -0500

    Checkpoint
    - Basics of cell types
    - Persistence
    - Iterate on styling

--- packages/libs/eda/src/lib/notebook/EdaNotebookAnalysis.tsx
+++ packages/libs/eda/src/lib/notebook/EdaNotebookAnalysis.tsx
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
-import {
-  Filter,
-  useAnalysis,
-  useStudyEntities,
-  useStudyMetadata,
-  useStudyRecord,
-} from '../core';
+// Notes
+// =====
+//
+// - For now, we will only support "fixed" notebooks. If we want to allow "custom" notebooks,
+//   we have to make some decisions.
+// - Do we want a top-down data flow? E.g., subsetting is global for an analysis.
+// - Do we want to separate compute config from visualization? If so, how do we
+//   support that in the UI?
+// - Do we want text-based cells?
+// - Do we want download cells? It could have a preview.
+//
+
+import React, { useCallback, useMemo } from 'react';
+import { useAnalysis, useStudyRecord } from '../core';
 import { safeHtml } from '@veupathdb/wdk-client/lib/Utils/ComponentUtils';
 import { SaveableTextEditor } from '@veupathdb/wdk-client/lib/Components';
-import Subsetting from '../workspace/Subsetting';
-import { useEntityCounts } from '../core/hooks/entityCounts';
-import FilterChipList from '../core/components/FilterChipList';
+import { ExpandablePanel } from '@veupathdb/coreui';
+import { NotebookCell as NotebookCellType } from './Types';
+import { NotebookCell } from './NotebookCell';
+
+import './EdaNotebook.scss';
+
+interface NotebookSettings {
+  /** Ordered array of notebook cells */
+  cells: NotebookCellType[];
+}
+
+const NOTEBOOK_UI_SETTINGS_KEY = '@@NOTEBOOK@@';
 
 interface Props {
   analysisId: string;
@@ -18,69 +33,69 @@ interface Props {
 
 export function EdaNotebookAnalysis(props: Props) {
   const { analysisId } = props;
+  const studyRecord = useStudyRecord();
   const analysisState = useAnalysis(
     analysisId === 'new' ? undefined : analysisId
   );
-  const studyRecord = useStudyRecord();
-  const studyMetadata = useStudyMetadata();
-  const entities = useStudyEntities();
-  const totalCountsResult = useEntityCounts();
-  const filteredCountsResult = useEntityCounts(
-    analysisState.analysis?.descriptor.subset.descriptor
+  const { analysis } = analysisState;
+  const notebookSettings = useMemo((): NotebookSettings => {
+    const storedSettings =
+      analysis?.descriptor.subset.uiSettings[NOTEBOOK_UI_SETTINGS_KEY];
+    if (storedSettings == null)
+      return {
+        cells: [
+          {
+            type: 'subset',
+            title: 'Subset data',
+          },
+        ],
+      };
+    return storedSettings as any as NotebookSettings;
+  }, [analysis]);
+  const updateCell = useCallback(
+    (cell: Partial<Omit<NotebookCellType, 'type'>>, cellIndex: number) => {
+      const oldCell = notebookSettings.cells[cellIndex];
+      const newCell = { ...oldCell, ...cell };
+      const nextCells = notebookSettings.cells.concat();
+      nextCells[cellIndex] = newCell;
+      const nextSettings = {
+        ...notebookSettings,
+        cells: nextCells,
+      };
+      analysisState.setVariableUISettings({
+        [NOTEBOOK_UI_SETTINGS_KEY]: nextSettings,
+      });
+    },
+    [analysisState, notebookSettings]
   );
-  const [entityId, setEntityId] = useState<string>();
-  const [variableId, setVariableId] = useState<string>();
   return (
-    <div>
-      <h1>EDA Notebook</h1>
-      {safeHtml(studyRecord.displayName, null, 'h2')}
-      <h3>
-        <SaveableTextEditor
-          value={analysisState.analysis?.displayName ?? ''}
-          onSave={analysisState.setName}
-        />
-      </h3>
-      <details>
-        <summary>
-          Subset &nbsp;&nbsp;
-          <FilterChipList
-            filters={analysisState.analysis?.descriptor.subset.descriptor}
-            entities={entities}
-            selectedEntityId={entityId}
-            selectedVariableId={variableId}
-            removeFilter={(filter) =>
-              analysisState.setFilters((filters) =>
-                filters.filter(
-                  (f) =>
-                    f.entityId !== filter.entityId ||
-                    f.variableId !== filter.variableId
-                )
-              )
-            }
-            variableLinkConfig={{
-              type: 'button',
-              onClick: (value) => {
-                setEntityId(value?.entityId);
-                setVariableId(value?.variableId);
-              },
-            }}
-          />
-        </summary>
-        <Subsetting
-          analysisState={analysisState}
-          entityId={entityId ?? ''}
-          variableId={variableId ?? ''}
-          totalCounts={totalCountsResult.value}
-          filteredCounts={filteredCountsResult.value}
-          variableLinkConfig={{
-            type: 'button',
-            onClick: (value) => {
-              setEntityId(value?.entityId);
-              setVariableId(value?.variableId);
-            },
-          }}
-        />
-      </details>
+    <div className="EdaNotebook">
+      <div className="Heading">
+        <h1>EDA Notebook</h1>
+      </div>
+      <div className="Paper">
+        <div>
+          <h2>
+            <SaveableTextEditor
+              className="Title"
+              value={analysisState.analysis?.displayName ?? ''}
+              onSave={analysisState.setName}
+            />
+          </h2>
+          <h3>Study: {safeHtml(studyRecord.displayName)}</h3>
+        </div>
+        {notebookSettings.cells.map((cell, index) => (
+          <ExpandablePanel title={cell.title} subTitle={{}} themeRole="primary">
+            <div style={{ padding: '1em' }}>
+              <NotebookCell
+                analysisState={analysisState}
+                cell={cell}
+                updateCell={(update) => updateCell(update, index)}
+              />
+            </div>
+          </ExpandablePanel>
+        ))}
+      </div>
     </div>
   );
 }
