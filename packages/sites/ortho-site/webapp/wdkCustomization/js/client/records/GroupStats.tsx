473e4df98cb3367d426010c6467e28d5cb213825
Bob MacCallum <uncoolbob@gmail.com>
2024-12-04 23:39:48 +0000

    updated histogram image, placement and verbiage

--- packages/sites/ortho-site/webapp/wdkCustomization/js/client/records/GroupStats.tsx
+++ packages/sites/ortho-site/webapp/wdkCustomization/js/client/records/GroupStats.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { RecordTable } from './SequenceRecordClasses.SequenceRecordClass';
 import { RecordTableProps, WrappedComponentProps } from './Types';
 
-import eval_hist_img from './eval-hist.png';
+import MGD_hist_img from './MGD-hist.png';
 
 export function RecordTable_GroupStats(
   props: WrappedComponentProps<RecordTableProps>
@@ -13,30 +13,28 @@ export function RecordTable_GroupStats(
     <div
       style={{
         display: 'flex',
-        flexDirection: 'row',
-        gap: 10,
+        flexDirection: 'column',
       }}
     >
       {regularRecordTable}
       <figure
         style={{
-          width: 480,
+          width: 390,
         }}
       >
         <img
-          alt="Histogram of median inter-group e-values for both core-only and core+peripheral proteins. The distributions of both peak at around 1e-20 to 1e-60 with a substantial tail out to e-values of 1e-300."
-          width={480}
-          height={480}
-          src={eval_hist_img}
+          alt="The histogram shows the distribution of the median percent identity cohesiveness indicator across all orthologous groups. There is a skewed distribution with a peak at 0-5% identity."
+          width={390}
+          height={336}
+          src={MGD_hist_img}
         />
         <figcaption
           style={{
             fontSize: '90%',
           }}
         >
-          This histogram is provided to aid the interpretation of E-values in
-          the adjoining table. E-values have been transformed using a negative
-          logarithm, so higher significance is represented further to the right.
+          The histogram shows the distribution of the median percent identity
+          cohesiveness indicator across all orthologous groups.
         </figcaption>
       </figure>
     </div>
