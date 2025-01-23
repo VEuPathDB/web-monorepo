064fc32fe4e07e4103536e017b816419897337c9
Dave Falke <dfalke@uga.edu>
2025-01-23 15:19:34 -0500

    Expand nav panel category when shortcut of child is clicked

--- packages/libs/wdk-client/src/Utils/CategoryUtils.tsx
+++ packages/libs/wdk-client/src/Utils/CategoryUtils.tsx
@@ -13,7 +13,7 @@ import {
 } from '../Utils/OntologyUtils';
 import { areTermsInString } from '../Utils/SearchUtils';
 import { Seq } from '../Utils/IterableUtils';
-import { preorderSeq, getBranches } from '../Utils/TreeUtils';
+import { preorderSeq, getBranches, foldStructure } from '../Utils/TreeUtils';
 import { Question, RecordClass } from '../Utils/WdkModel';
 // import {Tooltip} from '@veupathdb/components/lib/components/widgets/Tooltip';
 
@@ -134,6 +134,30 @@ export function getSynonyms(node: CategoryTreeNode) {
 
 export function getChildren(node: CategoryTreeNode) {
   return node.children;
+}
+
+export function getAncestors(
+  tree: CategoryTreeNode,
+  descendantId: string
+): CategoryTreeNode[] {
+  return foldStructure<CategoryTreeNode, CategoryTreeNode[]>(
+    (acc, node) => {
+      const directDescendantId = acc[0] && getId(acc[0]);
+      if (
+        node.children.some((childNode) => {
+          const childNodeId = getId(childNode);
+          return (
+            childNodeId === directDescendantId || childNodeId === descendantId
+          );
+        })
+      ) {
+        return [node, ...acc];
+      }
+      return acc;
+    },
+    [],
+    tree
+  ).slice(0, -1);
 }
 
 // TODO Make this more genericL createCategoryNode and createWdkEntityNode (or, createLeafNode??)
