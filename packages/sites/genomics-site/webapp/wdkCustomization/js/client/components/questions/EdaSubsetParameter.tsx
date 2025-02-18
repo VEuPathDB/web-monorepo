ecf238fcbe0f64ccfd50b25615b69f2f3715fdb0
Dave Falke <dfalke@uga.edu>
2025-02-18 11:36:28 -0500

    Add custom formatter for stepDetails

--- packages/sites/genomics-site/webapp/wdkCustomization/js/client/components/questions/EdaSubsetParameter.tsx
+++ packages/sites/genomics-site/webapp/wdkCustomization/js/client/components/questions/EdaSubsetParameter.tsx
@@ -1,10 +1,14 @@
 import React, { useCallback, useMemo, useState } from 'react';
 
 import { Props } from '@veupathdb/wdk-client/lib/Views/Question/Params/Utils';
-import { StringParam } from '@veupathdb/wdk-client/lib/Utils/WdkModel';
+import {
+  Parameter,
+  StringParam,
+} from '@veupathdb/wdk-client/lib/Utils/WdkModel';
 import { Subsetting } from '@veupathdb/eda/lib/workspace';
 import { WorkspaceContainer } from '@veupathdb/eda/lib/workspace/WorkspaceContainer';
 import {
+  Analysis,
   AnalysisChangeHandler,
   AnalysisState,
   Filter,
@@ -21,6 +25,13 @@ import { useEntityCounts } from '@veupathdb/eda/lib/core/hooks/entityCounts';
 import FilterChipList from '@veupathdb/eda/lib/core/components/FilterChipList';
 
 import './EdaSubsetParameter.scss';
+import {
+  defaultFormatParameterValue,
+  DefaultStepDetailsContent,
+  LeafStepDetailsContentProps,
+} from '@veupathdb/wdk-client/lib/Views/Strategy/StepDetails';
+import { formatFilterDisplayValue } from '@veupathdb/eda/lib/core/utils/study-metadata';
+import { DatasetItem } from '@veupathdb/wdk-client/lib/Views/Question/Params/DatasetParamUtils';
 
 const datasetIdParamName = 'eda_dataset_id';
 
@@ -146,4 +157,35 @@ function parseJson(str: string) {
   } catch {
     return undefined;
   }
+}
+
+export function EdaSubsetStepDetails(props: LeafStepDetailsContentProps) {
+  return (
+    <DefaultStepDetailsContent
+      {...props}
+      formatParameterValue={formatParameterValue}
+    />
+  );
+}
+
+function formatParameterValue(
+  parameter: Parameter,
+  value: string | undefined,
+  datasetParamItems: Record<string, DatasetItem[]> | undefined
+) {
+  if (parameter.name === 'eda_analysis_spec' && value != null) {
+    const obj = parseJson(value);
+    if (NewAnalysis.is(obj) || Analysis.is(obj)) {
+      return (
+        <div style={{ whiteSpace: 'pre-line' }}>
+          {obj.descriptor.subset.descriptor.map((filter) => (
+            <div>
+              {filter.variableId}: {formatFilterDisplayValue(filter)}
+            </div>
+          ))}
+        </div>
+      );
+    }
+  }
+  return defaultFormatParameterValue(parameter, value, datasetParamItems);
 }
