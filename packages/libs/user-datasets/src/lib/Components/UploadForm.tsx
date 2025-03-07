d7b10976047f3f6399979c767b923c575e1af189
asizemore <annsize@seas.upenn.edu>
2025-03-07 17:36:21 -0500

    contacts added

--- packages/libs/user-datasets/src/lib/Components/UploadForm.tsx
+++ packages/libs/user-datasets/src/lib/Components/UploadForm.tsx
@@ -30,6 +30,7 @@ import {
   NewUserDataset,
   ResultUploadConfig,
   UserDataset,
+  UserDatasetContact,
   UserDatasetHyperlink,
   UserDatasetPublication,
 } from '../Utils/types';
@@ -85,6 +86,7 @@ interface FormContent {
   publications?: UserDatasetPublication[];
   hyperlinks?: UserDatasetHyperlink[];
   organisms?: string[];
+  contacts?: UserDatasetContact[];
 }
 
 export type FormValidation = InvalidForm | ValidForm;
@@ -154,6 +156,8 @@ function UploadForm({
   );
   const [hyperlinks, setHyperlinks] = useState<UserDatasetHyperlink[]>([]);
   const [organisms, setOrganisms] = useState<string[]>([]);
+  const [contacts, setContacts] = useState<UserDatasetContact[]>([]);
+  console.log(contacts);
   const [nPublicationInputBoxes, setNPublicationInputBoxes] = useState(0);
   const [nHyperlinkInputBoxes, setNHyperlinkInputBoxes] = useState(0);
   const [nOrganismInputBoxes, setNOrganismInputBoxes] = useState(0);
@@ -262,6 +266,7 @@ function UploadForm({
           publications,
           hyperlinks,
           organisms,
+          contacts,
         }
       );
 
@@ -289,6 +294,7 @@ function UploadForm({
       publications,
       hyperlinks,
       organisms,
+      contacts,
     ]
   );
 
@@ -721,6 +727,113 @@ function UploadForm({
                 icon={AddIcon}
               />
             </div>
+            <div className="additionalDetailsFormSection additionalDetailsFormSection--data-set-contacts">
+              <FieldLabel
+                htmlFor="data-set-publications-contacts"
+                required={false}
+              >
+                Contacts (Optional)
+              </FieldLabel>
+              {contacts.map((contact, index) => {
+                return (
+                  <ContactInput
+                    n={index}
+                    name={contact.name}
+                    onAddName={(value: string) => {
+                      const updatedContacts = [...contacts];
+                      updatedContacts[index] = {
+                        ...contact,
+                        name: value,
+                      };
+                      setContacts(updatedContacts);
+                    }}
+                    email={contact.email}
+                    onAddEmail={(value: string) => {
+                      const updatedContacts = [...contacts];
+                      updatedContacts[index] = {
+                        ...contact,
+                        email: value,
+                      };
+                      setContacts(updatedContacts);
+                    }}
+                    affiliation={contact.affiliation}
+                    onAddAffiliation={(value: string) => {
+                      const updatedContacts = [...contacts];
+                      updatedContacts[index] = {
+                        ...contact,
+                        affiliation: value,
+                      };
+                      setContacts(updatedContacts);
+                    }}
+                    city={contact.city}
+                    onAddCity={(value: string) => {
+                      const updatedContacts = [...contacts];
+                      updatedContacts[index] = {
+                        ...contact,
+                        city: value,
+                      };
+                      setContacts(updatedContacts);
+                    }}
+                    state={contact.state}
+                    onAddState={(value: string) => {
+                      const updatedContacts = [...contacts];
+                      updatedContacts[index] = {
+                        ...contact,
+                        state: value,
+                      };
+                      setContacts(updatedContacts);
+                    }}
+                    country={contact.country}
+                    onAddCountry={(value: string) => {
+                      const updatedContacts = [...contacts];
+                      updatedContacts[index] = {
+                        ...contact,
+                        country: value,
+                      };
+                      setContacts(updatedContacts);
+                    }}
+                    address={contact.address}
+                    onAddAddress={(value: string) => {
+                      const updatedContacts = [...contacts];
+                      updatedContacts[index] = {
+                        ...contact,
+                        address: value,
+                      };
+                      setContacts(updatedContacts);
+                    }}
+                    isPrimary={contact.isPrimary}
+                    onAddIsPrimary={(value: boolean | undefined) => {
+                      const updatedContacts = [...contacts];
+                      updatedContacts[index] = {
+                        ...contact,
+                        isPrimary: value,
+                      };
+                      setContacts(updatedContacts);
+                      return;
+                    }}
+                    onRemoveContact={(
+                      event: React.MouseEvent<HTMLButtonElement>
+                    ) => {
+                      event.preventDefault();
+                      const updatedContacts = [...contacts];
+                      updatedContacts.splice(index, 1);
+                      setContacts(updatedContacts);
+                    }}
+                  />
+                );
+              })}
+              <FloatingButton
+                text="Add Contact"
+                onPress={(event: React.MouseEvent<HTMLButtonElement>) => {
+                  event.preventDefault();
+                  setContacts((contacts) => [
+                    ...contacts,
+                    {} as UserDatasetContact,
+                  ]);
+                }}
+                icon={AddIcon}
+              />
+            </div>
           </details>
         </div>
         {datasetUploadType.formConfig.dependencies && (
@@ -1058,6 +1171,148 @@ function HyperlinkInput(props: HyperlinkInputProps): JSX.Element {
           }
           onChange={(value) => {
             onAddIsPublication(
+              value === 'true' ? true : value === 'false' ? false : undefined
+            );
+          }}
+          items={[
+            { value: 'true', display: 'Yes' },
+            { value: 'false', display: 'No' },
+            { value: 'undefined', display: 'NA' },
+          ]}
+        />
+      </div>
+    </div>
+  );
+}
+
+// UI for contacts
+interface ContactInputProps {
+  n: number;
+  name: string;
+  email?: string;
+  affiliation?: string;
+  city?: string;
+  state?: string;
+  country?: string;
+  address?: string;
+  isPrimary?: boolean;
+  onAddName: (value: string) => void;
+  onAddEmail: (value: string) => void;
+  onAddAffiliation: (value: string) => void;
+  onAddCity: (value: string) => void;
+  onAddState: (value: string) => void;
+  onAddCountry: (value: string) => void;
+  onAddAddress: (value: string) => void;
+  onAddIsPrimary: (value: boolean | undefined) => void;
+  onRemoveContact: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+function ContactInput(props: ContactInputProps): JSX.Element {
+  const {
+    n,
+    name,
+    email,
+    affiliation,
+    city,
+    state,
+    country,
+    address,
+    isPrimary,
+    onAddName,
+    onAddEmail,
+    onAddAffiliation,
+    onAddCity,
+    onAddState,
+    onAddCountry,
+    onAddAddress,
+    onAddIsPrimary,
+    onRemoveContact,
+  } = props;
+  return (
+    <div className={cx('--NestedInputContainer')}>
+      <div className={cx('--NestedInputTitle')}>
+        <FieldLabel required={false} style={{ fontSize: '1.2em' }}>
+          Contact {n + 1}
+        </FieldLabel>
+        <FloatingButton text="Remove" onPress={onRemoveContact} icon={Trash} />
+      </div>
+      <div className={cx('--NestedInputFields')}>
+        <FieldLabel required>Name</FieldLabel>
+        <TextBox
+          type="input"
+          id={`data-set-contacts-name-${n}`}
+          placeholder="Name"
+          required
+          value={name}
+          onChange={onAddName}
+        />
+        <FieldLabel required={false}>Email</FieldLabel>
+        <TextBox
+          type="input"
+          id={`data-set-contacts-email-${n}`}
+          placeholder="Email"
+          required={false}
+          value={email}
+          onChange={onAddEmail}
+        />
+        <FieldLabel required={false}>Affiliation</FieldLabel>
+        <TextBox
+          type="input"
+          id={`data-set-contacts-affiliation-${n}`}
+          placeholder="Affiliation"
+          required={false}
+          value={affiliation}
+          onChange={onAddAffiliation}
+        />
+        <FieldLabel required={false}>City</FieldLabel>
+        <TextBox
+          type="input"
+          id={`data-set-contacts-city-${n}`}
+          placeholder="City"
+          required={false}
+          value={city}
+          onChange={onAddCity}
+        />
+        <FieldLabel required={false}>State</FieldLabel>
+        <TextBox
+          type="input"
+          id={`data-set-contacts-state-${n}`}
+          placeholder="State"
+          required={false}
+          value={state}
+          onChange={onAddState}
+        />
+        <FieldLabel required={false}>Country</FieldLabel>
+        <TextBox
+          type="input"
+          id={`data-set-contacts-country-${n}`}
+          placeholder="Country"
+          required={false}
+          value={country}
+          onChange={onAddCountry}
+        />
+        <FieldLabel required={false}>Address</FieldLabel>
+        <TextBox
+          type="input"
+          id={`data-set-contacts-address-${n}`}
+          placeholder="Address"
+          required={false}
+          value={address}
+          onChange={onAddAddress}
+        />
+        <FieldLabel required={false}>Is primary?</FieldLabel>
+        <RadioList
+          name={`isPrimary-${n}`}
+          className="horizontal"
+          value={
+            isPrimary === true
+              ? 'true'
+              : isPrimary === false
+              ? 'false'
+              : 'undefined'
+          }
+          onChange={(value) => {
+            onAddIsPrimary(
               value === 'true' ? true : value === 'false' ? false : undefined
             );
           }}
