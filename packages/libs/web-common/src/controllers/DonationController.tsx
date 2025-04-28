9e0cad4dfea965e3867846e920f80f56f51895e3
Ryan Doherty <tech@conical.org>
2025-04-28 14:30:05 -0400

    Improve form submission

--- packages/libs/web-common/src/controllers/DonationController.tsx
+++ packages/libs/web-common/src/controllers/DonationController.tsx
@@ -9,27 +9,43 @@ interface Props {}
 
 const EXTERNAL_CONTENT_CONTROLLER_CLASSNAME = 'DonationController';
 
-function visitCreditCardForm(amount: string) {}
+function visitCreditCardForm(
+  amount: string,
+  setErrorMessage: (s: string) => void
+) {
+  const amountNum: number = parseFloat(amount);
+  if (isNaN(amountNum) || amountNum <= 0) {
+    setErrorMessage('You must enter a positive number amount.');
+  }
+  console.log('Submitting form with donation amount $' + amountNum);
+}
 
 export default function DonationController(props: Props) {
   const [amount, setAmount] = useState('0.00');
+  const [errorMessage, setErrorMessage] = useState('');
 
   return (
     <div>
       <h3>Please Consider Donating to VEuPathDB</h3>
       <hr />
-      <span>How much can you donate today? $</span>
-      <input
-        type="text"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-      />
-      <br />
-      <input
-        type="button"
-        value="Pay Now via Credit Card"
-        onClick={(e) => visitCreditCardForm(amount)}
-      />
+      <div>
+        <span style={{ color: 'red' }}>${errorMessage}</span>
+      </div>
+      <div>
+        <span>How much can you donate today? $</span>
+        <input
+          type="text"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="button"
+          value="Pay Now via Credit Card"
+          onClick={(e) => visitCreditCardForm(amount, setErrorMessage)}
+        />
+      </div>
     </div>
   );
 }
