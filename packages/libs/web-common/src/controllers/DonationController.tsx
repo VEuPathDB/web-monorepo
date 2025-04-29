ed28374f4626baf92770ae0e5145f82aa94e8a73
Ryan Doherty <tech@conical.org>
2025-04-29 09:28:33 -0400

    More full implementation of form creation and submission

--- packages/libs/web-common/src/controllers/DonationController.tsx
+++ packages/libs/web-common/src/controllers/DonationController.tsx
@@ -1,51 +1,112 @@
-import React, { useState } from 'react';
-//import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { webAppUrl } from '../config';
 
+// may want to use this later
 //import { Loading } from '@veupathdb/wdk-client/lib/Components';
-//import { usePromise } from '@veupathdb/wdk-client/lib/Hooks/PromiseHook';
-//import { safeHtml } from '@veupathdb/wdk-client/lib/Utils/ComponentUtils';
 
 interface Props {}
 
-const EXTERNAL_CONTENT_CONTROLLER_CLASSNAME = 'DonationController';
+interface FormProps {
+  actionUrl: string;
+  params: any;
+}
 
-function visitCreditCardForm(
-  amount: string,
+function AutoSubmitForm(props: FormProps) {
+  // submit form as soon as it is rendered
+  const formRef = useRef<HTMLFormElement>(null);
+  useEffect(() => {
+    if (formRef.current !== null) {
+      formRef.current.submit();
+    }
+  }, []);
+
+  // create a hidden form with the passed action URL and parameters
+  return (
+    <form ref={formRef} method="POST" action={props.actionUrl}>
+      {Object.keys(props.params).map((name) => (
+        <input type="hidden" name={name} value={props.params[name]}></input>
+      ))}
+    </form>
+  );
+}
+
+async function getFormData(
+  amount: number,
   setErrorMessage: (s: string) => void
 ) {
-  const amountNum: number = parseFloat(amount);
+  try {
+    const url = webAppUrl + '/service/donation-form-content?amount=' + amount;
+    const response = await fetch(url);
+    if (!response.ok) {
+      setErrorMessage('Cannot connect to payment system.');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function generateForm(
+  amount: string,
+  setFormData: (b: any) => void,
+  setErrorMessage: (s: string) => void
+) {
+  var amountNum: number = parseFloat(amount);
   if (isNaN(amountNum) || amountNum <= 0) {
     setErrorMessage('You must enter a positive number amount.');
+  } else {
+    setErrorMessage('Please wait...');
+    amountNum = Math.floor(amountNum * 100) / 100;
+    console.log('Submitting form with donation amount $' + amountNum);
+    getFormData(amountNum, setErrorMessage)
+      .then((formData) => {
+        setFormData(formData);
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage('Cannot connect to payment system.');
+      });
   }
-  console.log('Submitting form with donation amount $' + amountNum);
 }
 
 export default function DonationController(props: Props) {
+  const [formData, setFormData] = useState(null);
   const [amount, setAmount] = useState('0.00');
   const [errorMessage, setErrorMessage] = useState('');
 
+  // initially show the starter form
+  if (formData == null) {
+    return (
+      <div>
+        <h3>Please Consider Donating to VEuPathDB</h3>
+        <hr />
+        <div>
+          <span style={{ color: 'red' }}>{errorMessage}</span>
+        </div>
+        <div>
+          <span>How much can you donate today? $</span>
+          <input
+            type="text"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="button"
+            value="Pay Now with Credit Card"
+            onClick={(e) => generateForm(amount, setFormData, setErrorMessage)}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  // once form data has been fetched, return form which will automatically submit when rendered
   return (
-    <div>
-      <h3>Please Consider Donating to VEuPathDB</h3>
-      <hr />
-      <div>
-        <span style={{ color: 'red' }}>${errorMessage}</span>
-      </div>
-      <div>
-        <span>How much can you donate today? $</span>
-        <input
-          type="text"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="button"
-          value="Pay Now via Credit Card"
-          onClick={(e) => visitCreditCardForm(amount, setErrorMessage)}
-        />
-      </div>
-    </div>
+    <AutoSubmitForm
+      actionUrl="https://secureacceptance.cybersource.com/pay"
+      params={formData}
+    />
   );
 }
