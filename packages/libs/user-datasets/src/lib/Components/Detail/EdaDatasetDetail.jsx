6198abfbfeea3b798e1c991100f181aeb8c0fe94
asizemore <annsize@seas.upenn.edu>
2025-06-05 13:50:31 -0400

    redo eda linkout in dataset detail page

--- packages/libs/user-datasets/src/lib/Components/Detail/EdaDatasetDetail.jsx
+++ packages/libs/user-datasets/src/lib/Components/Detail/EdaDatasetDetail.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
-
 import UserDatasetDetail from './UserDatasetDetail';
+import { makeClassifier } from '../UserDatasetUtils';
+
+const classify = makeClassifier('UserDatasetDetail');
 
 class EdaDatasetDetail extends UserDatasetDetail {
   constructor(props) {
@@ -26,40 +28,67 @@ class EdaDatasetDetail extends UserDatasetDetail {
     if (edaWorkspaceUrl == null && edaMapUrl == null) return null;
 
     return (
-      <ul className="eda-linkout">
+      // <ul className="eda-linkout">
+      <div className="eda-linkout">
         {!edaWorkspaceUrl ? null : (
-          <li>
-            <Link to={edaWorkspaceUrl}>
-              <i className="ebrc-icon-edaIcon"></i> Explore in {displayName}
-            </Link>
-          </li>
+          // <li>
+          <Link to={edaWorkspaceUrl}>
+            <i className="ebrc-icon-edaIcon"></i> Explore in {displayName}
+          </Link>
+          // </li>
         )}
         {!edaMapUrl ? null : (
-          <li>
-            <Link to={edaMapUrl}>
-              <i className="ebrc-icon-edaIcon"></i> Explore in MapVEu
-            </Link>
-          </li>
+          // <li>
+          <Link to={edaMapUrl}>
+            <i className="ebrc-icon-edaIcon"></i> Explore in MapVEu
+          </Link>
+          // </li>
         )}
-      </ul>
+      </div>
     );
   }
 
-  getAttributes() {
-    const attributes = super.getAttributes();
+  //   getAttributes() {
+  //     const attributes = super.getAttributes();
 
-    if (!this.isInstalled()) return attributes;
+  //     if (!this.isInstalled()) return attributes;
 
-    const edaLinks = {
-      attribute: 'Explore',
-      value: this.renderEdaLinkout(),
-    };
-    const spliceIndex = this.props.includeNameHeader ? 2 : 1;
-    return [
-      ...attributes.slice(0, spliceIndex),
-      edaLinks,
-      ...attributes.slice(spliceIndex),
-    ];
+  //     const edaLinks = {
+  //       attribute: 'Explore',
+  //       value: this.renderEdaLinkout(),
+  //     };
+
+  //     return [
+  //       edaLinks,
+  //       ...attributes,
+  //     ];
+  //   }
+  // }
+
+  renderHeaderSection() {
+    const AllLink = this.renderAllDatasetsLink;
+    const Subtitle = this.renderSubtitle;
+    const AttributeList = this.renderAttributeList;
+    const DatasetActions = this.renderDatasetActions;
+    const DatasetName = this.renderDatasetName;
+    const EdaLinkout = this.renderEdaLinkout;
+
+    return (
+      <section id="dataset-header">
+        <AllLink />
+        <div className={classify('Header')}>
+          <div className={classify('Header-Attributes')}>
+            <DatasetName />
+            <Subtitle />
+            <EdaLinkout />
+            <AttributeList />
+          </div>
+          <div className={classify('Header-Actions')}>
+            <DatasetActions />
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 
