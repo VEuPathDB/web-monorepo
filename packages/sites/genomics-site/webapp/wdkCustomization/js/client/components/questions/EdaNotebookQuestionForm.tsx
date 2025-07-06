07c07b3c1358ec57004f34c0e7a62d2f34144e8c
Bob <uncoolbob@gmail.com>
2025-07-06 23:11:18 +0100

    remove foo code and unused imports

--- packages/sites/genomics-site/webapp/wdkCustomization/js/client/components/questions/EdaNotebookQuestionForm.tsx
+++ packages/sites/genomics-site/webapp/wdkCustomization/js/client/components/questions/EdaNotebookQuestionForm.tsx
@@ -3,20 +3,13 @@ import DefaultQuestionForm, {
 } from '@veupathdb/wdk-client/lib/Views/Question/DefaultQuestionForm';
 import React, { useCallback } from 'react';
 import { EdaNotebookParameter } from './EdaNotebookParameter';
-import {
-  ParameterGroup,
-  Parameter,
-  ParameterValues,
-} from '@veupathdb/wdk-client/lib/Utils/WdkModel';
+import { Parameter } from '@veupathdb/wdk-client/lib/Utils/WdkModel';
 
 export const EdaNotebookQuestionForm = (props: Props) => {
   const { searchName } = props;
   if (!searchName) {
     throw new Error('No search defined.');
   }
-
-  const foo = props.state.paramValues;
-  const asd = foo['asd'];
 
   // We'll use this function throughout the notebook to update any wdk parameters.
   const updateWdkParamValue = useCallback(
