2020fd36deb82290fca13ada956622e46f6b2921
asizemore <annsize@seas.upenn.edu>
2025-08-27 15:26:02 -0400

    unwrap UserSubscriptionManagement

--- packages/libs/wdk-client/src/Views/User/UserSubscriptionManagement.tsx
+++ packages/libs/wdk-client/src/Views/User/UserSubscriptionManagement.tsx
@@ -184,4 +184,4 @@ const UserSubscriptionManagement: React.FC<UserSubscriptionManagementProps> = ({
   );
 };
 
-export default wrappable(UserSubscriptionManagement);
+export default UserSubscriptionManagement;
