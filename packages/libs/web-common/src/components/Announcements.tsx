d874e9fd28d57f9007c9f321cd06ad2745c9854d
Bob <uncoolbob@gmail.com>
2025-09-03 00:12:18 +0100

    clean up announcement logic; was showing for guests

--- packages/libs/web-common/src/components/Announcements.tsx
+++ packages/libs/web-common/src/components/Announcements.tsx
@@ -17,6 +17,7 @@ import Banner, {
 } from '@veupathdb/coreui/lib/components/banners/Banner';
 import { SubscriptionManagementBanner } from './SubscriptionManagementBanner';
 import { BannerDismissal } from '../hooks/announcements';
+import { userIsSubscribed } from '../../../wdk-client/lib/Utils/Subscriptions';
 
 // Type definitions
 interface AnnouncementsProps {
@@ -152,17 +153,13 @@ const siteAnnouncements: SiteAnnouncement[] = [
     }: AnnouncementRenderProps) => {
       if (
         !currentUser ||
+        currentUser.isGuest ||
         subscriptionGroups == null ||
         subscriptionGroups.length === 0
       )
         return null;
 
-      const isSubscribed =
-        !currentUser.isGuest &&
-        subscriptionGroups.filter(
-          (g: SubscriptionGroup) =>
-            g.subscriptionToken === currentUser.properties['subscriptionToken']
-        ).length > 0;
+      const isSubscribed = userIsSubscribed(currentUser, subscriptionGroups);
 
       if (isSubscribed) return null;
 
