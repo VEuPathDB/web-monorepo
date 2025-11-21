ae40d242a1b0c7f0d8c84d3f33cbbc510ad4a513
Ryan Doherty <385683+ryanrdoherty@users.noreply.github.com>
2025-11-21 10:44:19 -0500

    Show group members (#1537)
    * Show members of groups a user leads on their subscription tab
    
    * Handle managed groups with react-query
    
    * Show any groups present
    
    * bugs and formatting
    
    * Fix formatting a bit- still needs work
    
    * cleanup
    
    * Comply with new, better active status field
    
    * subscriberName is declared optional; reflect in decoder
    
    * Try commenting out the subscriberName decoder- seemed to work in the old version
    
    * adjust styling for subscription groups management
    
    * use nicer classes for subscription management
    
    * adjust spacing
    
    * remove unnecessary comment
    
    ---------
    
    Co-authored-by: asizemore <annsize@seas.upenn.edu>
    (cherry picked from commit cfc770b750233adc0e3edb7bc9467513d975af2e)

--- packages/libs/wdk-client/src/Service/Mixins/OauthService.ts
+++ packages/libs/wdk-client/src/Service/Mixins/OauthService.ts
@@ -1,28 +1,60 @@
 import { ServiceBase } from '../../Service/ServiceBase';
 import * as Decode from '../../Utils/Json';
 
-export type GroupLead = {
+export type UserBasicInfo = {
   name: string;
   organization: string;
 };
 
-export type SubscriptionGroup = {
+type SubscriptionGroupBase = {
   groupName: string;
   subscriptionToken: string;
-  subscriberName?: string;
-  groupLeads: GroupLead[];
+  groupLeads: UserBasicInfo[];
 };
 
-const GroupLeadDecoder: Decode.Decoder<GroupLead> = Decode.combine(
+// if changed, change decoder below
+type ActiveStatus = 'never_subscribed' | 'active' | 'grace_period' | 'expired';
+
+export type SubscriptionGroup = SubscriptionGroupBase & {
+  subscriberName?: string; // omitted if identical to groupName
+  activeStatus: ActiveStatus;
+};
+
+export type SubscriptionGroupWithMembers = SubscriptionGroupBase & {
+  members: UserBasicInfo[];
+};
+
+const UserBasicInfoDecoder: Decode.Decoder<UserBasicInfo> = Decode.combine(
   Decode.field('name', Decode.string),
   Decode.field('organization', Decode.string)
 );
 
-const SubscriptionGroupDecoder: Decode.Decoder<SubscriptionGroup> =
+const SubscriptionGroupBaseDecoder: Decode.Decoder<SubscriptionGroupBase> =
   Decode.combine(
     Decode.field('groupName', Decode.string),
     Decode.field('subscriptionToken', Decode.string),
-    Decode.field('groupLeads', Decode.arrayOf(GroupLeadDecoder))
+    Decode.field('groupLeads', Decode.arrayOf(UserBasicInfoDecoder))
+  );
+
+const SubscriptionGroupDecoder: Decode.Decoder<SubscriptionGroup> =
+  Decode.combine(
+    SubscriptionGroupBaseDecoder,
+    //Decode.optional(Decode.field('subscriberName', Decode.string)),
+    Decode.field(
+      'activeStatus',
+      Decode.oneOf(
+        Decode.constant('never_subscribed'),
+        Decode.constant('active'),
+        Decode.constant('grace_period'),
+        Decode.constant('expired')
+      )
+    )
+  );
+
+const SubscriptionGroupWithMembersDecoder: Decode.Decoder<SubscriptionGroupWithMembers> =
+  Decode.combine(
+    SubscriptionGroupBaseDecoder,
+    Decode.field('members', Decode.arrayOf(UserBasicInfoDecoder))
   );
 
 export default (base: ServiceBase) => {
@@ -58,14 +90,29 @@ export default (base: ServiceBase) => {
   function getSubscriptionGroups() {
     return base.sendRequest(Decode.arrayOf(SubscriptionGroupDecoder), {
       method: 'get',
-      path: '/subscription-groups',
+      path: '/subscription-groups?filter=active_and_expired',
       useCache: false,
     });
+  }
+
+  /**
+   * Get subscription groups for user profile and subscription-based UI features.
+   */
+  function getManagedGroupsForUser() {
+    return base.sendRequest(
+      Decode.arrayOf(SubscriptionGroupWithMembersDecoder),
+      {
+        method: 'get',
+        path: '/my-managed-groups',
+        useCache: false,
+      }
+    );
   }
 
   return {
     getOauthStateToken,
     getUserProfileVocabulary,
     getSubscriptionGroups,
+    getManagedGroupsForUser,
   };
 };
