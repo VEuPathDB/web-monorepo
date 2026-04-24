9185fb94a7c157b5353ab26ede0f326926c3c11e
Bob MacCallum <uncoolbob@gmail.com>
2026-04-24 19:08:19 +0100

    went all-const and moved the orgParam modification into the 'query function' and added a dependency array to prevent infinite requests

--- packages/libs/web-common/src/components/reporters/TranscriptTableReporterForm.jsx
+++ packages/libs/web-common/src/components/reporters/TranscriptTableReporterForm.jsx
@@ -1,75 +1,88 @@
 import TableReporterForm from './TableReporterForm';
-import { OrganismParam, MAX_RECOMMENDED_PROPERTY } from '@veupathdb/preferred-organisms/lib/components/OrganismParam';
+import {
+  OrganismParam,
+  MAX_RECOMMENDED_PROPERTY,
+} from '@veupathdb/preferred-organisms/lib/components/OrganismParam';
 import { useWdkService } from '@veupathdb/wdk-client/lib/Hooks/WdkServiceHook';
 
 // Transcript Table Reporter is the same as a regular Table Reporter, but need to
 //   override the recordClass (Transcript) with Gene to get Gene tables for a Transcript result
-let recordClassOverride = {
+const recordClassOverride = {
   recordClass: {
     fullName: 'GeneRecordClasses.GeneRecordClass',
-    displayNamePlural: 'Genes'
+    displayNamePlural: 'Genes',
   },
 };
 
 const ORTHOLOGS_TABLE_NAME = 'OrthologsLite';
-const ORGANISM_SEARCH_NAME = "GenesByTaxon";
+const ORGANISM_SEARCH_NAME = 'GenesByTaxon';
 const ORGANISM_PARAM_NAME = 'organism';
 const MAX_ORTHOLOG_SELECTED_ORGANISMS = 5;
 
-let OrganismSelection = ({ props }) => {
-  let {
-    formState,
-    formUiState,
-    updateFormState,
-    updateFormUiState
-  } = props;
+const OrganismSelection = ({ props }) => {
+  const { formState, formUiState, updateFormState, updateFormUiState } = props;
 
-  let orgParam = useWdkService(
+  const orgParam = useWdkService(
     async (wdkService) => {
-      const question = await wdkService.getQuestionAndParameters(ORGANISM_SEARCH_NAME);
-      return question.parameters.find(p => p.name === ORGANISM_PARAM_NAME);
-    }
+      const question = await wdkService.getQuestionAndParameters(
+        ORGANISM_SEARCH_NAME
+      );
+      const param = question.parameters.find(
+        (p) => p.name === ORGANISM_PARAM_NAME
+      );
+      if (param == null) return null;
+      // Remove MAX_RECOMMENDED_PROPERTY to disable the advisory-only maxRecommendedGate;
+      // the hard limit is enforced by maxSelectedCount and the submit handler instead.
+      const properties = !param.properties
+        ? {}
+        : { ...param.properties, [MAX_RECOMMENDED_PROPERTY]: undefined };
+      return {
+        ...param,
+        minSelectedCount: 1,
+        maxSelectedCount: MAX_ORTHOLOG_SELECTED_ORGANISMS,
+        properties,
+      };
+    },
+    [MAX_RECOMMENDED_PROPERTY]
   );
 
   // may need to wait for param to populate (async)
   if (orgParam == null) return null;
 
-  let replacementProperties = !orgParam.properties ? {} :
-    { ...orgParam.properties, [MAX_RECOMMENDED_PROPERTY]: undefined };
-  orgParam = Object.assign({}, orgParam, {
-    minSelectedCount: 1,
-    maxSelectedCount: MAX_ORTHOLOG_SELECTED_ORGANISMS,
-    properties: replacementProperties
-  });
+  const orgParamValue = JSON.stringify(formState.orthologOrganisms);
 
-  let orgParamValue = JSON.stringify(formState.orthologOrganisms);
-
-  let paramContext = {
+  const paramContext = {
     searchName: ORGANISM_SEARCH_NAME,
     parameter: orgParam,
-    paramValues: { [ORGANISM_PARAM_NAME]: orgParamValue }
+    paramValues: { [ORGANISM_PARAM_NAME]: orgParamValue },
   };
 
   const defaultParamUiState = {
     expandedList: [],
-    searchTerm: ''
+    searchTerm: '',
   };
 
-  let uiConfig = Object.assign({}, defaultParamUiState, formUiState);
+  const uiConfig = Object.assign({}, defaultParamUiState, formUiState);
 
   // use a custom dispatch function to handle UI state events
-  let customDispatch = (action) => {
+  const customDispatch = (action) => {
     switch (action.type) {
-      case "enum-param-treebox/set-expanded-list":
-        updateFormUiState(Object.assign({}, uiConfig, { "expandedList": action.payload.expandedList }));
+      case 'enum-param-treebox/set-expanded-list':
+        updateFormUiState(
+          Object.assign({}, uiConfig, {
+            expandedList: action.payload.expandedList,
+          })
+        );
         return;
-      case "enum-param-treebox/set-search-term":
-        updateFormUiState(Object.assign({}, uiConfig, { "searchTerm": action.payload.searchTerm }));
+      case 'enum-param-treebox/set-search-term':
+        updateFormUiState(
+          Object.assign({}, uiConfig, { searchTerm: action.payload.searchTerm })
+        );
         return;
     }
-  }
+  };
 
-  let orgParamProps = {
+  const orgParamProps = {
     isSearchPage: false,
     ctx: paramContext,
     parameter: orgParam,
@@ -79,14 +92,16 @@ let OrganismSelection = ({ props }) => {
     dispatch: customDispatch,
     onParamValueChange: (value) => {
       let newOrgArray = JSON.parse(value); // should be an array
-      updateFormState(Object.assign({}, formState, { orthologOrganisms: newOrgArray }));
-    }
+      updateFormState(
+        Object.assign({}, formState, { orthologOrganisms: newOrgArray })
+      );
+    },
   };
 
   return (
     <div>
       <h3>Choose the organisms you want orthologs for</h3>
-      <OrganismParam {...orgParamProps}/>
+      <OrganismParam {...orgParamProps} />
     </div>
   );
 };
@@ -107,47 +122,60 @@ function findLeaf(term, vocabNode) {
 }
 
 /** @type import('./Types').ReporterFormComponent */
-let TranscriptTableReporterForm = (props) => {
-
+const TranscriptTableReporterForm = (props) => {
   // need org tree here to limit number of selected organisms
-  const orgParam = useWdkService(
-    async (wdkService) => {
-      const question = await wdkService.getQuestionAndParameters(ORGANISM_SEARCH_NAME);
-      return question.parameters.find(p => p.name === ORGANISM_PARAM_NAME);
-    }
-  );
+  const orgParam = useWdkService(async (wdkService) => {
+    const question = await wdkService.getQuestionAndParameters(
+      ORGANISM_SEARCH_NAME
+    );
+    return question.parameters.find((p) => p.name === ORGANISM_PARAM_NAME);
+  });
 
-  let showOrgSelector = props.formState.tables.length !== 0 && props.formState.tables[0] === ORTHOLOGS_TABLE_NAME;
-  let orgSelector = showOrgSelector ? <OrganismSelection props={props} /> : null;
+  const showOrgSelector =
+    props.formState.tables.length !== 0 &&
+    props.formState.tables[0] === ORTHOLOGS_TABLE_NAME;
+  const orgSelector = showOrgSelector ? (
+    <OrganismSelection props={props} />
+  ) : null;
 
-  let onSubmit = !showOrgSelector ? props.onSubmit : () => {
-    let selectedOrgs = props.formState.orthologOrganisms;
-    if (props.formState.orthologOrganisms == null || props.formState.orthologOrganisms.length === 0) {
-      alert ("You must select at least one organism to request an Orthologs table.");
-      return;
-    }
+  const onSubmit = !showOrgSelector
+    ? props.onSubmit
+    : () => {
+        const selectedOrgs = props.formState.orthologOrganisms;
+        if (
+          props.formState.orthologOrganisms == null ||
+          props.formState.orthologOrganisms.length === 0
+        ) {
+          alert(
+            'You must select at least one organism to request an Orthologs table.'
+          );
+          return;
+        }
 
-    let leafOrgs = selectedOrgs.filter(org => {
-      let result = findLeaf(org, orgParam.vocabulary);
-      return result.found && result.isLeaf;
-    });
+        const leafOrgs = selectedOrgs.filter((org) => {
+          let result = findLeaf(org, orgParam.vocabulary);
+          return result.found && result.isLeaf;
+        });
 
-    if (leafOrgs.length > MAX_ORTHOLOG_SELECTED_ORGANISMS) {
-      alert("You can select no more than " + MAX_ORTHOLOG_SELECTED_ORGANISMS + " organisms.");
-    }
-    else {
-      props.onSubmit();
-    }
-  };
-  let newProps = Object.assign({}, props, recordClassOverride, {
+        if (leafOrgs.length > MAX_ORTHOLOG_SELECTED_ORGANISMS) {
+          alert(
+            'You can select no more than ' +
+              MAX_ORTHOLOG_SELECTED_ORGANISMS +
+              ' organisms.'
+          );
+        } else {
+          props.onSubmit();
+        }
+      };
+  const newProps = Object.assign({}, props, recordClassOverride, {
     postTableSelectionElement: orgSelector,
-    onSubmit: onSubmit
+    onSubmit: onSubmit,
   });
   return <TableReporterForm {...newProps} />;
 };
 
 TranscriptTableReporterForm.getInitialState = (downloadFormStoreState) => {
-  let newDownloadFormStoreState = Object.assign(
+  const newDownloadFormStoreState = Object.assign(
     {},
     downloadFormStoreState,
     recordClassOverride,
