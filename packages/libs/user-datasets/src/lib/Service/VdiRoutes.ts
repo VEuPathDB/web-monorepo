fa3e7e637cf51bd4443cca1727744919b027ccaf
Elizabeth Harper <elizabeth.harper@foxcapades.io>
2026-05-29 16:10:51 -0400

    correct url path

--- packages/libs/user-datasets/src/lib/Service/VdiRoutes.ts
+++ packages/libs/user-datasets/src/lib/Service/VdiRoutes.ts
@@ -14,7 +14,7 @@ export class VdiRoutes {
   static readonly FilesPathSegment = '/files';
   static readonly SelfUserPathSegment = '/self';
   static readonly SharesPathSegment = '/shares';
-  static readonly VariablePropertiesPathSegment = '/variable-properties';
+  static readonly VariablePropertiesPathSegment = '/dataset-properties';
 
   // Full Static Paths
   static readonly UserShareOffersPath =
@@ -44,7 +44,7 @@ export class VdiRoutes {
     );
   }
 
-  static datasetVariablePropertiesFileUri(id: DatasetId, file: string) {
+  static datasetPropertiesFileUri(id: DatasetId, file: string) {
     return (
       VdiRoutes.datasetFilesUri(id) +
       VdiRoutes.VariablePropertiesPathSegment +
