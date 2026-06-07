c475e7c6477158b289a6f1203bd83a6469609b63
Bob MacCallum <uncoolbob@gmail.com>
2026-06-07 16:57:35 +0100

    Fix: render multi-line database text with line breaks (#1740)
    * renderAttributeValueAutoBreak
    
    * single newline rule and only process plain text
    
    ---------
    
    Co-authored-by: aurreco-uga <aurreco@uga.edu>

--- packages/libs/wdk-client/src/Utils/ComponentUtils.tsx
+++ packages/libs/wdk-client/src/Utils/ComponentUtils.tsx
@@ -238,6 +238,10 @@ function logShallowComparison<P extends AnyObject>(
   console.groupEnd();
 }
 
+function isPlainText(str: string): boolean {
+  return str.indexOf('<') === -1 && !/(\&(.+?);)/.test(str);
+}
+
 /** Create a React Element using preformatted HTML */
 export function safeHtml<P>(
   str: string,
@@ -260,12 +264,7 @@ export function safeHtml<P>(
   Component: any = 'span'
 ): JSX.Element {
   str = str ?? '';
-  /**
-   * To improve performance, let's skip the element creation and innerHTML magic
-   * when we detect neither HTML nor an HTML entity in the string
-   */
-  const isHtmlEntityFound = /(\&(.+?);)/.test(str);
-  if (str.indexOf('<') === -1 && !isHtmlEntityFound) {
+  if (isPlainText(str)) {
     return <Component {...props}>{str}</Component>;
   }
   return (
@@ -311,9 +310,22 @@ export function formatAttributeValue(value?: AttributeValue): string {
 export function renderAttributeValue<P>(
   value: AttributeValue,
   props?: P,
+  Component = 'span',
+  autoBreak = false
+) {
+  let str = formatAttributeValue(value);
+  if (autoBreak && str && isPlainText(str)) {
+    str = str.replace(/\r?\n/g, '<br/>');
+  }
+  return safeHtml(str, props, Component);
+}
+
+export function renderAttributeValueAutoBreak<P>(
+  value: AttributeValue,
+  props?: P,
   Component = 'span'
 ) {
-  return safeHtml(formatAttributeValue(value), props, Component);
+  return renderAttributeValue(value, props, Component, true);
 }
 
 /**
