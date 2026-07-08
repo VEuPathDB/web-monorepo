b5099d1e59b3b9b9b480ee70712a045ffee4c269
steve-fischer-200 <sfischer@upenn.edu>
2026-07-08 16:33:30 -0400

    adjust newline rendering

--- packages/libs/user-datasets/src/lib/Components/Management/DatasetManagement.tsx
+++ packages/libs/user-datasets/src/lib/Components/Management/DatasetManagement.tsx
@@ -381,11 +381,21 @@ class DatasetManagement<
             },
         {
           attribute: 'Summary',
-          value: userDataset.summary,
+          value: userDataset.summary?.split('\n').map((line, i, arr) => (
+            <React.Fragment key={i}>
+              {line}
+              {i < arr.length - 1 && <br />}
+            </React.Fragment>
+          )),
         },
         {
           attribute: 'Description',
-          value: userDataset.description ?? '',
+          value: userDataset.description?.split('\n').map((line, i, arr) => (
+            <React.Fragment key={i}>
+              {line}
+              {i < arr.length - 1 && <br />}
+            </React.Fragment>
+          )),
         },
         {
           attribute: 'Created',
